11496800326c471d1c1a6fd4ad23bb7f49ae77de
v.audacity <v.audacity@47890b92-0858-11df-a26f-8b716316a5bc>
2010-07-16 05:41:22 +0000

    (working on bug 113)
    Update header block. 
    Move declarations that aren't used elsewhere, into cpp file. No need to expose them here.
    Get rid of vim/emacs declarations. Matt said they're unnecessary, ages ago.

--- src/Dependencies.h
+++ src/Dependencies.h
@@ -1,44 +1,20 @@
 /**********************************************************************
 
-  Audacity: A Digital Audio Editor
+   Audacity: A Digital Audio Editor
+   Audacity(R) is copyright (c) 1999-2008 Audacity Team.
+   License: GPL v2.  See License.txt.
   
-  Dependencies.h
- 
-  Dominic Mazzoni
+   Dependencies.h
 
-  The primary function provided in this source file is
-  ShowDependencyDialogIfNeeded.  It checks a project to see if
-  any of its WaveTracks contain AliasBlockFiles; if so it
-  presents a dialog to the user and lets them copy those block
-  files into the project, making it self-contained.
+   Dominic Mazzoni
+   Vaughan Johnson
 
 **********************************************************************/
 
 #ifndef __AUDACITY_DEPENDENCIES__
 #define __AUDACITY_DEPENDENCIES__
 
-#include <wx/defs.h>
-#include <wx/filename.h>
-
-#include "Track.h"
-
-class AliasedFile {
- public:
-  AliasedFile(wxFileName fileName, wxLongLong bytes) {
-    this->fileName = fileName;
-    this->bytes = bytes;
-  }
-  wxFileName  fileName;
-  wxLongLong  bytes; // if stored as current default sample format
-};
-
 class AudacityProject;
-
-WX_DECLARE_OBJARRAY(AliasedFile, AliasedFileArray);
-
-// True = success, whether dependencies were found or not
-bool FindDependencies(AudacityProject *project,
-		      AliasedFileArray *aliasedFiles);
 
 // Checks for alias block files, modifies the project if the
 // user requests it, and returns True if the user continues.
@@ -48,14 +24,3 @@ bool ShowDependencyDialogIfNeeded(AudacityProject *project,
                                   bool isSaving);
 
 #endif
-
-// Indentation settings for Vim and Emacs.
-// Please do not modify past this point.
-//
-// Local Variables:
-// c-basic-offset: 3
-// indent-tabs-mode: nil
-// End:
-//
-// vim: et sts=3 sw=3
-
