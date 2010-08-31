95b89cd122f32015684fe4a95d673be0d3362d7c
v.audacity <v.audacity@47890b92-0858-11df-a26f-8b716316a5bc>
2010-08-31 03:56:47 +0000

    Leland's patch that fixes bug 199

--- src/effects/FindClipping.cpp
+++ src/effects/FindClipping.cpp
@@ -183,7 +183,7 @@ bool EffectFindClipping::ProcessOne(LabelTrack * l,
             if (stoprun >= mStop) {
                l->AddLabel(startTime,
                            t->LongSamplesToTime(start + s - mStop),
-                           wxString::Format(wxT("%d of %d"), startrun, samps - mStop));
+                           wxString::Format(wxT("%lld of %lld"), startrun, samps - mStop));
                startrun = 0;
                stoprun = 0;
                samps = 0;
