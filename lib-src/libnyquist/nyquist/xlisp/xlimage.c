37f52c4695b4a2a1d3a40a944cd4d626058473de
v.audacity <v.audacity@47890b92-0858-11df-a26f-8b716316a5bc>
2010-09-02 04:02:23 +0000

    Eliminate MSVC compiler warnings.

--- lib-src/libnyquist/nyquist/xlisp/xlimage.c
+++ lib-src/libnyquist/nyquist/xlisp/xlimage.c
@@ -397,7 +397,7 @@ LOCAL OFFTYPE cvoptr(LVAL p)
 
     /* pointer not within any segment */
     xlerror("bad pointer found during image save",p);
-    return (NIL);
+    return ((OFFTYPE)NIL);
 }
 
 #endif
