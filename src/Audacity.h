1a05037beb6e97389bde055e388444af78479eb8
v.audacity <v.audacity@47890b92-0858-11df-a26f-8b716316a5bc>
2010-09-09 22:12:35 +0000

    (bug 123, P2) Add a macro for Martyn to use in his work on fixing this bug.

--- src/Audacity.h
+++ src/Audacity.h
@@ -152,14 +152,5 @@ void QuitAudacity();
 #endif
 #endif
 
-// Indentation settings for Vim and Emacs and unique identifier for Arch, a
-// version control system. Please do not modify past this point.
-//
-// Local Variables:
-// c-basic-offset: 3
-// indent-tabs-mode: nil
-// End:
-//
-// vim: et sts=3 sw=3
-// arch-tag: 21aef079-ec47-4ff9-a359-7d159e2ba0e6
-
+// This macro is used widely, so declared here.
+#define QUANTIZED_TIME(time, rate) ((double)((sampleCount)floor(((time) * (rate)) + 0.5))) / (rate)
