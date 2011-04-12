a1bed1ca5925cb9a7f295557792d2a5008c73c81
james.k.crook@gmail.com <james.k.crook@gmail.com@47890b92-0858-11df-a26f-8b716316a5bc>
2011-04-12 15:35:55 +0000

    path now starts at man.

--- scripts/mw2html_audacity/wiki2htm.bat
+++ scripts/mw2html_audacity/wiki2htm.bat
@@ -1,4 +1,4 @@
-python mw2html.py http://manual.audacityteam.org ..\..\help\temp -s
+python mw2html.py http://manual.audacityteam.org/man ..\..\help\temp -s
 rmdir /S /Q ..\..\help\manual
 mkdir ..\..\help\manual
 xcopy ..\..\help\temp\manual.audacityteam.org ..\..\help\manual\ /E /C /Y /Q
