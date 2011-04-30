a47d981410ec6c6a86d8f37c3fd4434927c62076
james.k.crook@gmail.com <james.k.crook@gmail.com@47890b92-0858-11df-a26f-8b716316a5bc>
2011-04-30 17:31:17 +0000

    Now starts at /man directory.

--- scripts/mw2html_audacity/wiki2htm.sh
+++ scripts/mw2html_audacity/wiki2htm.sh
@@ -2,7 +2,7 @@
 
 helpdir="../../help/manual"
 tmpdir_="../../help/temp"
-srcuri="http://manual.audacityteam.org"
+srcuri="http://manual.audacityteam.org/man"
 
 mkdir -p "${tmpdir_}"
 python mw2html.py "${srcuri}" "${tmpdir_}" -s
