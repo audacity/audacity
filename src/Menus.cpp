73e4ed2cc1e48c5d7b92711683a56630aeb7de1b
windinthew <windinthew@47890b92-0858-11df-a26f-8b716316a5bc>
2011-12-17 00:20:42 +0000

    Give "Audio Device Info" unique access key so that it doesn't block "A" opening "About Audacity".

--- src/Menus.cpp
+++ src/Menus.cpp
@@ -1042,7 +1042,7 @@ void AudacityProject::CreateMenusAndCommands()
 
       c->AddSeparator();   
 
-      c->AddItem(wxT("DeviceInfo"), _("&Audio Device Info..."), FN(OnAudioDeviceInfo));
+      c->AddItem(wxT("DeviceInfo"), _("Au&dio Device Info..."), FN(OnAudioDeviceInfo));
       c->AddItem(wxT("Log"), _("Show &Log..."), FN(OnShowLog));
    }
 
