d0922d821b8cbd3279c25faa7e50c22b57b4db39
v.audacity <v.audacity@47890b92-0858-11df-a26f-8b716316a5bc>
2012-02-15 17:59:02 +0000

    Bug 179 (P4) Former fix was incorrect for Mac. Ed has updated his change for this, and I'm applying it.

--- src/effects/nyquist/LoadNyquist.cpp
+++ src/effects/nyquist/LoadNyquist.cpp
@@ -51,7 +51,7 @@ void LoadNyquistPlugins()
    }
 
    wxGetApp().FindFilesInPathList(wxT("*.ny"), pathList, files);
-#ifndef  __WXMSW__
+#ifndef  __WXGTK__
    wxGetApp().FindFilesInPathList(wxT("*.NY"), pathList, files); // Ed's fix for bug 179
 #endif
 
