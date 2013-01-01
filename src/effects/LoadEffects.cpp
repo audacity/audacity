f72f520b3867af632cf22ca5a3aea0b2d011be2b
richardash1981 <richardash1981@47890b92-0858-11df-a26f-8b716316a5bc>
2013-01-01 15:35:23 +0000

    remove trailing junk

--- src/effects/LoadEffects.cpp
+++ src/effects/LoadEffects.cpp
@@ -326,14 +326,3 @@ void UnloadEffects()
 #endif
 }
 
-// Indentation settings for Vim and Emacs and unique identifier for Arch, a
-// version control system. Please do not modify past this point.
-//
-// Local Variables:
-// c-basic-offset: 3
-// indent-tabs-mode: nil
-// End:
-//
-// vim: et sts=3 sw=3
-// arch-tag: 3a6c5930-9015-4fd0-96f2-2eb31da1c785
-
