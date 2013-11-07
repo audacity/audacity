1686b38f3c89bbae3863eddc781f71e7b0c52116
benjamin.drung@gmail.com <benjamin.drung@gmail.com@47890b92-0858-11df-a26f-8b716316a5bc>
2013-11-07 23:23:32 +0000

    Replace tabs by spaces.

--- lib-src/lib-widget-extra/NonGuiThread.h
+++ lib-src/lib-widget-extra/NonGuiThread.h
@@ -16,19 +16,19 @@ typedef void (*tGenericFn)(void);
 //#include "AllCommands.h" // for tGenericFn
 //#include "WidgetExtra.h"
 
-class /*WIDGET_EXTRA_DLL*/ NonGuiThread : public wxThread  
+class /*WIDGET_EXTRA_DLL*/ NonGuiThread : public wxThread
 {
 public:
-	NonGuiThread(tGenericFn pFn);
-	virtual ~NonGuiThread();
+   NonGuiThread(tGenericFn pFn);
+   virtual ~NonGuiThread();
    NonGuiThread::ExitCode Entry();
    static void RunInThread(tGenericFn pFn);
    static NonGuiThread * StartChild( tGenericFn pFn );
 
 public:
-	bool mbExit;
+   bool mbExit;
    tGenericFn mpFn;
-	static bool IsLive;
+   static bool IsLive;
 };
 
 #endif // !defined(AFX_NONGUITHREAD_H__E8F7FC2B_CB13_497B_A556_18551596AFD9__INCLUDED_)
