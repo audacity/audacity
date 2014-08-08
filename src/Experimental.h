c0a12667df9d42a17e7ef6617ca8124d732adfe3
v.audacity <v.audacity@47890b92-0858-11df-a26f-8b716316a5bc>
2014-08-08 03:42:11 +0000

    Turn off WDM-KS and WASAPI again for 2.0.6 release.

--- src/Experimental.h
+++ src/Experimental.h
@@ -36,7 +36,7 @@
 
 // LLL, 09 Nov 2013:
 // Allow all WASAPI devices, not just loopback
-#define EXPERIMENTAL_FULL_WASAPI
+// Not in for Audacity 2.0.6:   #define EXPERIMENTAL_FULL_WASAPI
 
 // JKC (effect by Norm C, 02 Oct 2013)
 //#define EXPERIMENTAL_SCIENCE_FILTERS
