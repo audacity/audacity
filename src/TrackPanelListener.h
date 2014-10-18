9884538167488723a5402e3126717a8b5022d805
james.k.crook@gmail.com <james.k.crook@gmail.com@47890b92-0858-11df-a26f-8b716316a5bc>
2014-10-18 14:25:44 +0000

    New header files that I missed out from the previous commit (Spectral editing)

---
+++ src/TrackPanelListener.h
@@ -0,0 +1,43 @@
+/**********************************************************************
+
+  Audacity: A Digital Audio Editor
+
+  TrackPanelListener.h
+
+  Dominic Mazzoni
+
+**********************************************************************/
+
+#ifndef __AUDACITY_TRACK_PANEL_LISTENER__
+#define __AUDACITY_TRACK_PANEL_LISTENER__
+
+class ToolsToolBar;
+class ControlToolBar;
+
+class AUDACITY_DLL_API TrackPanelListener {
+
+ public:
+   TrackPanelListener(){};
+   virtual ~TrackPanelListener(){};
+
+   virtual void TP_DisplaySelection() = 0;
+   virtual void TP_DisplayStatusMessage(wxString msg) = 0;
+
+   virtual int TP_GetCurrentTool() = 0;
+   virtual ToolsToolBar * TP_GetToolsToolBar() = 0;
+   virtual ControlToolBar * TP_GetControlToolBar() = 0;
+
+   virtual void TP_OnPlayKey() = 0;
+   virtual void TP_PushState(wxString shortDesc, wxString longDesc,
+                            int flags = PUSH_AUTOSAVE | PUSH_CALC_SPACE) = 0;
+   virtual void TP_ModifyState(bool bWantsAutoSave) = 0;    // if true, writes auto-save file. Should set only if you really want the state change restored after
+                                                            // a crash, as it can take many seconds for large (eg. 10 track-hours) projects
+   virtual void TP_RedrawScrollbars() = 0;
+   virtual void TP_ScrollLeft() = 0;
+   virtual void TP_ScrollRight() = 0;
+   virtual void TP_ScrollWindow(double scrollto) = 0;
+   virtual void TP_ScrollUpDown(int delta) = 0;
+   virtual void TP_HandleResize() = 0;
+};
+
+#endif
---
+++ src/toolbars/SelectionBarListener.h
@@ -0,0 +1,32 @@
+/**********************************************************************
+
+  Audacity: A Digital Audio Editor
+
+  SelectionBarListener.h
+
+  Dominic Mazzoni
+
+**********************************************************************/
+
+#ifndef __AUDACITY_SELECTION_BAR_LISTENER__
+#define __AUDACITY_SELECTION_BAR_LISTENER__
+
+class SelectedRegion;
+
+class AUDACITY_DLL_API SelectionBarListener {
+
+ public:
+
+   SelectionBarListener(){};
+   virtual ~SelectionBarListener(){};
+
+   virtual double AS_GetRate() = 0;
+   virtual void AS_SetRate(double rate) = 0;
+   virtual int AS_GetSnapTo() = 0;
+   virtual void AS_SetSnapTo(int snap) = 0;
+   virtual const wxString & AS_GetSelectionFormat() = 0;
+   virtual void AS_SetSelectionFormat(const wxString & format) = 0;
+   virtual void AS_ModifySelection(double &start, double &end, bool done) = 0;
+};
+
+#endif
