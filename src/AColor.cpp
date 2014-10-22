421bc768565bb90f11113d8d851044f89debd13a
james.k.crook@gmail.com <james.k.crook@gmail.com@47890b92-0858-11df-a26f-8b716316a5bc>
2014-10-22 22:45:29 +0000

    Fix my regression in previous patch: Edges on grayscale should be white, not black.

--- src/AColor.cpp
+++ src/AColor.cpp
@@ -627,13 +627,13 @@ void AColor::PreComputeGradient() {
                      b *= 0.5f;
                      break;
 
-                  // For now edge colour is just black.
+                  // For now edge colour is just black (or white if grey-scale)
                   // Later we might invert or something else funky.
                   case ColorGradientEdge:
                      // fully dimmed
-                     r = 0.0f;
-                     g = 0.0f;
-                     b = 0.0f;
+                     r = 1.0f * grayscale;
+                     g = 1.0f * grayscale;
+                     b = 1.0f * grayscale;
                      break;
                   }
                   gradient_pre[selected][grayscale][i][0] = (unsigned char) (255 * r);
