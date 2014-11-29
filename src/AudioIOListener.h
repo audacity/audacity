e94785b37219163c7cb6d3f8ba4408ed02217afa
james.k.crook@gmail.com <james.k.crook@gmail.com@47890b92-0858-11df-a26f-8b716316a5bc>
2014-11-29 23:37:45 +0000

    Missing file now added.  Sorry!

---
+++ src/AudioIOListener.h
@@ -0,0 +1,29 @@
+/**********************************************************************
+
+  Audacity: A Digital Audio Editor
+
+  AudioIOListener.h
+
+  Dominic Mazzoni
+
+  Use the PortAudio library to play and record sound
+
+**********************************************************************/
+
+#ifndef __AUDACITY_AUDIO_IO_LISTENER__
+#define __AUDACITY_AUDIO_IO_LISTENER__
+
+#include <wx/string.h>
+
+class AUDACITY_DLL_API AudioIOListener {
+public:
+   AudioIOListener() {}
+   virtual ~AudioIOListener() {}
+
+   virtual void OnAudioIORate(int rate) = 0;
+   virtual void OnAudioIOStartRecording() = 0;
+   virtual void OnAudioIOStopRecording() = 0;
+   virtual void OnAudioIONewBlockFiles(const wxString& blockFileLog) = 0;
+};
+
+#endif
