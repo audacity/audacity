audacity/audacity

commit f7424927d28124d6c529c0154c04cd054db8f0c2

Do autorecovery before parsing command-line arguments.
git-svn-id: http://audacity.googlecode.com/svn/sf-cvs/trunk/audacity-src@5527 47890b92-0858-11df-a26f-8b716316a5bc

src/AudacityApp.cpp
@@ -561,10 +561,26 @@ bool AudacityApp::OnInit()
 
    delete temporarywindow;
 
+   //
+   // Auto-recovery
+   //
+
+   bool didRecoverAnything = false;
+   if (!ShowAutoRecoveryDialogIfNeeded(&project, &didRecoverAnything))
+   {
+      // Important: Prevent deleting any temporary files!
+      DirManager::SetDontDeleteTempFiles();
+      QuitAudacity(true);
+   }
+
+   //
+   // Command-line parsing, but only if we didn't recover
+   //
+
 #if !defined(__CYGWIN__)
 
    // Parse command-line arguments
-   if (argc > 1) {
+   if (argc > 1 && !didRecoverAnything) {
       for (int option = 1; option < argc; option++) {
          if (!argv[option])
             continue;
@@ -633,7 +649,7 @@ bool AudacityApp::OnInit()
 #else
 	
    // Cygwin command line parser (by Dave Fancella)
-   if (argc > 1) {
+   if (argc > 1 && !didRecoverAnything) {
       int optionstart = 1;
       bool startAtOffset = false;
 		
@@ -718,13 +734,6 @@ bool AudacityApp::OnInit()
 #endif // Cygwin command-line parser
 
    gInited = true;
-   
-   if (!ShowAutoRecoveryDialogIfNeeded(&project))
-   {
-      // Important: Prevent deleting any temporary files!
-      DirManager::SetDontDeleteTempFiles();
-      QuitAudacity(true);
-   }
    
    return TRUE;
 }

src/AutoRecovery.cpp
@@ -198,8 +198,12 @@ static bool RecoverAllProjects(AudacityProject** pproj)
    return true;
 }
 
-bool ShowAutoRecoveryDialogIfNeeded(AudacityProject** pproj)
+bool ShowAutoRecoveryDialogIfNeeded(AudacityProject** pproj,
+                                    bool *didRecoverAnything)
 {
+   if (didRecoverAnything)
+      *didRecoverAnything = false;
+
    if (HaveFilesToRecover())
    {
       AutoRecoveryDialog dlg(*pproj);
@@ -211,6 +215,8 @@ bool ShowAutoRecoveryDialogIfNeeded(AudacityProject** pproj)
          return RemoveAllAutoSaveFiles();
    
       case ID_RECOVER_ALL:
+         if (didRecoverAnything)
+            *didRecoverAnything = true;
          return RecoverAllProjects(pproj);
 
       default:
@@ -302,3 +308,13 @@ XMLTagHandler* RecordingRecoveryHandler::HandleXMLChild(const wxChar *tag)
    
    return NULL;
 }
+
+// Indentation settings for Vim and Emacs.
+// Please do not modify past this point.
+//
+// Local Variables:
+// c-basic-offset: 3
+// indent-tabs-mode: nil
+// End:
+//
+// vim: et sts=3 sw=3

src/AutoRecovery.h
@@ -16,7 +16,8 @@
 // Returns: True, if the start of Audacity should continue as normal
 //          False if Audacity should be quit immediately
 //
-bool ShowAutoRecoveryDialogIfNeeded(AudacityProject** pproj);
+bool ShowAutoRecoveryDialogIfNeeded(AudacityProject** pproj,
+                                    bool *didRecoverAnything);
 
 //
 // XML Handler for a <recordingrecovery> tag
@@ -38,3 +39,13 @@ private:
 };
 
 #endif
+
+// Indentation settings for Vim and Emacs.
+// Please do not modify past this point.
+//
+// Local Variables:
+// c-basic-offset: 3
+// indent-tabs-mode: nil
+// End:
+//
+// vim: et sts=3 sw=3

commit 000f353f5725b309c2a0080eff445ba18ce23511

Fix for problem caused by r5527, circa Sep. 2006
The problem was that if the user chose to quit Audacity when
the auto recovery dialog was displayed, Audacity would crash
because the command line was still parsed and any filenames
listed on the command line would still be opened.  But, that
would failed because "quit" processing had already been done.

Anyway, a simple one line fix.

src/AudacityApp.cpp
@@ -1329,6 +1329,7 @@ void AudacityApp::FinishInits()
       // Important: Prevent deleting any temporary files!
       DirManager::SetDontDeleteTempFiles();
       QuitAudacity(true);
+      return;
    }
 
    //