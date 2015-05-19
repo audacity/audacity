c391ebc12dd2deec5966e20bf4985728c6662152
Cory Cook <clovecooks@gmail.com>
2015-05-19 13:17:42 -0700

    Revert "Fix remnant effects on batch command for multiple files."
    This reverts commit dbac9b7cd7b4f56efcbcd1670466e575f01f51cb.

--- src/BatchProcessDialog.cpp
+++ src/BatchProcessDialog.cpp
@@ -336,8 +336,6 @@ void BatchProcessDialog::OnApplyToFiles(wxCommandEvent & WXUNUSED(event))
       }
       UndoManager *um = project->GetUndoManager();
       um->ClearStates();
-	  project->OnSelectAll();
-	  project->OnRemoveTracks();
    }
    project->OnRemoveTracks();
 }
