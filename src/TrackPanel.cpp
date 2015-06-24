9c957d43b11f7250f280a1f76c502762bdbe4022
Paul Licameli <paul.licameli@gmail.com>
2015-06-24 13:31:09 -0400

    Bug1013: normalize keystroke named in status bar, for Mac

--- src/TrackPanel.cpp
+++ src/TrackPanel.cpp
@@ -212,6 +212,8 @@ is time to refresh some aspect of the screen.
 #include "ViewInfo.h"
 #include "WaveTrack.h"
 
+#include "commands/Keyboard.h"
+
 #include "ondemand/ODManager.h"
 
 #include "prefs/SpectrumPrefs.h"
@@ -1908,6 +1910,8 @@ void TrackPanel::SetCursorAndTipWhenSelectTool( Track * t,
          // No keyboard preference defined for opening Preferences dialog
          /* i18n-hint: These are the names of a menu and a command in that menu */
          keyStr = _("Edit, Preferences...");
+      else
+         keyStr = KeyStringDisplay(keyStr);
       static wxString result;
       /* i18n-hint: %s is usually replaced by "Ctrl+P" for Windows/Linux, "Command+," for Mac */
       result = wxString::Format(
