ae47b4f3ef851ec802168596d85772d19ef9498f
Leland Lucius <github@homerow.net>
2015-08-24 15:32:04 -0500

    Bring back the inverted cursor for wxGTK3 built against GTK2

--- src/AColor.cpp
+++ src/AColor.cpp
@@ -105,7 +105,7 @@ void AColor::Line(wxDC & dc, wxCoord x1, wxCoord y1, wxCoord x2, wxCoord y2)
    // last point since it is now based on the new wxGraphicsContext system.
    // Make the other platforms do the same thing since the other platforms
    // "may" follow they get wxGraphicsContext going.
-#if defined(__WXMAC__) || defined(__WXGTK__) 
+#if defined(__WXMAC__) || defined(__WXGTK3__)
    dc.DrawLine(x1, y1, x2, y2);
 #else
    bool point = false;
@@ -305,7 +305,7 @@ void AColor::CursorColor(wxDC * dc)
 {
    if (!inited)
       Init();
-#if defined(__WXMAC__) || defined(__WXGTK__)
+#if defined(__WXMAC__) || defined(__WXGTK3__)
    dc->SetLogicalFunction(wxCOPY);
    dc->SetPen(wxColor(0, 0, 0, 128));
 #else
