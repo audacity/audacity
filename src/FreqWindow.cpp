2ae7fb18e9d480a3934e037541d2ca0e70c25837
Leland Lucius <github@homerow.net>
2015-09-02 00:52:20 -0500

    Fix for bug #1105 part deux

--- src/FreqWindow.cpp
+++ src/FreqWindow.cpp
@@ -993,7 +993,11 @@ void FreqWindow::Recalc()
    int windowFunc = mFuncChoice->GetSelection();
 
    wxWindow *hadFocus = FindFocus();
-   wxWindowDisabler *blocker = new wxWindowDisabler(mProgress);
+   // In wxMac, the skipped window MUST be a top level window.  I'd originally made it
+   // just the mProgress window with the idea of preventing user interaction with the
+   // controls while the plot was being recalculated.  This doesn't appear to be necessary
+   // so just use the the top level window instead.
+   wxWindowDisabler *blocker = new wxWindowDisabler(this);
    wxYieldIfNeeded();
 
    mAnalyst->Calculate(alg, windowFunc, mWindowSize, mRate,
