bf34e1e28c8b32efb496fbfdc20d1c4ff23191b8
tip2tail <mark@tip2tail.scot>
2016-02-25 13:47:48 +0000

    Implemented changes for automatic save and export or project from Timer Recording.

--- src/export/Export.h
+++ src/export/Export.h
@@ -152,6 +152,14 @@ public:
 
    const ExportPluginArray GetPlugins();
 
+   // Auto Export from Timer Recording
+   bool ProcessFromTimed(AudacityProject *project, bool selectedOnly, double t0, double t1, wxFileName fnFile, int iFormat, int iSubFormat, int iFilterIndex);
+   bool SetAutoExportOptions(AudacityProject *project);
+   int GetAutoExportFormat();
+   int GetAutoExportSubFormat();
+   int GetAutoExportFilterIndex();
+   wxFileName GetAutoExportFileName();
+
 private:
 
    bool ExamineTracks();
