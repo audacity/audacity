ed80964fabc33339935133ec22ba85f194f7a36f
tip2tail <mark@tip2tail.scot>
2016-02-28 18:17:08 +0000

    Removed unrequired #include that was causing Travis to fail

--- src/Project.h
+++ src/Project.h
@@ -30,7 +30,6 @@
 #include "xml/XMLTagHandler.h"
 #include "toolbars/SelectionBarListener.h"
 #include "toolbars/SpectralSelectionBarListener.h"
-#include "export\Export.h";
 
 #include "MemoryX.h"
 #include <wx/defs.h>
