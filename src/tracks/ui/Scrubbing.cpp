66454b7e4c02ed9e9aacd4ed8786f8cac2dd9e63
Paul Licameli <paul.licameli@audacityteam.org>
2016-05-06 12:17:33 -0400

    Fix status bar messages and disabling of pause button for scrubbing.

--- src/tracks/ui/Scrubbing.cpp
+++ src/tracks/ui/Scrubbing.cpp
@@ -208,6 +208,12 @@ void Scrubber::MarkScrubStart(
 
    ControlToolBar * const ctb = mProject->GetControlToolBar();
    ctb->SetPlay(true, ControlToolBar::PlayAppearance::Scrub);
+   if(gAudioIO->IsPaused())
+      ctb->Pause(); // un-pause
+
+   // This disables the pause button.
+   ctb->EnableDisableButtons();
+
    ctb->UpdateStatusBar(mProject);
 
    CheckMenuItem();
@@ -245,8 +251,11 @@ bool Scrubber::MaybeStartScrubbing(wxCoord xx)
          );
          if (time1 != time0)
          {
-            if (busy)
+            if (busy) {
+               auto position = mScrubStartPosition;
                ctb->StopPlaying();
+               mScrubStartPosition = position;
+            }
 
             AudioIOStartStreamOptions options(mProject->GetDefaultPlayOptions());
             options.timeTrack = NULL;
