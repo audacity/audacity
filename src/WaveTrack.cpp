audacity/audacity

commit 3015a9c8d9229406f8c33fd32ffc64c11d77ca54

Track cut and copy functions return a unique_ptr argument...
... Because they are factory functions.

src/WaveTrack.cpp
@@ -531,38 +531,51 @@ bool WaveTrack::IsEmpty(double t0, double t1)
    return true;
 }
 
-bool WaveTrack::Cut(double t0, double t1, Track **dest)
+Track::Holder WaveTrack::Cut(double t0, double t1)
 {
    if (t1 < t0)
-      return false;
+      return{};
 
-   if (!Copy(t0, t1, dest))
-      return false;
+   auto tmp = Copy(t0, t1);
 
-   return Clear(t0, t1);
+   if (!tmp)
+      return{};
+
+   if (!Clear(t0, t1))
+      return{};
+
+   return std::move(tmp);
 }
 
-bool WaveTrack::SplitCut(double t0, double t1, Track **dest)
+Track::Holder WaveTrack::SplitCut(double t0, double t1)
 {
    if (t1 < t0)
-      return false;
+      return{};
 
    // SplitCut is the same as 'Copy', then 'SplitDelete'
-   if (!Copy(t0, t1, dest))
-      return false;
-   return SplitDelete(t0, t1);
+   auto tmp = Copy(t0, t1);
+   if (!tmp)
+      return{};
+   if (!SplitDelete(t0, t1))
+      return{};
+
+   return std::move(tmp);
 }
 
 #if 0
-bool WaveTrack::CutAndAddCutLine(double t0, double t1, Track **dest)
+Track::Holder WaveTrack::CutAndAddCutLine(double t0, double t1)
 {
    if (t1 < t0)
-      return false;
+      return {};
 
    // Cut is the same as 'Copy', then 'Delete'
-   if (!Copy(t0, t1, dest))
-      return false;
-   return ClearAndAddCutLine(t0, t1);
+   auto tmp = Copy(t0, t1);
+   if (!tmp)
+      return {};
+   if (!ClearAndAddCutLine(t0, t1))
+      return {};
+
+   return std::move(tmp);
 }
 #endif
 
@@ -625,14 +638,14 @@ bool WaveTrack::Trim (double t0, double t1)
 
 
 
-bool WaveTrack::Copy(double t0, double t1, Track **dest) const
+Track::Holder WaveTrack::Copy(double t0, double t1) const
 {
-   *dest = NULL;
-
    if (t1 <= t0)
-      return false;
+      return{};
 
-   WaveTrack *newTrack = new WaveTrack(mDirManager);
+   WaveTrack *newTrack;
+   Track::Holder result
+   { newTrack = safenew WaveTrack{ mDirManager } };
 
    newTrack->Init(*this);
 
@@ -710,14 +723,12 @@ bool WaveTrack::Copy(double t0, double t1, Track **dest) const
       }
    }
 
-   *dest = newTrack;
-
-   return true;
+   return std::move(result);
 }
 
-bool WaveTrack::CopyNonconst(double t0, double t1, Track **dest)
+Track::Holder WaveTrack::CopyNonconst(double t0, double t1)
 {
-   return Copy(t0, t1, dest);
+   return Copy(t0, t1);
 }
 
 bool WaveTrack::Clear(double t0, double t1)
@@ -1150,13 +1161,11 @@ bool WaveTrack::SyncLockAdjust(double oldT1, double newT1)
          bool clipsCanMove = true;
          gPrefs->Read(wxT("/GUI/EditClipCanMove"), &clipsCanMove);
          if (clipsCanMove) {
-            Track *tmp = NULL;
-            ret = Cut (oldT1, GetEndTime() + 1.0/GetRate(), &tmp);
+            auto tmp = Cut (oldT1, GetEndTime() + 1.0/GetRate());
             if (!ret) return false;
 
-            ret = Paste(newT1, tmp);
+            ret = Paste(newT1, tmp.get());
             wxASSERT(ret);
-            delete tmp;
          }
 
          return ret;
@@ -1240,12 +1249,10 @@ bool WaveTrack::Paste(double t0, const Track *src)
          // We need to insert multiple clips, so split the current clip and
          // move everything to the right, then try to paste again
          if (!IsEmpty(t0, GetEndTime())) {
-            Track *tmp = NULL;
-            Cut(t0, GetEndTime()+1.0/mRate, &tmp);
-            bool bResult = Paste(t0 + insertDuration, tmp);
+            auto tmp = Cut(t0, GetEndTime()+1.0/mRate);
+            bool bResult = Paste(t0 + insertDuration, tmp.get());
             wxASSERT(bResult); // TO DO: Actually handle this.
             wxUnusedVar(bResult);
-            delete tmp;
          }
       } else
       {

src/Benchmark.cpp
@@ -345,7 +345,6 @@ void BenchmarkDialog::OnRun( wxCommandEvent & WXUNUSED(event))
    ZoomInfo zoomInfo(0.0, ZoomInfo::GetDefaultZoom());
    DirManager *d = new DirManager();
    const auto t = TrackFactory{ d, &zoomInfo }.NewWaveTrack(int16Sample);
-   Track *tmp = NULL;
 
    t->SetRate(1);
 
@@ -418,7 +417,7 @@ void BenchmarkDialog::OnRun( wxCommandEvent & WXUNUSED(event))
       if (mEditDetail)
          Printf(wxT("Cut: %d - %d \n"), x0 * chunkSize, (x0 + xlen) * chunkSize);
 
-      t->Cut(double (x0 * chunkSize), double ((x0 + xlen) * chunkSize), &tmp);
+      auto tmp = t->Cut(double (x0 * chunkSize), double ((x0 + xlen) * chunkSize));
       if (!tmp) {
          Printf(wxT("Trial %d\n"), z);
          Printf(wxT("Cut (%d, %d) failed.\n"), (x0 * chunkSize),
@@ -432,7 +431,7 @@ void BenchmarkDialog::OnRun( wxCommandEvent & WXUNUSED(event))
       if (mEditDetail)
          Printf(wxT("Paste: %d\n"), y0 * chunkSize);
 
-      if (!t->Paste((double)(y0 * chunkSize), tmp))
+      if (!t->Paste((double)(y0 * chunkSize), tmp.get()))
       {
          Printf(wxT("Trial %d\nFailed on Paste.\n"), z);
          goto fail;
@@ -530,9 +529,6 @@ void BenchmarkDialog::OnRun( wxCommandEvent & WXUNUSED(event))
    Printf(wxT("TEST FAILED!!!\n"));
 
  success:
-   if (tmp)
-      delete tmp;
-
    delete[]small1;
    delete[]small2;
    delete[]block;

src/LabelTrack.cpp
@@ -2398,33 +2398,36 @@ bool LabelTrack::Save(wxTextFile * out, bool overwrite)
 }
 #endif
 
-bool LabelTrack::Cut(double t0, double t1, Track **dest)
+Track::Holder LabelTrack::Cut(double t0, double t1)
 {
-   if (!Copy(t0, t1, dest))
-      return false;
+   auto tmp = Copy(t0, t1);
+   if (!tmp)
+      return{};
    if (!Clear(t0, t1))
-      return false;
+      return{};
 
-   return true;
+   return std::move(tmp);
 }
 
 #if 0
-bool LabelTrack::SplitCut(double t0, double t1, Track ** dest)
+Track::Holder LabelTrack::SplitCut(double t0, double t1)
 {
    // SplitCut() == Copy() + SplitDelete()
 
-   if (!Copy(t0, t1, dest))
-      return false;
+   Track::Holder tmp = Copy(t0, t1);
+   if (!tmp)
+      return {};
    if (!SplitDelete(t0, t1))
-      return false;
+      return {};
 
-   return true;
+   return std::move(tmp);
 }
 #endif
 
-bool LabelTrack::Copy(double t0, double t1, Track ** dest) const
+Track::Holder LabelTrack::Copy(double t0, double t1) const
 {
-   *dest = new LabelTrack(GetDirManager());
+   auto tmp = std::make_unique<LabelTrack>(GetDirManager());
+   const auto lt = static_cast<LabelTrack*>(tmp.get());
    int len = mLabels.Count();
 
    for (int i = 0; i < len; i++) {
@@ -2437,14 +2440,14 @@ bool LabelTrack::Copy(double t0, double t1, Track ** dest) const
                             label.getT0() - t0,
                             label.getT1() - t0,
                             label.title);
-         ((LabelTrack *) (*dest))->mLabels.Add(l);
+         lt->mLabels.Add(l);
       }
       else if (relation == LabelStruct::WITHIN_LABEL) {
          const LabelStruct &label = *mLabels[i];
          LabelStruct *l =
             new LabelStruct(label.selectedRegion, 0, t1-t0,
                             label.title);
-         ((LabelTrack *) (*dest))->mLabels.Add(l);
+         lt->mLabels.Add(l);
       }
       else if (relation == LabelStruct::BEGINS_IN_LABEL) {
          const LabelStruct &label = *mLabels[i];
@@ -2453,7 +2456,7 @@ bool LabelTrack::Copy(double t0, double t1, Track ** dest) const
                             0,
                             label.getT1() - t0,
                             label.title);
-         ((LabelTrack *) (*dest))->mLabels.Add(l);
+         lt->mLabels.Add(l);
       }
       else if (relation == LabelStruct::ENDS_IN_LABEL) {
          const LabelStruct &label = *mLabels[i];
@@ -2462,12 +2465,12 @@ bool LabelTrack::Copy(double t0, double t1, Track ** dest) const
                             label.getT0() - t0,
                             t1 - t0,
                             label.title);
-         ((LabelTrack *) (*dest))->mLabels.Add(l);
+         lt->mLabels.Add(l);
       }
    }
-   ((LabelTrack *) (*dest))->mClipLen = (t1 - t0);
+   lt->mClipLen = (t1 - t0);
 
-   return true;
+   return std::move(tmp);
 }
 
 

src/LabelTrack.h
@@ -153,12 +153,12 @@ class AUDACITY_DLL_API LabelTrack final : public Track
    bool Save(wxTextFile * out, bool overwrite) override;
 #endif
 
-   bool Cut  (double t0, double t1, Track ** dest) override;
+   Track::Holder Cut  (double t0, double t1) override;
    // JKC Do not add the const modifier to Copy(), Clear()
    // or Paste() because then it
    // is no longer recognised as a virtual function matching the
    // one in Track.
-   bool Copy (double t0, double t1, Track ** dest) const override;
+   Track::Holder Copy (double t0, double t1) const override;
    bool Clear(double t0, double t1) override;
    bool Paste(double t, const Track * src) override;
    bool Repeat(double t0, double t1, int n);
@@ -224,7 +224,7 @@ class AUDACITY_DLL_API LabelTrack final : public Track
    bool PasteOver(double t, const Track *src);
 
    // PRL:  These functions were not used because they were not overrides!  Was that right?
-   //bool SplitCut(double b, double e, Track **dest) /* not override */;
+   //Track::Holder SplitCut(double b, double e) /* not override */;
    //bool SplitDelete(double b, double e) /* not override */;
 
    void ShiftLabelsOnInsert(double length, double pt);

src/Menus.cpp
@@ -3815,7 +3815,6 @@ void AudacityProject::OnCut()
 {
    TrackListIterator iter(mTracks);
    Track *n = iter.First();
-   Track *dest;
 
    // This doesn't handle cutting labels, it handles
    // cutting the _text_ inside of labels, i.e. if you're
@@ -3837,22 +3836,22 @@ void AudacityProject::OnCut()
    n = iter.First();
    while (n) {
       if (n->GetSelected()) {
-         dest = NULL;
+         Track::Holder dest;
 #if defined(USE_MIDI)
          if (n->GetKind() == Track::Note)
             // Since portsmf has a built-in cut operator, we use that instead
-            n->Cut(mViewInfo.selectedRegion.t0(),
-                   mViewInfo.selectedRegion.t1(), &dest);
+            dest = n->Cut(mViewInfo.selectedRegion.t0(),
+                   mViewInfo.selectedRegion.t1());
          else
 #endif
-            n->Copy(mViewInfo.selectedRegion.t0(),
-                    mViewInfo.selectedRegion.t1(), &dest);
+            dest = n->Copy(mViewInfo.selectedRegion.t0(),
+                    mViewInfo.selectedRegion.t1());
 
          if (dest) {
             dest->SetChannel(n->GetChannel());
             dest->SetLinked(n->GetLinked());
             dest->SetName(n->GetName());
-            msClipboard->Add(dest);
+            msClipboard->Add(dest.release());
          }
       }
       n = iter.Next();
@@ -3904,21 +3903,21 @@ void AudacityProject::OnSplitCut()
 {
    TrackListIterator iter(mTracks);
    Track *n = iter.First();
-   Track *dest;
 
    ClearClipboard();
    while (n) {
       if (n->GetSelected()) {
-         dest = NULL;
+         Track::Holder dest;
          if (n->GetKind() == Track::Wave)
          {
-            ((WaveTrack*)n)->SplitCut(
+            dest = ((WaveTrack*)n)->SplitCut(
                mViewInfo.selectedRegion.t0(),
-               mViewInfo.selectedRegion.t1(), &dest);
-         } else
+               mViewInfo.selectedRegion.t1());
+         }
+         else
          {
-            n->Copy(mViewInfo.selectedRegion.t0(),
-                    mViewInfo.selectedRegion.t1(), &dest);
+            dest = n->Copy(mViewInfo.selectedRegion.t0(),
+                    mViewInfo.selectedRegion.t1());
             n->Silence(mViewInfo.selectedRegion.t0(),
                        mViewInfo.selectedRegion.t1());
          }
@@ -3926,7 +3925,7 @@ void AudacityProject::OnSplitCut()
             dest->SetChannel(n->GetChannel());
             dest->SetLinked(n->GetLinked());
             dest->SetName(n->GetName());
-            msClipboard->Add(dest);
+            msClipboard->Add(dest.release());
          }
       }
       n = iter.Next();
@@ -3948,7 +3947,6 @@ void AudacityProject::OnCopy()
    TrackListIterator iter(mTracks);
 
    Track *n = iter.First();
-   Track *dest;
 
    while (n) {
       if (n->GetSelected()) {
@@ -3966,14 +3964,13 @@ void AudacityProject::OnCopy()
    n = iter.First();
    while (n) {
       if (n->GetSelected()) {
-         dest = NULL;
-         n->Copy(mViewInfo.selectedRegion.t0(),
-                 mViewInfo.selectedRegion.t1(), &dest);
+         auto dest = n->Copy(mViewInfo.selectedRegion.t0(),
+                 mViewInfo.selectedRegion.t1());
          if (dest) {
             dest->SetChannel(n->GetChannel());
             dest->SetLinked(n->GetLinked());
             dest->SetName(n->GetName());
-            msClipboard->Add(dest);
+            msClipboard->Add(dest.release());
          }
       }
       n = iter.Next();
@@ -4560,13 +4557,12 @@ void AudacityProject::OnDuplicate()
 
    while (n) {
       if (n->GetSelected()) {
-         Track *dest = NULL;
-         n->Copy(mViewInfo.selectedRegion.t0(),
-                 mViewInfo.selectedRegion.t1(), &dest);
+         auto dest = n->Copy(mViewInfo.selectedRegion.t0(),
+                 mViewInfo.selectedRegion.t1());
          if (dest) {
             dest->Init(*n);
             dest->SetOffset(wxMax(mViewInfo.selectedRegion.t0(), n->GetOffset()));
-            mTracks->Add(dest);
+            mTracks->Add(dest.release());
          }
       }
 
@@ -4820,22 +4816,23 @@ void AudacityProject::OnSplitNew()
 
    for (Track *n = iter.First(); n; n = iter.Next()) {
       if (n->GetSelected()) {
-         Track *dest = NULL;
+         Track::Holder dest;
          double newt0 = 0, newt1 = 0;
          double offset = n->GetOffset();
          if (n->GetKind() == Track::Wave) {
+            const auto wt = static_cast<WaveTrack*>(n);
             // Clips must be aligned to sample positions or the NEW clip will not fit in the gap where it came from
-            offset = ((WaveTrack*)n)->LongSamplesToTime(((WaveTrack*)n)->TimeToLongSamples(offset));
-            newt0 = ((WaveTrack*)n)->LongSamplesToTime(((WaveTrack*)n)->TimeToLongSamples(mViewInfo.selectedRegion.t0()));
-            newt1 = ((WaveTrack*)n)->LongSamplesToTime(((WaveTrack*)n)->TimeToLongSamples(mViewInfo.selectedRegion.t1()));
-            ((WaveTrack*)n)->SplitCut(newt0, newt1, &dest);
+            offset = wt->LongSamplesToTime(wt->TimeToLongSamples(offset));
+            newt0 = wt->LongSamplesToTime(wt->TimeToLongSamples(mViewInfo.selectedRegion.t0()));
+            newt1 = wt->LongSamplesToTime(wt->TimeToLongSamples(mViewInfo.selectedRegion.t1()));
+            dest = wt->SplitCut(newt0, newt1);
          }
 #if 0
          // LL:  For now, just skip all non-wave tracks since the other do not
          //      yet support proper splitting.
          else {
-            n->Cut(mViewInfo.selectedRegion.t0(),
-                   mViewInfo.selectedRegion.t1(), &dest);
+            dest = n->Cut(mViewInfo.selectedRegion.t0(),
+                   mViewInfo.selectedRegion.t1());
          }
 #endif
          if (dest) {
@@ -4843,7 +4840,7 @@ void AudacityProject::OnSplitNew()
             dest->SetLinked(n->GetLinked());
             dest->SetName(n->GetName());
             dest->SetOffset(wxMax(newt0, offset));
-            mTracks->Add(dest);
+            mTracks->Add(dest.release());
          }
       }
 

src/NoteTrack.cpp
@@ -443,15 +443,13 @@ int NoteTrack::GetVisibleChannels()
    return mVisibleChannels;
 }
 
-bool NoteTrack::Cut(double t0, double t1, Track **dest){
-
-   //dest goes onto clipboard
-   *dest = NULL; // This is redundant
+Track::Holder NoteTrack::Cut(double t0, double t1)
+{
    if (t1 <= t0)
-      return false;
+      return{};
    double len = t1-t0;
 
-   NoteTrack *newTrack = new NoteTrack(mDirManager);
+   auto newTrack = std::make_unique<NoteTrack>(mDirManager);
 
    newTrack->Init(*this);
 
@@ -463,20 +461,16 @@ bool NoteTrack::Cut(double t0, double t1, Track **dest){
    //(mBottomNote, mDirManager, mLastMidiPosition,
    // mSerializationBuffer, mSerializationLength, mVisibleChannels)
 
-   *dest = newTrack;
-
-   return true;
+   return std::move(newTrack);
 }
 
-bool NoteTrack::Copy(double t0, double t1, Track **dest) const {
-
-   //dest goes onto clipboard
-   *dest = NULL; // This is redundant and matches WaveTrack::Copy
+Track::Holder NoteTrack::Copy(double t0, double t1) const
+{
    if (t1 <= t0)
-      return false;
+      return{};
    double len = t1-t0;
 
-   NoteTrack *newTrack = new NoteTrack(mDirManager);
+   auto newTrack = std::make_unique<NoteTrack>(mDirManager);
 
    newTrack->Init(*this);
 
@@ -488,9 +482,7 @@ bool NoteTrack::Copy(double t0, double t1, Track **dest) const {
    //(mBottomNote, mDirManager, mLastMidiPosition,
    // mSerializationBuffer, mSerializationLength, mVisibleChannels)
 
-   *dest = newTrack;
-
-   return true;
+   return std::move(newTrack);
 }
 
 bool NoteTrack::Trim(double t0, double t1)

src/NoteTrack.h
@@ -90,8 +90,8 @@ class AUDACITY_DLL_API NoteTrack final : public Track {
 //   }
 
    // High-level editing
-   bool Cut  (double t0, double t1, Track **dest) override;
-   bool Copy (double t0, double t1, Track **dest) const override;
+   Track::Holder Cut  (double t0, double t1) override;
+   Track::Holder Copy (double t0, double t1) const override;
    bool Trim (double t0, double t1) /* not override */;
    bool Clear(double t0, double t1) override;
    bool Paste(double t, const Track *src) override;

src/Project.cpp
@@ -4595,20 +4595,18 @@ void AudacityProject::EditClipboardByLabel( EditDestFunction action )
       if( n->GetKind() == Track::Wave && ( allTracks || n->GetSelected() ) )
       {
          WaveTrack *wt = ( WaveTrack* )n;
-         WaveTrack *merged = NULL;
+         Track::Holder merged;
          for( int i = ( int )regions.size() - 1; i >= 0; i-- )
          {
-            Track *dest = NULL;
             const Region &region = regions.at(i);
-            ( wt->*action )( region.start, region.end,
-                             &dest );
+            auto dest = ( wt->*action )( region.start, region.end );
             if( dest )
             {
                dest->SetChannel( wt->GetChannel() );
                dest->SetLinked( wt->GetLinked() );
                dest->SetName( wt->GetName() );
                if( !merged )
-                  merged = ( WaveTrack* )dest;
+                  merged = std::move(dest);
                else
                {
                   // Paste to the beginning; unless this is the first region,
@@ -4617,10 +4615,9 @@ void AudacityProject::EditClipboardByLabel( EditDestFunction action )
                      merged->Offset(
                         regions.at(i + 1).start - region.end);
 
-                  bool bResult = merged->Paste( 0.0 , dest );
+                  bool bResult = merged->Paste( 0.0 , dest.get() );
                   wxASSERT(bResult); // TO DO: Actually handle this.
                   wxUnusedVar(bResult);
-                  delete dest;
                }
             }
             else  // nothing copied but there is a 'region', so the 'region' must be a 'point label' so offset
@@ -4630,7 +4627,7 @@ void AudacityProject::EditClipboardByLabel( EditDestFunction action )
                         regions.at(i + 1).start - region.end);
          }
          if( merged )
-            msClipboard->Add( merged );
+            msClipboard->Add( merged.release() );
       }
    }
 

commit 079eb3e98e9fb2cb5857e1d4acdcfd4b7a6c2a31

Bug1375: Fix sync-lock behavior ...
... which was broken by commit 3015a9c8d9229406f8c33fd32ffc64c11d77ca54

src/WaveTrack.cpp
@@ -1155,14 +1155,15 @@ bool WaveTrack::SyncLockAdjust(double oldT1, double newT1)
       // If track is empty at oldT1 insert whitespace; otherwise, silence
       if (IsEmpty(oldT1, oldT1))
       {
-         bool ret = false;
+         bool ret = true;
 
          // Check if clips can move
          bool clipsCanMove = true;
          gPrefs->Read(wxT("/GUI/EditClipCanMove"), &clipsCanMove);
          if (clipsCanMove) {
             auto tmp = Cut (oldT1, GetEndTime() + 1.0/GetRate());
-            if (!ret) return false;
+            if (!tmp)
+               return false;
 
             ret = Paste(newT1, tmp.get());
             wxASSERT(ret);