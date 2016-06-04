0e09f2ce04cc1492d078e592ce8c678c6bb74179
Paul Licameli <paul.licameli@audacityteam.org>
2016-06-04 19:07:57 -0400

    Seek, scrub images have wider gap between the triangles

--- src/AllThemeResources.h
+++ src/AllThemeResources.h
@@ -67,10 +67,10 @@ from there.  Audacity will look for a file called "Pause.png".
    DEFINE_IMAGE( bmpCutPreviewDisabled, wxImage( 16, 16 ), wxT("CutPreviewDisabled"));
    DEFINE_IMAGE( bmpAppendRecord, wxImage( 16, 16 ), wxT("AppendRecord"));
    DEFINE_IMAGE( bmpAppendRecordDisabled, wxImage( 16, 16 ), wxT("AppendRecordDisabled"));
-   DEFINE_IMAGE( bmpScrubDisabled, wxImage( 16, 16 ), wxT("ScrubDisabled"));
-   DEFINE_IMAGE( bmpScrub, wxImage( 16, 16 ), wxT("Scrub"));
-   DEFINE_IMAGE( bmpSeekDisabled, wxImage( 24, 16 ), wxT("SeekDisabled"));
-   DEFINE_IMAGE( bmpSeek, wxImage( 24, 16 ), wxT("Seek"));
+   DEFINE_IMAGE( bmpScrubDisabled, wxImage( 18, 16 ), wxT("ScrubDisabled"));
+   DEFINE_IMAGE( bmpScrub, wxImage( 18, 16 ), wxT("Scrub"));
+   DEFINE_IMAGE( bmpSeekDisabled, wxImage( 26, 16 ), wxT("SeekDisabled"));
+   DEFINE_IMAGE( bmpSeek, wxImage( 26, 16 ), wxT("Seek"));
 
    SET_THEME_FLAGS(  resFlagNewLine  );
    DEFINE_IMAGE( bmpUpButtonLarge, wxImage( 48, 48 ), wxT("UpButtonLarge"));
