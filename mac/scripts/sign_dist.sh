29a392f67507c74b2c1138d7538771b2f3ee2fec
James Crook <james.k.crook@gmail.com>
2017-01-03 15:28:42 +0000

    Script tweaks.
    After getting git ident, make sure AboutDialog.cpp rebuilds.

--- mac/scripts/sign_dist.sh
+++ mac/scripts/sign_dist.sh
@@ -4,8 +4,10 @@
 
 hdiutil resize -size 200m /private/tmp/TMP.dmg
 hdiutil mount /private/tmp/TMP.dmg
+#Now move the folders out the way so they don't clutter appearance.
+#Also optionally add Portable Settings and Auto Save with chmod 777.
 codesign --deep -s IDENTITY -v "/Volumes/Audacity VERSION/Audacity.app" 
-spctl -s -v "Volumes/Audacity VERSION/Audacity.app"
+spctl -a -v "/Volumes/Audacity VERSION/Audacity.app"
 hdiutil eject "/Volumes/Audacity VERSION/"
 
 # Compress and prepare for Internet delivery
