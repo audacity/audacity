audacity/audacity

commit 9da999d40c1b4f4f15ffe69d7d4e34cf181ac228

Use OffsetTimeByPixels correctly...
That is, don't assume uniformity of pixels per second, but supply the correct
first argument.

This will matter when there is a magnifier.  But that will not be in the next
version.

src/TrackPanel.cpp
@@ -4017,17 +4017,21 @@ void TrackPanel::OnClipMove(bool right, bool keyUp)
       mCapturedClipIsSelection = track->GetSelected() && !mViewInfo->selectedRegion.isPoint();
       mTrackExclusions.clear();
 
-      CreateListOfCapturedClips(mViewInfo->selectedRegion.t0());
+      auto t0 = mViewInfo->selectedRegion.t0();
+      CreateListOfCapturedClips( t0 );
 
-      double desiredSlideAmount = mViewInfo->OffsetTimeByPixels(0.0, 1);
+      auto newT0 = mViewInfo->OffsetTimeByPixels( t0, ( right ? 1 : -1 ) );
+      auto desiredSlideAmount = newT0 - t0;
 
       // set it to a sample point, and minimum of 1 sample point
+      if (!right)
+         desiredSlideAmount *= -1;
       double nSamples = rint(wt->GetRate() * desiredSlideAmount);
       nSamples = std::max(nSamples, 1.0);
       desiredSlideAmount = nSamples / wt->GetRate();
-
       if (!right)
          desiredSlideAmount *= -1;
+
       mHSlideAmount = desiredSlideAmount;
       DoSlideHorizontal();
       mHSlideAmountTotal += mHSlideAmount;
@@ -4035,12 +4039,11 @@ void TrackPanel::OnClipMove(bool right, bool keyUp)
       // update t0 and t1. There is the possibility that the updated
       // t0 may no longer be within the clip due to rounding errors,
       // so t0 is adjusted so that it is.
-      double newT0 = mViewInfo->selectedRegion.t0() + mHSlideAmount;
       if (newT0 < mCapturedClip->GetStartTime())
          newT0 = mCapturedClip->GetStartTime();
       if (newT0 > mCapturedClip->GetEndTime())
          newT0 = mCapturedClip->GetEndTime();
-      double diff = mViewInfo->selectedRegion.t1() - mViewInfo->selectedRegion.t0();
+      double diff = mViewInfo->selectedRegion.duration();
       mViewInfo->selectedRegion.setTimes(newT0, newT0 + diff);
 
       ScrollIntoView(mViewInfo->selectedRegion.t0());

commit f9827a57a728ed28a3d8cfffc50c362f32f3b5e9

Minor fix for commands for shifting clips using the keyboard.
Fix for minor bug introduced in commit 9da999d. The selection wasn't being updated by the actual slide amount.

src/TrackPanel.cpp
@@ -4019,8 +4019,8 @@ double TrackPanel::OnClipMove
       CreateListOfCapturedClips
          ( state, viewInfo, *track, trackList, syncLocked, t0 );
 
-      auto newT0 = viewInfo.OffsetTimeByPixels( t0, ( right ? 1 : -1 ) );
-      auto desiredSlideAmount = newT0 - t0;
+      auto desiredT0 = viewInfo.OffsetTimeByPixels( t0, ( right ? 1 : -1 ) );
+      auto desiredSlideAmount = desiredT0 - t0;
 
       // set it to a sample point, and minimum of 1 sample point
       if (!right)
@@ -4037,6 +4037,7 @@ double TrackPanel::OnClipMove
       // update t0 and t1. There is the possibility that the updated
       // t0 may no longer be within the clip due to rounding errors,
       // so t0 is adjusted so that it is.
+      double newT0 = t0 + state.hSlideAmount;
       if (newT0 < state.capturedClip->GetStartTime())
          newT0 = state.capturedClip->GetStartTime();
       if (newT0 > state.capturedClip->GetEndTime())