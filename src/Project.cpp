6b5c5a0335cb07c304f3b37bba4e649c48423380
Paul Licameli <paul.licameli@audacityteam.org>
2017-06-11 11:21:36 -0400

    More simplifications by use of SelectionStateChanger

--- src/Project.cpp
+++ src/Project.cpp
@@ -4073,7 +4073,7 @@ bool AudacityProject::Save(bool overwrite /* = true */ ,
       {
          if (pTrack->GetKind() == Track::Wave)
          {
-            auto wasSelected = pTrack->GetSelected();
+            SelectionStateChanger changer{ GetSelectionState(), *GetTracks() };
             pTrack->SetSelected(true);
             if (pTrack->GetLinked())
             {
@@ -4082,14 +4082,6 @@ bool AudacityProject::Save(bool overwrite /* = true */ ,
             }
             else
                pRightTrack = NULL;
-
-            auto cleanup = finally( [&] {
-               if (!wasSelected) {
-                  pTrack->SetSelected(false);
-                  if (pRightTrack)
-                     pRightTrack->SetSelected(false);
-               }
-            } );
 
             uniqueTrackFileName = wxFileName(strDataDirPathName, pTrack->GetName(), wxT("ogg"));
             FileNames::MakeNameUnique(mStrOtherNamesArray, uniqueTrackFileName);
