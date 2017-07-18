audacity/audacity

commit ad04187a4122556c9a91ce1ac6d6a7cf3b4162ac

Change sampleCount arguments, variables, return values to size_t...
... whenever they really describe the size of a buffer that fits in memory, or
of a block file (which is never now more than a megabyte and so could be fit in
memory all at once), or a part thereof.

src/Mix.cpp
@@ -241,7 +241,7 @@ Mixer::WarpOptions::WarpOptions(double min, double max)
 Mixer::Mixer(const WaveTrackConstArray &inputTracks,
              const WarpOptions &warpOptions,
              double startTime, double stopTime,
-             unsigned numOutChannels, int outBufferSize, bool outInterleaved,
+             unsigned numOutChannels, size_t outBufferSize, bool outInterleaved,
              double outRate, sampleFormat outFormat,
              bool highQuality, MixerSpec *mixerSpec)
 {
@@ -405,7 +405,7 @@ void MixBuffers(unsigned numChannels, int *channelFlags, float *gains,
    }
 }
 
-sampleCount Mixer::MixVariableRates(int *channelFlags, WaveTrackCache &cache,
+size_t Mixer::MixVariableRates(int *channelFlags, WaveTrackCache &cache,
                                     sampleCount *pos, float *queue,
                                     int *queueStart, int *queueLen,
                                     Resample * pResample)
@@ -546,7 +546,7 @@ sampleCount Mixer::MixVariableRates(int *channelFlags, WaveTrackCache &cache,
    return out;
 }
 
-sampleCount Mixer::MixSameRate(int *channelFlags, WaveTrackCache &cache,
+size_t Mixer::MixSameRate(int *channelFlags, WaveTrackCache &cache,
                                sampleCount *pos)
 {
    const WaveTrack *const track = cache.GetTrack();
@@ -601,7 +601,7 @@ sampleCount Mixer::MixSameRate(int *channelFlags, WaveTrackCache &cache,
    return slen;
 }
 
-sampleCount Mixer::Process(sampleCount maxToProcess)
+size_t Mixer::Process(size_t maxToProcess)
 {
    // MB: this is wrong! mT represented warped time, and mTime is too inaccurate to use
    // it here. It's also unnecessary I think.

commit fcd175183f11c4cd132195fa1931b6f26b3aa185

Bug1632: correct length of non-WAV exports >= 2^32 samples long...
... WAV format simply can't do that, others (FLAC, ogg, mp3) can and should

Some history:

This got broken very badly for an interim starting at
919d77d1760bab320ae963e1e54fc6e4981b6000.
Exported files were only tens of bytes!

This was broken differently after
ad04187a4122556c9a91ce1ac6d6a7cf3b4162ac with symptoms as in the bug report.

src/Mix.cpp
@@ -540,7 +540,6 @@ size_t Mixer::MixSameRate(int *channelFlags, WaveTrackCache &cache,
                                sampleCount *pos)
 {
    const WaveTrack *const track = cache.GetTrack();
-   auto slen = mMaxOut;
    const double t = ( *pos ).as_double() / track->GetRate();
    const double trackEndTime = track->GetEndTime();
    const double trackStartTime = track->GetStartTime();
@@ -553,10 +552,13 @@ size_t Mixer::MixSameRate(int *channelFlags, WaveTrackCache &cache,
    if ((backwards ? t <= tEnd : t >= tEnd))
       return 0;
    //if we're about to approach the end of the track or selection, figure out how much we need to grab
-   slen = std::min<decltype(slen)>( slen,
-      ((backwards ? t - tEnd : tEnd - t) * track->GetRate() + 0.5)
+   auto slen = limitSampleBufferSize(
+      mMaxOut,
+      // PRL: maybe t and tEnd should be given as sampleCount instead to
+      // avoid trouble subtracting one large value from another for a small
+      // difference
+      sampleCount{ (backwards ? t - tEnd : tEnd - t) * track->GetRate() + 0.5 }
    );
-   slen = std::min(slen, mMaxOut);
 
    if (backwards) {
       auto results = cache.Get(floatSample, *pos - (slen - 1), slen, mMayThrow);