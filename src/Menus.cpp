audacity/audacity

commit 79c3bef2ced0b388ba284e9ba3e7af906ff60afb

Exception safety in: general effect performing functions

src/Menus.cpp
@@ -3628,6 +3628,27 @@ bool AudacityProject::OnEffect(const PluginID & ID, int flags)
    WaveTrack *newTrack{};
    wxWindow *focus = wxWindow::FindFocus();
 
+   bool success = false;
+   auto cleanup = finally( [&] {
+
+      if (!success) {
+         if (newTrack) {
+            mTracks->Remove(newTrack);
+            mTrackPanel->Refresh(false);
+         }
+
+         // For now, we're limiting realtime preview to a single effect, so
+         // make sure the menus reflect that fact that one may have just been
+         // opened.
+         UpdateMenus(false);
+      }
+
+      if (focus != NULL) {
+         focus->SetFocus();
+      }
+
+   } );
+
    //double prevEndTime = mTracks->GetEndTime();
    int count = 0;
    bool clean = true;
@@ -3650,24 +3671,13 @@ bool AudacityProject::OnEffect(const PluginID & ID, int flags)
 
    EffectManager & em = EffectManager::Get();
 
-   bool success = em.DoEffect(ID, this, mRate,
+   success = em.DoEffect(ID, this, mRate,
                                GetTracks(), GetTrackFactory(),
                                &mViewInfo.selectedRegion,
                                (flags & OnEffectFlags::kConfigured) == 0);
 
-   if (!success) {
-      if (newTrack) {
-         mTracks->Remove(newTrack);
-         mTrackPanel->Refresh(false);
-      }
-
-      // For now, we're limiting realtime preview to a single effect, so
-      // make sure the menus reflect that fact that one may have just been
-      // opened.
-      UpdateMenus(false);
-
+   if (!success)
       return false;
-   }
 
    if (em.GetSkipStateFlag())
       flags = flags | OnEffectFlags::kSkipState;
@@ -3681,7 +3691,7 @@ bool AudacityProject::OnEffect(const PluginID & ID, int flags)
 
    if (!(flags & OnEffectFlags::kDontRepeatLast))
    {
-      // Only remember a successful effect, don't rmemeber insert,
+      // Only remember a successful effect, don't remember insert,
       // or analyze effects.
       if (type == EffectTypeProcess) {
          wxString shortDesc = em.GetEffectName(ID);
@@ -3705,9 +3715,6 @@ bool AudacityProject::OnEffect(const PluginID & ID, int flags)
          //  mTrackPanel->Refresh(false);
    }
    RedrawProject();
-   if (focus != NULL) {
-      focus->SetFocus();
-   }
    mTrackPanel->EnsureVisible(mTrackPanel->GetFirstSelectedTrack());
 
    mTrackPanel->Refresh(false);

include/audacity/EffectInterface.h
@@ -131,7 +131,8 @@ public:
 
    virtual bool IsReady() = 0;
    virtual bool ProcessInitialize(sampleCount totalLen, ChannelNames chanMap = NULL) = 0;
-   virtual bool ProcessFinalize() = 0;
+   // This may be called during stack unwinding:
+   virtual bool ProcessFinalize() /* noexcept */ = 0;
    virtual size_t ProcessBlock(float **inBlock, float **outBlock, size_t blockLen) = 0;
 
    virtual bool RealtimeInitialize() = 0;

src/effects/Effect.h
@@ -287,7 +287,9 @@ protected:
    virtual bool InitPass2();
    virtual int GetPass();
 
-   // clean up any temporary memory
+   // clean up any temporary memory, needed only per invocation of the
+   // effect, after either successful or failed or exception-aborted processing.
+   // Invoked inside a "finally" block so it must be no-throw.
    virtual void End();
 
    // Most effects just use the previewLength, but time-stretching/compressing
@@ -470,8 +472,12 @@ protected:
                      WaveTrack *right,
                      sampleCount leftStart,
                      sampleCount rightStart,
-                     sampleCount len);
- 
+                     sampleCount len,
+                     FloatBuffers &inBuffer,
+                     FloatBuffers &outBuffer,
+                     ArrayOf< float * > &inBufPos,
+                     ArrayOf< float *> &outBufPos);
+
  //
  // private data
  //
@@ -504,11 +510,6 @@ private:
    EffectClientInterface *mClient;
    size_t mNumAudioIn;
    size_t mNumAudioOut;
-
-   FloatBuffers mInBuffer, mOutBuffer;
-   
-   using Positions = ArrayOf < float* > ; // Array of non-owning pointers into the above
-   Positions mInBufPos, mOutBufPos;
 
    size_t mBufferSize;
    size_t mBlockSize;

src/effects/EffectRack.cpp
@@ -17,6 +17,8 @@
 #if defined(EXPERIMENTAL_EFFECTS_RACK)
 
 #include "../MemoryX.h"
+#include "../UndoManager.h"
+
 #include <wx/access.h>
 #include <wx/defs.h>
 #include <wx/bmpbuttn.h>
@@ -289,17 +291,36 @@ void EffectRack::OnTimer(wxTimerEvent & WXUNUSED(evt))
 void EffectRack::OnApply(wxCommandEvent & WXUNUSED(evt))
 {
    AudacityProject *project = GetActiveProject();
-   
+
+   bool success = false;
+   auto state = project->GetUndoManager()->GetCurrentState();
+   auto cleanup = finally( [&] {
+      if(!success)
+         project->SetStateTo(state);
+   } );
+
    for (size_t i = 0, cnt = mEffects.size(); i < cnt; i++)
    {
       if (mPowerState[i])
       {
-         project->OnEffect(mEffects[i]->GetID(),
-                           AudacityProject::OnEffectFlags::kConfigured);
+         if (!project->OnEffect(mEffects[i]->GetID(),
+                           AudacityProject::OnEffectFlags::kConfigured))
+            // If any effect fails (or throws), then stop.
+            return;
+      }
+   }
 
+   success = true;
+
+   // Only after all succeed, do the following.
+   for (size_t i = 0, cnt = mEffects.size(); i < cnt; i++)
+   {
+      if (mPowerState[i])
+      {
          mPowerState[i] = false;
 
-         wxBitmapButton *btn = static_cast<wxBitmapButton *>(FindWindowById(ID_POWER + i));
+         wxBitmapButton *btn =
+            static_cast<wxBitmapButton *>(FindWindowById(ID_POWER + i));
          btn->SetBitmapLabel(mPowerRaised);
          btn->SetBitmapSelected(mPowerRaised);
       }

commit c1c19774b436acc8a684f42f097cc31a37101e2b

When a VST effect is opened, the dialog is not the focus.
Problem caused by commit: 79c3bef
SetFocus() is being called when !success.

Fix: Move the call to SetFocus back to its original place in the code.

src/Menus.cpp
@@ -4218,10 +4218,6 @@ bool AudacityProject::OnEffect(const PluginID & ID, int flags)
          UpdateMenus(false);
       }
 
-      if (focus != NULL) {
-         focus->SetFocus();
-      }
-
    } );
 
    //double prevEndTime = mTracks->GetEndTime();
@@ -4290,6 +4286,9 @@ bool AudacityProject::OnEffect(const PluginID & ID, int flags)
          //  mTrackPanel->Refresh(false);
    }
    RedrawProject();
+   if (focus != nullptr) {
+      focus->SetFocus();
+   }
    mTrackPanel->EnsureVisible(mTrackPanel->GetFirstSelectedTrack());
 
    mTrackPanel->Refresh(false);