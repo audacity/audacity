025eb3075a1f2702e88b0b5df075e1da26e95b10
Paul Licameli <paul.licameli@audacityteam.org>
2017-09-25 03:06:24 -0400

    One more trick needed to get eu distinct from eu_ES in preferences

--- src/AudacityApp.cpp
+++ src/AudacityApp.cpp
@@ -977,10 +977,13 @@ AudacityLogger *AudacityApp::GetLogger()
 #define WL(lang,sublang)
 #endif
 
-#if !wxCHECK_VERSION(3, 0, 1)
+#if wxCHECK_VERSION(3, 0, 1)
 wxLanguageInfo userLangs[] =
 {
-   { wxLANGUAGE_USER_DEFINED, wxT("bs"), WL(0, SUBLANG_DEFAULT) wxT("Bosnian"), wxLayout_LeftToRight }
+   // Bosnian is defined in wxWidgets already
+//   { wxLANGUAGE_USER_DEFINED, wxT("bs"), WL(0, SUBLANG_DEFAULT) wxT("Bosnian"), wxLayout_LeftToRight },
+
+   { wxLANGUAGE_USER_DEFINED, wxT("eu"), WL(0, SUBLANG_DEFAULT) wxT("Basque"), wxLayout_LeftToRight },
 };
 #endif
 
@@ -1373,7 +1376,7 @@ bool AudacityApp::OnInit()
    //
    // TODO:  The whole Language initialization really need to be reworked.
    //        It's all over the place.
-#if !wxCHECK_VERSION(3, 0, 1)
+#if wxCHECK_VERSION(3, 0, 1)
    for (size_t i = 0, cnt = WXSIZEOF(userLangs); i < cnt; i++)
    {
       wxLocale::AddLanguage(userLangs[i]);
