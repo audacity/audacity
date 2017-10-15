audacity/audacity

commit a0ea5d64db415e478cc1382628e762138da38d2f

Bug 1469 - Shift key does not modify Play/Record buttons when Karaoke view has focus

src/commands/CommandManager.cpp
@@ -1204,12 +1204,17 @@ bool CommandManager::FilterKeyEvent(AudacityProject *project, const wxKeyEvent &
    {
       wxWindow * pWnd = wxWindow::FindFocus();
       wxWindow * pTrackPanel = (wxWindow*)GetActiveProject()->GetTrackPanel();
+      bool bIntercept = pWnd !=  pTrackPanel;
+      // Intercept keys from windows that are NOT panels
+      if( bIntercept ){
+         bIntercept = pWnd && ( dynamic_cast<wxPanel*>(pWnd) == NULL );
+      }
       //wxLogDebug("Focus: %p TrackPanel: %p", pWnd, pTrackPanel );
       // We allow the keystrokes below to be handled by wxWidgets controls IF we are 
       // in some sub window rather than in the TrackPanel itself.
       // Otherwise they will go to our command handler and if it handles them 
       // they will NOT be available to wxWidgets.
-      if( pWnd !=  pTrackPanel ){
+      if( bIntercept ){
          switch( evt.GetKeyCode() ){
          case WXK_LEFT:
          case WXK_RIGHT:

src/Lyrics.cpp
@@ -461,6 +461,7 @@ void Lyrics::OnKeyEvent(wxKeyEvent & event)
 {
    AudacityProject *project = GetActiveProject();
    project->GetCommandManager()->FilterKeyEvent(project, event, true);
+   event.Skip();
 }
 
 void Lyrics::OnPaint(wxPaintEvent & WXUNUSED(event))

commit cbfde23e42e32b51c87a31526cf49218eac16654

Fix for 1753: keyboard interaction with meter buttons broken
There are two problems with using Enter to press the meter drop down buttons:
1. It only works the first time.
2. If auto-select auto is on, then audio is auto-selected.

This was caused by commit a0ea5d6. The problem with this commit is that meters and sliders are also derived from wxPanel, and therefore affected by the fix.

To fix this, make the above fix specific to the Lyrics panel.

src/commands/CommandManager.cpp
@@ -89,6 +89,7 @@ CommandManager.  It holds the callback for one command.
 #include "../AudacityException.h"
 #include "../Prefs.h"
 #include "../Project.h"
+#include "../Lyrics.h"
 
 #include "Keyboard.h"
 #include "../PluginManager.h"
@@ -1380,9 +1381,9 @@ bool CommandManager::FilterKeyEvent(AudacityProject *project, const wxKeyEvent &
       wxWindow * pWnd = wxWindow::FindFocus();
       wxWindow * pTrackPanel = (wxWindow*)GetActiveProject()->GetTrackPanel();
       bool bIntercept = pWnd !=  pTrackPanel;
-      // Intercept keys from windows that are NOT panels
+      // Intercept keys from windows that are NOT the Lyrics panel
       if( bIntercept ){
-         bIntercept = pWnd && ( dynamic_cast<wxPanel*>(pWnd) == NULL );
+         bIntercept = pWnd && ( dynamic_cast<Lyrics*>(pWnd) == NULL );
       }
       //wxLogDebug("Focus: %p TrackPanel: %p", pWnd, pTrackPanel );
       // We allow the keystrokes below to be handled by wxWidgets controls IF we are 