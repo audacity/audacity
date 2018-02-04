035e3babc43c26795220ad738edeb2db000de14f
andheh <36114788+andheh@users.noreply.github.com>
2018-02-04 23:11:36 +0100

    Clamp.h: removed constexpr
    MSVC before Visual Studio 2015 does not support constexpr
    => remove it for now.

--- src/Clamp.h
+++ src/Clamp.h
@@ -3,7 +3,7 @@
 
 // May be replaced with C++17 std::clamp once C++17 is default.
 template <typename T>
-constexpr const T& Clamp(const T& v, const T& min, const T& max)
+const T& Clamp(const T& v, const T& min, const T& max)
 {
     return v < min ? min : max < v ? max : v;
 }
