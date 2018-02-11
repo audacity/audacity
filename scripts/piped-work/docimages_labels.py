62ca1cce26ba22628c7f08dc5ddbf12d58e0453d
James Crook <james.k.crook@gmail.com>
2018-02-11 21:54:19 +0000

    Improved labels script.

--- scripts/piped-work/docimages_labels.py
+++ scripts/piped-work/docimages_labels.py
@@ -74,6 +74,10 @@ def makeWayForTracks(  ) :
     do( 'RemoveTracks' )
 
 
+def capture( what ) :
+    global path
+    do( 'Screenshot: Path='+path+' CaptureWhat=' + what )
+
 def makeMonoTracks( num ) :
     makeWayForTracks( )
     for i in range( 0, num ):
@@ -110,15 +114,93 @@ def addLabels():
     do( 'SetLabel: LabelIndex=3 Text="Blood in These Veins" Start=16 End=16 ')
     do( 'SelectTime: StartTime=9 EndTime=12' )
 
-# A mono track complete with ruler
+def addLabels2():
+    do( 'SelectTime: StartTime=0 EndTime=1' )
+    do( 'AddLabel' )
+    do( 'AddLabel' )
+    do( 'AddLabel' )
+    do( 'AddLabel' )
+    do( 'AddLabel' )
+    do( 'NewLabelTrack' )
+    #do( 'SelectTracks: FirstTrack=2 LastTrack=2' )
+    do( 'AddLabel' )
+    do( 'AddLabel' )
+    do( 'AddLabel' )
+    do( 'AddLabel' )
+    do( 'AddLabel' )
+    do( 'SetLabel: LabelIndex=0 Selected=0 Text="intro" Start=0.1 End=0.1 ')
+    do( 'SetLabel: LabelIndex=1 Text="thoughtpiece" Start=3 End=3 ')
+    do( 'SetLabel: LabelIndex=2 Text="discussion" Start=12 End=12 ')
+    do( 'SetLabel: LabelIndex=3 Text="summary" Start=20 End=20 ')
+    do( 'SetLabel: LabelIndex=4 Text="credits" Start=25 End=25 ')
+    do( 'SetLabel: LabelIndex=5 Selected=0 Text="Bach" Start=0.1 End=0.1 ')
+    do( 'SetLabel: LabelIndex=6 Text="Vivaldi" Start=6 End=6 ')
+    do( 'SetLabel: LabelIndex=7 Text="Mozart" Start=12 End=12 ')
+    do( 'SetLabel: LabelIndex=8 Text="Satie" Start=18 End=18 ')
+    do( 'SetLabel: LabelIndex=9 Text="Chopin" Start=24 End=24 ')
+    do( 'SelectTracks: FirstTrack=3 LastTrack=3' )
+
+
+# A stero track with four labels.
 def image1() :
-    global path
     makeStereoTracks(1)
     addLabels()
-    do( 'Screenshot: Path='+path+' CaptureWhat=First_Two_Tracks' )
+    capture( 'First_Two_Tracks' )
 
+# Removing a label with delete (fraud - we used split delete)
+def image2() :
+    makeStereoTracks(1)
+    addLabels()
+    do( "SelectTime: StartTime=8.9 EndTime=12.1" )
+    do( "SelectTracks: FirstTrack=2 LastTrack=2" )
+    do( "SplitDelete" )
+    do( "SelectTime: StartTime=0 EndTime=0" )
+    do( "SelectTracks: FirstTrack=0 LastTrack=2" )
+    capture( 'First_Two_Tracks' )
+
+# Removing a label with split-delete step 1
+def image3() :
+    makeStereoTracks(1)
+    addLabels()
+    do( "SelectTime: StartTime=8.9 EndTime=12.1" )
+    do( "SelectTracks: FirstTrack=2 LastTrack=2" )
+    capture( 'First_Two_Tracks' )
+
+# Removing a label with split-delete step 1
+def image4() :
+    makeStereoTracks(1)
+    addLabels()
+    do( "SelectTime: StartTime=8.9 EndTime=12.1" )
+    do( "SelectTracks: FirstTrack=2 LastTrack=2" )
+    do( "SplitDelete" )
+    capture( 'First_Two_Tracks' )
+
+def image5() :
+    makeStereoTracks(1)
+    addLabels2()
+    do( "SelectTime: StartTime=0 EndTime=0" )
+    capture( 'First_Three_Tracks' )
+
+def image6() :
+    makeStereoTracks(1)
+    addLabels2()
+    do( "SelectTime: StartTime=5.7 EndTime=11.7" )
+    capture( 'First_Three_Tracks' )
+
+def image7() :
+    makeStereoTracks(1)
+    addLabels2()
+    do( "SelectTime: StartTime=5.7 EndTime=11.7" )
+    do( "Delete" )
+    do( "SelectTime: StartTime=0 EndTime=0" )
+    capture( 'First_Three_Tracks' )
     
 #quickTest()
 setup()
 image1()
-
+image2()
+image3()
+image4()
+image5()
+image6()
+image7()
