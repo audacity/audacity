6236a9fb9422f9561feb5545c631192d2f386ec5
James Crook <james.k.crook@gmail.com>
2018-02-12 18:07:33 +0000

    Add support for aud-do command into Nyquist.
    Examples:
    
    (aud-do "GetInfo")
    (aud-do "Select: Start=4 End=7")

--- lib-src/libnyquist/nyquist/nyqsrc/localptrs.h
+++ lib-src/libnyquist/nyquist/nyqsrc/localptrs.h
@@ -2,8 +2,11 @@
  *
  * CHANGE LOG
  * 28-Apr-03  rbd  Removed "include switches.h" -- already included
+ * 12-Feb-18  jkc  Added "include audacityfnintptrs.h" -- for internal Audacity commands
  */
  
 /* extension to xlisp */
 #include "sndfnintptrs.h"
 #include "seqfnintptrs.h"
+#include "audacityfnintptrs.h"
+
