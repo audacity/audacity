7cfe4ac20a1398d08e6281081b4691581cc5c96c
James Crook <james.k.crook@gmail.com>
2018-02-16 12:50:48 +0000

    Add space and dc bias to afters script.

--- scripts/piped-work/docimages_after.py
+++ scripts/piped-work/docimages_after.py
@@ -14,13 +14,22 @@ Commands = ["Cut", "Split", "SplitCut",
             "Silence", "Trim",
             "SplitNew", "Duplicate",
             "ZoomIn", "ZoomOut", "ZoomNormal", "ZoomSel", "FitInWindow", "FitV" 
-            "Amplify","BassAndTreble","ChangePitch","ChangeSpeed",
-            "ChangeTempo","Compressor","Distortion","Echo",
-            "Equalization","Normalize","Paulstretch","Phaser",
-            "Repeat","Reverb","TimeScale","Wahwah",
-            "HighPassFilter","LowPassFilter","Vocoder",
+            ]
+
+Effects  = [
+            "Reverse","FadeIn", "FadeOut", "Invert",
+            "Amplify","BassAndTreble","Compressor","Distortion","Echo",
+            "Equalization","Normalize","Phaser",
+            "Repeat","Reverb","Wahwah",
+            "HighPassFilter","LowPassFilter",
             "NotchFilter","AdjustableFade","Delay","Limiter"               
             ]
+
+Slow = ["ChangePitch","ChangeSpeed", "ChangeTempo","Paulstretch",
+         "TimeScale"
+            ]
+
+
 Generators = ["Chirp","DtmfTones","Noise","Tone","Pluck"]
 
 
@@ -47,6 +56,8 @@ ToDo = ["Paste", "Join", "DisJoin",
 "CrossfadeClips",
 "CrossfadeTracks",
 "RegularIntervalLabels",
+
+"Vocoder", #stereo        
         
 ]
 
@@ -56,18 +67,41 @@ def starterTrack():
     do( 'ZoomSel' )
     do( 'Select: Start=55 End=70')
 
-def imageAfters():
+def withDcBias( amount ):
+    loadMonoTracks(1)
+    do( 'NewMonoTrack' )
+    do( 'Select: First=1 Last=1 Start=0 End=0.5' )
+    do( 'Tone: Frequency=1.0 Amplitude='+str(amount) +
+        ' Waveform=Square Interpolation=Linear' )
+    do( 'Repeat: Count=300' )
+    do( 'Join' );
+    do( 'Select: First=0 Last=1 Start=0 End=150' )
+    do( 'MixAndRender' );
+    do( 'Select: First=0 Last=0 Start=0 End=0' )
+    do( 'SetTrack: Name="Track with DC Bias"')
+    
+def spaceyTrack() :   
+    loadMonoTracks(1)
+    do( 'Select: First=0 Last=0 Start=20 End=40')
+    do( 'Silence' )
+    do( 'Select: First=0 Last=0 Start=60 End=100')
+    do( 'Silence' )
+    do( 'Select: First=0 Last=0 Start=10 End=140')
+
+
+def imageAfters( commands, doWhat):
     starterTrack()
-    capture( 'BeforeEffect.png', 'All_Tracks_plus' )
-    for name in Commands :
+    capture( 'BeforeEffect.png', 'All_Tracks_Plus' )
+    for name in commands :
         starterTrack()
         do( 'SetTrack: Name="'+name+'"')
         do( name )
-        capture( 'After' + name + '.png' , 'All_Tracks_Plus' )
+        capture( 'After' + name + '1.png' , doWhat )
+        do( 'Select: Start=0 End=0')
+        capture( 'After' + name + '2.png' , doWhat )
 
 
 def generators():
-   
     for name in Generators :
         makeWayForTracks()
         do( 'NewMonoTrack' )
@@ -77,7 +111,33 @@ def generators():
         do( 'ZoomSel' )
         do( 'Select: Start=0 End=0' )
         capture( 'After' + name + '.png' , 'All_Tracks_Plus' )
+
+
+def spaceDemo():
+    spaceyTrack()
+    capture( 'BeforeTruncateSilence.png' , 'All_Tracks' )
+    do( 'Select: First=0 Last=0 Start=0 End=0')
+    capture( 'SpaceyTrack.png' , 'All_Tracks' )
+    do( 'TruncateSilence' )
+    capture( 'AfterTruncateSilence1.png' , 'All_Tracks' )
+    do( 'Select: First=0 Last=0 Start=0 End=0')
+    capture( 'AfterTruncateSilence2.png' , 'All_Tracks' )
+    spaceyTrack()
+    do( 'Disjoin' )
+    capture( 'AfterDisjoin1.png' , 'All_Tracks' )
+    do( 'Select: First=0 Last=0 Start=0 End=0')
+    capture( 'AfterDisjoin2.png' , 'All_Tracks' )
     
-imageAfters()
-generators()
+
+def biasDemo():
+    withDcBias( 0.1 )
+    capture( 'DcBias.png' , 'All_Tracks' )
+    
+#imageAfters( Commands, 'All_Tracks_Plus' ) # With ruler
+#imageAfters( Effects, 'All_Tracks' ) # Without ruler
+#imageAfters( Slow, 'All_Tracks' ) # Without ruler
+#generators()
+#spaceDemo()
+biasDemo()    
+
 
