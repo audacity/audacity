93275b0ff86e2e79b5c0d9ecf996010ce9adaf2e
James Crook <james.k.crook@gmail.com>
2018-02-16 11:04:23 +0000

    Effect examples added

--- scripts/piped-work/docimages_after.py
+++ scripts/piped-work/docimages_after.py
@@ -13,13 +13,42 @@ exec( open("docimages_core.py" ).read() )
 Commands = ["Cut", "Split", "SplitCut",
             "Silence", "Trim",
             "SplitNew", "Duplicate",
-            "ZoomIn", "ZoomOut", "ZoomNormal", "ZoomSel", "FitInWindow", "FitV"
+            "ZoomIn", "ZoomOut", "ZoomNormal", "ZoomSel", "FitInWindow", "FitV" 
+            "Amplify","BassAndTreble","ChangePitch","ChangeSpeed",
+            "ChangeTempo","Compressor","Distortion","Echo",
+            "Equalization","Normalize","Paulstretch","Phaser",
+            "Repeat","Reverb","TimeScale","Wahwah",
+            "HighPassFilter","LowPassFilter","Vocoder",
+            "NotchFilter","AdjustableFade","Delay","Limiter"               
             ]
+Generators = ["Chirp","DtmfTones","Noise","Tone","Pluck"]
+
+
 
 # "Delete", "Copy", - look same as cut
 # "SplitDelete" - same as splitcut
 
-ToDo = ["Paste", "Join", "DisJoin"]
+ToDo = ["Paste", "Join", "DisJoin",
+        
+"ClipFix",
+"AutoDuck",
+"ClickRemoval",
+"FindClipping",
+"TruncateSilence",
+"SilenceFinder",
+"BeatFinder",
+"NyquistPrompt",
+"RhythmTrack",
+"SoundFinder",
+"SpectralEditMultiTool",
+"SpectralEditParametricEq",
+"SpectralEditShelves",
+"VocalReductionAndIsolation",
+"CrossfadeClips",
+"CrossfadeTracks",
+"RegularIntervalLabels",
+        
+]
 
 def starterTrack():
     loadMonoTracks(1)
@@ -29,13 +58,26 @@ def starterTrack():
 
 def imageAfters():
     starterTrack()
-    capture( 'BeforeEffect.png', 'First_Track' )
+    capture( 'BeforeEffect.png', 'All_Tracks_plus' )
     for name in Commands :
         starterTrack()
         do( 'SetTrack: Name="'+name+'"')
         do( name )
         capture( 'After' + name + '.png' , 'All_Tracks_Plus' )
+
+
+def generators():
+   
+    for name in Generators :
+        makeWayForTracks()
+        do( 'NewMonoTrack' )
+        do( 'SetTrack: Name="'+name+'"')
+        do( 'Select: Start=0 End=10' )
+        do( name )
+        do( 'ZoomSel' )
+        do( 'Select: Start=0 End=0' )
+        capture( 'After' + name + '.png' , 'All_Tracks_Plus' )
     
 imageAfters()
-
+generators()
 
