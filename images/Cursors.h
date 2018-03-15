3d9b0f991a88fb744756e4401f48861ce8b6a9bd
andheh <36114788+andheh@users.noreply.github.com>
2018-03-15 15:24:48 +0100

    moved comment from Cursors.h to Cursors.cpp

--- images/Cursors.h
+++ images/Cursors.h
@@ -1,12 +1,3 @@
-// Mac, Win and Linux all support 32 x 32 cursors now.
-
-// Cursors MUST be 32x32 (or they will be resized).
-// You only get black, white and transparent to use.
-
-// N.B. Under windows the mask color must be in 
-// the color palette.  To cater for Win2K a mask
-// color of saturated red was used.  (middle grey 
-
 #ifndef AUDACITY_CURSORS_H
 #define AUDACITY_CURSORS_H
 
