f7515c90d87887419fd170c9303c94ffde73f363
Paul Licameli <paul.licameli@audacityteam.org>
2018-04-16 13:57:07 -0400

    Now I can revert my partial reversion of b56059a

--- include/audacity/Types.h
+++ include/audacity/Types.h
@@ -45,6 +45,7 @@
 #include <algorithm>
 #include <wx/string.h>
 #include <wx/arrstr.h>
+#include <type_traits>
 
 // ----------------------------------------------------------------------------
 // TODO:  I'd imagine this header may be replaced by other public headers. But,
@@ -90,7 +91,7 @@ public:
 
    size_t as_size_t() const {
       wxASSERT(value >= 0);
-      wxASSERT(value <= std::numeric_limits<size_t>::max());
+      wxASSERT(static_cast<std::make_unsigned<type>::type>(value) <= std::numeric_limits<size_t>::max());
       return value;
    }
 
