d99089023206b8c598df228373ca9f0220ed19d4
Steve Daulton <stevedaulton@gmail.com>
2018-07-22 15:07:52 +0100

    Update alpha manual URLs in mw2html scripts

--- scripts/mw2html_audacity/wiki2htm.sh
+++ scripts/mw2html_audacity/wiki2htm.sh
@@ -2,7 +2,7 @@
 
 helpdir="../../help/manual"
 tmpdir_="../../help/temp"
-srcuri="http://alphamanual.audacityteam.org/man"
+srcuri="https://alphamanual.audacityteam.org/man"
 
 mkdir -p "${tmpdir_}"
 python mw2html.py "${srcuri}" "${tmpdir_}" -s
