d79c071d6d42eeb20ed26d4312e2c4bbc8743969
James Crook <james.k.crook@gmail.com>
2018-08-25 17:41:28 +0100

    Change 'mouse_move' to 'move_mouse'.
    Fixing a hyperlink.

--- src/commands/DragCommand.h
+++ src/commands/DragCommand.h
@@ -32,7 +32,7 @@ public:
    void PopulateOrExchange(ShuttleGui & S) override;
 
    // AudacityCommand overrides
-   wxString ManualPage() override {return wxT("Extra_Menu:_Scriptables_II#mouse_move");};
+   wxString ManualPage() override {return wxT("Extra_Menu:_Scriptables_II#move_mouse");};
 
    bool Apply(const CommandContext & context) override;
 
