878818a1aac1f436d8d0e8409595c240934fb505
James Crook <james.k.crook@gmail.com>
2018-08-27 14:23:45 +0100

    Wording: "Duplex" -> "Overdub"

--- src/Menus.cpp
+++ src/Menus.cpp
@@ -953,7 +953,7 @@ void AudacityProject::CreateMenusAndCommands()
                   // Switching of scrolling on and off is permitted even during transport
                   AlwaysEnabledFlag, AlwaysEnabledFlag);
 
-      c->AddCheck(wxT("Duplex"), XXO("&Overdub (on/off)"), FN(OnTogglePlayRecording), 0,
+      c->AddCheck(wxT("Overdub"), XXO("&Overdub (on/off)"), FN(OnTogglePlayRecording), 0,
                   AudioIONotBusyFlag | CanStopAudioStreamFlag,
                   AudioIONotBusyFlag | CanStopAudioStreamFlag);
       c->AddCheck(wxT("SWPlaythrough"), XXO("So&ftware Playthrough (on/off)"), FN(OnToggleSWPlaythrough), 0,
