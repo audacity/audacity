audacity/audacity

commit cb55164c4360ed3fb2730b23ae3ad57e9d1e0cbd

Add Project Rate to SetProjectCommand

src/commands/SetProjectCommand.cpp
@@ -25,6 +25,7 @@
 #include "../WaveTrack.h"
 #include "../ShuttleGui.h"
 #include "CommandContext.h"
+#include "toolbars/SelectionBar.h"
 
 SetProjectCommand::SetProjectCommand()
 {
@@ -33,6 +34,7 @@ SetProjectCommand::SetProjectCommand()
 
 bool SetProjectCommand::DefineParams( ShuttleParams & S ){ 
    S.OptionalN( bHasName        ).Define(     mName,        wxT("Name"),       _("Project") );
+   S.OptionalY( bHasRate        ).Define(     mRate,        wxT("Rate"),       44100.0, 1.0, 1000000.0);
    S.OptionalY( bHasSizing      ).Define(     mPosX,        wxT("X"),          10.0, 0.0, 2000.0);
    S.OptionalY( bHasSizing      ).Define(     mPosY,        wxT("Y"),          10.0, 0.0, 2000.0);
    S.OptionalY( bHasSizing      ).Define(     mWidth,       wxT("Width"),      1000.0, 200.0, 4000.0);
@@ -46,7 +48,9 @@ void SetProjectCommand::PopulateOrExchange(ShuttleGui & S)
    S.StartMultiColumn(3, wxALIGN_CENTER);
    {
       S.Optional( bHasName      ).TieTextBox(         _("Name:"),     mName );
+      S.Optional( bHasName      ).TieTextBox(         _("Rate:"),     mRate );
       S.TieCheckBox( _("Resize:"), bHasSizing    );
+      S.AddSpace(0,0);
    }
    S.EndMultiColumn();
    S.StartMultiColumn(2, wxALIGN_CENTER);
@@ -64,6 +68,15 @@ bool SetProjectCommand::Apply(const CommandContext & context)
    AudacityProject * pProj = context.GetProject();
    if( bHasName )
       pProj->SetLabel(mName);
+
+   if( bHasRate && mRate >= 1 && mRate <= 1000000 )
+   {
+      auto *bar = pProj->GetSelectionBar();
+      if( bar ){
+         bar->SetRate( mRate );
+      }
+   }
+
    if( bHasSizing )
    {
       pProj->SetPosition( wxPoint( mPosX, mPosY));

src/commands/SetProjectCommand.h
@@ -44,10 +44,12 @@ public:
    int mPosY;
    int mWidth;
    int mHeight;
+   double mRate;
 
 // For tracking optional parameters.
    bool bHasName;
    bool bHasSizing;
+   bool bHasRate;
 };
 
 

commit 03b82618b265355441e73df7e555f89a4d2882a9

Fix typo error in SetProjectCommand
Error was introduced in cb55164c4360.

src/commands/SetProjectCommand.cpp
@@ -34,7 +34,7 @@ SetProjectCommand::SetProjectCommand()
 
 bool SetProjectCommand::DefineParams( ShuttleParams & S ){ 
    S.OptionalN( bHasName        ).Define(     mName,        wxT("Name"),       _("Project") );
-   S.OptionalY( bHasRate        ).Define(     mRate,        wxT("Rate"),       44100.0, 1.0, 1000000.0);
+   S.OptionalN( bHasRate        ).Define(     mRate,        wxT("Rate"),       44100.0, 1.0, 1000000.0);
    S.OptionalY( bHasSizing      ).Define(     mPosX,        wxT("X"),          10.0, 0.0, 2000.0);
    S.OptionalY( bHasSizing      ).Define(     mPosY,        wxT("Y"),          10.0, 0.0, 2000.0);
    S.OptionalY( bHasSizing      ).Define(     mWidth,       wxT("Width"),      1000.0, 200.0, 4000.0);
@@ -48,7 +48,7 @@ void SetProjectCommand::PopulateOrExchange(ShuttleGui & S)
    S.StartMultiColumn(3, wxALIGN_CENTER);
    {
       S.Optional( bHasName      ).TieTextBox(         _("Name:"),     mName );
-      S.Optional( bHasName      ).TieTextBox(         _("Rate:"),     mRate );
+      S.Optional( bHasRate      ).TieTextBox(         _("Rate:"),     mRate );
       S.TieCheckBox( _("Resize:"), bHasSizing    );
       S.AddSpace(0,0);
    }