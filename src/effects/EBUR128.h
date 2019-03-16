a94cda94aea9b0cf8350a199d736433a4ebfaedc
Max Maisel <max.maisel@posteo.de>
2019-03-16 10:14:15 +0100

    Create empty EBUR128 class.
    This class will contain all EBU-R128 related analysis code.
    Add all new files to all build systems as well.

---
+++ src/effects/EBUR128.h
@@ -0,0 +1,24 @@
+/**********************************************************************
+
+Audacity: A Digital Audio Editor
+
+EBUR128.h
+
+Max Maisel
+
+***********************************************************************/
+
+#ifndef __EBUR128_H__
+#define __EBUR128_H__
+
+#include "MemoryX.h"
+
+/// \brief Implements EBU-R128 loudness measurement.
+class EBUR128
+{
+public:
+
+private:
+};
+
+#endif
