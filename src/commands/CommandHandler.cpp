audacity/audacity

commit 9eb9104859837772fa249106e3cb68465511584c

Remove CommandContext::GetProject() ...
... which called ::GetActiveProject(), but one purpose of the CommandContext
class was to eliminate many uses of that global function, because a
CommandContext must always be constructed from a reference to a project
(which was always in fact the active one), then passed around to where it is
needed!

Also, just use the project member directly -- because CommandContext was
intended as just a P.O.D (plain-old-data) structure.

This also eliminates a dependency of CommandContext.cpp on Project.cpp.

This is not enough by itself to break any dependency cycles.

src/commands/CommandHandler.cpp
@@ -59,5 +59,5 @@ void CommandHandler::OnReceiveCommand(AppCommandEvent &event)
    wxUnusedVar(result);
 
    // Redraw the project
-   mCurrentContext->GetProject()->RedrawProject();
+   mCurrentContext->project.RedrawProject();
 }

src/Screenshot.cpp
@@ -95,7 +95,7 @@ class ScreenFrame final : public wxFrame
    wxStatusBar *mStatus;
 
    std::unique_ptr<ScreenshotCommand> mCommand;
-   CommandContext mContext;
+   const CommandContext mContext;
 
    DECLARE_EVENT_TABLE()
 };
@@ -469,7 +469,7 @@ void ScreenFrame::PopulateOrExchange(ShuttleGui & S)
       CentreOnParent();
    }
 
-   SetIcon(mContext.GetProject()->GetIcon());
+   SetIcon(mContext.project.GetIcon());
 }
 
 bool ScreenFrame::ProcessEvent(wxEvent & e)
@@ -564,8 +564,8 @@ void ScreenFrame::SizeMainWindow(int w, int h)
 {
    int top = 20;
 
-   mContext.GetProject()->Maximize(false);
-   mContext.GetProject()->SetSize(16, 16 + top, w, h);
+   mContext.project.Maximize(false);
+   mContext.project.SetSize(16, 16 + top, w, h);
    //Bug383 - Toolbar Resets not wanted.
    //mContext.GetProject()->GetToolManager()->Reset();
 }
@@ -669,9 +669,9 @@ void ScreenFrame::OnCaptureSomething(wxCommandEvent &  event)
 void ScreenFrame::TimeZoom(double seconds)
 {
    int width, height;
-   mContext.GetProject()->GetClientSize(&width, &height);
-   mContext.GetProject()->mViewInfo.SetZoom((0.75 * width) / seconds);
-   mContext.GetProject()->RedrawProject();
+   mContext.project.GetClientSize(&width, &height);
+   mContext.project.mViewInfo.SetZoom((0.75 * width) / seconds);
+   mContext.project.RedrawProject();
 }
 
 void ScreenFrame::OnOneSec(wxCommandEvent & WXUNUSED(event))
@@ -708,7 +708,7 @@ void ScreenFrame::SizeTracks(int h)
    // If there should be more-than-stereo tracks, this makes
    // each channel as high as for a stereo channel
 
-   auto tracks = mContext.GetProject()->GetTracks();
+   auto tracks = mContext.project.GetTracks();
    for (auto t : tracks->Leaders<WaveTrack>()) {
       auto channels = TrackList::Channels(t);
       auto nChannels = channels.size();
@@ -716,15 +716,15 @@ void ScreenFrame::SizeTracks(int h)
       for (auto channel : channels)
          channel->SetHeight(height);
    }
-   mContext.GetProject()->RedrawProject();
+   mContext.project.RedrawProject();
 }
 
 void ScreenFrame::OnShortTracks(wxCommandEvent & WXUNUSED(event))
 {
-   for (auto t : mContext.GetProject()->GetTracks()->Any<WaveTrack>())
+   for (auto t : mContext.project.GetTracks()->Any<WaveTrack>())
       t->SetHeight(t->GetMinimizedHeight());
 
-   mContext.GetProject()->RedrawProject();
+   mContext.project.RedrawProject();
 }
 
 void ScreenFrame::OnMedTracks(wxCommandEvent & WXUNUSED(event))

src/commands/Command.h
@@ -65,7 +65,7 @@ public:
    ApplyAndSendResponse(const OldStyleCommandPointer &cmd, std::unique_ptr<CommandOutputTargets> &target);
    bool Apply() override;
    bool Apply(const CommandContext &context) override;// Error to use this.
-   std::unique_ptr<CommandContext> mCtx;
+   std::unique_ptr<const CommandContext> mCtx;
 
 };
 

src/commands/CommandContext.cpp
@@ -24,6 +24,8 @@ messaging from a command back to its invoker.
 #include "CommandContext.h"
 
 #include <map>
+#include <wx/app.h>
+#include <wx/log.h>
 #include <wx/string.h>
 #include <wx/variant.h>
 #include <wx/arrstr.h>
@@ -31,8 +33,6 @@ messaging from a command back to its invoker.
 #include "CommandBuilder.h"
 #include "CommandTargets.h"
 #include "CommandDirectory.h"
-
-#include "../Project.h"
 
 CommandContext::CommandContext(
       AudacityProject &p
@@ -91,9 +91,6 @@ void CommandContext::Progress( double d ) const
 
 AudacityApp * CommandContext::GetApp() const
 {  return (AudacityApp *) wxTheApp;}
-
-AudacityProject *CommandContext::GetProject() const
-{  return GetActiveProject();}
 
 void CommandContext::StartArray() const
 {

src/commands/CommandContext.h
@@ -56,6 +56,5 @@ public:
    int index;
    CommandParameter parameter;
    AudacityApp *GetApp() const;
-   AudacityProject *GetProject() const;
 };
 #endif

src/commands/CommandHandler.h
@@ -25,7 +25,7 @@ class CommandContext;
 class CommandHandler
 {
    private:
-      std::unique_ptr<CommandContext> mCurrentContext;
+      std::unique_ptr<const CommandContext> mCurrentContext;
 
    public:
       CommandHandler();

src/commands/CompareAudioCommand.cpp
@@ -107,7 +107,7 @@ inline int min(int a, int b)
 
 bool CompareAudioCommand::Apply(const CommandContext & context)
 {
-   if (!GetSelection(context, *context.GetProject()))
+   if (!GetSelection(context, context.project))
    {
       return false;
    }

src/commands/DragCommand.cpp
@@ -90,7 +90,7 @@ bool DragCommand::Apply(const CommandContext & context)
    if( !bHasToY )
       mToY = 10;
 
-   wxWindow * pWin = context.GetProject();
+   wxWindow * pWin = &context.project;
    wxWindow * pWin1 = nullptr;
    wxMouseEvent Evt( wxEVT_MOTION );
    Evt.m_x = mFromX;

commit 02d5e6c5975fc2f3e56285d4be3a2c1355a8d4e4

Fix crash in script pipe caused by commit 9eb9104

src/commands/CommandHandler.cpp
@@ -27,9 +27,8 @@
 #include "../commands/Command.h"
 
 CommandHandler::CommandHandler()
- : mCurrentContext(std::make_unique<CommandContext>
-                   (*GetActiveProject()))
-{ }
+{
+}
 
 CommandHandler::~CommandHandler()
 {
@@ -38,7 +37,6 @@ CommandHandler::~CommandHandler()
 void CommandHandler::SetProject(AudacityProject *)
 {
    // TODO:  Review if the extend command handling is ever utilized
-   // mCurrentContext->proj = proj;
 }
 
 void CommandHandler::OnReceiveCommand(AppCommandEvent &event)
@@ -54,11 +52,12 @@ void CommandHandler::OnReceiveCommand(AppCommandEvent &event)
    // Then apply it to current application & project.  Note that the
    // command may change the context - for example, switching to a
    // different project.
+   CommandContext context{ *GetActiveProject() };
    auto result = GuardedCall<bool>( [&] {
-      return cmd->Apply(*mCurrentContext);
+      return cmd->Apply( context );
    });
    wxUnusedVar(result);
 
    // Redraw the project
-   mCurrentContext->project.RedrawProject();
+   context.project.RedrawProject();
 }