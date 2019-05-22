audacity/audacity

commit 7c0d05fc18df73789f8af0c89d75ff6fe5fadec5

Fix for bug 2011 - Label Editor
Linux: Audacity may stop responding to GUI after opening Label Editor

src/LabelDialog.cpp
@@ -350,10 +350,12 @@ bool LabelDialog::Show(bool show)
 {
    bool ret = wxDialogWrapper::Show(show);
 
+   mGrid->SetFocus();   // Required (at least on Linux).
+
    // Set initial row
    // (This will not work until the grid is actually displayed)
    if (show && mInitialRow != -1) {
-      mGrid->SetGridCursor(mInitialRow, Col_Label);
+      mGrid->GoToCell(mInitialRow, Col_Label);
    }
 
    return ret;

src/LabelTrack.cpp
@@ -3073,6 +3073,8 @@ void LabelTrack::DoEditLabels
                    viewInfo, rate,
                    format, freqFormat);
 
+   dlg.Raise();   // Required on Linux: bug 2011
+
    if (dlg.ShowModal() == wxID_OK) {
       project.PushState(_("Edited labels"), _("Label"));
       project.RedrawProject();

commit 14601660d98ec41470161c2292361d34d668f6b8

Bug 2113: When the label editor closes, no control is the focus.
This was caused by commit: 7c0d05f
Problems:
1. LabelDialog::Show() is called both when dialog opens and closes, so SetFocus() was being called when the dialog closed.
2. The call for SetFocus() was only needed for mac and linux, and it would have been better to #ifdef it to prevent unforeseen bugs on other platforms.

This commits fixes the two issues.

src/LabelDialog.cpp
@@ -350,7 +350,11 @@ bool LabelDialog::Show(bool show)
 {
    bool ret = wxDialogWrapper::Show(show);
 
-   mGrid->SetFocus();   // Required for Linux and Mac.
+#if defined(__WXMAC__) || defined(__WXGTK__)
+   if (show) {
+      mGrid->SetFocus();   // Required for Linux and Mac.
+   }
+#endif
 
    // Set initial row
    // (This will not work until the grid is actually displayed)