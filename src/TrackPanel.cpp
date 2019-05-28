audacity/audacity

commit 9c75ebe7f704cde2f690493542f8799624c95141

TrackPanel subscribes directly to ODManager for events...
... cutting middleman AudacityProject out

src/TrackPanel.cpp
@@ -88,6 +88,9 @@ is time to refresh some aspect of the screen.
 #include "NoteTrack.h"
 #endif
 
+#include "ondemand/ODManager.h"
+#include "ondemand/ODTask.h"
+
 #include "toolbars/ControlToolBar.h"
 #include "toolbars/ToolsToolBar.h"
 
@@ -180,6 +183,9 @@ BEGIN_EVENT_TABLE(TrackPanel, CellularPanel)
     EVT_PAINT(TrackPanel::OnPaint)
 
     EVT_TIMER(wxID_ANY, TrackPanel::OnTimer)
+
+    EVT_COMMAND(wxID_ANY, EVT_ODTASK_UPDATE,   TrackPanel::OnODTask)
+    EVT_COMMAND(wxID_ANY, EVT_ODTASK_COMPLETE, TrackPanel::OnODTask)
 END_EVENT_TABLE()
 
 /// Makes a cursor from an XPM, uses CursorId as a fallback.
@@ -471,6 +477,14 @@ void TrackPanel::OnTimer(wxTimerEvent& )
    }
    if(mTimeCount > 1000)
       mTimeCount = 0;
+}
+
+///Handles the redrawing necessary for tasks as they partially update in the
+///background, or finish.
+void TrackPanel::OnODTask(wxCommandEvent & WXUNUSED(event))
+{
+   //todo: add track data to the event - check to see if the project contains it before redrawing.
+   Refresh(false);
 }
 
 double TrackPanel::GetScreenEndTime() const

src/Project.cpp
@@ -1098,8 +1098,6 @@ BEGIN_EVENT_TABLE(AudacityProject, wxFrame)
    EVT_COMMAND(wxID_ANY, EVT_OPEN_AUDIO_FILE, AudacityProject::OnOpenAudioFile)
    EVT_COMMAND(wxID_ANY, EVT_TOOLBAR_UPDATED, AudacityProject::OnToolBarUpdate)
    //mchinen:multithreaded calls - may not be threadsafe with CommandEvent: may have to change.
-   EVT_COMMAND(wxID_ANY, EVT_ODTASK_UPDATE, AudacityProject::OnODTaskUpdate)
-   EVT_COMMAND(wxID_ANY, EVT_ODTASK_COMPLETE, AudacityProject::OnODTaskComplete)
 END_EVENT_TABLE()
 
 AudacityProject::AudacityProject(wxWindow * parent, wxWindowID id,
@@ -2379,22 +2377,6 @@ void AudacityProject::OnToolBarUpdate(wxCommandEvent & event)
    HandleResize();
 
    event.Skip(false);             /* No need to propagate any further */
-}
-
-///Handles the redrawing necessary for tasks as they partially update in the background.
-void AudacityProject::OnODTaskUpdate(wxCommandEvent & WXUNUSED(event))
-{
-   //todo: add track data to the event - check to see if the project contains it before redrawing.
-   if(mTrackPanel)
-      mTrackPanel->Refresh(false);
-
-}
-
-//redraws the task and does other book keeping after the task is complete.
-void AudacityProject::OnODTaskComplete(wxCommandEvent & WXUNUSED(event))
-{
-  if(mTrackPanel)
-      mTrackPanel->Refresh(false);
 }
 
 void AudacityProject::OnScroll(wxScrollEvent & WXUNUSED(event))

src/Project.h
@@ -384,8 +384,6 @@ public:
    void OnTimer(wxTimerEvent & event);
    void OnToolBarUpdate(wxCommandEvent & event);
    void OnOpenAudioFile(wxCommandEvent & event);
-   void OnODTaskUpdate(wxCommandEvent & event);
-   void OnODTaskComplete(wxCommandEvent & event);
 
    void HandleResize();
    void UpdateLayout();

src/TrackPanel.h
@@ -280,6 +280,7 @@ class AUDACITY_DLL_API TrackPanel final
 
    void OnIdle(wxIdleEvent & event);
    void OnTimer(wxTimerEvent& event);
+   void OnODTask(wxCommandEvent &event);
 
    int GetLeftOffset() const { return GetLabelWidth() + 1;}
 

commit 441763cabd77e06cd6265c48696d5bba7c8f4c34

Fix 9c75ebe, which caused TrackPanel not to get the message

src/TrackPanel.cpp
@@ -185,8 +185,6 @@ BEGIN_EVENT_TABLE(TrackPanel, CellularPanel)
 
     EVT_TIMER(wxID_ANY, TrackPanel::OnTimer)
 
-    EVT_COMMAND(wxID_ANY, EVT_ODTASK_UPDATE,   TrackPanel::OnODTask)
-    EVT_COMMAND(wxID_ANY, EVT_ODTASK_COMPLETE, TrackPanel::OnODTask)
 END_EVENT_TABLE()
 
 /// Makes a cursor from an XPM, uses CursorId as a fallback.
@@ -316,6 +314,9 @@ TrackPanel::TrackPanel(wxWindow * parent, wxWindowID id,
    wxTheApp->Bind(EVT_AUDIOIO_PLAYBACK,
                      &TrackPanel::OnPlayback,
                      this);
+
+   GetProject()->Bind(EVT_ODTASK_UPDATE, &TrackPanel::OnODTask, this);
+   GetProject()->Bind(EVT_ODTASK_COMPLETE, &TrackPanel::OnODTask, this);
 
    UpdatePrefs();
 }

src/ondemand/ODManager.cpp
@@ -145,7 +145,7 @@ pfodman ODManager::Instance = &(ODManager::InstanceFirstTime);
 //libsndfile is not threadsafe - this deals with it
 static ODLock sLibSndFileMutex;
 
-DEFINE_EVENT_TYPE(EVT_ODTASK_UPDATE)
+wxDEFINE_EVENT(EVT_ODTASK_UPDATE, wxCommandEvent);
 
 //using this with wxStringArray::Sort will give you a list that
 //is alphabetical, without depending on case.  If you use the

src/ondemand/ODManager.h
@@ -30,7 +30,8 @@ number of threads.
 #include <time.h>
 #endif //__WXMAC__
 
-DECLARE_EXPORTED_EVENT_TYPE(AUDACITY_DLL_API, EVT_ODTASK_UPDATE, -1)
+wxDECLARE_EXPORTED_EVENT(AUDACITY_DLL_API,
+                         EVT_ODTASK_UPDATE, wxCommandEvent);
 
 ///wxstring compare function for sorting case, which is needed to load correctly.
 int CompareNoCaseFileName(const wxString& first, const wxString& second);

src/ondemand/ODTask.cpp
@@ -29,7 +29,7 @@ in a background thread.
 //#include "../Profiler.h"
 
 
-DEFINE_EVENT_TYPE(EVT_ODTASK_COMPLETE)
+wxDEFINE_EVENT(EVT_ODTASK_COMPLETE, wxCommandEvent);
 
 /// Constructs an ODTask
 ODTask::ODTask()

src/ondemand/ODTask.h
@@ -32,7 +32,8 @@ class Track;
 class WaveTrack;
 
 
-DECLARE_EXPORTED_EVENT_TYPE(AUDACITY_DLL_API, EVT_ODTASK_COMPLETE, -1)
+wxDECLARE_EXPORTED_EVENT(AUDACITY_DLL_API,
+                         EVT_ODTASK_COMPLETE, wxCommandEvent);
 
 /// A class representing a modular task to be used with the On-Demand structures.
 class ODTask /* not final */