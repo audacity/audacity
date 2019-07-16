20772c995d62e3552ae8432e6e3fbc5b65a94ed2
James Crook <james.k.crook@gmail.com>
2019-07-16 13:59:59 +0100

    Bug 2137 - Nyquist: Assert when receiving multibyte characters

--- src/effects/nyquist/Nyquist.cpp
+++ src/effects/nyquist/Nyquist.cpp
@@ -3199,8 +3199,8 @@ void * nyq_reformat_aud_do_response(const wxString & Str) {
    LVAL dst;
    LVAL message;
    LVAL success;
-   wxString Left = Str.BeforeLast('\n').BeforeLast('\n');
-   wxString Right = Str.BeforeLast('\n').AfterLast('\n');
+   wxString Left = Str.BeforeLast('\n').BeforeLast('\n').ToAscii();
+   wxString Right = Str.BeforeLast('\n').AfterLast('\n').ToAscii();
    message = cvstring(Left);
    success = Right.EndsWith("OK") ? s_true : nullptr;
    dst = cons(message, success);
