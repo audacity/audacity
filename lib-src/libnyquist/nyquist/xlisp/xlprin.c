5ba6072bbb10c796353c25c0b0cfba6cd00de378
Leland Lucius <github@homerow.net>
2020-01-06 22:01:08 -0600

    Workaround for bug #2264
    Reverting back to previous behavior to get proper
    escaping.

--- lib-src/libnyquist/nyquist/xlisp/xlprin.c
+++ lib-src/libnyquist/nyquist/xlisp/xlprin.c
@@ -209,7 +209,7 @@ LOCAL void putqstring(LVAL fptr, LVAL str)
     for (p = getstring(str); (ch = *p) != '\0'; ++p)
 
         /* check for a control character */
-        if (ch < 040 || ch == '\\' || ch > 0176 || ch == '"') {
+        if (ch < 040 || ch == '\\' || ch > 0176 /* || ch == '"' */) {
             xlputc(fptr,'\\');
             switch (ch) {
             case '\011':
