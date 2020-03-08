5900c524928be07d620bd7bfe282d53f839811f8
Leland Lucius <github@homerow.net>
2020-03-07 18:49:37 -0600

    Fix a small conflict between the CMake and Xcode builds

--- mac/scripts/get_gitident.sh
+++ mac/scripts/get_gitident.sh
@@ -1,2 +1,10 @@
-git show -s --format="#define REV_LONG \"%H\"%n#define REV_TIME \"%cd\"%n" | tee ../src/RevisionIdent.h
+fmt='
+#if !defined(REV_LONG)
+#define REV_LONG "%H"
+#endif
+#if !defined(REV_TIME)
+#define REV_TIME "%cd"
+#endif
+'
+git show -s --format="${fmt}" | tee ../src/RevisionIdent.h
 touch ../src/AboutDialog.cpp
