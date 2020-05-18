audacity/audacity

commit c2feee6ceaf795886a88902498336d3cfb8994cc

Bug2346: Complete fix...
... without making undesirable dependency cycles.

Eliminate calls to NewWaveTrack in effects, but in Edit>Copy too, which was
not mentioned in the bug report.  (Copying a track, deselecting all, and pasting
preserved CLIP colors, but not the TRACK color setting which applies to newly
generated clips.)

Instead, always use the new function WaveTrack::EmptyCopy from the track to be
later replaced, getting color information.

NewWaveTrack is still used in benchmark test, import, the Track menu
commands that make new tracks, recording to new tracks, and generators without
a selection, where there is no track to copy from.

Also when deserializing tracks from the .aup file, in which case the saved
color is later retrieved from the file.

Also, in mix-and-render, where other logic decides whether to copy colors
afterward.

See commit a9658e6ef7f7eaefce4dc37a93d389cca7705f41

src/WaveTrack.cpp
@@ -530,15 +530,20 @@ void WaveTrack::Trim (double t0, double t1)
 
 
 
+WaveTrack::Holder WaveTrack::EmptyCopy() const
+{
+   auto result = std::make_shared<WaveTrack>( mDirManager, mFormat, mRate );
+   result->Init(*this);
+   return result;
+}
+
 Track::Holder WaveTrack::Copy(double t0, double t1, bool forClipboard) const
 {
    if (t1 < t0)
       THROW_INCONSISTENCY_EXCEPTION;
 
-   auto result = std::make_shared<WaveTrack>( mDirManager, mFormat, mRate );
+   auto result = EmptyCopy();
    WaveTrack *newTrack = result.get();
-
-   newTrack->Init(*this);
 
    // PRL:  Why shouldn't cutlines be copied and pasted too?  I don't know, but
    // that was the old behavior.  But this function is also used by the

src/WaveTrack.h
@@ -158,6 +158,10 @@ private:
 
    Track::Holder Cut(double t0, double t1) override;
 
+   // Make another track copying format, rate, color, etc. but containing no
+   // clips
+   Holder EmptyCopy() const;
+
    // If forClipboard is true,
    // and there is no clip at the end time of the selection, then the result
    // will contain a "placeholder" clip whose only purpose is to make

src/menus/EditMenus.cpp
@@ -101,8 +101,7 @@ bool DoPasteNothingSelected(AudacityProject &project)
                   // Cause duplication of block files on disk, when copy is
                   // between projects
                   locker.emplace(wc);
-               uNewTrack = trackFactory.NewWaveTrack(
-                  wc->GetSampleFormat(), wc->GetRate()),
+               uNewTrack = wc->EmptyCopy();
                pNewTrack = uNewTrack.get();
             },
 #ifdef USE_MIDI
@@ -597,8 +596,7 @@ void OnPaste(const CommandContext &context)
                wt->ClearAndPaste(t0, t1, wc, true, true);
             }
             else {
-               auto tmp = trackFactory.NewWaveTrack(
-                  wt->GetSampleFormat(), wt->GetRate());
+               auto tmp = wt->EmptyCopy();
                tmp->InsertSilence( 0.0,
                   // MJS: Is this correct?
                   clipboard.Duration() );

src/ProjectAudioManager.cpp
@@ -606,7 +606,7 @@ bool ProjectAudioManager::DoRecord(AudacityProject &project,
 
                // Pad the recording track with silence, up to the
                // maximum time.
-               auto newTrack = TrackFactory::Get( *p ).NewWaveTrack();
+               auto newTrack = pending->EmptyCopy();
                newTrack->InsertSilence(0.0, t0 - endTime);
                newTrack->Flush();
                pending->Clear(endTime, t0);

src/effects/ChangeSpeed.cpp
@@ -479,8 +479,8 @@ bool EffectChangeSpeed::ProcessOne(WaveTrack * track,
    // initialization, per examples of Mixer::Mixer and
    // EffectSoundTouch::ProcessOne
 
-   auto outputTrack = mFactory->NewWaveTrack(track->GetSampleFormat(),
-                                                    track->GetRate());
+   
+   auto outputTrack = track->EmptyCopy();
 
    //Get the length of the selection (as double). len is
    //used simple to calculate a progress meter, so it is easier

src/effects/Effect.cpp
@@ -1613,13 +1613,10 @@ bool Effect::ProcessTrack(int count,
       cleared = true;
 
       // Create temporary tracks
-      genLeft = mFactory->NewWaveTrack(left->GetSampleFormat(), left->GetRate());
-      genLeft->SetWaveColorIndex( left->GetWaveColorIndex() );
+      genLeft = left->EmptyCopy();
 
-      if (right) {
-         genRight = mFactory->NewWaveTrack(right->GetSampleFormat(), right->GetRate());
-         genRight->SetWaveColorIndex( right->GetWaveColorIndex() );
-      }
+      if (right)
+         genRight = right->EmptyCopy();
    }
 
    // Call the effect until we run out of input or delayed samples

src/effects/Equalization.cpp
@@ -1288,7 +1288,8 @@ bool EffectEqualization::ProcessOne(int count, WaveTrack * t,
                                     sampleCount start, sampleCount len)
 {
    // create a NEW WaveTrack to hold all of the output, including 'tails' each end
-   auto output = mFactory->NewWaveTrack(floatSample, t->GetRate());
+   auto output = t->EmptyCopy();
+   t->ConvertToSampleFormat( floatSample );
 
    wxASSERT(mM - 1 < windowSize);
    size_t L = windowSize - (mM - 1);   //Process L samples at a go

src/effects/Equalization48x.cpp
@@ -410,7 +410,8 @@ bool EffectEqualization48x::DeltaTrack(
    Floats buffer1{ trackBlockSize };
    Floats buffer2{ trackBlockSize };
 
-   auto output = mEffectEqualization->mFactory->NewWaveTrack(floatSample, t->GetRate());
+   auto output = t->EmptyCopy();
+   t->ConvertToSampleFormat( floatSample );
    auto originalLen = len;
    auto currentSample = start;
 
@@ -632,7 +633,8 @@ bool EffectEqualization48x::ProcessOne1x(int count, WaveTrack * t,
 
    auto trackBlockSize = t->GetMaxBlockSize();
 
-   auto output = mEffectEqualization->mFactory->NewWaveTrack(floatSample, t->GetRate());
+   auto output = t->EmptyCopy();
+   t->ConvertToSampleFormat( floatSample );
 
    mEffectEqualization->TrackProgress(count, 0.0);
    int subBufferSize=mBufferCount==8?(mSubBufferSize>>1):mSubBufferSize; // half the buffers if avx is active
@@ -818,7 +820,8 @@ bool EffectEqualization48x::ProcessOne4x(int count, WaveTrack * t,
 
    auto trackBlockSize = t->GetMaxBlockSize();
 
-   auto output = mEffectEqualization->mFactory->NewWaveTrack(floatSample, t->GetRate());
+   auto output = t->EmptyCopy();
+   t->ConvertToSampleFormat( floatSample );
 
    mEffectEqualization->TrackProgress(count, 0.0);
    auto bigRuns = len/(subBufferSize-mBlockSize);
@@ -907,7 +910,8 @@ bool EffectEqualization48x::ProcessOne1x4xThreaded(int count, WaveTrack * t,
    for(int i=0;i<mThreadCount;i++)
       mEQWorkers[i].mProcessingType=processingType;
 
-   auto output = mEffectEqualization->mFactory->NewWaveTrack(floatSample, t->GetRate());
+   auto output = t->EmptyCopy();
+   t->ConvertToSampleFormat( floatSample );
 
    auto trackBlockSize = t->GetMaxBlockSize();
    mEffectEqualization->TrackProgress(count, 0.0);
@@ -1152,7 +1156,8 @@ bool EffectEqualization48x::ProcessOne8x(int count, WaveTrack * t,
 
    auto trackBlockSize = t->GetMaxBlockSize();
 
-   auto output = mEffectEqualization->mFactory->NewWaveTrack(floatSample, t->GetRate());
+   auto output = t->EmptyCopy();
+   t->ConvertToSampleFormat( floatSample );
 
    mEffectEqualization->TrackProgress(count, 0.0);
    int bigRuns=len/(mSubBufferSize-mBlockSize);
@@ -1203,7 +1208,8 @@ bool EffectEqualization48x::ProcessOne8xThreaded(int count, WaveTrack * t,
    if(mThreadCount<=0 || blockCount<256) // dont do it without cores or big data
       return ProcessOne4x(count, t, start, len);
 
-   auto output = mEffectEqualization->mFactory->NewWaveTrack(floatSample, t->GetRate());
+   auto output = t->EmptyCopy();
+   t->ConvertToSampleFormat( floatSample );
 
    auto trackBlockSize = t->GetMaxBlockSize();
    mEffectEqualization->TrackProgress(count, 0.0);

commit 242b7fd7b7a7e4a720706a8bb6cf1d4a17099e2e

Bug2436: Cross-project paste should duplicate block files...
... to avoid data loss!

Bug caused by commit c2feee6

src/DirManager.cpp
@@ -400,7 +400,7 @@ DirManager::DirManager()
    srand(time(NULL));
 
    // Set up local temp subdir
-   // Previously, Audacity just named project temp directories "project0",
+   // Previously, Audacity just na med project temp directories "project0",
    // "project1" and so on. But with the advent of recovery code, we need a
    // unique name even after a crash. So we create a random project index
    // and make sure it is not used already. This will not pose any performance

src/WaveTrack.cpp
@@ -530,10 +530,12 @@ void WaveTrack::Trim (double t0, double t1)
 
 
 
-WaveTrack::Holder WaveTrack::EmptyCopy() const
+WaveTrack::Holder WaveTrack::EmptyCopy(
+   const std::shared_ptr<DirManager> &pDirManager ) const
 {
    auto result = std::make_shared<WaveTrack>( mDirManager, mFormat, mRate );
    result->Init(*this);
+   result->mDirManager = pDirManager ? pDirManager : mDirManager;
    return result;
 }
 

src/WaveTrack.h
@@ -160,7 +160,8 @@ private:
 
    // Make another track copying format, rate, color, etc. but containing no
    // clips
-   Holder EmptyCopy() const;
+   Holder EmptyCopy(
+      const std::shared_ptr<DirManager> &pDirManager = {} ) const;
 
    // If forClipboard is true,
    // and there is no clip at the end time of the selection, then the result

src/menus/EditMenus.cpp
@@ -2,6 +2,7 @@
 #include "../AdornedRulerPanel.h"
 #include "../Clipboard.h"
 #include "../CommonCommandFlags.h"
+#include "../DirManager.h"
 #include "../LabelTrack.h"
 #include "../Menus.h"
 #include "../NoteTrack.h"
@@ -75,6 +76,7 @@ bool DoPasteText(AudacityProject &project)
 bool DoPasteNothingSelected(AudacityProject &project)
 {
    auto &tracks = TrackList::Get( project );
+   auto &dirManager = DirManager::Get( project );
    auto &trackFactory = TrackFactory::Get( project );
    auto &selectedRegion = ViewInfo::Get( project ).selectedRegion;
    auto &window = ProjectWindow::Get( project );
@@ -101,7 +103,7 @@ bool DoPasteNothingSelected(AudacityProject &project)
                   // Cause duplication of block files on disk, when copy is
                   // between projects
                   locker.emplace(wc);
-               uNewTrack = wc->EmptyCopy();
+               uNewTrack = wc->EmptyCopy( dirManager.shared_from_this() );
                pNewTrack = uNewTrack.get();
             },
 #ifdef USE_MIDI
@@ -379,6 +381,7 @@ void OnPaste(const CommandContext &context)
 {
    auto &project = context.project;
    auto &tracks = TrackList::Get( project );
+   auto &dirManager = DirManager::Get( project );
    auto &trackFactory = TrackFactory::Get( project );
    auto &selectedRegion = ViewInfo::Get( project ).selectedRegion;
    const auto &settings = ProjectSettings::Get( project );
@@ -596,7 +599,7 @@ void OnPaste(const CommandContext &context)
                wt->ClearAndPaste(t0, t1, wc, true, true);
             }
             else {
-               auto tmp = wt->EmptyCopy();
+               auto tmp = wt->EmptyCopy( dirManager.shared_from_this() );
                tmp->InsertSilence( 0.0,
                   // MJS: Is this correct?
                   clipboard.Duration() );