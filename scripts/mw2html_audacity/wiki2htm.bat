63fcdf7ce954bdad04b5d6100125f3f7f43ef095
Jack Thomson <thomsonjack41@gmail.com>
2020-05-27 13:49:17 -0400

    Update Windows Batch scripts to use python3

--- scripts/mw2html_audacity/wiki2htm.bat
+++ scripts/mw2html_audacity/wiki2htm.bat
@@ -1,4 +1,4 @@
-python mw2html.py https://alphamanual.audacityteam.org/man ..\..\help\temp -s
+python3 mw2html.py https://alphamanual.audacityteam.org/man ..\..\help\temp -s
 rmdir /S /Q ..\..\help\manual
 mkdir ..\..\help\manual
 xcopy ..\..\help\temp\alphamanual.audacityteam.org ..\..\help\manual\ /E /C /Y /Q
--- scripts/mw2html_audacity/wiki2htmtemp.bat
+++ scripts/mw2html_audacity/wiki2htmtemp.bat
@@ -1,5 +1,5 @@
 @REM wiki2htm without the xcopy.  The results stay in the temp directory.  
 @REM Can be convenient when testing/modifying the script.
-python mw2html.py https://alphamanual.audacityteam.org/man ..\..\help\temp -s
+python3 mw2html.py https://alphamanual.audacityteam.org/man ..\..\help\temp -s
 
 
