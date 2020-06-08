5245f7528fe2e8a820d628f5e4673d9c55db5429
James Crook <james.k.crook@gmail.com>
2020-06-08 17:50:33 +0100

    Update Script update_po_files.sh

--- locale/update_po_files.sh
+++ locale/update_po_files.sh
@@ -10,7 +10,7 @@ xargs xgettext \
 --add-location=file  \
 --copyright-holder='Audacity Team' \
 --package-name="audacity" \
---package-version='2.4.0' \
+--package-version='2.4.2' \
 --msgid-bugs-address="audacity-translation@lists.sourceforge.net" \
 --add-location=file -L C -o audacity.pot 
 echo ";; Adding nyquist files to audacity.pot"
@@ -24,12 +24,12 @@ xargs xgettext \
 --add-location=file  \
 --copyright-holder='Audacity Team' \
 --package-name="audacity" \
---package-version='2.4.0' \
+--package-version='2.4.2' \
 --msgid-bugs-address="audacity-translation@lists.sourceforge.net" \
 --add-location=file -L Lisp -j -o audacity.pot 
 echo ";; Updating the .po files - Updating Project-Id-Version"
 for i in *.po; do
-    sed -i '/^"Project-Id-Version:/c\"Project-Id-Version: audacity 2.4.0\\n"' $i
+    sed -i '/^"Project-Id-Version:/c\"Project-Id-Version: audacity 2.4.2\\n"' $i
 done
 echo ";; Updating the .po files"
 sed 's/.*/echo "msgmerge --lang=& &.po audacity.pot -o &.po";\
