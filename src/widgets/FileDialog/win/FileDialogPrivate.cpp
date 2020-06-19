5e53f64229c88f02b33fc6d18a53c82124d3972f
Paul Licameli <paul.licameli@audacityteam.org>
2020-06-18 23:39:50 -0400

    Rewrite one #include of wx header with <> not ""

--- src/widgets/FileDialog/win/FileDialogPrivate.cpp
+++ src/widgets/FileDialog/win/FileDialogPrivate.cpp
@@ -20,7 +20,7 @@
 // ----------------------------------------------------------------------------
 
 // For compilers that support precompilation, includes "wx.h".
-#include "wx/wxprec.h"
+#include <wx/wxprec.h>
 
 #ifdef __BORLANDC__
 #pragma hdrstop
