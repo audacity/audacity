4552aed3c1ebd9f70632d4af27e5bccfadb9b8fa
Paul Licameli <paul.licameli@audacityteam.org>
2020-09-28 15:06:51 -0400

    Track menus in a module

--- libraries/lib-module-manager/ModuleSettings.cpp
+++ libraries/lib-module-manager/ModuleSettings.cpp
@@ -21,6 +21,7 @@ static const std::unordered_set<wxString> &autoEnabledModules()
    // with Audacity and enable automatically.
    static std::unordered_set<wxString> modules{
       "mod-clip-menu-items",
+      "mod-track-menus",
    };
    return modules;
 }
