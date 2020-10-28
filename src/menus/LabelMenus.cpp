8c4830d08f095b1aee954c3ba1216d29fae3b64b
Paul Licameli <paul.licameli@audacityteam.org>
2020-10-28 15:17:46 -0400

    Register the factory menu items under Tracks

--- src/menus/LabelMenus.cpp
+++ src/menus/LabelMenus.cpp
@@ -8,7 +8,8 @@
 #include "../ProjectAudioIO.h"
 #include "../ProjectHistory.h"
 #include "../ProjectSettings.h"
-#include "../SelectFile.h"
+#include "../ProjectWindow.h"
+#include "../SelectUtilities.h"
 #include "../SyncLock.h"
 #include "../TrackPanelAx.h"
 #include "../TrackPanel.h"
@@ -672,6 +673,25 @@ void OnDisjoinLabels(const CommandContext &context)
       XO( "Detach Labeled Audio" ) );
 }
 
+void OnNewLabelTrack(const CommandContext &context)
+{
+   auto &project = context.project;
+   auto &tracks = TrackList::Get( project );
+   auto &window = ProjectWindow::Get( project );
+
+   auto t = tracks.Add( std::make_shared<LabelTrack>() );
+
+   SelectUtilities::SelectNone( project );
+
+   t->SetSelected(true);
+
+   ProjectHistory::Get( project )
+      .PushState(XO("Created new label track"), XO("New Track"));
+
+   TrackFocus::Get(project).Set(t);
+   t->EnsureVisible();
+}
+
 }; // struct Handler
 
 } // namespace
@@ -795,6 +815,12 @@ AttachedItem sAttachment1{
    Shared( LabelEditMenus() )
 };
 
-}
+AttachedItem sAttachment2{ wxT("Tracks/Add/Add"),
+   ( FinderScope{ findCommandHandler },
+   Command( wxT("NewLabelTrack"), XXO("&Label Track"),
+      FN(OnNewLabelTrack), AudioIONotBusyFlag() )
+   )
+};
 
+}
 #undef FN
---
+++ src/tracks/playabletrack/notetrack/ui/NoteTrackMenuItems.cpp
@@ -0,0 +1,15 @@
+/**********************************************************************
+
+Audacity: A Digital Audio Editor
+
+@file NoteTrackMenuItems.cpp
+@brief Injects menu items using NoteTrack but not the views of it
+
+Paul Licameli split from TrackMenus.cpp
+
+**********************************************************************/
+
+/*!
+ There is no "New Note Track" menu item corresponding to the other track types.  Note tracks are created
+ when importing MIDI files.
+ */
