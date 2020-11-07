d0fd68a4a21794612baf57feac24c8ef158612d3
Paul Licameli <paul.licameli@audacityteam.org>
2020-11-07 12:24:55 -0500

    Lower some functions out of TransportMenus

---
+++ src/TransportUtilities.h
@@ -0,0 +1,40 @@
+/**********************************************************************
+
+  Audacity: A Digital Audio Editor
+
+  @file TransportUtilities.h
+  @brief Some UI related to starting and stopping play and record
+
+  Paul Licameli split from TransportMenus.h
+
+**********************************************************************/
+
+#ifndef __AUDACITY_TRANSPORT_UTILITIES__
+#define __AUDACITY_TRANSPORT_UTILITIES__
+
+struct AudioIOStartStreamOptions;
+class CommandContext;
+class SelectedRegion;
+enum class PlayMode : int;
+
+struct AUDACITY_DLL_API TransportUtilities
+{
+   static void PlayCurrentRegionAndWait(
+      const CommandContext &context,
+      bool newDefault = false,
+      bool cutpreview = false);
+   static void PlayPlayRegionAndWait(
+      const CommandContext &context,
+      const SelectedRegion &selectedRegion,
+      const AudioIOStartStreamOptions &options,
+      PlayMode mode);
+   static void RecordAndWait(
+      const CommandContext &context, bool altAppearance);
+
+   static void DoStartPlaying(
+      const CommandContext &context, bool newDefault = false);
+   static bool DoStopPlaying(const CommandContext &context);
+
+};
+
+#endif
