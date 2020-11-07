5ec9e4ff0f5165c8f0f2fe40622d553925f3c5cd
Paul Licameli <paul.licameli@audacityteam.org>
2020-11-07 11:28:05 -0500

    Register some menu items from new file LabelNavigationMenus.cpp

--- src/menus/TransportMenus.cpp
+++ src/menus/TransportMenus.cpp
@@ -4,7 +4,6 @@
 #include "../AudioIO.h"
 #include "../CommonCommandFlags.h"
 #include "DeviceManager.h"
-#include "../LabelTrack.h"
 #include "../Menus.h"
 #include "Prefs.h"
 #include "Project.h"
@@ -20,7 +19,6 @@
 #include "../SelectUtilities.h"
 #include "../SoundActivatedRecord.h"
 #include "../TimerRecordDialog.h"
-#include "../TrackPanelAx.h"
 #include "../TrackPanel.h"
 #include "../TransportUtilities.h"
 #include "UndoManager.h"
@@ -28,7 +26,6 @@
 #include "../prefs/RecordingPrefs.h"
 #include "../prefs/TracksPrefs.h"
 #include "../WaveTrack.h"
-#include "ViewInfo.h"
 #include "../commands/CommandContext.h"
 #include "../commands/CommandManager.h"
 #include "../toolbars/ControlToolBar.h"
@@ -78,76 +75,11 @@ bool MakeReadyToPlay(AudacityProject &project)
    return true;
 }
 
-void DoMoveToLabel(AudacityProject &project, bool next)
-{
-   auto &tracks = TrackList::Get( project );
-   auto &trackFocus = TrackFocus::Get( project );
-   auto &window = ProjectWindow::Get( project );
-   auto &projectAudioManager = ProjectAudioManager::Get(project);
-
-   // Find the number of label tracks, and ptr to last track found
-   auto trackRange = tracks.Any<LabelTrack>();
-   auto lt = *trackRange.rbegin();
-   auto nLabelTrack = trackRange.size();
-
-   if (nLabelTrack == 0 ) {
-      trackFocus.MessageForScreenReader(XO("no label track"));
-   }
-   else if (nLabelTrack > 1) {
-      // find first label track, if any, starting at the focused track
-      lt =
-         *tracks.Find(trackFocus.Get()).Filter<LabelTrack>();
-      if (!lt)
-         trackFocus.MessageForScreenReader(
-            XO("no label track at or below focused track"));
-   }
-
-   // If there is a single label track, or there is a label track at or below
-   // the focused track
-   auto &selectedRegion = ViewInfo::Get( project ).selectedRegion;
-   if (lt) {
-      int i;
-      if (next)
-         i = lt->FindNextLabel(selectedRegion);
-      else
-         i = lt->FindPrevLabel(selectedRegion);
-
-      if (i >= 0) {
-         const LabelStruct* label = lt->GetLabel(i);
-         bool newDefault = projectAudioManager.Looping();
-         if (ProjectAudioIO::Get( project ).IsAudioActive()) {
-            TransportUtilities::DoStopPlaying(project);
-            selectedRegion = label->selectedRegion;
-            window.RedrawProject();
-            TransportUtilities::DoStartPlaying(project, newDefault);
-         }
-         else {
-            selectedRegion = label->selectedRegion;
-            window.ScrollIntoView(selectedRegion.t0());
-            window.RedrawProject();
-         }
-         /* i18n-hint:
-            String is replaced by the name of a label,
-            first number gives the position of that label in a sequence
-            of labels,
-            and the last number is the total number of labels in the sequence.
-         */
-         auto message = XO("%s %d of %d")
-            .Format( label->title, i + 1, lt->GetNumLabels() );
-         trackFocus.MessageForScreenReader(message);
-      }
-      else {
-         trackFocus.MessageForScreenReader(XO("no labels in label track"));
-      }
-   }
-}
-
 bool IsLoopingEnabled(const AudacityProject& project)
 {
    auto &playRegion = ViewInfo::Get(project).playRegion;
     return playRegion.Active();
 }
-
 }
 
 // Strings for menu items and also for dialog titles
@@ -894,18 +826,6 @@ void OnPlaySpeedDec(const CommandContext &context)
    }
 }
 
-void OnMoveToPrevLabel(const CommandContext &context)
-{
-   auto &project = context.project;
-   DoMoveToLabel(project, false);
-}
-
-void OnMoveToNextLabel(const CommandContext &context)
-{
-   auto &project = context.project;
-   DoMoveToLabel(project, true);
-}
-
 #if 0
 // Legacy handlers, not used as of version 2.3.0
 void OnStopSelect(const CommandContext &context)
@@ -1147,26 +1067,6 @@ BaseItemSharedPtr ExtraPlayAtSpeedMenu()
 AttachedItem sAttachment3{
    wxT("Optional/Extra/Part1"),
    Shared( ExtraPlayAtSpeedMenu() )
-};
-
-BaseItemSharedPtr ExtraSelectionItems()
-{
-   using Options = CommandManager::Options;
-   static BaseItemSharedPtr items{
-   Items(wxT("MoveToLabel"),
-      Command(wxT("MoveToPrevLabel"), XXO("Move to Pre&vious Label"),
-         OnMoveToPrevLabel,
-         CaptureNotBusyFlag() | TrackPanelHasFocus(), wxT("Alt+Left")),
-      Command(wxT("MoveToNextLabel"), XXO("Move to Ne&xt Label"),
-         OnMoveToNextLabel,
-         CaptureNotBusyFlag() | TrackPanelHasFocus(), wxT("Alt+Right"))
-   ) };
-   return items;
-}
-
-AttachedItem sAttachment4{
-  { wxT("Optional/Extra/Part1/Select"), { OrderingHint::End, {} } },
-  Shared(ExtraSelectionItems())
 };
 
 }
