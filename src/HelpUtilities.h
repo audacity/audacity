b1a9f9463b467e70ab6169900c4e8a47e815909f
Paul Licameli <paul.licameli@audacityteam.org>
2020-11-11 14:06:23 -0500

    Register the "MIDI Device Info..." menu item

---
+++ src/HelpUtilities.h
@@ -0,0 +1,23 @@
+/**********************************************************************
+
+Audacity: A Digital Audio Editor
+
+@file HelpUtilities.cpp
+
+Paul Licameli split from HelpMenus.cpp
+
+**********************************************************************/
+
+#ifndef __AUDACITY_HELP_UTILITIES__
+#define __AUDACITY_HELP_UTILITIES__
+
+class AudacityProject;
+class wxString;
+class TranslatableString;
+
+void AUDACITY_DLL_API ShowDiagnostics(
+   AudacityProject &project, const wxString &info,
+   const TranslatableString &description, const wxString &defaultPath,
+   bool fixedWidth = false);
+
+#endif
