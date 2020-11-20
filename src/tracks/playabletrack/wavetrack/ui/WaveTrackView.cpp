3c76cea190f0aaa0200b9ff66aaad500b296ceae
Paul Licameli <paul.licameli@audacityteam.org>
2020-11-19 21:32:59 -0500

    Bug2588: Click to join clips shouldn't depend on channel visibility

--- src/tracks/playabletrack/wavetrack/ui/WaveTrackView.cpp
+++ src/tracks/playabletrack/wavetrack/ui/WaveTrackView.cpp
@@ -729,7 +729,12 @@ void WaveTrackSubView::DrawBoldBoundaries(
    const auto artist = TrackArtist::Get( context );
 
    // Update cache for locations, e.g. cutlines and merge points
-   track->UpdateLocationsCache();
+   // Bug2588: do this for both channels, even if one is not drawn, so that
+   // cut-line editing (which depends on the locations cache) works properly.
+   // If both channels are visible, we will duplicate this effort, but that
+   // matters little.
+   for( auto channel: TrackList::Channels(track))
+      channel->UpdateLocationsCache();
 
    const auto &zoomInfo = *artist->pZoomInfo;
 
