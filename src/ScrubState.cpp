ee5bde4d93ca6a657987d6aa93c6763a81011dc2
Paul Licameli <paul.licameli@audacityteam.org>
2020-12-17 07:04:31 -0500

    New files src/ScrubState.*, nothing in them yet

---
+++ src/ScrubState.cpp
@@ -0,0 +1,11 @@
+/*!********************************************************************
+ 
+ Audacity: A Digital Audio Editor
+ 
+ @file ScrubState.cpp
+ 
+ Paul Licameli split from AudioIO.cpp
+ 
+ **********************************************************************/
+
+#include "ScrubState.h"
---
+++ src/ScrubState.h
@@ -0,0 +1,13 @@
+/*!********************************************************************
+ 
+ Audacity: A Digital Audio Editor
+ 
+ @file ScrubState.h
+ 
+ Paul Licameli split from AudioIO.cpp
+ 
+ **********************************************************************/
+
+#ifndef __AUDACITY_SCRUB_STATE__
+#define __AUDACITY_SCRUB_STATE__
+#endif
