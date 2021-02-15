3ecc5717addc13e321c27a4cecf80246a81cad3b
Leland Lucius <github@homerow.net>
2021-02-15 09:13:33 -0600

    Bug 2120 - Spectral Selection Toolbar can be truncated when undocked

--- src/toolbars/ToolManager.cpp
+++ src/toolbars/ToolManager.cpp
@@ -95,7 +95,15 @@ ToolFrame::ToolFrame
 
    // Transfer the bar to the ferry
    bar->Reparent(this);
-   SetMinSize(bar->GetDockedSize());
+
+   // Bug 2120 (comment 6 residual): No need to set a minimum size
+   // if the toolbar is not resizable. On GTK, setting a minimum
+   // size will prevent the frame from shrinking if the toolbar gets
+   // reconfigured and needs to resize smaller.
+   if (bar->IsResizable())
+   {
+      SetMinSize(bar->GetDockedSize());
+   }
 
    {
       // We use a sizer to maintain proper spacing
