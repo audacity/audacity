14b767b9ebbc326e15a8ab847fe5b8acd37589a5
James Crook <james.k.crook@gmail.com>
2021-03-17 22:47:53 +0000

    Fix bad line break in #error directive.
    Thanks to Jack L for reporting this.

--- lib-src/libnyquist/nyquist/xlisp/xlfio.c
+++ lib-src/libnyquist/nyquist/xlisp/xlfio.c
@@ -19,8 +19,7 @@
 /* do some sanity checking: */
 #ifndef XL_BIG_ENDIAN
 #ifndef XL_LITTLE_ENDIAN
-#error configuration error -- either XL_BIG_ or XL_LITTLE_ENDIAN must be defined
-in xlisp.h
+#error configuration error -- either XL_BIG_ or XL_LITTLE_ENDIAN must be defined in xlisp.h
 #endif
 #endif
 #ifdef XL_BIG_ENDIAN
