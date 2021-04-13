ebf0c103b6ca0a9034f0392aa114840160a6b496
James Crook <james.k.crook@gmail.com>
2021-04-13 20:53:06 +0100

    Bug 2744 - Manual: Incorrect escaping of backslash character

--- scripts/mw2html_audacity/wiki2htmtemp.bat
+++ scripts/mw2html_audacity/wiki2htmtemp.bat
@@ -1,5 +1,5 @@
 @REM wiki2htm without the xcopy.  The results stay in the temp directory.  
 @REM Can be convenient when testing/modifying the script.
-python3 mw2html.py https://alphamanual.audacityteam.org/man ..\..\help\temp -s
+python mw2html.py https://alphamanual.audacityteam.org/man ..\..\help\temp -s
 
 
