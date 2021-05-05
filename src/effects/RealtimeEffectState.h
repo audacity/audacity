c763bf2e406befe2ca090dd577adbbe1f775843f
Leland Lucius <github@homerow.net>
2021-05-05 01:51:35 -0500

    RealtimeEffectState holds a PluginID and its own Effect object

--- src/effects/RealtimeEffectState.h
+++ src/effects/RealtimeEffectState.h
@@ -27,22 +27,39 @@ public:
       std::unique_ptr<EffectProcessor>(const PluginID &)
    >{};
 
-   explicit RealtimeEffectState( EffectProcessor &effect );
+   explicit RealtimeEffectState(const PluginID & id);
+   ~RealtimeEffectState();
 
-   EffectProcessor &GetEffect() const { return mEffect; }
+   //! May be called with nonempty id at most once in the lifetime of a state
+   /*!
+    Call with empty id is ignored.
+    Called by the constructor that takes an id */
+   void SetID(const PluginID & id);
+   EffectProcessor *GetEffect();
 
    bool Suspend();
    bool Resume() noexcept;
+
+   //! Main thread sets up for playback
+   bool Initialize(double rate);
    bool AddTrack(int group, unsigned chans, float rate);
+   //! Worker thread begins a batch of samples
+   bool ProcessStart();
+   //! Worker thread processes part of a batch of samples
    size_t Process(int group,
       unsigned chans, float **inbuf, float **outbuf, size_t numSamples);
+   //! Worker thread finishes a batch of samples
+   bool ProcessEnd();
    bool IsActive() const noexcept;
+   //! Main thread cleans up playback
+   bool Finalize();
 
 private:
-   EffectProcessor &mEffect;
+   PluginID mID;
+   std::unique_ptr<EffectProcessor> mEffect;
 
    std::vector<int> mGroupProcessor;
-   int mCurrentProcessor;
+   size_t mCurrentProcessor{ 0 };
 
    std::atomic<int> mSuspendCount{ 1 };    // Effects are initially suspended
 };
