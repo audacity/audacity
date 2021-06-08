d0381bd8113e308dfd842c6ad908dad5f5d03de5
James Crook <james.k.crook@gmail.com>
2021-06-08 22:05:56 +0100

    Bug 2803 - Audacity unresponsive after 'Fit to Height'
    a) Fixed root cause, which is conversion of a
    negative integer to a large unsigned, leading to
    a very tall track.
    b) Improved mitigation (in projects that already
    have over tall tracks) by setting a smaller minimum
    for the allowed size.

--- src/tracks/ui/TrackView.cpp
+++ src/tracks/ui/TrackView.cpp
@@ -97,10 +97,10 @@ bool TrackView::HandleXMLAttribute( const wxChar *attr, const wxChar *value )
    long nValue;
    if (!wxStrcmp(attr, wxT("height")) &&
          XMLValueChecker::IsGoodInt(strValue) && strValue.ToLong(&nValue)) {
-      // Extreme values for track height (likely caused by integer overflow)
+      // Bug 2803: Extreme values for track height (caused by integer overflow)
       // will stall Audacity as it tries to create an enormous vertical ruler.
       // So clamp to reasonable values.
-      nValue = std::max( 150l, std::min( nValue, 1000l ));
+      nValue = std::max( 40l, std::min( nValue, 1000l ));
       SetHeight(nValue);
       return true;
    }
