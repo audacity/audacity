6da25e16462ed9e5af0dc0588e0f7baaea238755
Dmitry Vedenko <vedenko@gmail.com>
2021-06-15 16:13:28 +0300

    Add targets for packaging DMG and InnoSetup
    Fixes configure.sh
    
    Fixes Windows code signing
    
    Fixes an issue with conan cache on windows
    
    Fixes build manual script
    
    Fixes build manual
    
    Remove unused props
    
    Use long options
    
    Yet another manual fix
    
    Fixes iss

--- cmake-proxies/cmake-modules/Package.cmake
+++ cmake-proxies/cmake-modules/Package.cmake
@@ -2,6 +2,8 @@
 set(CPACK_PACKAGE_VERSION_MAJOR "${AUDACITY_VERSION}") # X
 set(CPACK_PACKAGE_VERSION_MINOR "${AUDACITY_RELEASE}") # Y
 set(CPACK_PACKAGE_VERSION_PATCH "${AUDACITY_REVISION}") # Z
+set(CPACK_PACKAGE_VENDOR "Audacity")
+set(CPACK_PACKAGE_HOMEPAGE_URL "https://audacityteam.org")
 
 # X.Y.Z-alpha-20210615
 set(CPACK_PACKAGE_VERSION "${AUDACITY_VERSION}.${AUDACITY_RELEASE}.${AUDACITY_REVISION}${AUDACITY_SUFFIX}")
@@ -21,5 +23,23 @@ endif()
 set(CPACK_PACKAGE_DIRECTORY "${CMAKE_BINARY_DIR}/package")
 
 set(CPACK_GENERATOR ZIP)
+
+if( CMAKE_SYSTEM_NAME STREQUAL "Darwin" )
+   set( CPACK_GENERATOR DragNDrop )
+
+   set( CPACK_DMG_BACKGROUND_IMAGE "${CMAKE_SOURCE_DIR}/mac/Resources/Audacity-DMG-background.png")
+   set( CPACK_DMG_DS_STORE_SETUP_SCRIPT "${CMAKE_SOURCE_DIR}/scripts/build/macOS/DMGSetup.scpt")
+   
+   if( ${_OPT}perform_codesign )
+      set( CPACK_APPLE_CODESIGN_IDENTITY ${APPLE_CODESIGN_IDENTITY} )
+      set( CPACK_APPLE_NOTARIZATION_USER_NAME ${APPLE_NOTARIZATION_USER_NAME} )
+      set( CPACK_APPLE_NOTARIZATION_PASSWORD ${APPLE_NOTARIZATION_PASSWORD} )
+      set( CPACK_APPLE_SIGN_SCRIPTS "${CMAKE_SOURCE_DIR}/scripts/build/macOS" )
+      set( CPACK_PERFORM_NOTARIZATION ${${_OPT}perform_notarization} )
+      
+      # CPACK_POST_BUILD_SCRIPTS was added in 3.19, but we only need it on macOS
+      SET( CPACK_POST_BUILD_SCRIPTS "${CMAKE_SOURCE_DIR}/scripts/build/macOS/DMGSign.cmake" )
+   endif()
+endif()
 
 include(CPack) # do this last
