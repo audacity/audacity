2a2477eda0cb32b5d4dc2fb47aeebc02336eca39
Dmitry Vedenko <vedenko@gmail.com>
2021-07-14 22:15:11 +0300

    Adds a dialog, that notifies the user about update checks.
    This dialog is only shown once. It has links to privacy policy and application preferences.

---
+++ src/update/UpdateNoticeDialog.h
@@ -0,0 +1,30 @@
+/*!********************************************************************
+
+ Audacity: A Digital Audio Editor
+
+ @file UpdateNoticeDialog.h
+ @brief Define a dialog to notify the user about automatic update checking.
+
+ Dmitry Vedenko
+ **********************************************************************/
+
+#pragma once
+
+#include "widgets/wxPanelWrapper.h"
+#include "wx/string.h"
+
+class HtmlWindow;
+class wxWindow;
+
+//! Dialog, that notifies the users about automatic updates checking
+class UpdateNoticeDialog final : public wxDialogWrapper
+{
+    DECLARE_DYNAMIC_CLASS (AboutDialog)
+public:
+    explicit UpdateNoticeDialog (wxWindow* parent);
+
+ private:
+    void OnOk (wxCommandEvent& event);
+
+    DECLARE_EVENT_TABLE ()
+};
