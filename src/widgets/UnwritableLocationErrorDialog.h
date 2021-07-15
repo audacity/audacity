41341e12cdd25d374bee1ca973c494ad036f5b01
Dmitry Vedenko <vedenko@gmail.com>
2021-07-15 19:13:38 +0300

    Add new error dialog for unwritable directory.

---
+++ src/widgets/UnwritableLocationErrorDialog.h
@@ -0,0 +1,27 @@
+/**********************************************************************
+
+  Audacity: A Digital Audio Editor
+
+  UnwritableLocationError.h
+
+  Dmitry Vedenko
+
+**********************************************************************/
+
+#pragma once
+
+	
+#include "widgets/wxPanelWrapper.h"
+
+/// An error dialog about unwritable location, that allows to navigate to settings quickly
+class UnwritableLocationErrorDialog final : public wxDialogWrapper
+{
+public:
+   explicit UnwritableLocationErrorDialog(wxWindow* parent, const wxString& path);
+   virtual ~UnwritableLocationErrorDialog();
+
+   void OnOk(wxCommandEvent& event);
+   void OnError(wxCommandEvent& event);
+
+   DECLARE_EVENT_TABLE()
+};
