6477c9e0a6cb93b781c6c83153f1d036a7cc4c13
Paul Licameli <paul.licameli@audacityteam.org>
2021-08-31 12:40:57 -0400

    Duplicate Project in ProjectWindows

---
+++ src/ProjectWindows.cpp
@@ -0,0 +1,172 @@
+/**********************************************************************
+
+  Audacity: A Digital Audio Editor
+
+  Project.cpp
+
+  Dominic Mazzoni
+  Vaughan Johnson
+
+*//*******************************************************************/
+
+
+#include "Project.h"
+
+#include "widgets/wxWidgetsBasicUI.h"
+
+#include <wx/display.h>
+#include <wx/filename.h>
+#include <wx/frame.h>
+
+wxDEFINE_EVENT(EVT_TRACK_PANEL_TIMER, wxCommandEvent);
+
+size_t AllProjects::size() const
+{
+   return gAudacityProjects.size();
+}
+
+auto AllProjects::begin() const -> const_iterator
+{
+   return gAudacityProjects.begin();
+}
+
+auto AllProjects::end() const -> const_iterator
+{
+   return gAudacityProjects.end();
+}
+
+auto AllProjects::rbegin() const -> const_reverse_iterator
+{
+   return gAudacityProjects.rbegin();
+}
+
+auto AllProjects::rend() const -> const_reverse_iterator
+{
+   return gAudacityProjects.rend();
+}
+
+auto AllProjects::Remove( AudacityProject &project ) -> value_type
+{
+   std::lock_guard<std::mutex> guard{ Mutex() };
+   auto start = begin(), finish = end(), iter = std::find_if(
+      start, finish,
+      [&]( const value_type &ptr ){ return ptr.get() == &project; }
+   );
+   if (iter == finish)
+      return nullptr;
+   auto result = *iter;
+   gAudacityProjects.erase( iter );
+   return result;
+}
+
+void AllProjects::Add( const value_type &pProject )
+{
+   if (!pProject) {
+      wxASSERT(false);
+      return;
+   }
+   std::lock_guard<std::mutex> guard{ Mutex() };
+   gAudacityProjects.push_back( pProject );
+}
+
+std::mutex &AllProjects::Mutex()
+{
+   static std::mutex theMutex;
+   return theMutex;
+}
+
+int AudacityProject::mProjectCounter=0;// global counter.
+
+/* Define Global Variables */
+//This array holds onto all of the projects currently open
+AllProjects::Container AllProjects::gAudacityProjects;
+
+AudacityProject::AudacityProject()
+{
+   mProjectNo = mProjectCounter++; // Bug 322
+   AttachedObjects::BuildAll();
+   // But not for the attached windows.  They get built only on demand, such as
+   // from menu items.
+}
+
+AudacityProject::~AudacityProject()
+{
+}
+
+void AudacityProject::SetFrame( wxFrame *pFrame )
+{
+   mFrame = pFrame;
+}
+
+void AudacityProject::SetPanel( wxWindow *pPanel )
+{
+   mPanel = pPanel;
+}
+
+const wxString &AudacityProject::GetProjectName() const
+{
+   return mName;
+}
+
+void AudacityProject::SetProjectName(const wxString &name)
+{
+   mName = name;
+}
+
+FilePath AudacityProject::GetInitialImportPath() const
+{
+   return mInitialImportPath;
+}
+
+void AudacityProject::SetInitialImportPath(const FilePath &path)
+{
+   if (mInitialImportPath.empty())
+   {
+      mInitialImportPath = path;
+   }
+}
+
+AUDACITY_DLL_API wxFrame &GetProjectFrame( AudacityProject &project )
+{
+   auto ptr = project.GetFrame();
+   if ( !ptr )
+      THROW_INCONSISTENCY_EXCEPTION;
+   return *ptr;
+}
+
+AUDACITY_DLL_API const wxFrame &GetProjectFrame( const AudacityProject &project )
+{
+   auto ptr = project.GetFrame();
+   if ( !ptr )
+      THROW_INCONSISTENCY_EXCEPTION;
+   return *ptr;
+}
+
+std::unique_ptr<const BasicUI::WindowPlacement>
+ProjectFramePlacement( AudacityProject *project )
+{
+   if (!project)
+      return std::make_unique<BasicUI::WindowPlacement>();
+   return std::make_unique<wxWidgetsWindowPlacement>(
+      &GetProjectFrame(*project));
+}
+
+AUDACITY_DLL_API wxWindow &GetProjectPanel( AudacityProject &project )
+{
+   auto ptr = project.GetPanel();
+   if ( !ptr )
+      THROW_INCONSISTENCY_EXCEPTION;
+   return *ptr;
+}
+
+AUDACITY_DLL_API const wxWindow &GetProjectPanel(
+   const AudacityProject &project )
+{
+   auto ptr = project.GetPanel();
+   if ( !ptr )
+      THROW_INCONSISTENCY_EXCEPTION;
+   return *ptr;
+}
+
+// Generate the needed, linkable registry functions
+DEFINE_XML_METHOD_REGISTRY( ProjectFileIORegistry );
