0427d1afed2c46e0e238645f3f709bb166da0c0c
David Bailes <DavidBailes@users.noreply.github.com>
2021-09-20 11:27:16 +0100

    Issue 1681: Position of New Label dialog is inconvenient
    If the "use dialog for name of new label" check box is checked in the track behaviour page of preferences, then when adding a label at either selection or playback position, a dialog is used for entering the name of the new label. For convenience the horizontal position of the dialog should be such that the initial horizontal position of the cursor in the edit box is roughly the same as the horizontal position of the cursor or playback position respectively.
    
    This behaviour was broken by commit 8f8ec41.
    In this commit in LabelTrackView::DialogForLabelName(), the two arguments passed to viewInfo.TimeToPosition() are in wrong way round.
    
    The fix: swap the order of these arguments.

--- src/tracks/labeltrack/ui/LabelTrackView.cpp
+++ src/tracks/labeltrack/ui/LabelTrackView.cpp
@@ -2257,7 +2257,7 @@ int LabelTrackView::DialogForLabelName(
    // if it's a point label, or the start of its region if it's a region label.
    position.x +=
       + std::max(0, static_cast<int>(viewInfo.TimeToPosition(
-         viewInfo.GetLeftOffset(), region.t0())))
+         region.t0(), viewInfo.GetLeftOffset())))
       - 39;
    position.y += 2;  // just below the bottom of the track
    position = trackPanel.ClientToScreen(position);
