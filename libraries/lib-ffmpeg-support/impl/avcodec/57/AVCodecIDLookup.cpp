5b096377cb44f1a6301ecfac62ac2613dd98519c
Dmitry Vedenko <dmitry@crsib.me>
2021-10-04 19:50:27 +0300

    Adds avformat-57 for support for Ubuntu 18.04

---
+++ libraries/lib-ffmpeg-support/impl/avcodec/57/AVCodecIDLookup.cpp
@@ -0,0 +1,37 @@
+extern "C"
+{
+#include "../../avutil/55/avconfig.h"
+#include "../../ffmpeg-3.4.8-single-header.h"
+}
+
+#include <algorithm>
+
+#include "AVCodecID.h"
+
+#include "../../FFmpegAPIResolver.h"
+
+#define AV_CODEC_ID_ESCAPE130_DEPRECATED AV_CODEC_ID_ESCAPE130
+#define AV_CODEC_ID_G2M_DEPRECATED AV_CODEC_ID_G2M
+#define AV_CODEC_ID_WEBP_DEPRECATED AV_CODEC_ID_WEBP
+#define AV_CODEC_ID_HEVC_DEPRECATED AV_CODEC_ID_HEVC
+#define AV_CODEC_ID_PCM_S24LE_PLANAR_DEPRECATED AV_CODEC_ID_PCM_S24LE_PLANAR
+#define AV_CODEC_ID_PCM_S32LE_PLANAR_DEPRECATED AV_CODEC_ID_PCM_S32LE_PLANAR
+#define AV_CODEC_ID_OPUS_DEPRECATED AV_CODEC_ID_OPUS
+#define AV_CODEC_ID_TAK_DEPRECATED AV_CODEC_ID_TAK
+
+#define AV_CODEC_ID_VIMA AV_CODEC_ID_ADPCM_VIMA
+
+namespace avcodec_57
+{
+#include "../../AVCodecIDLookup.inl"
+
+const bool registered = ([]() {
+   FFmpegAPIResolver::Get().AddAVCodecIDResolver(57, {
+      &GetAVCodeID,
+      &GetAudacityCodecID
+   });
+
+   return true;
+})();
+
+}
