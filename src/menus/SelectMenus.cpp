ba463ffa54217d958dc569b8cd05d17295fe1a52
Vitaly Sverchinsky <sverchinsky@gmail.com>
2021-10-20 20:40:15 +0300

    Text selection with Ctrl+A in labels

--- src/menus/SelectMenus.cpp
+++ src/menus/SelectMenus.cpp
@@ -17,10 +17,12 @@
 #include "../SelectUtilities.h"
 #include "../TrackPanel.h"
 #include "../WaveTrack.h"
+#include "../LabelTrack.h"
 #include "../commands/CommandContext.h"
 #include "../commands/CommandManager.h"
 #include "../toolbars/ControlToolBar.h"
 #include "../tracks/ui/SelectHandle.h"
+#include "../tracks/labeltrack/ui/LabelTrackView.h"
 #include "../tracks/playabletrack/wavetrack/ui/WaveTrackView.h"
 #include "../tracks/playabletrack/wavetrack/ui/WaveTrackViewConstants.h"
 
@@ -457,6 +459,16 @@ struct Handler
 
 void OnSelectAll(const CommandContext &context)
 {
+   auto& trackPanel = TrackPanel::Get(context.project);
+   auto& tracks = TrackList::Get(context.project);
+   
+   for (auto lt : tracks.Selected< LabelTrack >()) {
+      auto& view = LabelTrackView::Get(*lt);
+      if (view.SelectAllText(context.project)) {
+         trackPanel.Refresh(false);
+         return;
+      }
+   }
    SelectUtilities::DoSelectAll( context.project );
 }
 
