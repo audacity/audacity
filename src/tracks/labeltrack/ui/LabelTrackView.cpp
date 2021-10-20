18cc6b35a7c68d946d712d16b9a17da830e50f38
Vitaly Sverchinsky <sverchinsky@gmail.com>
2021-10-20 21:15:51 +0300

    Fixes text navigation with arrow
    mInitialCursorPos is always updated to match mCurrentCursorPos after
    arrow has been pressed (without Shift)

--- src/tracks/labeltrack/ui/LabelTrackView.cpp
+++ src/tracks/labeltrack/ui/LabelTrackView.cpp
@@ -1610,32 +1610,41 @@ bool LabelTrackView::DoKeyDown(
       case WXK_LEFT:
       case WXK_NUMPAD_LEFT:
          // Moving cursor left
-         while ((mCurrentCursorPos > 0) && more) {
-            wchar = title.at( mCurrentCursorPos-1 );
-            more = !( ((int)wchar > 0xDFFF) || ((int)wchar <0xDC00));
+         if (mods != wxMOD_SHIFT && mCurrentCursorPos != mInitialCursorPos)
+            //put cursor to the left edge of selection
+            mInitialCursorPos = mCurrentCursorPos =
+               std::min(mInitialCursorPos, mCurrentCursorPos);
+         else
+         {
+            while ((mCurrentCursorPos > 0) && more) {
+               wchar = title.at(mCurrentCursorPos - 1);
+               more = !(((int)wchar > 0xDFFF) || ((int)wchar < 0xDC00));
 
-            mCurrentCursorPos--;
-            if (mods == wxMOD_SHIFT)
-               ;
-            else
-               mInitialCursorPos = mCurrentCursorPos =
-                  std::min(mInitialCursorPos, mCurrentCursorPos);
+               --mCurrentCursorPos;
+            }
+            if (mods != wxMOD_SHIFT)
+               mInitialCursorPos = mCurrentCursorPos;
          }
+         
          break;
 
       case WXK_RIGHT:
       case WXK_NUMPAD_RIGHT:
          // Moving cursor right
-         while ((mCurrentCursorPos < (int)title.length())&& more) {
-            wchar = title.at( mCurrentCursorPos );
-            more = !( ((int)wchar > 0xDBFF) || ((int)wchar <0xD800));
+         if(mods != wxMOD_SHIFT && mCurrentCursorPos != mInitialCursorPos)
+            //put cursor to the right edge of selection
+            mInitialCursorPos = mCurrentCursorPos =
+               std::max(mInitialCursorPos, mCurrentCursorPos);
+         else
+         {
+            while ((mCurrentCursorPos < (int)title.length()) && more) {
+               wchar = title.at(mCurrentCursorPos);
+               more = !(((int)wchar > 0xDBFF) || ((int)wchar < 0xD800));
 
-            mCurrentCursorPos++;
-            if (mods == wxMOD_SHIFT)
-               ;
-            else
-               mInitialCursorPos = mCurrentCursorPos =
-                  std::max(mInitialCursorPos, mCurrentCursorPos);
+               ++mCurrentCursorPos;
+            }
+            if (mods != wxMOD_SHIFT)
+               mInitialCursorPos = mCurrentCursorPos;
          }
          break;
 
