6ccc4f54b4711ee65f43393321f893323446550e
Vitaly Sverchinsky <sverchinsky@gmail.com>
2021-10-20 21:15:51 +0300

    Fixes text navigation with arrow
    mInitialCursorPos is always updated to match mCurrentCursorPos after
    arrow has been pressed (without Shift)

--- src/tracks/ui/TextEditHelper.cpp
+++ src/tracks/ui/TextEditHelper.cpp
@@ -180,34 +180,43 @@ bool TextEditHelper::OnKeyDown(int keyCode, int mods, AudacityProject* project)
     case WXK_LEFT:
     case WXK_NUMPAD_LEFT:
         // Moving cursor left
-        while ((mCurrentCursorPos > 0) && more) {
-            wchar = mText.at(mCurrentCursorPos - 1);
-            more = !(((int)wchar > 0xDFFF) || ((int)wchar < 0xDC00));
+       if (mods != wxMOD_SHIFT && mCurrentCursorPos != mInitialCursorPos)
+          //put cursor to the left edge of selection
+          mInitialCursorPos = mCurrentCursorPos =
+          std::min(mInitialCursorPos, mCurrentCursorPos);
+       else
+       {
+          while ((mCurrentCursorPos > 0) && more) {
+             wchar = mText.at(mCurrentCursorPos - 1);
+             more = !(((int)wchar > 0xDFFF) || ((int)wchar < 0xDC00));
 
-            mCurrentCursorPos--;
-            if (mods == wxMOD_SHIFT)
-                ;
-            else
-                mInitialCursorPos = mCurrentCursorPos =
-                std::min(mInitialCursorPos, mCurrentCursorPos);
-        }
-        return true;
+             --mCurrentCursorPos;
+          }
+          if (mods != wxMOD_SHIFT)
+             mInitialCursorPos = mCurrentCursorPos;
+       }
+       return true;
 
     case WXK_RIGHT:
     case WXK_NUMPAD_RIGHT:
-        // Moving cursor right
-        while ((mCurrentCursorPos < (int)mText.length()) && more) {
-            wchar = mText.at(mCurrentCursorPos);
-            more = !(((int)wchar > 0xDBFF) || ((int)wchar < 0xD800));
+       // Moving cursor right
+       if (mods != wxMOD_SHIFT && mCurrentCursorPos != mInitialCursorPos)
+          //put cursor to the right edge of selection
+          mInitialCursorPos = mCurrentCursorPos =
+          std::max(mInitialCursorPos, mCurrentCursorPos);
+       else
+       {
+          while ((mCurrentCursorPos < (int)mText.length()) && more) {
+             wchar = mText.at(mCurrentCursorPos);
+             more = !(((int)wchar > 0xDBFF) || ((int)wchar < 0xD800));
 
-            mCurrentCursorPos++;
-            if (mods == wxMOD_SHIFT)
-                ;
-            else
-                mInitialCursorPos = mCurrentCursorPos =
-                std::max(mInitialCursorPos, mCurrentCursorPos);
-        }
-        return true;
+             ++mCurrentCursorPos;
+          }
+          if (mods != wxMOD_SHIFT)
+             mInitialCursorPos = mCurrentCursorPos;
+       }
+
+       return true;
 
     case WXK_ESCAPE:
         delegate->OnTextEditCancelled(project);
