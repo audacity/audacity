71532a2c77aafd3b8823c3f128c234bc2384f167
Dmitry Vedenko <dmitry@crsib.me>
2021-10-26 17:49:48 +0300

    Adds a checkmark to "Loop on/off" menu

--- src/SelectUtilities.cpp
+++ src/SelectUtilities.cpp
@@ -29,6 +29,8 @@
 #include "ViewInfo.h"
 #include "WaveTrack.h"
 
+#include "commands/CommandManager.h"
+
 namespace {
 
 // Temporal selection (not TimeTrack selection)
@@ -178,6 +180,9 @@ void ActivatePlayRegion(AudacityProject &project)
          // Arbitrary first four seconds
          playRegion.SetTimes(0.0, 4.0);
    }
+
+   // Ensure the proper state of looping in the menu
+   CommandManager::Get( project ).UpdateCheckmarks( project );
 }
 
 void InactivatePlayRegion(AudacityProject &project)
@@ -189,6 +194,9 @@ void InactivatePlayRegion(AudacityProject &project)
    // the last-active times that are used for display.
    playRegion.SetActive( false );
    playRegion.SetTimes( selectedRegion.t0(), selectedRegion.t1() );
+
+   // Ensure the proper state of looping in the menu
+   CommandManager::Get( project ).UpdateCheckmarks( project );
 }
 
 void TogglePlayRegion(AudacityProject &project)
