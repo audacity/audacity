001aee1ef8401702b9f1615f796480c917e32773
Paul Licameli <paul.licameli@audacityteam.org>
2021-10-27 20:29:20 -0400

    Separate lib-theme-resources from lib-theme

---
+++ libraries/lib-theme-resources/LoadThemeResources.h
@@ -0,0 +1,18 @@
+/**********************************************************************
+
+  Audacity: A Digital Audio Editor
+
+  LoadThemeResources.cpp
+
+  Paul Licameli
+
+**********************************************************************/
+
+#ifndef __AUDACITY_LOAD_THEME_RESOURCES__
+#define __AUDACITY_LOAD_THEME_RESOURCES__
+
+namespace ThemeResources {
+   THEME_RESOURCES_API void Load();
+}
+
+#endif
