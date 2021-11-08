05d42ad58f8be29b1f7eb79310ecf7294af97694
Dmitry Vedenko <dmitry@crsib.me>
2021-11-08 17:09:45 +0300

    Allow iterating MemoryStream chunk by chunk to avoid memory copying

--- libraries/lib-utility/MemoryStream.h
+++ libraries/lib-utility/MemoryStream.h
@@ -15,13 +15,15 @@
 #include <list>
 #include <vector>
 
+#include "MemoryX.h"
+
 /*!
  * @brief A low overhead memory stream with O(1) append, low heap fragmentation and a linear memory view.
  *
  * wxMemoryBuffer always appends 1Kb to the end of the buffer, causing severe performance issues
- * and significant heap fragmentation. There is no possibility to controll the increment value.
+ * and significant heap fragmentation. There is no possibility to control the increment value.
  *
- * std::vector doubles it's memory size which can be problematic for large projects as well.
+ * std::vector doubles its memory size which can be problematic for large projects as well.
  * Not as bad as wxMemoryBuffer though.
  * 
  */
@@ -29,6 +31,29 @@ class UTILITY_API MemoryStream final
 {
 public:
    using StreamData = std::vector<uint8_t>;
+   using StreamChunk = std::pair<const void*, size_t>;
+
+private:
+   static constexpr size_t ChunkSize =
+      1024 * 1024 - // 1Mb
+      2 * sizeof(void*) - // account for the list node pointers
+      sizeof(size_t); // account for the bytes used member
+
+   struct Chunk final
+   {
+      std::array<uint8_t, ChunkSize> Data;
+      size_t BytesUsed { 0 };
+
+      // Returns data size left to append
+      size_t Append(StreamChunk& dataView);
+   };
+
+   using ChunksList = std::list<Chunk>;
+
+public:
+
+   MemoryStream() = default;
+   MemoryStream(MemoryStream&&) = default;
 
    void Clear();
 
@@ -38,23 +63,38 @@ public:
    // This function possibly has O(size) complexity as it may
    // require copying bytes to a linear chunk
    const void* GetData() const;
-   const size_t GetSize() const;
-private:
-   static constexpr size_t ChunkSize = 1024 * 1024;
+   const size_t GetSize() const noexcept;
 
-   using AppendDataView = std::pair<const void*, size_t>;
+   bool IsEmpty() const noexcept;
 
-   struct Chunk final
+   struct UTILITY_API Iterator :
+       ValueIterator<const StreamChunk, std::forward_iterator_tag>
    {
-      std::array<uint8_t, ChunkSize> Data;
-      size_t BytesUsed { 0 };
+      Iterator(const Iterator&) = default;
 
-      // Returns data size left to append
-      size_t Append(AppendDataView& dataView);
+      Iterator& operator++();
+
+      Iterator operator++(int);
+
+      StreamChunk operator*() const;
+      StreamChunk operator->() const;
+
+      bool operator==(const Iterator& rhs) const noexcept;
+      bool operator!=(const Iterator& rhs) const noexcept;
+   private:
+      Iterator(const MemoryStream* stream, bool isBegin);
+      
+      const MemoryStream* mStream { nullptr };
+      ChunksList::const_iterator mListIterator;
+      bool mShowLinearPart { false };
+
+      friend class MemoryStream;
    };
 
-   using ChunksList = std::list<Chunk>;
+   Iterator begin() const;
+   Iterator end() const;
 
+private:
    // This structures are lazily updated by get data
    mutable ChunksList mChunks;
    mutable StreamData mLinearData;
