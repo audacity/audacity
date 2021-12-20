0eefdb39d7830f50db041aa2b65b4c477769ad89
Dmitry Vedenko <dmitry@crsib.me>
2021-12-20 16:59:00 +0300

    Properly select GPL version in Windows installer

--- cmake-proxies/cmake-modules/AudacityInnoSetup.cmake
+++ cmake-proxies/cmake-modules/AudacityInnoSetup.cmake
@@ -28,6 +28,7 @@ if( INNO_SETUP_COMPILER )
                 -DWINDOWS_CERTIFICATE=${WINDOWS_CERTIFICATE}
                 -D WINDOWS_CERTIFICATE_PASSWORD=${WINDOWS_CERTIFICATE_PASSWORD}
                 -D CONFIG=$<CONFIG>
+                -D USE_GPL3=${${_OPT}bundle_gplv3}
                 -P "${CMAKE_SOURCE_DIR}/win/Inno_Setup_Wizard/BuildInnoSetupInstaller.cmake"
         VERBATIM
     )
--- win/Inno_Setup_Wizard/BuildInnoSetupInstaller.cmake
+++ win/Inno_Setup_Wizard/BuildInnoSetupInstaller.cmake
@@ -9,6 +9,7 @@
 # SIGN - sign the installer
 # WINDOWS_CERTIFICATE - path to PFX file. If not present, env:WINDOWS_CERTIFICATE will be used
 # WINDOWS_CERTIFICATE_PASSWORD - password for the PFX file. If not present, env:WINDOWS_CERTIFICATE_PASSWORD will be used
+# USE_GPL3 - set the license to GPLv3 in the installer
 
 if( BUILDING_64_BIT )
     set( INSTALLER_SUFFIX "x64" )
@@ -39,10 +40,19 @@ else()
     set( MANUAL )
 endif()
 
+if( USE_GPL3 )
+   set( GPL_VERSION 3 )
+   set( GPL_URL "https://www.gnu.org/licenses/gpl-3.0.en.html")
+else()
+set( GPL_VERSION 2 )
+set( GPL_URL "http://www.gnu.org/licenses/old-licenses/gpl-2.0.html")
+endif()
+
 # Prepare the output directory
 
 file(COPY "${SOURCE_DIR}/win/Inno_Setup_Wizard/" DESTINATION "${OUTPUT_DIR}")
 configure_file("${OUTPUT_DIR}/audacity.iss.in" "${OUTPUT_DIR}/audacity.iss")
+configure_file("${OUTPUT_DIR}/audacity_InnoWizard_InfoBefore.rtf.in" "${OUTPUT_DIR}/audacity_InnoWizard_InfoBefore.rtf")
 
 # Copy additional files
 
