162a077f35df3130b0d6da26cb4cd3b1f5570de6
pietro68 <pietro.marcello.68@tiscali.it>
2022-03-25 18:31:59 +0100

    created duplication of EffectAmplify to work on

---
+++ src/effects/AmplifySL.h
@@ -0,0 +1,104 @@
+/**********************************************************************
+
+  Audacity: A Digital Audio Editor
+
+  Amplify.h
+
+  Dominic Mazzoni
+
+  This rewritten class supports a smart Amplify effect - it calculates
+  the maximum amount of gain that can be applied to all tracks without
+  causing clipping and selects this as the default parameter.
+
+**********************************************************************/
+
+#ifndef __AUDACITY_EFFECT_AMPLIFY_SL__
+#define __AUDACITY_EFFECT_AMPLIFY_SL__
+
+#include "Effect.h"
+#include "../ShuttleAutomation.h"
+
+
+class wxSlider;
+class wxCheckBox;
+class wxTextCtrl;
+class ShuttleGui;
+
+class EffectAmplifySL final : public Effect
+{
+public:
+   static inline EffectAmplifySL*
+   FetchParameters(EffectAmplifySL&e, EffectSettings &){ return &e; }
+   static const ComponentInterfaceSymbol Symbol;
+
+   EffectAmplifySL();
+   virtual ~EffectAmplifySL();
+
+   // ComponentInterface implementation
+
+   ComponentInterfaceSymbol GetSymbol() const override;
+   TranslatableString GetDescription() const override;
+   ManualPageID ManualPage() const override;
+
+   // EffectDefinitionInterface implementation
+
+   EffectType GetType() const override;
+   bool LoadFactoryDefaults(EffectSettings &settings) const override;
+   bool DoLoadFactoryDefaults();
+
+   // EffectProcessor implementation
+
+   unsigned GetAudioInCount() const override;
+   unsigned GetAudioOutCount() const override;
+   size_t ProcessBlock(EffectSettings &settings,
+      const float *const *inBlock, float *const *outBlock, size_t blockLen)
+      override;
+
+   // Effect implementation
+
+   bool Init() override;
+   void Preview(EffectSettingsAccess &access, bool dryOnly) override;
+   std::unique_ptr<EffectUIValidator> PopulateOrExchange(
+      ShuttleGui & S, EffectSettingsAccess &access) override;
+   bool TransferDataToWindow(const EffectSettings &settings) override;
+   bool TransferDataFromWindow(EffectSettings &settings) override;
+
+private:
+   void ClampRatio();
+
+   // EffectAmplify implementation
+
+   void OnAmpText(wxCommandEvent & evt);
+   void OnPeakText(wxCommandEvent & evt);
+   void OnAmpSlider(wxCommandEvent & evt);
+   void OnClipCheckBox(wxCommandEvent & evt);
+   void CheckClip();
+
+private:
+   double mPeak;
+
+   double mRatio;
+   double mRatioClip;   // maximum value of mRatio which does not cause clipping
+   double mAmp;
+   double mNewPeak;
+   bool mCanClip;
+
+   wxSlider *mAmpS;
+   wxTextCtrl *mAmpT;
+   wxTextCtrl *mNewPeakT;
+   wxCheckBox *mClip;
+
+   const EffectParameterMethods& Parameters() const override;
+
+   DECLARE_EVENT_TABLE()
+
+static constexpr EffectParameter Ratio{ &EffectAmplifySL::mRatio,
+   L"Ratio",            0.9f,       0.003162f,  316.227766f,   1.0f  };
+// Amp is not saved in settings!
+static constexpr EffectParameter Amp{ &EffectAmplifySL::mAmp,
+   L"",                -0.91515f,  -50.0f,     50.0f,         10.0f };
+static constexpr EffectParameter Clipping{ &EffectAmplifySL::mCanClip,
+   L"AllowClipping",    false,    false,  true,    1  };
+};
+
+#endif // __AUDACITY_EFFECT_AMPLIFY__
