a61183cce7614b6f7dd09da4d2007e9862907a9a
Vitaly Sverchinsky <sverchinsky@gmail.com>
2022-03-30 13:45:08 +0300

    PluginDescriptor split into separate file, public interface changes
    Setters has been moved to public section

---
+++ libraries/lib-module-manager/PluginDescriptor.h
@@ -0,0 +1,157 @@
+/**********************************************************************
+
+  Audacity: A Digital Audio Editor
+
+  PluginDescriptor.h
+
+  Split from PluginManager.h
+
+**********************************************************************/
+
+#pragma once
+#include <wx/string.h>
+
+#include "PluginInterface.h"
+#include "wxArrayStringEx.h"
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// PluginDescriptor
+//
+///////////////////////////////////////////////////////////////////////////////
+
+typedef enum : unsigned {
+   PluginTypeNone = 0,          // 2.1.0 placeholder entries...not used by 2.1.1 or greater
+   PluginTypeStub =1,               // Used for plugins that have not yet been registered
+   PluginTypeEffect =1<<1,
+   PluginTypeAudacityCommand=1<<2,
+   PluginTypeExporter=1<<3,
+   PluginTypeImporter=1<<4,
+   PluginTypeModule=1<<5,
+} PluginType;
+
+class PluginDescriptorXMLTagHandler;
+
+// TODO:  Convert this to multiple derived classes
+//! Represents either a PluginProvider or a loaded plug-in and caches some
+//! information about it
+class MODULE_MANAGER_API PluginDescriptor
+{
+public:
+
+   PluginDescriptor();
+   PluginDescriptor &operator =(PluginDescriptor &&);
+   virtual ~PluginDescriptor();
+
+   bool IsLoaded() const;
+
+   PluginType GetPluginType() const;
+
+   // All plugins
+
+   // These return untranslated strings
+   const wxString & GetID() const;
+   const wxString & GetProviderID() const;
+   const PluginPath & GetPath() const;
+   const ComponentInterfaceSymbol & GetSymbol() const;
+
+   wxString GetUntranslatedVersion() const;
+   // There is no translated version
+
+   wxString GetVendor() const;
+
+   bool IsEnabled() const;
+   bool IsValid() const;
+
+   void SetEnabled(bool enable);
+   void SetValid(bool valid);
+
+   // Effect plugins only
+
+   // Internal string only, no translated counterpart!
+   // (Use Effect::GetFamilyName instead)
+   // This string persists in configuration files
+   // So config compatibility will break if it is changed across Audacity versions
+   wxString GetEffectFamily() const;
+
+   EffectType GetEffectType() const;
+   bool IsEffectDefault() const;
+   bool IsEffectInteractive() const;
+   bool IsEffectLegacy() const;
+   bool IsEffectRealtime() const;
+   bool IsEffectAutomatable() const;
+
+   // Importer plugins only
+
+   const wxString & GetImporterIdentifier() const;
+   const TranslatableString & GetImporterFilterDescription() const;
+   const FileExtensions & GetImporterExtensions() const;
+
+   void SetPluginType(PluginType type);
+
+   // These should be passed an untranslated value
+   void SetID(const PluginID & ID);
+   void SetProviderID(const PluginID & providerID);
+   void SetPath(const PluginPath & path);
+   void SetSymbol(const ComponentInterfaceSymbol & symbol);
+
+   // These should be passed an untranslated value wrapped in XO() so
+   // the value will still be extracted for translation
+   void SetVersion(const wxString & version);
+   void SetVendor(const wxString & vendor);
+
+   // "family" should be an untranslated string wrapped in wxT()
+   void SetEffectFamily(const wxString & family);
+   void SetEffectType(EffectType type);
+   void SetEffectDefault(bool dflt);
+   void SetEffectInteractive(bool interactive);
+   void SetEffectLegacy(bool legacy);
+   void SetEffectRealtime(bool realtime);
+   void SetEffectAutomatable(bool automatable);
+
+   void SetImporterIdentifier(const wxString & identifier);
+   void SetImporterFilterDescription(const TranslatableString & filterDesc);
+   void SetImporterExtensions(FileExtensions extensions);
+
+private:
+   
+   friend class PluginManager;
+
+   // Load a plug-in, or locate a PluginProvider
+   ComponentInterface *Load();
+   void Set(std::unique_ptr<ComponentInterface> instance);
+
+   // Common
+
+   // Among other purposes, PluginDescriptor acts as the resource handle,
+   // or smart pointer, to a resource created in a plugin library, and is responsible
+   // for a cleanup of this pointer.
+   std::unique_ptr<ComponentInterface> muInstance; // may be null for a module
+   ComponentInterface *mInstance;
+
+   PluginType mPluginType;
+
+   wxString mID;
+   PluginPath mPath;
+   ComponentInterfaceSymbol mSymbol;
+   wxString mVersion;
+   wxString mVendor;
+   wxString mProviderID;
+   bool mEnabled;
+   bool mValid;
+
+   // Effects
+
+   wxString mEffectFamily;
+   EffectType mEffectType;
+   bool mEffectInteractive;
+   bool mEffectDefault;
+   bool mEffectLegacy;
+   bool mEffectRealtime;
+   bool mEffectAutomatable;
+
+   // Importers
+
+   wxString mImporterIdentifier;
+   FileExtensions mImporterExtensions;
+};
