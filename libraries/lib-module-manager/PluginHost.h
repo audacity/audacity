b2fc96bc976a50cfce2a235474173f88c33fa8f3
Vitaly Sverchinsky <sverchinsky@gmail.com>
2022-05-13 00:33:59 +0300

    Plugin host
    Introduces class that can be used to run Audacity as a host application which is capable to perform a limited check of plugin modules for stability and report what plugins it provides.

---
+++ libraries/lib-module-manager/PluginHost.h
@@ -0,0 +1,70 @@
+/**********************************************************************
+
+  Audacity: A Digital Audio Editor
+
+  @file PluginHost.h
+
+  @author Vitaly Sverchinsky
+
+  Part of lib-module-manager library
+
+**********************************************************************/
+
+#pragma once
+
+#include "IPCChannel.h"
+#include <memory>
+#include <mutex>
+#include <optional>
+#include <condition_variable>
+#include <wx/string.h>
+
+#include "PluginIPCUtils.h"
+
+class IPCClient;
+
+/**
+ * \brief Internal class, processes plugin validation requests
+ * from the main app. Request is a simple string formatted by
+ * detail::MakeRequestString. After connection is established
+ * host starts to wait for a request from server. Once request
+ * is successfully processed host sends a reply. Host is capable
+ * to handle only one request at a time, so it's not allowed to
+ * send another request until host hasn't finish processing
+ * previous request.
+ */
+class PluginHost final : public IPCChannelStatusCallback
+{
+   static constexpr auto HostArgument =  "--host";
+
+   std::unique_ptr<IPCClient> mClient;
+   IPCChannel* mChannel{nullptr};
+   detail::InputMessageReader mInputMessageReader;
+   std::mutex mSync;
+   std::condition_variable mRequestCondition;
+   std::optional<wxString> mRequest;
+
+   bool mRunning{true};
+
+   void Stop() noexcept;
+
+public:
+   /**
+    * \brief Attempts to start a host application (should be called from
+    * the main application)
+    * \return true if host has started successfully
+    */
+   static bool Start();
+
+   ///Returns true if current process is considered to be a plugin host process
+   static bool IsHostProcess();
+
+   PluginHost();
+
+   void OnConnect(IPCChannel& channel) noexcept override;
+   void OnDisconnect() noexcept override;
+   void OnConnectionError() noexcept override;
+   void OnDataAvailable(const void* data, size_t size) noexcept override;
+
+   bool Serve();
+};
---
+++ libraries/lib-module-manager/PluginIPCUtils.h
@@ -0,0 +1,85 @@
+/**********************************************************************
+
+  Audacity: A Digital Audio Editor
+
+  @file PluginIPCUtils.h
+
+  @author Vitaly Sverchinsky
+
+  @brief Provides a set of internal commonly used functions/structures
+
+  Part of lib-module-manager library.
+
+**********************************************************************/
+
+#pragma once
+
+#include <vector>
+#include <wx/string.h>
+#include "XMLTagHandler.h"
+
+class IPCChannel;
+class PluginDescriptor;
+class XMLWriter;
+
+namespace detail
+{
+   ///Helper function that extracts plugin provider id and plugin path from the item string
+   ///return true if item string was successfully parsed
+   bool ParseRequestString(const wxString& req, wxString& providerId, wxString& pluginPath);
+
+   ///Helper function that builds item string from given plugin provider id and plugin path strings
+   ///return Item string that can be passed as argument to wxConnection::Request
+   wxString MakeRequestString(const wxString& providerId, const wxString& pluginPath);
+
+   ///Writes the length of the string and string bytes into the channel.
+   ///Message can be then extracted with InputMessageReader
+   void PutMessage(IPCChannel& channel, const wxString& value);
+
+   ///Stores consumed bytes into buffer,
+   ///so that individual messages can be extracted later.
+   class InputMessageReader
+   {
+      std::vector<char> mBuffer;
+   public:
+      ///fills internal buffer
+      void ConsumeBytes(const void* bytes, size_t length);
+
+      ///@returns true if buffer contains at least one complete message
+      bool CanPop() const noexcept;
+      ///extracts message from the buffer, use CanPop() to check if message
+      ///is present first
+      wxString Pop();
+   };
+
+   ///Host uses this structure to build a response and store it
+   ///into xml form before sending to the other side. Main application
+   ///uses this structure to restore response from the xml document received
+   ///as a message
+   class PluginValidationResult final : public XMLTagHandler
+   {
+      std::vector<PluginDescriptor> mDescriptors;
+      wxString mErrorMessage;
+      bool mHasError{false};
+   public:
+
+      ///Result is valid if error flag was not set and
+      ///contains at least one plugin descriptor.
+      bool IsValid() const noexcept;
+
+      bool HasError() const noexcept;
+      const wxString& GetErrorMessage() const noexcept;
+
+      void Add(PluginDescriptor&& desc);
+      void SetError(const wxString& msg);
+
+      const std::vector<PluginDescriptor>& GetDescriptors() const noexcept;
+
+      bool HandleXMLTag(const std::string_view& tag, const AttributesList& attrs) override;
+      void HandleXMLEndTag(const std::string_view&) override;
+      XMLTagHandler* HandleXMLChild(const std::string_view& tag) override;
+
+      void WriteXML(XMLWriter& writer) const;
+   };
+
+}
