54048bcc37e8454bce0da9f53a19366c528acf45
Paul Licameli <paul.licameli@audacityteam.org>
2022-05-17 07:13:12 -0400

    AudioUnit validator in new files, not dependent on the effect...
    ... but using the Instance

--- src/effects/audiounits/AudioUnitEffect.cpp
+++ src/effects/audiounits/AudioUnitEffect.cpp
@@ -18,7 +18,7 @@
 #include "AudioUnitEffect.h"
 #include "AudioUnitEffectOptionsDialog.h"
 #include "AudioUnitInstance.h"
-#include "AUControl.h"
+#include "AudioUnitValidator.h"
 #include "SampleCount.h"
 #include "ConfigInterface.h"
 
@@ -39,10 +39,10 @@
 #include <wx/frame.h>
 #include <wx/listctrl.h>
 #include <wx/log.h>
-#include <wx/sizer.h>
 #include <wx/settings.h>
 #include <wx/stattext.h>
 #include <wx/textctrl.h>
+#include <wx/osx/core/private.h>
 
 #include "../../SelectFile.h"
 #include "../../ShuttleGui.h"
@@ -268,106 +268,6 @@ bool AudioUnitEffect::FullyInitializePlugin()
    return true;
 }
 
-class AudioUnitValidator : public EffectUIValidator {
-   struct CreateToken{};
-public:
-   static std::unique_ptr<EffectUIValidator> Create(
-      EffectUIClientInterface &effect, ShuttleGui &S,
-      const wxString &uiType,
-      EffectInstance &instance, EffectSettingsAccess &access);
-
-   AudioUnitValidator(CreateToken,
-      EffectUIClientInterface &effect, EffectSettingsAccess &access,
-      AudioUnitInstance &instance, AUControl *pControl);
-
-   ~AudioUnitValidator() override;
-
-   bool UpdateUI() override;
-   bool ValidateUI() override;
-
-private:
-   static void EventListenerCallback(void *inCallbackRefCon,
-      void *inObject, const AudioUnitEvent *inEvent,
-      UInt64 inEventHostTime, AudioUnitParameterValue inParameterValue);
-   void EventListener(const AudioUnitEvent *inEvent,
-      AudioUnitParameterValue inParameterValue);
-   bool FetchSettingsFromInstance(EffectSettings &settings);
-   bool StoreSettingsToInstance(const EffectSettings &settings);
-
-   void Notify();
-
-   using EventListenerPtr =
-      AudioUnitCleanup<AUEventListenerRef, AUListenerDispose>;
-
-   EventListenerPtr MakeListener();
-
-   // The lifetime guarantee is assumed to be provided by the instance.
-   // See contract of PopulateUI
-   AudioUnitInstance &mInstance;
-   const EventListenerPtr mEventListenerRef;
-   AUControl *const mpControl{};
-};
-
-AudioUnitValidator::AudioUnitValidator(CreateToken,
-   EffectUIClientInterface &effect,
-   EffectSettingsAccess &access, AudioUnitInstance &instance,
-   AUControl *pControl
-)  : EffectUIValidator{ effect, access }
-   , mInstance{ instance }
-   , mEventListenerRef{ MakeListener() }
-   , mpControl{ pControl }
-{
-   // Make the settings of the instance up to date before using it to
-   // build a UI
-   StoreSettingsToInstance(mAccess.Get());
-}
-
-AudioUnitValidator::~AudioUnitValidator()
-{
-   if (mpControl)
-      mpControl->Close();
-}
-
-auto AudioUnitValidator::MakeListener()
-   -> EventListenerPtr
-{
-   const auto unit = mInstance.GetAudioUnit();
-   EventListenerPtr result;
-
-   // Register a callback with the audio unit
-   AUEventListenerRef eventListenerRef{};
-   if (AUEventListenerCreate(AudioUnitValidator::EventListenerCallback,
-      this,
-      static_cast<CFRunLoopRef>( const_cast<void*>(
-         GetCFRunLoopFromEventLoop(GetCurrentEventLoop()))),
-      kCFRunLoopDefaultMode, 0.0, 0.0, &eventListenerRef))
-      return nullptr;
-   result.reset(eventListenerRef);
-
-   // AudioUnitEvent is a struct with a discriminator field and a union
-   AudioUnitEvent event{ kAudioUnitEvent_ParameterValueChange };
-   // Initialize union member -- the ID (second field) reassigned later
-   auto &parameter = event.mArgument.mParameter;
-   parameter = AudioUnitUtils::Parameter{ unit, kAudioUnitScope_Global };
-
-   // Register each parameter as something we're interested in
-   if (auto &parameters = mInstance.GetParameters())
-      for (const auto &ID : parameters) {
-         parameter.mParameterID = ID;
-         if (AUEventListenerAddEventType(result.get(), this, &event))
-            return nullptr;
-      }
-
-   // Now set up the other union member
-   event = { kAudioUnitEvent_PropertyChange };
-   event.mArgument.mProperty = AudioUnitUtils::Property{
-      unit, kAudioUnitProperty_Latency, kAudioUnitScope_Global };
-   if (AUEventListenerAddEventType(result.get(), this, &event))
-      return nullptr;
-
-   return result;
-}
-
 unsigned AudioUnitEffect::GetAudioInCount() const
 {
    return mAudioIns;
@@ -478,46 +378,6 @@ bool AudioUnitEffect::LoadSettings(
    return true;
 }
 
-bool AudioUnitValidator::UpdateUI()
-{
-   // Update parameter values in AudioUnit, and propagate to any listeners
-   if (StoreSettingsToInstance(mAccess.Get())) {
-      // See AUView::viewWillDraw
-      if (mpControl)
-         mpControl->ForceRedraw();
-   
-      // This will be the AudioUnit of a stateful instance, not of the effect
-      Notify();
-
-      return true;
-   }
-   return false;
-}
-
-bool AudioUnitValidator::ValidateUI()
-{
-   mAccess.ModifySettings([this](EffectSettings &settings){
-#if 0
-      // This analogy with other generators doesn't seem to fit AudioUnits
-      // How can we define the control mDuration?
-      if (GetType() == EffectTypeGenerate)
-         settings.extra.SetDuration(mDuration->GetValue());
-#endif
-      FetchSettingsFromInstance(settings);
-   });
-   return true;
-}
-
-bool AudioUnitValidator::FetchSettingsFromInstance(EffectSettings &settings)
-{
-   return mInstance.FetchSettings(AudioUnitInstance::GetSettings(settings));
-}
-
-bool AudioUnitValidator::StoreSettingsToInstance(const EffectSettings &settings)
-{
-   return mInstance.StoreSettings(AudioUnitInstance::GetSettings(settings));
-}
-
 bool AudioUnitEffect::LoadUserPreset(
    const RegistryPath & name, EffectSettings &settings) const
 {
@@ -567,64 +427,6 @@ RegistryPaths AudioUnitEffect::GetFactoryPresets() const
 // ============================================================================
 // EffectUIClientInterface Implementation
 // ============================================================================
-
-std::unique_ptr<EffectUIValidator> AudioUnitValidator::Create(
-   EffectUIClientInterface &effect, ShuttleGui &S,
-   const wxString &uiType,
-   EffectInstance &instance, EffectSettingsAccess &access)
-{
-   const auto parent = S.GetParent();
-   // Cast is assumed to succeed because only this effect's own instances
-   // are passed back by the framework
-   auto &myInstance = dynamic_cast<AudioUnitInstance&>(instance);
-
-   AUControl *pControl{};
-   wxPanel *container{};
-   {
-      auto mainSizer = std::make_unique<wxBoxSizer>(wxVERTICAL);
-      wxASSERT(parent); // To justify safenew
-      container = safenew wxPanelWrapper(parent, wxID_ANY);
-      mainSizer->Add(container, 1, wxEXPAND);
-      parent->SetSizer(mainSizer.release());
-   }
-
-#if defined(HAVE_AUDIOUNIT_BASIC_SUPPORT)
-   if (uiType == BasicValue.MSGID().GET()) {
-      if (!CreatePlain(mParent))
-         return nullptr;
-   }
-   else
-#endif
-   {
-      auto uControl = Destroy_ptr<AUControl>(safenew AUControl);
-      if (!uControl)
-         return nullptr;
-      pControl = uControl.get();
-
-      if (!pControl->Create(container, myInstance.GetComponent(),
-         myInstance.GetAudioUnit(),
-         uiType == FullValue.MSGID().GET()))
-         return nullptr;
-
-      {
-         auto innerSizer = std::make_unique<wxBoxSizer>(wxVERTICAL);
-
-         innerSizer->Add(uControl.release(), 1, wxEXPAND);
-         container->SetSizer(innerSizer.release());
-      }
-
-      parent->SetMinSize(wxDefaultSize);
-
-#ifdef __WXMAC__
-#ifdef __WX_EVTLOOP_BUSY_WAITING__
-      wxEventLoop::SetBusyWaiting(true);
-#endif
-#endif
-   }
-
-   return std::make_unique<AudioUnitValidator>(
-      CreateToken{}, effect, access, myInstance, pControl);
-}
 
 std::unique_ptr<EffectUIValidator> AudioUnitEffect::PopulateUI(ShuttleGui &S,
    EffectInstance &instance, EffectSettingsAccess &access)
@@ -870,36 +672,6 @@ TranslatableString AudioUnitEffect::Import(
       return error;
 
    return {};
-}
-
-void AudioUnitValidator::Notify()
-{
-   AudioUnitParameter aup = {};
-   aup.mAudioUnit = mInstance.GetAudioUnit();
-   aup.mParameterID = kAUParameterListener_AnyParameter;
-   aup.mScope = kAudioUnitScope_Global;
-   aup.mElement = 0;
-   AUParameterListenerNotify(NULL, NULL, &aup);
-}
-
-void AudioUnitValidator::EventListener(const AudioUnitEvent *inEvent,
-   AudioUnitParameterValue inParameterValue)
-{
-   // Modify the instance and its workers
-   mInstance.EventListener(inEvent, inParameterValue);
-   // Fetch changed settings and send them to the framework
-   // (Maybe we need a way to fetch just one changed setting, but this is
-   // the easy way to write it)
-   ValidateUI();
-}
-
-// static
-void AudioUnitValidator::EventListenerCallback(void *inCallbackRefCon,
-   void *inObject, const AudioUnitEvent *inEvent, UInt64 inEventHostTime,
-   AudioUnitParameterValue inParameterValue)
-{
-   static_cast<AudioUnitValidator *>(inCallbackRefCon)
-      ->EventListener(inEvent, inParameterValue);
 }
 
 void AudioUnitEffect::GetChannelCounts()
