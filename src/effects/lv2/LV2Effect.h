ceae288e0d47f89f2e94b502d3b7ce0238b0ced0
Paul Licameli <paul.licameli@audacityteam.org>
2022-06-16 06:18:26 -0400

    Move LV2FeaturesList to new files

--- src/effects/lv2/LV2Effect.h
+++ src/effects/lv2/LV2Effect.h
@@ -28,19 +28,15 @@ class wxArrayString;
 
 #include "lv2/atom/forge.h"
 #include "lv2/data-access/data-access.h"
-#include "lv2/log/log.h"
-#include "lv2/options/options.h"
 #include "lv2/state/state.h"
-#include "lv2/uri-map/uri-map.h"
 #include "lv2/worker/worker.h"
 #include <suil/suil.h>
 #include "lv2_external_ui.h"
 
-#include "../StatefulPerTrackEffect.h"
+#include "LV2FeaturesList.h"
 #include "../../ShuttleGui.h"
 #include "SampleFormat.h"
 
-#include "LV2Symbols.h"
 #include "NativeWindow.h"
 
 #include "zix/ring.h"
@@ -48,7 +44,6 @@ class wxArrayString;
 #include <unordered_map>
 
 using LilvInstancePtr = Lilv_ptr<LilvInstance, lilv_instance_free>;
-using LilvNodesPtr = Lilv_ptr<LilvNodes, lilv_nodes_free>;
 using LilvScalePointsPtr = Lilv_ptr<LilvScalePoints, lilv_scale_points_free>;
 using LilvUIsPtr = Lilv_ptr<LilvUIs, lilv_uis_free>;
 using SuilHostPtr = Lilv_ptr<SuilHost, suil_host_free>;
@@ -68,9 +63,6 @@ class NumericTextCtrl;
 #define LV2EFFECTS_FAMILY XO("LV2")
 
 // DECLARE_LOCAL_EVENT_TYPE(EVT_SIZEWINDOW, -1);
-
-// Define a maximum block size in number of samples (not bytes)
-#define DEFAULT_BLOCKSIZE 1048576
 
 class LV2Port
 {
@@ -250,122 +242,6 @@ using LV2ControlPortPtr = std::shared_ptr<LV2ControlPort>;
 using LV2ControlPortArray = std::vector<LV2ControlPortPtr>;
 
 class LV2Wrapper;
-
-// Define a reasonable default sequence size in bytes
-#define DEFAULT_SEQSIZE 8192
-
-class LV2FeaturesList : public StatefulPerTrackEffect {
-public:
-   explicit LV2FeaturesList(const LilvPlugin *plug);
-
-   //! @return success
-   bool InitializeOptions();
-
-   //! To be called after InitializeOptions()
-   //! @return success
-   bool InitializeFeatures();
-
-   //! Get vector of pointers to features, whose `.data()` can be passed to lv2
-   using FeaturePointers = std::vector<const LV2_Feature *>;
-   FeaturePointers GetFeaturePointers() const;
-
-   //! @return whether our host should reciprocally supply the
-   //! LV2_Worker_Schedule interface to the plug-in
-   static bool SuppliesWorkerInterface(const LilvPlugin *plug);
-
-   //! @return whether our host should reciprocally supply the
-   //! LV2_Worker_Schedule interface to the plug-in
-   bool SuppliesWorkerInterface() const { return mSuppliesWorkerInterface; }
-   //! @return may be null
-   const LV2_Options_Option *NominalBlockLengthOption() const;
-   //! @return may be null
-   const LV2_Options_Option *SampleRateOption() const;
-   //! @return a port number, or a negative
-   int LatencyPort() const { return mLatencyPort; }
-
-   size_t AddOption(LV2_URID, uint32_t size, LV2_URID, const void *value);
-
-   /*!
-    @param subject URI of a plugin
-    @return whether all required features of subject are supported
-    */
-   bool ValidateOptions(const LilvNode *subject);
-
-   /*!
-    @param subject URI of a plugin
-    @param required whether to check required or optional features of subject
-    @return true only if `!required` or else all checked features are supported
-    */
-   bool CheckOptions(const LilvNode *subject, bool required);
-
-   void AddFeature(const char *uri, const void *data);
-
-   /*!
-    @param subject URI of the host or of the UI identifies a resource in lv2
-    @return whether all required features of subject are supported
-    */
-   bool ValidateFeatures(const LilvNode *subject);
-
-   /*!
-    @param subject URI of the host or of the UI identifies a resource in lv2
-    @param required whether to check required or optional features of subject
-    @return true only if `!required` or else all checked features are supported
-    */
-   bool CheckFeatures(const LilvNode *subject, bool required);
-
-protected:
-   const LilvPlugin *const mPlug;
-
-   // lv2 functions require a pointer to non-const in places, but presumably
-   // have no need to mutate the members of this structure
-   LV2_URID_Map *URIDMapFeature() const
-   { return const_cast<LV2_URID_Map*>(&mURIDMapFeature); }
-
-   static uint32_t uri_to_id(LV2_URI_Map_Callback_Data callback_data,
-                             const char *map,
-                             const char *uri);
-   static LV2_URID urid_map(LV2_URID_Map_Handle handle, const char *uri);
-   LV2_URID URID_Map(const char *uri);
-
-   static const char *urid_unmap(LV2_URID_Unmap_Handle handle, LV2_URID urid);
-   const char *URID_Unmap(LV2_URID urid);
-
-   static int log_printf(LV2_Log_Handle handle, LV2_URID type, const char *fmt, ...);
-   static int log_vprintf(LV2_Log_Handle handle, LV2_URID type, const char *fmt, va_list ap);
-   int LogVPrintf(LV2_URID type, const char *fmt, va_list ap);
-
-   // These objects contain C-style virtual function tables that we fill in
-   const LV2_URI_Map_Feature mUriMapFeature{
-      this, LV2FeaturesList::uri_to_id }; // Features we support
-   const LV2_URID_Map mURIDMapFeature{ this, LV2FeaturesList::urid_map };
-   const LV2_URID_Unmap mURIDUnmapFeature{ this, LV2FeaturesList::urid_unmap };
-   const LV2_Log_Log mLogFeature{
-      this, LV2FeaturesList::log_printf, LV2FeaturesList::log_vprintf };
-
-   // Declare local URI map
-   LV2Symbols::URIDMap mURIDMap;
-
-   std::vector<LV2_Options_Option> mOptions;
-   size_t mSampleRateOption{};
-   size_t mBlockSizeOption{};
-
-   std::vector<LV2_Feature> mFeatures;
-
-   float mSampleRate{ 44100 };
-   size_t mBlockSize{ DEFAULT_BLOCKSIZE };
-   int mSeqSize{ DEFAULT_SEQSIZE };
-
-   size_t mMinBlockSize{ 1 };
-   size_t mMaxBlockSize{ mBlockSize };
-
-   int mLatencyPort{ -1 };
-
-   const bool mSuppliesWorkerInterface;
-   bool mSupportsNominalBlockLength{ false };
-   bool mSupportsSampleRate{ false };
-
-   bool mNoResize{ false };
-};
 
 class LV2Effect final : public LV2FeaturesList
 {
