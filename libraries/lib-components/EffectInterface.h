054d63046621425c7f294ffd61eed042557e9554
Dmitry Vedenko <dmitry@crsib.me>
2022-07-20 22:48:08 +0300

    Forward OnClose event of the EffectHostUI to the Effect
    As a part of transition to the new Effect architecture `EffectUIValidator` subclasses now handle the events from wxWidgets. This makes deleting `EffectUIValidator` instance from the `EffectUIHost::OnClose` to be unsafe and in fact it has introduced #3220: LV2Validator called `Close` from the `OnIdle` handler, which deleted the handler mid-processing.  
    
    However, the destructor of `EffectUIValidator` had a side effect of calling `CloseUI` on the plugin itself. This broke Nyquist debug mode: after pressing Debug button `EffectUIHost::Destroy` was called, which has delayed the `EffectUIHost` and `EffectUIValidator` destructors until the next idle event in the run loop. However, `NyquistEffect` relied on `CloseUI` to correctly setup parent of the `NyquistOutputDialog`. Omission of `CloseUI` from the `EffectUIHost::OnClose` results in attempt to `delete` a stack object, because `NyquistOutputDialog` is attached to the `EffectUIHost` in the destroying state.

--- libraries/lib-components/EffectInterface.h
+++ libraries/lib-components/EffectInterface.h
@@ -543,6 +543,11 @@ public:
     */
    virtual bool IsGraphicalUI();
 
+   /*!
+    Handle the UI OnClose event. Default implementation calls mEffect.CloseUI()
+   */
+   virtual void OnClose();
+
 protected:
    // Convenience function template for binding event handler functions
    template<typename EventTag, typename Class, typename Event>
@@ -554,6 +559,8 @@ protected:
 
    EffectUIClientInterface &mEffect;
    EffectSettingsAccess &mAccess;
+
+   bool mUIClosed { false };
 };
 
 /*************************************************************************************//**
