5219aac23f055f2689b2d1acc72cc0db5c256807
Paul Licameli <paul.licameli@audacityteam.org>
2022-07-27 12:43:54 -0400

    Move EffectStage to lib-audio-graph

--- src/effects/PerTrackEffect.h
+++ src/effects/PerTrackEffect.h
@@ -189,70 +189,7 @@ private:
 
 namespace AudioGraph {
 
-//! Decorate a source with a non-timewarping effect, which may have latency
-class EffectStage final : public Source {
-public:
-   using Instance = EffectInstanceEx;
-
-   //! Completes `instance.ProcessInitialize()` or throws a std::exception
-   /*!
-    @pre `upstream.AcceptsBlockSize(inBuffers.BlockSize())`
-    @post `AcceptsBlockSize(inBuffers.BlockSize())`
-    */
-   EffectStage(Source &upstream, Buffers &inBuffers,
-      Instance &instance, EffectSettings &settings, double sampleRate,
-      std::optional<sampleCount> genLength, ChannelNames map);
-   EffectStage(const EffectStage&) = delete;
-   EffectStage &operator =(const EffectStage &) = delete;
-   //! Finalizes the instance
-   ~EffectStage() override;
-
-   /*!
-    @return true
-    */
-   bool AcceptsBuffers(const Buffers &buffers) const override;
-   //! See postcondition of constructor
-   bool AcceptsBlockSize(size_t size) const override;
-
-   std::optional<size_t> Acquire(Buffers &data, size_t bound) override;
-   sampleCount Remaining() const override;
-   bool Release() override;
-
-private:
-   sampleCount DelayRemaining() const
-      { return std::max<sampleCount>(0, mDelayRemaining); }
-
-   //! Produce exactly `curBlockSize` samples in `data`
-   /*!
-    @pre curBlockSize <= data.BlockSize() - outBufferOffset
-    @pre curBlockSize <= data.Remaining() - outBufferOffset
-    @pre curBlockSize <= mInBuffers.Remaining()
-    @return success
-    */
-   bool Process(
-      const Buffers &data, size_t curBlockSize, size_t outBufferOffset) const;
-
-   std::optional<size_t> FetchProcessAndAdvance(
-      Buffers &data, size_t bound, bool doZeros, size_t outBufferOffset = 0);
-
-   void FinishUpstream();
-
-   Source &mUpstream;
-   //! @invariant mInBuffers.BlockSize() <= mInBuffers.Remaining()
-   Buffers &mInBuffers;
-   Instance &mInstance;
-   EffectSettings &mSettings;
-   const double mSampleRate;
-   const bool mIsProcessor;
-
-   sampleCount mDelay{};
-   sampleCount mDelayRemaining;
-   size_t mLastProduced{};
-   size_t mLastZeroes{};
-   bool mLatencyDone{ false };
-   bool mCleared{ false };
-};
-
+//! Copies from a Source to a Sink, mediated by Buffers
 struct Task {
 public:
    /*!
