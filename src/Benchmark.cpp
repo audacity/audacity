b3702b2101d45d40ba7c8e4d74568a0d055526fe
Vitaly Sverchinsky <sverchinsky@gmail.com>
2022-08-17 22:07:19 +0300

    Fixes built-in benchmark condition
    Since WaveClip copy constructor preserves underlying sequence but modifies trims instead the condition used in benchmark became incorrect

--- src/Benchmark.cpp
+++ src/Benchmark.cpp
@@ -428,11 +428,11 @@ void BenchmarkDialog::OnRun( wxCommandEvent & WXUNUSED(event))
    // as we're about to do).
    t->GetEndTime();
 
-   if (t->GetClipByIndex(0)->GetSequence()->GetNumSamples() != nChunks * chunkSize) {
+   if (t->GetClipByIndex(0)->GetPlaySamplesCount() != nChunks * chunkSize) {
       Printf( XO("Expected len %lld, track len %lld.\n")
          .Format(
             nChunks * chunkSize,
-            t->GetClipByIndex(0)->GetSequence()->GetNumSamples()
+            t->GetClipByIndex(0)->GetPlaySamplesCount()
                .as_long_long() ) );
       goto fail;
    }
@@ -465,7 +465,7 @@ void BenchmarkDialog::OnRun( wxCommandEvent & WXUNUSED(event))
          Printf( XO("Expected len %lld, track len %lld.\n")
             .Format(
                nChunks * chunkSize,
-               t->GetClipByIndex(0)->GetSequence()->GetNumSamples()
+               t->GetClipByIndex(0)->GetPlaySamplesCount()
                   .as_long_long() ) );
          goto fail;
       }
@@ -485,12 +485,12 @@ void BenchmarkDialog::OnRun( wxCommandEvent & WXUNUSED(event))
          goto fail;
       }
 
-      if (t->GetClipByIndex(0)->GetSequence()->GetNumSamples() != nChunks * chunkSize) {
+      if (t->GetClipByIndex(0)->GetPlaySamplesCount() != nChunks * chunkSize) {
          Printf( XO("Trial %d\n").Format( z ) );
          Printf( XO("Expected len %lld, track len %lld.\n")
             .Format(
                nChunks * chunkSize,
-               t->GetClipByIndex(0)->GetSequence()->GetNumSamples()
+               t->GetClipByIndex(0)->GetPlaySamplesCount()
                   .as_long_long() ) );
          goto fail;
       }
