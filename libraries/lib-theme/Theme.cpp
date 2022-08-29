a93af2774e806a9963981e3339588543628c4aa1
Ben Jackson <ben@ben.com>
2022-08-29 12:52:55 -0700

    Theme: SaveThemeComponents saves active components
    This makes it possible to capture the live theme (such as one loaded
    from an image cache) as a set of individual components.

--- libraries/lib-theme/Theme.cpp
+++ libraries/lib-theme/Theme.cpp
@@ -1137,19 +1137,24 @@ void ThemeBase::LoadOneThemeComponents( teThemeType id, bool bOkIfNotFound )
 
 void ThemeBase::SaveThemeComponents()
 {
+   // Save whatever is active (to extract from loaded caches)
+   SaveOneThemeComponents("active");
+
    ValueRestorer cleanup{ *mpSet };
-   for (auto &[key, data] : GetThemeCacheLookup())
-      if (!SaveOneThemeComponents( key.Internal() ))
+   for (auto& [key, data] : GetThemeCacheLookup())
+   {
+      SwitchTheme(key.Internal());
+      if (!SaveOneThemeComponents(key.Internal()))
          // Some file failed to save, message was given
          return;
+   }
    BasicUI::ShowMessageBox(
       XO("Themes written to:\n  %s/*/Components/.").Format(GetFilePath()));
 }
 
-bool ThemeBase::SaveOneThemeComponents( teThemeType id )
+bool ThemeBase::SaveOneThemeComponents(teThemeType id)
 {
    using namespace BasicUI;
-   SwitchTheme( id );
    auto &resources = *mpSet;
    // IF directory doesn't exist THEN create it
    const auto dir = ThemeComponentsDir(GetFilePath(), id);
