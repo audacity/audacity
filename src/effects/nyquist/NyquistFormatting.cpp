fca6981a643028d175c70e45f7cddfbe46312352
Paul Licameli <paul.licameli@audacityteam.org>
2022-09-24 08:01:28 -0400

    Define class NyquistFormatting::Assignments and its helpers

--- src/effects/nyquist/NyquistFormatting.cpp
+++ src/effects/nyquist/NyquistFormatting.cpp
@@ -13,9 +13,11 @@
 
 #include "Internat.h"
 #include "wxFileNameWrapper.h"
+#include "MemoryX.h"
 #include <wx/string.h>
 #include <wx/utils.h>
 #include <unordered_map>
+
 
 wxString NyquistFormatting::EscapeString(const wxString & inStr)
 {
@@ -101,4 +103,107 @@ void NyquistFormatting::resolveFilePath(
       if (!extension.empty())
          path = path + '.' + extension;
    }
+}
+
+namespace {
+wxString symbolName(wxString name)
+{
+   // process name so it can be given to the Lisp reader
+   // TODO:  might escape characters in name, so it could include spaces
+   return name;
+}
+}
+
+NyquistFormatting::Symbol::Symbol(const wxString &name)
+   : mName{ symbolName(name) }
+{
+}
+
+namespace NyquistFormatting{ namespace {
+struct ValueFormatter {
+   wxString operator()(std::monostate) const {
+      // Special right-hand-side for XLisp assignment unbinds a symbol
+      return "'*unbound*";
+   }
+   wxString operator()(nullptr_t) const {
+      return "nil";
+   }
+   wxString operator()(bool b) const {
+      return b ? "t" : "nil";
+   }
+   wxString operator()(char c) const {
+      // Syntax for character constant like 'x' in C, is #\x in Lisp
+      return wxString::Format("#\\%c", c);
+   }
+   // Be careful about the printf formatting of FIXTYPE
+   wxString operator()(FIXTYPE value) const {
+      static_assert(std::is_same_v<FIXTYPE, long>,
+         "ValueFormatter needs another overload of FormatIntegral");
+      return FormatIntegral(value);
+   }
+   wxString FormatIntegral(long value) const {
+      return wxString::Format("%ld", value);
+   }
+   // FLOTYPE can just promote to double if it isn't already
+   wxString operator()(double value) const {
+      // We use Internat::ToString() rather than "%f" here because we
+      // always have to use the dot as decimal separator when giving
+      // numbers to Nyquist, whereas using "%f" will use the user's
+      // decimal separator which may be a comma in some countries.
+      // Note: always format a double with positive precision, so the Lisp
+      // reader will construct a floating point number, and not a fixed.
+      return Internat::ToString(value, 14);
+   }
+   wxString operator()(const char *value) const {
+      // unrestricted value will become quoted UTF-8
+      return '"' + EscapeString(value) + '"';
+   }
+   wxString operator()(Multiple multiple, const char *function) const {
+      auto result = wxString{ '(' } + function + ' ';
+      for (auto &&value : multiple)
+         result += Visit(value) + ' ';
+      // overwrite last space
+      *result.rbegin() = ')';
+      return result;
+   }
+   wxString operator()(List list) const {
+      if (begin(list) == end(list))
+         return "nil";
+      return (*this)(list, "list");
+   }
+   wxString operator()(Vector vector) const {
+      // May format as the empty vector value, "(vector)"
+      return (*this)(vector, "vector");
+   }
+   wxString operator()(const Eval &eval) const {
+      return eval.form;
+   }
+
+   wxString Visit (const Value &value) const {
+      const ValueBase &base = value;
+      return ::Visit(*this, base);
+   }
+};
+} }
+
+wxString NyquistFormatting::Value::Format() const
+{
+   return ValueFormatter{}.Visit(*this);
+}
+
+NyquistFormatting::Assignments::Assignments(Arguments list)
+{
+   for (auto &argument : list)
+      Append(argument);
+}
+
+void NyquistFormatting::Assignments::Append(Assignment assignment)
+{
+   const auto valueString = assignment.value.Format();
+   if (assignment.property)
+      mCommand += wxString::Format(wxT("(putprop '%s %s '%s)\n"),
+         assignment.symbol->mName, valueString, assignment.property->mName);
+   else
+      mCommand += wxString::Format(wxT("(setf %s %s)\n"),
+         assignment.symbol->mName, valueString);
 }
--- src/effects/nyquist/NyquistFormatting.h
+++ src/effects/nyquist/NyquistFormatting.h
@@ -14,6 +14,12 @@
 #define __AUDACITY_EFFECT_NYQUIST_FORMATTING__
 
 #include "FileNames.h"
+#include <iterator>
+#include <type_traits>
+#include <variant>
+
+// Make sure to use the same typedefs as in xlisp for its data types
+#include "../../../lib-src/libnyquist/nyquist/xlisp/xlisp.h"
 
 namespace NyquistFormatting {
 
@@ -29,6 +35,154 @@ double GetCtrlValue(const wxString &s);
  If the path names only a directory, also append "/untitled" plus extension
  */
 void resolveFilePath(wxString & path, FileExtension extension = {});
+
+struct Value;
+
+//! A span of Values
+struct Multiple {
+   const Value *mBegin{};
+   const size_t mSize{};
+
+   Multiple() = default;
+   Multiple(const Value *begin, size_t size)
+      : mBegin{ begin }, mSize{ size }
+   {}
+   //! @pre end >= begin
+   inline Multiple(const Value *begin, const Value *end);
+
+   //! Construct from a contiguous container of const Value
+   template<typename Container,
+      // Deduced only for concept check
+      typename Iterator = decltype(std::declval<const Container>().begin()),
+      typename Traits = std::iterator_traits<Iterator>,
+      typename = std::enable_if_t<
+         std::is_same_v<const Value &, typename Traits::reference> &&
+         // C++20 contiguous_iterator_tag
+         std::is_base_of_v<std::random_access_iterator_tag,
+            typename Traits::iterator_category>
+      >
+   >
+   Multiple(const Container &c)
+      : Multiple{ c.size() ? &*c.begin() : nullptr, c.size() } {}
+};
+
+//! Can construct implicitly from a std::vector or initializer_list
+struct List : Multiple { using Multiple::Multiple; };
+
+// Less often used than List and requires explicit construction
+struct Vector : Multiple {
+   template<typename Container>
+   explicit Vector(const Container &c) : Multiple{ c } {}
+};
+
+struct Eval {
+   explicit Eval(wxString form) : form{ form } {}
+   explicit inline Eval(const Value& value);
+   wxString form;
+};
+
+//! Alternatives that can be formatted for the Lisp reader
+using ValueBase = std::variant<
+   const std::monostate, // to make a symbol unbound
+   const nullptr_t, // nil, distinct from unbound
+   const bool, // Lisp t or nil
+   const char,
+   const FIXTYPE,
+   const FLOTYPE,
+   const char *const,
+   const List, // construct list value as (list e1 e1 ...)
+   const Vector, // construct vector value as (vector e1 e1 ...)
+   const Eval  // just copy a string that is already formatted as a Lisp form
+>;
+
+struct Value : ValueBase {
+   using ValueBase::ValueBase;
+
+   //! Construct a value or a nil
+   template<typename T>
+   Value(std::optional<T> opt)
+      : ValueBase(opt.has_value()
+         ? ValueBase(*opt) : ValueBase(nullptr)
+      )
+   {}
+
+   //! Can also construct a nil directly from std::nullopt
+   Value(std::nullopt_t) : ValueBase{ nullptr } {}
+
+   //! Allow implicit conversion
+   Value(size_t size) : Value{ FIXTYPE(size) } {}
+
+   //! Convert to a string that the XLisp reader understands
+   wxString Format() const;
+};
+
+Eval::Eval(const Value &value) : Eval{ value.Format() } {}
+
+Multiple::Multiple(const Value *begin, const Value *end)
+   : Multiple{ begin, (size_t)(end - begin) }
+{
+   assert(end >= begin);
+}
+
+//! Make range-for work with Multiple
+inline const Value *begin(Multiple multiple) { return multiple.mBegin; }
+inline const Value *end(Multiple multiple)
+   { return multiple.mBegin + multiple.mSize; }
+
+//! Names a Lisp symbol
+struct Symbol {
+   Symbol(const wxString &name);
+   Symbol(const char *name) : Symbol{ wxString{name} } {}
+   Symbol(const wxChar *name) : Symbol{ wxString{name} } {}
+   const wxString mName;
+};
+
+struct Assignment {
+   //! Generate a setf form
+   Assignment(const Symbol &symbol, Value value)
+      : value{ value }, symbol{ &symbol }, property{ nullptr }
+   {}
+   //! Generate a putprop form
+   Assignment(const Symbol &symbol, Value value,
+      const Symbol &property
+   )  : value{ value }, symbol{ &symbol }, property{ &property }
+   {}
+
+   Assignment(Symbol&&, Value) = delete;
+   Assignment(Symbol&&, Value, Symbol&&) = delete;
+
+   const Value value;
+   const Symbol *symbol;
+   const Symbol *property;
+};
+
+//! Accumulates assignments to be made to Lisp variables and symbol properties
+class Assignments {
+public:
+   using Arguments = std::initializer_list<Assignment>;
+
+   Assignments() = default;
+   Assignments(Arguments list);
+
+   //! Accumulate one assignment to a variable
+   void Append(Assignment assignment);
+
+   //! Accumulate many assignments
+   void Append(Arguments list)
+      { mCommand += Assignments{ list }.Move(); }
+
+   //! Access the accumulated string that Lisp can interpret as assignments
+   const wxString &Get() const { return mCommand; }
+   //! Move the string out
+   wxString Move() && {
+      wxString result;
+      result.swap(mCommand);
+      return result;
+   }
+   operator wxString() && { return std::move(*this).Move(); }
+private:
+   wxString mCommand;
+};
 
 }
 #endif
