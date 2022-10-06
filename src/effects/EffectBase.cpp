audacity/audacity

commit 35581fa4b81753303a45b9229d89d8f2138c76e8

Lower the responsibility to make the instance into dialog factory...
... So if the dialog is connected to a realtime effect state, it can hold a
shared pointer to that state's instance; and each time play starts, the state
will re-use that instance rather than making a new one.

This means: if you open the old non-modal dialog for an effect, then you can
play, stop, and play again (and again and...), and whenever you play you can
adjust sliders with the correct results.

The sidebar dialogs are not yet non-modal but this will be a requirement for
them too when they are.

Connecting the effect dialog to the same instance that is doing the realtime
processing will also be necessary to fix output metering of effects.

Note that some steps of InitializeIntance() should not be done for the sidebar
dialog, and the constructor knows the difference because a state is given to
the constructor.  Likewise in CleanupRealtime().

src/effects/EffectBase.cpp
@@ -157,10 +157,8 @@ bool EffectBase::DoEffect(EffectSettings &settings, double projectRate,
 #endif
    CountWaveTracks();
 
-   // Note: Init may read parameters from preferences
-   auto pInstance = MakeInstance();
-   if (!(pInstance && pInstance->Init()))
-      return false;
+   // Allow the dialog factory to fill this in, but it might not
+   std::shared_ptr<EffectInstance> pInstance;
 
    // Prompting will be bypassed when applying an effect that has already
    // been configured, e.g. repeating the last effect on a different selection.
@@ -170,10 +168,23 @@ bool EffectBase::DoEffect(EffectSettings &settings, double projectRate,
       if (!ShowHostInterface(
          *pParent, dialogFactory, pInstance, *pAccess, IsBatchProcessing() ) )
          return false;
+      else if (!pInstance)
+         return false;
       else
          // Retrieve again after the dialog modified settings
          settings = pAccess->Get();
    }
+
+   if (!pInstance) {
+      // Path that skipped the dialog factory -- effect may be non-interactive
+      // or this is batch mode processing or repeat of last effect with stored
+      // settings.
+      pInstance = MakeInstance();
+      // Note: Init may read parameters from preferences
+      if (!pInstance || !pInstance->Init())
+         return false;
+   }
+
 
    // If the dialog was shown, then it has been closed without errors or
    // cancellation, and any change of duration has been saved in the config file

src/effects/EffectManager.cpp
@@ -333,7 +333,7 @@ bool EffectManager::PromptUser(
 {
    bool result = false;
    if (auto effect = GetEffect(ID)) {
-      auto pInstance = effect->MakeInstance(); // short-lived object
+      std::shared_ptr<EffectInstance> pInstance;
       //! Show the effect dialog, only so that the user can choose settings,
       //! for instance to define a macro.
       if (const auto pSettings = GetDefaultSettings(ID))

src/EffectPlugin.h
@@ -29,6 +29,10 @@ class EffectPlugin;
 
 //! Type of function that creates a dialog for an effect
 /*! The dialog may be modal or non-modal */
+   /*
+    @param[out] pInstance may construct
+    (and then must call Init() with success), or leave null for failure
+    */
 using EffectDialogFactory = std::function< wxDialog* (
    wxWindow &parent, EffectPlugin &, EffectUIClientInterface &,
    std::shared_ptr<EffectInstance> &,

src/RealtimeEffectPanel.cpp
@@ -424,8 +424,7 @@ namespace
          auto initialSettings = access->Get();
          auto cleanup = EffectManager::Get().SetBatchProcessing(ID);
 
-         std::shared_ptr<EffectInstance> pInstance =
-            effectPlugin->MakeInstance(); // short-lived object
+         std::shared_ptr<EffectInstance> pInstance;
 
          // Like the call in EffectManager::PromptUser, but access causes
          // the necessary inter-thread communication of settings changes

src/effects/EffectUI.cpp
@@ -211,9 +211,6 @@ EffectUIHost::EffectUIHost(wxWindow *parent,
                    wxDEFAULT_DIALOG_STYLE | wxRESIZE_BORDER | wxMINIMIZE_BOX | wxMAXIMIZE_BOX)
 , mEffectUIHost{ effect }
 , mClient{ client }
-// Grab a pointer to the instance object,
-// extending its lifetime while this remains:
-, mpInstance{ pInstance }
 // Grab a pointer to the access object,
 // extending its lifetime while this remains:
 , mpGivenAccess{ access.shared_from_this() }
@@ -222,7 +219,10 @@ EffectUIHost::EffectUIHost(wxWindow *parent,
 , mProject{ project }
 , mParent{ parent }
 , mSupportsRealtime{ mEffectUIHost.GetDefinition().SupportsRealtime() }
+, mpInstance{ InitializeInstance() }
 {
+   // Assign the out parameter
+   pInstance = mpInstance;
 #if defined(__WXMAC__)
    // Make sure the effect window actually floats above the main window
    [ [((NSView *)GetHandle()) window] setLevel:NSFloatingWindowLevel];
@@ -1217,9 +1217,13 @@ void EffectUIHost::LoadUserPresets()
    return;
 }
 
-void EffectUIHost::InitializeRealtime()
+std::shared_ptr<EffectInstance> EffectUIHost::InitializeInstance()
 {
-   {
+   // We are still constructing and the return initializes a const member
+   std::shared_ptr<EffectInstance> result;
+
+   bool priorState = (mpState != nullptr);
+   if (!priorState) {
       auto gAudioIO = AudioIO::Get();
       mDisableTransport = !gAudioIO->IsAvailable(mProject);
       mPlaying = gAudioIO->IsStreamActive(); // not exactly right, but will suffice
@@ -1227,42 +1231,53 @@ void EffectUIHost::InitializeRealtime()
    }
 
    if (mSupportsRealtime && !mInitialized) {
-      mpState =
-         AudioIO::Get()->AddState(mProject, nullptr, GetID(mEffectUIHost));
+      if (!priorState)
+         mpState =
+            AudioIO::Get()->AddState(mProject, nullptr, GetID(mEffectUIHost));
       if (mpState) {
+         // Find the right instance to connect to the dialog
+         if (!result) {
+            result = mpState->GetInstance();
+            if (result && !result->Init())
+               result.reset();
+         }
+
          mpAccess2 = mpState->GetAccess();
          if (!(mpAccess2->IsSameAs(*mpAccess)))
             // Decorate the given access object
             mpAccess = std::make_shared<EffectSettingsAccessTee>(
                *mpAccess, mpAccess2);
       }
-      /*
-      ProjectHistory::Get(mProject).PushState(
-         XO("Added %s effect").Format(mpState->GetEffect()->GetName()),
-         XO("Added Effect"),
-         UndoPush::NONE
-      );
-       */
-      mSubscription = AudioIO::Get()->Subscribe([this](AudioIOEvent event){
-         switch (event.type) {
-         case AudioIOEvent::PLAYBACK:
-            OnPlayback(event); break;
-         case AudioIOEvent::CAPTURE:
-            OnCapture(event); break;
-         default:
-            break;
-         }
-      });
+      if (!priorState) {
+         mSubscription = AudioIO::Get()->Subscribe([this](AudioIOEvent event){
+            switch (event.type) {
+            case AudioIOEvent::PLAYBACK:
+               OnPlayback(event); break;
+            case AudioIOEvent::CAPTURE:
+               OnCapture(event); break;
+            default:
+               break;
+            }
+         });
+      }
       
       mInitialized = true;
    }
+   else {
+      result = mEffectUIHost.MakeInstance();
+      if (result && !result->Init())
+         result.reset();
+   }
+   
+   return result;
 }
 
 void EffectUIHost::CleanupRealtime()
 {
+   bool noPriorState(mSubscription);
    mSubscription.Reset();
    if (mSupportsRealtime && mInitialized) {
-      if (mpState) {
+      if (noPriorState && mpState) {
          AudioIO::Get()->RemoveState(mProject, nullptr, mpState);
          mpState.reset();
       /*
@@ -1291,7 +1306,6 @@ wxDialog *EffectUI::DialogFactory( wxWindow &parent,
       return nullptr;
    Destroy_ptr<EffectUIHost> dlg{ safenew EffectUIHost{ &parent,
       *project, host, client, pInstance, access } };
-   dlg->InitializeRealtime();
    if (dlg->Initialize())
       // release() is safe because parent will own it
       return dlg.release();

src/effects/EffectUI.h
@@ -42,9 +42,14 @@ class EffectUIHost final : public wxDialogWrapper
 {
 public:
    // constructors and destructors
+   /*
+    @param[out] pInstance may construct
+    (and then must call Init() with success), or leave null for failure
+    */
    EffectUIHost(wxWindow *parent, AudacityProject &project,
       EffectPlugin &effect, EffectUIClientInterface &client,
-      std::shared_ptr<EffectInstance> &pInstance, EffectSettingsAccess &access,
+      std::shared_ptr<EffectInstance> &pInstance,
+      EffectSettingsAccess &access,
       const std::shared_ptr<RealtimeEffectState> &pPriorState = {});
    virtual ~EffectUIHost();
 
@@ -53,10 +58,11 @@ public:
 
    int ShowModal() override;
 
-   void InitializeRealtime();
    bool Initialize();
 
 private:
+   std::shared_ptr<EffectInstance> InitializeInstance();
+
    wxPanel *BuildButtonBar( wxWindow *parent );
 
    void OnInitDialog(wxInitDialogEvent & evt);
@@ -97,8 +103,6 @@ private:
    wxWindow *const mParent;
    EffectPlugin &mEffectUIHost;
    EffectUIClientInterface &mClient;
-   //! @invariant not null
-   const std::shared_ptr<EffectInstance> mpInstance;
    //! @invariant not null
    const EffectPlugin::EffectSettingsAccessPtr mpGivenAccess;
    EffectPlugin::EffectSettingsAccessPtr mpAccess;
@@ -144,6 +148,8 @@ private:
    // Used only in an assertion
    bool mClosed{ false };
 #endif
+
+   const std::shared_ptr<EffectInstance> mpInstance;
 
    DECLARE_EVENT_TABLE()
 };

src/effects/NoiseReduction.cpp
@@ -448,6 +448,11 @@ int EffectNoiseReduction::ShowHostInterface(
    std::shared_ptr<EffectInstance> &pInstance, EffectSettingsAccess &access,
    bool forceModal)
 {
+   // Assign the out parameter
+   pInstance = MakeInstance();
+   if (pInstance && !pInstance->Init())
+      pInstance.reset();
+
    // to do: use forceModal correctly
 
    // Doesn't use the factory but substitutes its own dialog

src/effects/NoiseRemoval.cpp
@@ -164,6 +164,11 @@ int EffectNoiseRemoval::ShowHostInterface(
    std::shared_ptr<EffectInstance> &pInstance, EffectSettingsAccess &access,
    bool forceModal )
 {
+   // Assign the out parameter
+   pInstance = MakeInstance();
+   if (pInstance && !pInstance->Init())
+      pInstance.reset();
+
    // to do: use forceModal correctly
    NoiseRemovalDialog dlog(this, access, &parent);
    dlog.mSensitivity = mSensitivity;

commit e4fb8fa56f6922c42f86e6c52bfe5d0e9b531653

Fix crash when editing macro parameters of Amplify and others...
... Commit 35581fa4 caused calls to Effect::Init that had not happened before.
Some of these overrides assumed the setting of EffectBase::mTracks was done
in DoEffect and got null pointer crashes.

Fix this by supplying an empty TrackList.

Also clean up mTracks in the effect object after DoEffect to avoid a dangling
pointer.

src/effects/EffectBase.cpp
@@ -73,7 +73,10 @@ bool EffectBase::DoEffect(EffectSettings &settings, double projectRate,
 
    mFactory = factory;
    mProjectRate = projectRate;
-   mTracks = list;
+
+   SetTracks(list);
+   // Don't hold a dangling pointer when done
+   Finally Do([&]{ SetTracks(nullptr); });
 
    // This is for performance purposes only, no additional recovery implied
    auto &pProject = *const_cast<AudacityProject*>(FindProject()); // how to remove this const_cast?

src/effects/EffectBase.h
@@ -35,6 +35,8 @@ public:
    EffectBase();
    ~EffectBase() override;
 
+   void SetTracks(TrackList *pTracks) { mTracks = pTracks; }
+
 protected:
    // The EffectBase class fully implements the Preview method for you.
    // Only override it if you need to do preprocessing or cleanup.

src/effects/EffectManager.cpp
@@ -33,6 +33,7 @@ effects.
 #include "../commands/CommandContext.h"
 #include "../commands/AudacityCommand.h"
 #include "PluginManager.h"
+#include "Track.h"
 
 
 /*******************************************************************************
@@ -333,6 +334,18 @@ bool EffectManager::PromptUser(
 {
    bool result = false;
    if (auto effect = GetEffect(ID)) {
+
+      auto empty = TrackList::Create(nullptr);
+      auto pEffectBase = dynamic_cast<EffectBase*>(effect);
+      if (pEffectBase)
+         // This allows effects to call Init() safely
+         pEffectBase->SetTracks(empty.get());
+      Finally Do([&]{
+         // reverse the side-effect
+         if (pEffectBase)
+            pEffectBase->SetTracks(nullptr);
+      });
+
       std::shared_ptr<EffectInstance> pInstance;
       //! Show the effect dialog, only so that the user can choose settings,
       //! for instance to define a macro.