a27f55cff8ee743d88c8369a38c3bec3d5cfc07b
Paul Licameli <paul.licameli@audacityteam.org>
2022-10-30 10:54:44 -0400

    Remove dead code

--- src/export/MP3Prefs.cpp
+++ src/export/MP3Prefs.cpp
@@ -19,61 +19,6 @@
 
 namespace {
 
-#if 0
-////////////////////////////////////////////////////////////////////////////////
-
-#define ID_FFMPEG_FIND_BUTTON       7003
-#define ID_FFMPEG_DOWN_BUTTON       7004
-
-BEGIN_EVENT_TABLE(LibraryPrefs, PrefsPanel)
-   EVT_BUTTON(ID_FFMPEG_FIND_BUTTON, LibraryPrefs::OnFFmpegFindButton)
-   EVT_BUTTON(ID_FFMPEG_DOWN_BUTTON, LibraryPrefs::OnFFmpegDownButton)
-END_EVENT_TABLE()
-
-LibraryPrefs::LibraryPrefs(wxWindow * parent, wxWindowID winid)
-/* i18-hint: refers to optional plug-in software libraries */
-:   PrefsPanel(parent, winid, XO("Libraries"))
-{
-   Populate();
-}
-
-LibraryPrefs::~LibraryPrefs()
-{
-}
-
-ComponentInterfaceSymbol LibraryPrefs::GetSymbol() const
-{
-   return LIBRARY_PREFS_PLUGIN_SYMBOL;
-}
-
-TranslatableString LibraryPrefs::GetDescription() const
-{
-   return XO("Preferences for Library");
-}
-
-ManualPageID LibraryPrefs::HelpPageName()
-{
-   return "Libraries_Preferences";
-}
-
-/// Creates the dialog and its contents.
-void LibraryPrefs::Populate()
-{
-   //------------------------- Main section --------------------
-   // Now construct the GUI itself.
-   // Use 'eIsCreatingFromPrefs' so that the GUI is
-   // initialised with values from gPrefs.
-   ShuttleGui S(this, eIsCreatingFromPrefs);
-   PopulateOrExchange(S);
-   // ----------------------- End of main section --------------
-
-   // Set the MP3 Version string.
-   SetMP3VersionText();
-   SetFFmpegVersionText();
-}
-
-#endif
-
 void AddControls( ShuttleGui &S )
 {
    S.StartStatic(XO("LAME MP3 Export Library"));
