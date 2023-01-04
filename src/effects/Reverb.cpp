865513e3ac7876f35c92c7de6f96751dede751ee
Paul Licameli <paul.licameli@audacityteam.org>
2023-01-03 20:44:34 -0500

    Changes of settings of stacked built-in effects dirty the project...
    ... so that it will prompt for save when closed.
    
    This includes now BassTreble, Distortion, Phaser, Reverb, and Wahwah.
    
    EffectSettingChanged messages are published with default values -- in fact the
    only observer of the messages does not use them.

--- src/effects/Reverb.cpp
+++ src/effects/Reverb.cpp
@@ -603,6 +603,7 @@ bool EffectReverb::Validator::UpdateUI()
       m ## n ## T->SetValue(wxString::Format(wxT("%d"), evt.GetInt())); \
       mProcessingEvent = false; \
       ValidateUI(); \
+      Publish(EffectSettingChanged{}); \
    } \
    void EffectReverb::Validator::On ## n ## Text(wxCommandEvent & evt) \
    { \
@@ -611,6 +612,7 @@ bool EffectReverb::Validator::UpdateUI()
       m ## n ## S->SetValue(std::clamp<long>(evt.GetInt(), n.min, n.max)); \
       mProcessingEvent = false; \
       ValidateUI(); \
+      Publish(EffectSettingChanged{}); \
    }
 
 SpinSliderHandlers(RoomSize)
@@ -626,6 +628,7 @@ SpinSliderHandlers(StereoWidth)
 void EffectReverb::Validator::OnCheckbox(wxCommandEvent &evt)
 {
    ValidateUI();
+   Publish(EffectSettingChanged{});
 }
 
 #undef SpinSliderHandlers
