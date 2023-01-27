1d289f19799b042f415e2a9455a4a704db36010a
Michael Papadopoulos <micpap25@gmail.com>
2023-01-27 08:25:48 -0500

    Add intermediate tick marks to Beats & Measures Ruler

--- src/widgets/BeatsFormat.cpp
+++ src/widgets/BeatsFormat.cpp
@@ -34,14 +34,18 @@ void BeatsFormat::SetLabelString(
    wxString& s, double d, double minor, int mDigits, TickType tickType
 ) const
 {
+   if (d < 0) {
+      return;
+   }
+
+   double val = (mBpm * ((double)mTimeSigLower / 4) * d) / (60 * mTimeSigUpper);
+   int beat = round((val - floor(val)) * mTimeSigUpper);
+
    if (tickType == RulerFormat::t_major) {
-      if (d < 0) {
-         return;
-      }
-
-      double val = (mBpm * ((double)mTimeSigLower / 4) * d) / (60 * mTimeSigUpper);
-
       s.Printf(wxT("%d"), (int)round(val + 1));
+   }
+   else if (tickType == RulerFormat::t_minor) {
+      s.Printf(wxT("%d.%d"), (int)floor(val + 1), (int)beat);
    }
 }
 
