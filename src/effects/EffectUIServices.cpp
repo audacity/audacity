c8e983d89a42cf825f85996984e27ec74f0f4189
Paul Licameli <paul.licameli@audacityteam.org>
2023-01-31 14:35:47 -0500

    EffectUIServices::MessageBox, like in Effect, but static

--- src/effects/EffectUIServices.cpp
+++ src/effects/EffectUIServices.cpp
@@ -8,5 +8,18 @@
 
 **********************************************************************/
 #include "EffectUIServices.h"
+#include "EffectPlugin.h"
+#include "widgets/AudacityMessageBox.h"
 
 EffectUIServices::~EffectUIServices() = default;
+
+int EffectUIServices::MessageBox(const EffectPlugin &plugin,
+   const TranslatableString& message,
+   long style, const TranslatableString &titleStr)
+{
+   auto title = titleStr.empty()
+      ? plugin.GetName()
+      : XO("%s: %s").Format(plugin.GetName(), titleStr);
+   return AudacityMessageBox(message, title, style);
+}
+
