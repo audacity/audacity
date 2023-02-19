1ae3b4e12fa5caecef0a6ff3d8d4039d8ab0a051
Paul Licameli <paul.licameli@audacityteam.org>
2023-02-19 10:11:38 -0500

    Move LadspaEffectsModule to new files

--- src/effects/ladspa/LadspaEffect.h
+++ src/effects/ladspa/LadspaEffect.h
@@ -34,8 +34,6 @@
 //
 ///////////////////////////////////////////////////////////////////////////////
 
-class LadspaEffectMeter;
-
 class LadspaEffect final
    : public EffectWithSettings<LadspaEffectSettings, StatelessPerTrackEffect>
 {
@@ -140,51 +138,4 @@ private:
 
    int mLatencyPort{ -1 };
 };
-
-///////////////////////////////////////////////////////////////////////////////
-//
-// LadspaEffectsModule
-//
-///////////////////////////////////////////////////////////////////////////////
-
-class LadspaEffectsModule final : public PluginProvider
-{
-public:
-   LadspaEffectsModule();
-   virtual ~LadspaEffectsModule();
-
-   // ComponentInterface implementation
-
-   PluginPath GetPath() const override;
-   ComponentInterfaceSymbol GetSymbol() const override;
-   VendorSymbol GetVendor() const override;
-   wxString GetVersion() const override;
-   TranslatableString GetDescription() const override;
-
-   // PluginProvider implementation
-
-   bool Initialize() override;
-   void Terminate() override;
-   EffectFamilySymbol GetOptionalFamilySymbol() override;
-
-   const FileExtensions &GetFileExtensions() override;
-   FilePath InstallPath() override;
-
-   void AutoRegisterPlugins(PluginManagerInterface & pm) override;
-   PluginPaths FindModulePaths(PluginManagerInterface & pm) override;
-   unsigned DiscoverPluginsAtPath(
-      const PluginPath & path, TranslatableString &errMsg,
-      const RegistrationCallback &callback)
-         override;
-   
-   bool CheckPluginExist(const PluginPath& path) const override;
-
-   std::unique_ptr<ComponentInterface>
-      LoadPlugin(const PluginPath & path) override;
-
-   // LadspaEffectModule implementation
-
-   FilePaths GetSearchPaths();
-};
-
 #endif
