07ea06bb502e5373158277af0feda57405fe0571
Paul Licameli <paul.licameli@audacityteam.org>
2023-02-19 11:34:19 -0500

    Move LadspaEffectOptionsDialog to new files

--- src/effects/ladspa/LadspaEffect.cpp
+++ src/effects/ladspa/LadspaEffect.cpp
@@ -14,19 +14,13 @@
 \brief An Effect that calls up a LADSPA plug in, i.e. many possible
 effects from this one class.
 
-*//****************************************************************//**
-
-\class LadspaEffectDialog
-\brief Dialog used with Effect
-
 *//*******************************************************************/
 #include "LadspaEffect.h"       // This class's header file
 #include "LadspaEditor.h"
-#include "LadspaEffectOptionsDialog.h"
 #include "ConfigInterface.h"
+#include "LadspaEffectOptionsDialog.h"
 
 #include <wx/log.h>
-#include "ShuttleGui.h"
 
 // Don't use the template-generated MakeSettings(), which default-constructs
 // the structure.  Instead allocate a number of values chosen by the plug-in
@@ -76,78 +70,6 @@ auto LadspaEffect::MakeOutputs() const -> std::unique_ptr<EffectOutputs>
    auto result = std::make_unique<LadspaEffectOutputs>();
    result->controls.resize(mData->PortCount);
    return result;
-}
-
-BEGIN_EVENT_TABLE(LadspaEffectOptionsDialog, wxDialogWrapper)
-   EVT_BUTTON(wxID_OK, LadspaEffectOptionsDialog::OnOk)
-END_EVENT_TABLE()
-
-LadspaEffectOptionsDialog::LadspaEffectOptionsDialog(
-   const EffectDefinitionInterface &effect
-)  : wxDialogWrapper{ nullptr, wxID_ANY, XO("LADSPA Effect Options") }
-   , mEffect{ effect }
-   , mUseLatency{ LadspaInstance::LoadUseLatency(mEffect) }
-{
-   ShuttleGui S(this, eIsCreating);
-   PopulateOrExchange(S);
-}
-
-LadspaEffectOptionsDialog::~LadspaEffectOptionsDialog()
-{
-}
-
-void LadspaEffectOptionsDialog::PopulateOrExchange(ShuttleGui & S)
-{
-   S.SetBorder(5);
-   S.StartHorizontalLay(wxEXPAND, 1);
-   {
-      S.StartVerticalLay(false);
-      {
-         S.StartStatic(XO("Latency Compensation"));
-         {
-            S.AddVariableText( XO(
-"As part of their processing, some LADSPA effects must delay returning "
-"audio to Audacity. When not compensating for this delay, you will "
-"notice that small silences have been inserted into the audio. "
-"Enabling this option will provide that compensation, but it may "
-"not work for all LADSPA effects."),
-               false, 0, 650);
-
-            S.StartHorizontalLay(wxALIGN_LEFT);
-            {
-               S.TieCheckBox(XXO("Enable &compensation"),
-                             mUseLatency);
-            }
-            S.EndHorizontalLay();
-         }
-         S.EndStatic();
-      }
-      S.EndVerticalLay();
-   }
-   S.EndHorizontalLay();
-
-   S.AddStandardButtons();
-
-   Layout();
-   Fit();
-   Center();
-}
-
-void LadspaEffectOptionsDialog::OnOk(wxCommandEvent & WXUNUSED(evt))
-{
-   if (!Validate())
-   {
-      return;
-   }
-
-   ShuttleGui S(this, eIsGettingFromDialog);
-   // Note this call re-visits the controls, not to create them but to fetch
-   // the values, in this case mUseLatency
-   PopulateOrExchange(S);
-
-   LadspaInstance::SaveUseLatency(mEffect, mUseLatency);
-
-   EndModal(wxID_OK);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
