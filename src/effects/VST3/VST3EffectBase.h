f295854fd28561b9f87924fb1d5f9b1c0a6ec633
Paul Licameli <paul.licameli@audacityteam.org>
2023-02-22 12:06:37 -0500

    Replicate VST3Effect.* in VST3EffectBase.*

---
+++ src/effects/VST3/VST3EffectBase.h
@@ -0,0 +1,135 @@
+/**********************************************************************
+
+  Audacity: A Digital Audio Editor
+
+  @file VST3Effect.h
+
+  @author Vitaly Sverchinsky
+
+  @brief Part of Audacity VST3 module
+
+**********************************************************************/
+
+
+#pragma once
+
+#include <wx/wx.h>
+
+#include <public.sdk/source/vst/hosting/module.h>
+
+#include "effects/StatelessPerTrackEffect.h"
+
+namespace Steinberg
+{
+   namespace Vst
+   {
+      class IEditController;
+   }
+}
+
+class NumericTextCtrl;
+class VST3Instance;
+
+class VST3ParametersWindow;
+
+/**
+ * \brief Objects of this class connect Audacity with VST3 effects
+ */
+class VST3EffectBase : public PerTrackEffect
+{
+   friend class VST3PluginValidator;
+
+protected:
+   // Keep strong reference to a module; this because it has to be destroyed in the destructor of this class,
+   // otherwise the destruction of mEditController and mEffectComponent would trigger a memory fault.
+   std::shared_ptr<VST3::Hosting::Module> mModule;
+   const VST3::Hosting::ClassInfo mEffectClassInfo;
+
+   // Mutable cache fields computed once on demand
+   mutable bool mRescanFactoryPresets { true };
+   mutable RegistryPaths mFactoryPresetNames;
+   mutable std::vector<wxString> mFactoryPresetIDs;
+
+public:
+
+   static EffectFamilySymbol GetFamilySymbol();
+
+   VST3EffectBase(
+      std::shared_ptr<VST3::Hosting::Module> module,
+      VST3::Hosting::ClassInfo effectClassInfo);
+
+   VST3EffectBase(const VST3EffectBase&) = delete;
+   VST3EffectBase(VST3EffectBase&&) = delete;
+   VST3EffectBase& operator=(const VST3EffectBase&) = delete;
+   VST3EffectBase& operator=(VST3EffectBase&) = delete;
+   
+   ~VST3EffectBase() override;
+
+   PluginPath GetPath() const override;
+   ComponentInterfaceSymbol GetSymbol() const override;
+   VendorSymbol GetVendor() const override;
+   wxString GetVersion() const override;
+   TranslatableString GetDescription() const override;
+
+   EffectType GetType() const override;
+   EffectFamilySymbol GetFamily() const override;
+   bool IsInteractive() const override;
+   bool IsDefault() const override;
+   RealtimeSince RealtimeSupport() const override;
+   bool SupportsAutomation() const override;
+   bool SaveSettings(
+      const EffectSettings &settings, CommandParameters & parms) const override;
+   bool LoadSettings(
+      const CommandParameters & parms, EffectSettings &settings) const override;
+   OptionalMessage LoadUserPreset(
+      const RegistryPath & name, EffectSettings &settings) const override;
+   bool SaveUserPreset(
+      const RegistryPath & name, const EffectSettings &settings) const override;
+   RegistryPaths GetFactoryPresets() const override;
+   OptionalMessage LoadFactoryPreset(int id, EffectSettings &settings)
+      const override;
+
+   std::shared_ptr<EffectInstance> MakeInstance() const override;
+
+   bool CanExportPresets() const override;
+
+   bool HasOptions() const override;
+
+   EffectSettings MakeSettings() const override;
+   bool CopySettingsContents(const EffectSettings& src, EffectSettings& dst) const override;
+
+protected:
+   void LoadPreset(const wxString& id, EffectSettings& settings) const;
+};
+
+class VST3Effect final
+   : public StatelessEffectUIServices
+   , public VST3EffectBase
+{
+public:
+   ~VST3Effect() override;
+
+   using VST3EffectBase::VST3EffectBase;
+
+   int ShowClientInterface(const EffectPlugin &plugin, wxWindow &parent,
+      wxDialog &dialog, EffectEditor *pEditor, bool forceModal)
+   const override;
+
+   std::unique_ptr<EffectEditor> PopulateUI(const EffectPlugin &plugin,
+      ShuttleGui &S, EffectInstance &instance, EffectSettingsAccess &access,
+      const EffectOutputs *pOutputs) const override;
+
+   void ExportPresets(
+      const EffectPlugin &plugin, const EffectSettings &settings)
+   const override;
+   OptionalMessage ImportPresets(
+      const EffectPlugin &plugin, EffectSettings &settings) const override;
+
+   void ShowOptions(const EffectPlugin &plugin) const override;
+
+private:
+   //! Will never be called
+   virtual std::unique_ptr<EffectEditor> MakeEditor(
+      ShuttleGui & S, EffectInstance &instance, EffectSettingsAccess &access,
+      const EffectOutputs *pOutputs) const final;
+};
