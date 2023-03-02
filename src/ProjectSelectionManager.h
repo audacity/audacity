bab9bc1be9538d094ead6935059cc1fd13bba28f
Paul Licameli <paul.licameli@audacityteam.org>
2023-03-01 19:57:41 -0500

    ProjectSelectionManager's member functions, mostly private

--- src/ProjectSelectionManager.h
+++ src/ProjectSelectionManager.h
@@ -19,6 +19,12 @@ Paul Licameli split from ProjectManager.cpp
 class AudacityProject;
 struct ProjectNumericFormatsEvent;
 
+//! This object is useful mostly as an observer of others in the project
+/*!
+ It listens for changes of selection formats and snap-to choices, and reacts
+ by updating persistent preferences, and updating the time selection to be
+ consistent with those choices.
+ */
 class AUDACITY_DLL_API ProjectSelectionManager final
    : public ClientData::Base
 {
@@ -26,27 +32,26 @@ public:
    static ProjectSelectionManager &Get( AudacityProject &project );
    static const ProjectSelectionManager &Get( const AudacityProject &project );
 
-   explicit ProjectSelectionManager( AudacityProject &project );
-   ProjectSelectionManager( const ProjectSelectionManager & ) = delete;
+   explicit ProjectSelectionManager(AudacityProject &project);
+   ProjectSelectionManager(const ProjectSelectionManager &) = delete;
    ProjectSelectionManager &operator=(
-      const ProjectSelectionManager & ) = delete;
+      const ProjectSelectionManager &) = delete;
    ~ProjectSelectionManager() override;
 
-private:
-   void OnFormatsChanged(ProjectNumericFormatsEvent);
-
-public:
-   void SetSelectionFormat(const NumericFormatID & format);
-   void SetAudioTimeFormat(const NumericFormatID & format);
    void ModifySelection(double &start, double &end, bool done);
-   void SetFrequencySelectionFormatName(
-      const NumericFormatID & formatName);
-   void SetBandwidthSelectionFormatName(
-      const NumericFormatID & formatName);
    void ModifySpectralSelection(
       double &bottom, double &top, bool done);
 
 private:
+   void OnFormatsChanged(ProjectNumericFormatsEvent);
+
+   void SetSelectionFormat(const NumericFormatID & format);
+   void SetAudioTimeFormat(const NumericFormatID & format);
+   void SetFrequencySelectionFormatName(
+      const NumericFormatID & formatName);
+   void SetBandwidthSelectionFormatName(
+      const NumericFormatID & formatName);
+
    void SnapSelection();
 
    Observer::Subscription mFormatsSubscription;
