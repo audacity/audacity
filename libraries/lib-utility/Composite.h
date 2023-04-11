dd8c416bb4686065cc603b02c84764d600224bbb
Paul Licameli <paul.licameli@audacityteam.org>
2023-04-11 09:57:01 -0400

    Composite::Builder can add type checking to push_back ...
    ... But that can still be evaded if there is a cast up to the base class

--- libraries/lib-utility/Composite.h
+++ libraries/lib-utility/Composite.h
@@ -12,6 +12,8 @@
 #ifndef __AUDACITY_COMPOSITE__
 #define __AUDACITY_COMPOSITE__
 
+#include <cassert>
+#include <iterator>
 #include <type_traits>
 #include <utility>
 #include <vector>
@@ -62,6 +64,78 @@ public:
 
 protected:
    Items items;
+};
+
+//! You may specialize this template, defining ItemBuilder that converts
+//! other types to Base::value_type
+/*!
+ A trait lets the Builder and Extension templates have parameter packs for other
+ purposes.
+ enables_item_type_v may be redefined to return false for certain types, which
+ removes the overload resolution candidate for push_back taking those types.
+ */
+template<typename Base, typename Derived> struct Traits {
+   static constexpr auto ItemBuilder = std::move<typename Base::value_type>;
+   template<typename T> static constexpr auto enables_item_type_v = true;
+};
+
+namespace detail {
+template<bool Deprecate> struct This;
+template<> struct This<false> {};
+template<> struct [[deprecated(
+   "Composite::Builder specialization does not enable Base::value_type"
+)]] This<true> {};
+}
+
+//! Specialization of Traits can wrap the push_back of the base class
+//! with a type-erasing gate-keeper, which can also do other value
+//! transformations
+/*!
+ This class comes between Base and Derived in the inheritance graph
+ */
+template<typename Base, typename Derived, typename... BaseArgs>
+struct Builder : Base
+{
+   using BaseType = Base;
+
+   //! Define a push_back, which makes this work also with back_inserter
+   //! Traits can determine whether to enable the overloads
+   template<typename Arg> auto push_back(Arg &&arg)
+      -> std::enable_if_t<Traits<Base, Derived>
+            ::template enables_item_type_v<Arg>,
+         void
+      >
+   {
+      static constexpr auto ItemBuilder = Traits<Base, Derived>::ItemBuilder;
+      Base::push_back(ItemBuilder(std::forward<Arg>(arg)));
+   }
+   //! This non-template overload is also needed so overload resolution does
+   //! not select the private inherited function
+   void push_back(typename Base::value_type arg) {
+      constexpr auto enable = Traits<Base, Derived>
+         ::template enables_item_type_v<typename Base::value_type>;
+      // We can't sfinae this overload away when traits want to disable it, but
+      // we can make a deprecation
+      static const detail::This<!enable> deprecator;
+      if constexpr(!enable) {
+         assert(false);
+      }
+      else {
+         static constexpr auto ItemBuilder = Traits<Base, Derived>::ItemBuilder;
+         Base::push_back(ItemBuilder(move(arg)));
+      }
+   }
+
+   //! Variadic constructor
+   template<typename... Items>
+   Builder(BaseArgs... args, Items&&... items)
+      : Base{ std::forward<BaseArgs>(args)... }
+   {
+      (..., push_back(std::forward<Items>(items)));
+   }
+
+private:
+   using BaseType::push_back;
 };
 
 //! Extend Base with extra fields, in a second, protected base class
--- libraries/lib-utility/tests/CompositeTest.cpp
+++ libraries/lib-utility/tests/CompositeTest.cpp
@@ -12,6 +12,7 @@
 #include "Composite.h"
 #include "Callable.h"
 #include <algorithm>
+#include <array>
 #include <iterator>
 #include <numeric>
 
@@ -69,9 +70,18 @@ bool compareSequences(const Container1 &c1, const Container2 &c2)
 }
 }
 
-template<typename Container, typename... Args>
+template<typename Container, auto Maker = nullptr, typename... Args>
 void DoTest(Args ...args)
 {
+   auto Make = [](int value){
+      if constexpr (!bool(Maker))
+         return Component(value, Ignore{});
+      else
+         return Maker(value);
+   };
+
+   using ComponentType = decltype(Make(0));
+   
    // CompositeBase passes constructor arguments to its Component
    Container container{ args... };
    REQUIRE(0 == container);
@@ -84,15 +94,16 @@ void DoTest(Args ...args)
    iota(values.begin(), values.end(), 1);
 
    // Make some components
-   vector<unique_ptr<MyComponent>> components;
+   vector<ComponentType> components;
    // Not yet equal
    REQUIRE(!compareSequences(values, components));
 
    for (size_t ii = 1; ii <= N; ++ii)
-      components.push_back(make_unique<MyComponent>(ii));
+      components.push_back(Make(ii));
 
    // Equal values so far
-   REQUIRE(compareSequences(values, components));
+   if (!bool(Maker))
+      REQUIRE(compareSequences(values, components));
 
    // Composite works with push_back and back_inserter
    move(components.begin(), components.end(), back_inserter(container));
@@ -104,7 +115,7 @@ void DoTest(Args ...args)
    REQUIRE(!compareSequences(values, container));
 
    // Restore equality (and note, Component can take more arguments)
-   container.push_back(Component(N + 1, Ignore{}));
+   container.push_back(Make(N + 1));
    REQUIRE(compareSequences(values, container));
 }
 
@@ -113,6 +124,52 @@ TEST_CASE("Composite::Base")
    DoTest<MyCompositeBase>(0);
    // Also test the extra arguments of MyComponent
    DoTest<MyCompositeBase2>(0, Ignore{});
+}
+
+namespace {
+struct MyComponentEx : MyComponent{
+   // Scramble the given value!!
+   MyComponentEx(int value) : MyComponent{ -value } {}
+};
+inline bool operator== (int n, const std::unique_ptr<MyComponentEx> &p)
+{
+   return n == *p;
+}
+static auto Maker (int value){return std::make_unique<MyComponentEx>(value); };
+struct MyBuilder;
+}
+
+// But define a trait that negates it again to unscramble it!
+// This specialization must be in the global namespace and precede the complete
+// definition of Builder
+template<> struct Composite::Traits<MyCompositeBase, MyBuilder> {
+   struct ItemBuilderType {
+      auto operator () (std::unique_ptr<MyComponent> ptr) const {
+         return std::make_unique<MyComponentEx>(ptr->value); };
+      auto operator () (int value) const {
+         return std::make_unique<MyComponentEx>(-value); };
+   };
+   static constexpr auto ItemBuilder = ItemBuilderType{};
+   template<typename T> static constexpr auto enables_item_type_v = true;
+};
+
+namespace {
+struct MyBuilder : Composite::Builder<MyCompositeBase, MyBuilder, int> {
+   using Builder::Builder;
+};
+}
+
+TEST_CASE("Composite::Builder")
+{
+   std::array values{ 0, 1, 2, 3, 4 };
+
+   DoTest<MyBuilder, Maker>(0);
+   // Test compilation of the variadic constructor and the overload of
+   // ItemBuilderType::operator ()
+   // Remember first 0 is not an element
+   MyBuilder builder{ 0, 0, 1, 2, 3, 4 };
+
+   REQUIRE(compareSequences(values, builder));
 }
 
 TEST_CASE("Composite::Extension")
@@ -131,4 +188,3 @@ TEST_CASE("Composite::Extension specialized for void")
    using Container2 = Extension<MyCompositeBase2, void, int, Ignore>;
    DoTest<Container2>(0, Ignore{});
 }
-
