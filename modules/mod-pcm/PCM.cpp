f393ebdd870fece8a46a0e191f5b486e8cec2bcd
Vitaly Sverchinsky <sverchinsky@gmail.com>
2023-04-11 20:58:12 +0300

    Extract PCM import/export into `mod-pcm`

---
+++ modules/mod-pcm/PCM.cpp
@@ -0,0 +1,13 @@
+/**********************************************************************
+
+  Audacity: A Digital Audio Editor
+
+  PCM.cpp
+
+  Vitaly Sverchinsky
+
+**********************************************************************/
+
+#include "ModuleConstants.h"
+
+DEFINE_MODULE_ENTRIES
