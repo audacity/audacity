5a9d7950f5356651b1b7a48c3d77c9591f889df6
Vitaly Sverchinsky <sverchinsky@gmail.com>
2023-04-11 19:11:31 +0300

    Extract MP3 export into `mod-mp3`

---
+++ modules/mod-mp3/MP3.cpp
@@ -0,0 +1,13 @@
+/**********************************************************************
+
+  Audacity: A Digital Audio Editor
+
+  MP3.cpp
+
+  Vitaly Sverchinsky
+
+**********************************************************************/
+
+#include "ModuleConstants.h"
+
+DEFINE_MODULE_ENTRIES
