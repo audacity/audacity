6e67a24b27eb69e979e121adccb77a2c40d1c6c5
Paul Licameli <paul.licameli@audacityteam.org>
2023-04-16 15:52:11 -0400

    New namespace Tuple in lib-utility...
    ... The omission of these very useful things from the standard library seems
    rather surprising.
    
    One could easily multiply utilities but I won't yet take it far beyond immediate
    needs to simplify Track::TypeSwitch.

---
+++ libraries/lib-utility/Tuple.cpp
@@ -0,0 +1,12 @@
+/*  SPDX-License-Identifier: GPL-2.0-or-later */
+/**********************************************************************
+
+  Audacity: A Digital Audio Editor
+
+  @file Tuple.cpp
+
+  Paul Licameli
+
+**********************************************************************/
+#include "Tuple.h"
+
