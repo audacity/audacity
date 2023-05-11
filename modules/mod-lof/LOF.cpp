9f021cc0d75875d46aa186e306e67ed242b18554
Vitaly Sverchinsky <sverchinsky@gmail.com>
2023-05-11 14:18:27 +0300

    Extract LOF import into `mod-lof`

---
+++ modules/mod-lof/LOF.cpp
@@ -0,0 +1,13 @@
+/**********************************************************************
+
+  Audacity: A Digital Audio Editor
+
+  LOF.cpp
+
+  Vitaly Sverchinsky
+
+**********************************************************************/
+
+#include "ModuleConstants.h"
+
+DEFINE_MODULE_ENTRIES
