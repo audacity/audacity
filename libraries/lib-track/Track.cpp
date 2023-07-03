3df7be87a7999e48942569b579866d3447cdbaa1
Paul Licameli <paul.licameli@audacityteam.org>
2023-07-03 19:40:57 -0400

    Move Channel and related classes to lib-channel

--- libraries/lib-track/Track.cpp
+++ libraries/lib-track/Track.cpp
@@ -36,60 +36,6 @@ and TimeTrack.
 #pragma warning( disable : 4786 )
 #endif
 
-ChannelGroupInterval::~ChannelGroupInterval() = default;
-
-ChannelInterval::~ChannelInterval() = default;
-
-WideChannelGroupInterval::WideChannelGroupInterval(
-   const ChannelGroup &group, double start, double end
-)  : ChannelGroupInterval{ start, end }
-   , mNChannels{ group.NChannels() }
-{
-   assert(group.IsLeader());
-   assert(mNChannels >= 1); // Post of ChannelGroup::NChannels
-}
-
-WideChannelGroupInterval::~WideChannelGroupInterval() = default;
-
-Channel::~Channel() = default;
-
-int Channel::FindChannelIndex() const
-{
-   auto &group = DoGetChannelGroup();
-   int index = -1;
-   for (size_t ii = 0, nn = group.NChannels(); ii < nn; ++ii)
-      if (group.GetChannel(ii).get() == this) {
-         index = ii;
-         break;
-      }
-   // post of DoGetChannelGroup
-   assert(index >= 0);
-
-   // TODO wide wave tracks -- remove this stronger assertion
-   assert(index == 0);
-
-   return index;
-}
-
-const ChannelGroup &Channel::GetChannelGroup() const
-{
-   assert(FindChannelIndex() >= 0);
-   return DoGetChannelGroup();
-}
-
-ChannelGroup &Channel::GetChannelGroup()
-{
-   assert(FindChannelIndex() >= 0);
-   return DoGetChannelGroup();
-}
-
-size_t Channel::GetChannelIndex() const
-{
-   return FindChannelIndex();
-}
-
-ChannelGroup::~ChannelGroup() = default;
-
 Track::Track()
 {
    mIndex = 0;
