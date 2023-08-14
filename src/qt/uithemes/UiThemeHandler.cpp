2b19d01b75c59a2ee527e4861a6bb8df832c8589
Vitaly Sverchinsky <sverchinsky@gmail.com>
2023-08-14 14:57:53 +0300

    Qt project stucture imprvovements

--- src/qt/uithemes/UiThemeHandler.cpp
+++ src/qt/uithemes/UiThemeHandler.cpp
@@ -8,7 +8,12 @@ QStringList UiThemeHandler::themeFiles()
    QStringList themes;
    QStringList excludeFiles{ "Theme.qml", "UiTheme.qml" };
 
-   QDirIterator it(":/uithemes", { "*.qml" }, QDir::Files, QDirIterator::Subdirectories);
+   QDirIterator it(
+      QString(":%1/Audacity/UiThemes")
+         .arg(uithemes_QML_IMPORT_PREFIX),
+      { "*.qml" },
+      QDir::Files,
+      QDirIterator::Subdirectories);
    while (it.hasNext())
    {
       auto path = it.next();
