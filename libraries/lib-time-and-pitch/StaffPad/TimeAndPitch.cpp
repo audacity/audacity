5ad282fec8b9265427eb6cbebb90e3aba22cb61d
Paul Licameli <paul.licameli@audacityteam.org>
2023-09-24 06:46:38 -0400

    Fix uninitialized buffer; render of time-stretch exactly repeatable (cherry-picks 9c75e64)

--- libraries/lib-time-and-pitch/StaffPad/TimeAndPitch.cpp
+++ libraries/lib-time-and-pitch/StaffPad/TimeAndPitch.cpp
@@ -156,6 +156,7 @@ void TimeAndPitch::reset()
     d->outCircularBuffer[ch].reset();
   }
   d->normalizationBuffer.reset();
+  d->last_mag.zeroOut();
   d->last_phase.zeroOut();
   d->phase_accum.zeroOut();
   _outBufferWriteOffset = 0;
