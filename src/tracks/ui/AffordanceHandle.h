e1f42897d90996930d4c7507fe0666bac023b0c1
Vitaly Sverchinsky <sverchinsky@gmail.com>
2023-09-29 16:20:34 +0300

    Affordance move threshold

--- src/tracks/ui/AffordanceHandle.h
+++ src/tracks/ui/AffordanceHandle.h
@@ -14,7 +14,12 @@
 
 class AUDACITY_DLL_API AffordanceHandle : public TimeShiftHandle
 {
+    constexpr static double MoveThreshold { 5.0 };
+
     static HitTestPreview HitPreview(const AudacityProject*, bool unsafe, bool moving);
+
+    bool mMoving { false };
+    wxPoint mClickPosition { };
 public:
 
     void Enter(bool forward, AudacityProject* pProject) override;
@@ -23,6 +28,7 @@ public:
     AffordanceHandle(const std::shared_ptr<Track>& track);
 
     Result Click(const TrackPanelMouseEvent& evt, AudacityProject* pProject) override;
+    Result Drag(const TrackPanelMouseEvent& event, AudacityProject* pProject) override;
     Result Release(const TrackPanelMouseEvent& event, AudacityProject* pProject, wxWindow* pParent) override;
 
 protected:
