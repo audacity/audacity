e12a29928e355ca448714454ff522d8ecf236906
Matthieu Hodgkinson <saintmatthieu@gmail.com>
2023-11-01 10:52:49 +0100

    Bugfix: silence audio does not delete clips

--- libraries/lib-wave-track/WaveTrack.cpp
+++ libraries/lib-wave-track/WaveTrack.cpp
@@ -266,11 +266,11 @@ WaveTrack::IntervalHolder WaveTrack::Interval::GetStretchRenderedCopy(
    const std::function<void(double)>& reportProgress, const ChannelGroup& group,
    const SampleBlockFactoryPtr& factory, sampleFormat format)
 {
+   if (GetStretchRatio() == 1)
+      return std::make_shared<Interval>(group, mpClip, mpClip1);
+
    const auto dst = std::make_shared<Interval>(
       group, NChannels(), factory, mpClip->GetRate(), format);
-
-   if (GetStretchRatio() == 1)
-      return dst;
 
    const auto originalPlayStartTime = GetPlayStartTime();
    const auto originalPlayEndTime = GetPlayEndTime();
@@ -2352,7 +2352,8 @@ void WaveTrack::ApplyStretchRatio(
    auto clip = GetIntervalAtTime(startTime);
    while (clip && clip->GetPlayStartTime() < endTime)
    {
-      srcIntervals.push_back(clip);
+      if (clip->GetStretchRatio() != 1)
+         srcIntervals.push_back(clip);
       clip = GetNextInterval(*clip, PlaybackDirection::forward);
    }
 
