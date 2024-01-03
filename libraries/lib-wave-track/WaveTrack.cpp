70cd3873143989cf95517d855a2263baffddf6bb
Paul Licameli <paul.licameli@audacityteam.org>
2024-01-02 22:12:20 -0500

    Fix error in 65340fe2

--- libraries/lib-wave-track/WaveTrack.cpp
+++ libraries/lib-wave-track/WaveTrack.cpp
@@ -947,7 +947,9 @@ bool WaveTrack::LinkConsistencyFix(bool doFix)
       // Set the common channel group rate from the leader's rate
       if (mLegacyRate > 0)
       {
-         auto next = *TrackList::Channels(this).first.advance(1);
+         WaveTrack *next{};
+         if (linkType != LinkType::None)
+            next = *TrackList::Channels(this).first.advance(1);
          SetRate(mLegacyRate);
          mLegacyRate = 0;
          if (next)
