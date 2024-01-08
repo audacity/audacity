3f56db26a9df47660729541e1173c05f15df54f0
Paul Licameli <paul.licameli@audacityteam.org>
2024-01-07 20:10:54 -0500

    Connect WaveTrackMeter to the data stream

--- src/tracks/playabletrack/wavetrack/ui/WaveTrackMeter.h
+++ src/tracks/playabletrack/wavetrack/ui/WaveTrackMeter.h
@@ -10,6 +10,7 @@
 #ifndef __AUDACITY_WAVE_TRACK_METER__
 #define __AUDACITY_WAVE_TRACK_METER__
 
+#include "Meter.h"
 #include "TrackAttachment.h"
 #include <memory>
 
@@ -18,16 +19,24 @@ class wxDC;
 struct wxRect;
 
 class WaveTrackMeter final
-   : public TrackAttachment
+   : public Meter
+   , public TrackAttachment
    , public std::enable_shared_from_this<WaveTrackMeter>
 {
 public:
    static WaveTrackMeter &Get(WaveTrack &track);
 
    WaveTrackMeter();
-   ~WaveTrackMeter();
+   ~WaveTrackMeter() override;
 
    void Draw(wxDC &dc, wxRect rect, bool selected);
+
+   void Update(unsigned numChannels,
+      unsigned long numFrames, const float *sampleData, bool interleaved)
+   override;
+   bool IsDisabled() const override;
+   void Clear() override;
+   void Reset(double sampleRate, bool resetClipping) override;
 
 private:
    struct Impl;
