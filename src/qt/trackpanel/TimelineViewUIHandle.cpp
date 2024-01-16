e36c02c02b530f74a1caf59efe05f074fa212288
Vitaly Sverchinsky <sverchinsky@gmail.com>
2024-01-16 03:16:36 +0300

    Core track panel UI classes

---
+++ src/qt/trackpanel/TimelineViewUIHandle.cpp
@@ -0,0 +1,23 @@
+#include "TimelineViewUIHandle.h"
+
+TimelineViewUIHandle::~TimelineViewUIHandle() = default;
+
+void TimelineViewUIHandle::OnMouseEnter(TimelineView&)
+{
+
+}
+
+void TimelineViewUIHandle::OnMouseMove(TimelineView&)
+{
+
+}
+
+void TimelineViewUIHandle::OnMousePress(TimelineView&)
+{
+
+}
+
+void TimelineViewUIHandle::OnMouseRelease(TimelineView&)
+{
+
+}
