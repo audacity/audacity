7dffa9baf5b501b1cc679016d8f714440ae368ef
Vitaly Sverchinsky <sverchinsky@gmail.com>
2024-01-16 03:20:37 +0300

    `WaveTrackView` added

---
+++ src/qt/trackpanel/items/WaveClipItem.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <QRect>
+
+#include "TimelineViewItem.h"
+#include "WaveTrack.h"
+
+class ZoomInfo;
+class ClipTimes;
+
+class WaveClipItem final : public TimelineViewItem
+{
+public:
+   static bool ClipDetailsVisible(const ClipTimes& clip, const ZoomInfo& zoomInfo, const QRect& viewRect);
+
+   WaveClipItem(WaveTrack& waveTrack);
+   ~WaveClipItem() override;
+
+   void SetInterval(std::shared_ptr<WaveTrack::Interval> interval);
+
+   void Paint(QQmlEngine& engine,
+              QPainter& painter,
+              const QRect& viewRect,
+              const TimelineContext& trackPanel) override;
+
+   std::unique_ptr<TimelineViewUIHandle> HitTest(const QPoint& at) const override;
+
+private:
+   bool mHovered{false};
+   WaveTrack& mWaveTrack;
+   std::shared_ptr<WaveTrack::Interval> mInterval;
+   QRect mClipRect;
+   QRect mHeaderRect;
+   QRect mMenuButtonRect;
+};
