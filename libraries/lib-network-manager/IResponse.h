2d9fdd0c97d2efc5e75d4bbd08adfa1d1bf91858
Dmitry Vedenko <dmitry@crsib.me>
2024-02-02 11:26:13 +0300

    Adds HTTP response codes

--- libraries/lib-network-manager/IResponse.h
+++ libraries/lib-network-manager/IResponse.h
@@ -44,6 +44,67 @@ enum class NetworkError
     HTTPError
 };
 
+namespace HttpCode
+{
+enum
+{
+   Continue = 100,
+   SwitchingProtocols = 101,
+   Processing = 102,
+   EarlyHints = 103,
+
+   OK = 200,
+   Created = 201,
+   Accepted = 202,
+   NonAuthoritativeInformation = 203,
+   NoContent = 204,
+   ResetContent = 205,
+   PartialContent = 206,
+
+   MultipleChoices = 300,
+   MovedPermanently = 301,
+   Found = 302,
+   SeeOther = 303,
+   NotModified = 304,
+   TemporaryRedirect = 307,
+   PermanentRedirect = 308,
+
+   BadRequest = 400,
+   Unauthorized = 401,
+   PaymentRequired = 402,
+   Forbidden = 403,
+   NotFound = 404,
+   MethodNotAllowed = 405,
+   NotAcceptable = 406,
+   RequestTimeout = 408,
+   Conflict = 409,
+   Gone = 410,
+   LengthRequired = 411,
+   PayloadTooLarge = 413,
+   URITooLong = 414,
+   UnsupportedMediaType = 415,
+   RangeNotSatisfiable = 416,
+   ExpectationFailed = 417,
+   MisdirectedRequest = 421,
+   UnprocessableEntity = 422,
+   Locked = 423,
+   FailedDependency = 424,
+   TooEarly = 425,
+   UpgradeRequired = 426,
+   PreconditionRequired = 428,
+   TooManyRequests = 429,
+   RequestHeaderFieldsTooLarge = 431,
+
+   InternalServerError = 500,
+   NotImplemented = 501,
+   BadGateway = 502,
+   ServiceUnavailable = 503,
+   GatewayTimeout = 504,
+   HTTPVersionNotSupported = 505,
+   NetworkAuthenticationRequired = 511
+};
+} // namespace HttpCode
+
 
 //! Interface, that provides access to the data from the HTTP response
 class NETWORK_MANAGER_API IResponse
