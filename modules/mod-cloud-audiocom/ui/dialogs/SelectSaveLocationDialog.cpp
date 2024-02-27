15d96167cc90c6a0dfbbb9af324d038627b8b008
Dmitry Vedenko <dmitry@crsib.me>
2024-02-27 14:07:58 +0300

    Resolves #6011

--- modules/mod-cloud-audiocom/ui/dialogs/SelectSaveLocationDialog.cpp
+++ modules/mod-cloud-audiocom/ui/dialogs/SelectSaveLocationDialog.cpp
@@ -24,6 +24,70 @@
 
 namespace audacity::cloud::audiocom::sync
 {
+namespace
+{
+/* i18n-hint: A title that is shown on the first project save that allows the
+ * user to select Cloud or local save. */
+const auto TitleText = XO("How would you like to save?");
+
+const auto CloudSaveTitle = XO("Save to the Cloud");
+
+const auto CloudSaveDescription = XO(
+   "Your project is backed up privately on audio.com. You can access your work from any device and collaborate on your project with others");
+
+const auto CloudSaveButton = XXO("&Save to Cloud");
+
+const auto LocalSaveTitle = XO("On your computer");
+
+const auto LocalSaveDescription = XO("Files are saved on your device");
+
+const auto LocalSaveButton = XXO("Save to &computer");
+
+
+constexpr auto leftPadding = 16;
+
+auto GetWrapWidth()
+{
+   return bin2c_SaveLocally_png->GetWidth() - leftPadding * 2;
+}
+
+std::unique_ptr<wxBoxSizer> SetupVerticalSizer(
+   wxStaticBitmap* image, wxStaticText* title, wxStaticText* description,
+   wxButton* button)
+{
+   const auto leftPaddingFlags = wxSizerFlags {}.Border(wxLEFT, leftPadding);
+
+   auto sizer = std::make_unique<wxBoxSizer>(wxVERTICAL);
+
+   sizer->Add(image);
+   sizer->AddSpacer(24);
+   sizer->Add(title, leftPaddingFlags);
+   sizer->AddSpacer(12);
+   sizer->Add(description, leftPaddingFlags);
+   sizer->AddSpacer(40);
+   sizer->AddStretchSpacer(1);
+   sizer->Add(button, leftPaddingFlags);
+
+   return sizer;
+}
+
+wxButton* CreateButton(wxWindow* parent, const wxFont& font, const TranslatableString& label)
+{
+   const auto transalatedLabel = label.Translation();
+
+   auto button =
+      safenew wxButton(parent, wxID_ANY, transalatedLabel);
+
+   button->SetFont(font);
+
+   const auto textSize = button->GetTextExtent(transalatedLabel);
+
+   button->SetMinSize({ textSize.x + 12 * 2, 32 });
+   
+   return button;
+}
+
+} // namespace
 SelectSaveLocationDialog::SelectSaveLocationDialog(wxWindow* parent)
     : wxDialogWrapper(
          parent, wxID_ANY, XO("Audacity"), wxDefaultPosition, { 442, -1 },
@@ -31,50 +95,42 @@ SelectSaveLocationDialog::SelectSaveLocationDialog(wxWindow* parent)
 {
    wxFont titleFont = GetFont();
    titleFont.SetWeight(wxFONTWEIGHT_BOLD);
-   titleFont.SetPointSize(titleFont.GetPointSize() * 2);
+   titleFont.SetPixelSize({ 0, 18 });
 
    wxFont descriptionFont = GetFont();
-   descriptionFont.SetPointSize(descriptionFont.GetPointSize() * 3 / 2);
+   descriptionFont.SetPixelSize({ 0, 12 });
 
-   auto title = safenew wxStaticText(
-      this, wxID_ANY, XO("How would you like to save?").Translation());
+   auto title = safenew wxStaticText(this, wxID_ANY, TitleText.Translation());
    title->SetFont(titleFont);
 
    auto saveToCloudImage =
       safenew wxStaticBitmap(this, wxID_ANY, *bin2c_SaveRemote_png);
 
-   auto saveToCloudTitle = safenew wxStaticText(
-      this, wxID_ANY, XO("Save to the &cloud").Translation());
+   auto saveToCloudTitle =
+      safenew wxStaticText(this, wxID_ANY, CloudSaveTitle.Translation());
    saveToCloudTitle->SetFont(titleFont);
 
-   auto saveToCloudDescription = safenew wxStaticText(
-      this, wxID_ANY,
-      XO("Your project is backed up privately on audio.com. You can access your work from any device and collaborate on your project with others")
-         .Translation());
+   auto saveToCloudDescription =
+      safenew wxStaticText(this, wxID_ANY, CloudSaveDescription.Translation());
    saveToCloudDescription->SetFont(descriptionFont);
-   saveToCloudDescription->Wrap(bin2c_SaveLocally_png->GetWidth() - 32);
+   saveToCloudDescription->Wrap(GetWrapWidth());
 
-   auto saveToCloudButton =
-      safenew wxButton(this, wxID_ANY, XXO("Save to &cloud").Translation());
-
-   saveToCloudButton->SetFont(descriptionFont);
+   auto saveToCloudButton = CreateButton(this, descriptionFont, CloudSaveButton);
 
    auto saveToComputerImage =
       safenew wxStaticBitmap(this, wxID_ANY, *bin2c_SaveLocally_png);
 
    auto saveToComputerTitle = safenew wxStaticText(
-      this, wxID_ANY, XO("On your c&omputer").Translation());
+      this, wxID_ANY, LocalSaveTitle.Translation());
    saveToComputerTitle->SetFont(titleFont);
 
    auto saveToComputerDescription = safenew wxStaticText(
-      this, wxID_ANY, XO("Files are saved on your device").Translation());
+      this, wxID_ANY, LocalSaveDescription.Translation());
    saveToComputerDescription->SetFont(descriptionFont);
-   saveToComputerDescription->Wrap(bin2c_SaveLocally_png->GetWidth() - 32);
+   saveToComputerDescription->Wrap(GetWrapWidth());
 
    auto saveToComputerButton =
-      safenew wxButton(this, wxID_ANY, XXO("Save to &computer").Translation());
-
-   saveToComputerButton->SetFont(descriptionFont);
+      CreateButton(this, descriptionFont, LocalSaveButton);
 
    auto rememberChoiceCheckbox = safenew wxCheckBox(
       this, wxID_ANY,
@@ -88,32 +144,18 @@ SelectSaveLocationDialog::SelectSaveLocationDialog(wxWindow* parent)
 
    auto topSizer = safenew wxBoxSizer(wxHORIZONTAL);
 
-   auto cloudSizer = safenew wxBoxSizer(wxVERTICAL);
+   auto cloudSizer = SetupVerticalSizer(
+      saveToCloudImage, saveToCloudTitle, saveToCloudDescription,
+      saveToCloudButton);
 
-   cloudSizer->Add(saveToCloudImage, wxSizerFlags {});
-   cloudSizer->Add(
-      saveToCloudTitle, wxSizerFlags {}.Border(wxTOP | wxBOTTOM | wxLEFT, 16));
-   cloudSizer->Add(saveToCloudDescription, wxSizerFlags {}.Border(wxLEFT, 16));
-   cloudSizer->AddStretchSpacer(1);
-   cloudSizer->Add(
-      saveToCloudButton, wxSizerFlags {}.Border(wxTOP | wxBOTTOM | wxLEFT, 16));
+   topSizer->Add(cloudSizer.release(), wxSizerFlags {}.Expand());
 
-   topSizer->Add(cloudSizer, wxSizerFlags {}.Expand());
+   auto computerSizer = SetupVerticalSizer(
+      saveToComputerImage, saveToComputerTitle, saveToComputerDescription,
+      saveToComputerButton);
 
-   auto computerSizer = safenew wxBoxSizer(wxVERTICAL);
-
-   computerSizer->Add(saveToComputerImage, wxSizerFlags {});
-   computerSizer->Add(
-      saveToComputerTitle,
-      wxSizerFlags {}.Border(wxTOP | wxBOTTOM | wxLEFT, 16));
-   computerSizer->Add(
-      saveToComputerDescription, wxSizerFlags {}.Border(wxLEFT, 16));
-   computerSizer->AddStretchSpacer(1);
-   computerSizer->Add(
-      saveToComputerButton,
-      wxSizerFlags {}.Border(wxTOP | wxBOTTOM | wxLEFT, 16));
-
-   topSizer->Add(computerSizer, wxSizerFlags {}.Expand().Border(wxLEFT, 8));
+   topSizer->Add(
+      computerSizer.release(), wxSizerFlags {}.Expand().Border(wxLEFT, 8));
 
    sizer->Add(topSizer, wxSizerFlags {}.Expand().Border(wxALL, 16));
 
