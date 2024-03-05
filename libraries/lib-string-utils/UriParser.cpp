dffa9981b4c04dd7cf107b9bd12af8b971779d82
Dmitry Vedenko <dmitry@crsib.me>
2024-03-05 19:05:12 +0300

    lib-string-utils extended
    * Adds `ToLower` and `ToUpper`
    * Adds `IsPrefixed`
    * Adds parser for URIs

---
+++ libraries/lib-string-utils/UriParser.cpp
@@ -0,0 +1,88 @@
+/*
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ * SPDX-FileName: UriParser.cpp
+ * SPDX-FileContributor: Dmitry Vedenko
+ */
+
+#include "UriParser.h"
+
+UriFields ParseUri(std::string_view uri) noexcept
+{
+   UriFields result;
+   auto schemeEnd = uri.find("://");
+   if (schemeEnd != std::string_view::npos)
+   {
+      result.Scheme = uri.substr(0, schemeEnd);
+      uri.remove_prefix(schemeEnd + 3);
+   }
+
+   auto fragmentStart = uri.find('#');
+   if (fragmentStart != std::string_view::npos)
+   {
+      result.Fragment = uri.substr(fragmentStart + 1);
+      uri.remove_suffix(uri.size() - fragmentStart);
+   }
+
+   auto queryStart = uri.find('?');
+   if (queryStart != std::string_view::npos)
+   {
+      result.Query = uri.substr(queryStart + 1);
+      uri.remove_suffix(uri.size() - queryStart);
+   }
+
+   auto pathStart = uri.find('/');
+   if (pathStart != std::string_view::npos)
+   {
+      result.Path = uri.substr(pathStart + 1);
+      uri.remove_suffix(uri.size() - pathStart);
+   }
+
+   auto userInfoEnd = uri.find('@');
+   if (userInfoEnd != std::string_view::npos)
+   {
+      result.UserInfo = uri.substr(0, userInfoEnd);
+      uri.remove_prefix(userInfoEnd + 1);
+   }
+
+   auto portStart = uri.find(':');
+   if (portStart != std::string_view::npos)
+   {
+      result.Port = uri.substr(portStart + 1);
+      uri.remove_suffix(uri.size() - portStart);
+   }
+
+   result.Host = uri;
+
+   return result;
+}
+
+QueryFields ParseUriQuery(std::string_view query, std::string_view delimiter) noexcept
+{
+   QueryFields result;
+
+   while (!query.empty())
+   {
+      const auto queryItemEnd = query.find(delimiter);
+      const auto queryItem = query.substr(0, queryItemEnd);
+
+      query.remove_prefix(
+         queryItemEnd != std::string_view::npos ? queryItemEnd + 1 :
+                                                  query.size());
+
+      const auto queryItemValueStart = queryItem.find('=');
+
+      const auto queryItemValue = queryItem.substr(
+         queryItemValueStart != std::string_view::npos ?
+            queryItemValueStart + 1 :
+            queryItem.size());
+
+      const auto queryItemKey = queryItem.substr(
+         0, queryItemValueStart != std::string_view::npos ?
+               queryItemValueStart :
+               queryItem.size());
+
+      result.emplace(queryItemKey, queryItemValue);
+   }
+
+   return result;
+}
---
+++ libraries/lib-string-utils/UriParser.h
@@ -0,0 +1,31 @@
+/*
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ * SPDX-FileName: UriParser.h
+ * SPDX-FileContributor: Dmitry Vedenko
+ */
+
+#pragma once
+
+#include <string_view>
+#include <unordered_map>
+
+struct UriFields final {
+    std::string_view Scheme;
+    std::string_view UserInfo;
+    std::string_view Host;
+    std::string_view Port;
+    std::string_view Path;
+    std::string_view Query;
+    std::string_view Fragment;
+};
+
+
+//! Parses URI and returns UriFields structure with parsed fields.
+//! {Scheme}://[{UserInfo}@]{Host}[:{Port}][/{Path}][?{Query}][#{Fragment}]
+STRING_UTILS_API UriFields ParseUri(std::string_view uri) noexcept;
+
+using QueryFields = std::unordered_map<std::string_view, std::string_view>;
+
+//! Parses URI query and returns QueryFields structure with parsed fields.
+STRING_UTILS_API QueryFields ParseUriQuery(
+   std::string_view query, std::string_view delimiter = "&") noexcept;
