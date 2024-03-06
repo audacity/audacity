f4679ba844cbed58f039a1ad4cbb96d054cbc96d
Dmitry Vedenko <dmitry@crsib.me>
2024-03-06 14:00:22 +0300

    Cloud images update to include export

--- modules/mod-cloud-audiocom/ui/images/CloudImages.hpp
+++ modules/mod-cloud-audiocom/ui/images/CloudImages.hpp
@@ -7,6 +7,8 @@ class wxBitmap;
 extern "C" void bin2c_init_CLOUDIMAGES_HPP(void);
 extern wxBitmap *bin2c_SaveLocally_png;
 extern wxBitmap *bin2c_SaveRemote_png;
+extern wxBitmap *bin2c_ExportLocally_png;
+extern wxBitmap *bin2c_ExportRemote_png;
 
 
 #endif /* CLOUDIMAGES_HPP */
