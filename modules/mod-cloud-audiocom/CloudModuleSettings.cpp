d1bc29f4071ecca298650883eeb38f4cb0c17b09
Dmitry Vedenko <dmitry@crsib.me>
2024-03-11 16:51:19 +0300

    Adds settings, related to the cloud sync

---
+++ modules/mod-cloud-audiocom/CloudModuleSettings.cpp
@@ -0,0 +1,35 @@
+/*  SPDX-License-Identifier: GPL-2.0-or-later */
+/*!********************************************************************
+
+  Audacity: A Digital Audio Editor
+
+  ModulePrefs.cpp
+
+  Dmitry Vedenko
+
+**********************************************************************/
+
+#include "CloudModuleSettings.h"
+
+namespace audacity::cloud::audiocom::sync
+{
+EnumSetting<CloudLocationMode> SaveLocationMode {
+   "/cloud/audiocom/SaveLocationMode",
+   EnumValueSymbols { L"ask", L"local", L"cloud" },
+   0,
+   { CloudLocationMode::Ask, CloudLocationMode::Local,
+     CloudLocationMode::Cloud }
+};
+
+EnumSetting<CloudLocationMode> ExportLocationMode {
+   "/cloud/audiocom/ExportLocationMode",
+   EnumValueSymbols { L"ask", L"local", L"cloud" },
+   0,
+   { CloudLocationMode::Ask, CloudLocationMode::Local,
+     CloudLocationMode::Cloud }
+};
+
+BoolSetting MixdownDialogShown {
+   "/cloud/audiocom/MixdownDialogShown", false
+};
+} // namespace audacity::cloud::audiocom::sync
---
+++ modules/mod-cloud-audiocom/CloudModuleSettings.h
@@ -0,0 +1,28 @@
+/*  SPDX-License-Identifier: GPL-2.0-or-later */
+/*!********************************************************************
+
+  Audacity: A Digital Audio Editor
+
+  ModulePrefs.h
+
+  Dmitry Vedenko
+
+**********************************************************************/
+#pragma once
+
+#include "Prefs.h"
+
+namespace audacity::cloud::audiocom::sync
+{
+enum class CloudLocationMode
+{
+   Ask,
+   Local,
+   Cloud,
+};
+
+extern EnumSetting<CloudLocationMode> SaveLocationMode;
+extern EnumSetting<CloudLocationMode> ExportLocationMode;
+
+extern BoolSetting MixdownDialogShown;
+} // namespace audacity::cloud::audiocom::sync
