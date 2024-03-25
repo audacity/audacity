8bcc7955e97c38dfd65f159b1e49b0a2919c9d2f
Dmitry Vedenko <dmitry@crsib.me>
2024-03-25 12:16:46 +0300

    Adds ISO 8601 date parser

--- libraries/lib-string-utils/DateTimeConversions.h
+++ libraries/lib-string-utils/DateTimeConversions.h
@@ -19,6 +19,8 @@ namespace audacity
 using SystemTime = std::chrono::system_clock::time_point;
 
 STRING_UTILS_API bool ParseRFC822Date (const std::string& dateString, SystemTime* time);
+STRING_UTILS_API bool ParseISO8601Date (const std::string& dateString, SystemTime* time);
+
 STRING_UTILS_API std::string SerializeRFC822Date (SystemTime timePoint);
 
 }
