02899953aecb833261bd23436b30302c6299d835
Paul Licameli <paul.licameli@audacityteam.org>
2024-03-25 12:51:03 -0400

    Commit EXPERIMENTAL_FULL_WASAPI

--- src/Experimental.cmake
+++ src/Experimental.cmake
@@ -13,10 +13,10 @@
   When the features become mainstream the options will then be retired.
 
   JKC: This file solves a problem of how to avoid forking the
-  code base when working on NEW features e.g:
+  code base when working on new features e.g:
     - Additional displays in Audacity
     - Modular architecture.
-  Add options in here for the NEW features, and make your code
+  Add options in here for the new features, and make your code
   conditional on them with #ifdef.
 
   For each name in the list, EXPERIMENTAL_{name} is the token to test with
@@ -25,16 +25,12 @@
   All the options are positive, i.e., when not commented out,
   they enable the feature.
 
-]]#
+]]
 
 set( EXPERIMENTAL_OPTIONS_LIST
    # ACH 08 Jan 2014
    # EQ accelerated code
    #EQ_SSE_THREADED
-
-   # LLL, 09 Nov 2013:
-   # Allow all WASAPI devices, not just loopback
-   FULL_WASAPI
 
    # JKC (effect by Norm C, 02 Oct 2013)
    SCIENCE_FILTERS
