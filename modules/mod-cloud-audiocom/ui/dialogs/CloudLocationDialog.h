fd6f05f20075b08e4c8633b807b3b13cc9641ed1
Dmitry Vedenko <dmitry@crsib.me>
2024-03-25 14:17:32 +0300

    Adds a dialog, that allows the user to select save or export location

---
+++ modules/mod-cloud-audiocom/ui/dialogs/CloudLocationDialog.h
@@ -0,0 +1,42 @@
+/*  SPDX-License-Identifier: GPL-2.0-or-later */
+/*!********************************************************************
+
+  Audacity: A Digital Audio Editor
+
+  CloudLocationDialog.h
+
+  Dmitry Vedenko
+
+**********************************************************************/
+#pragma once
+
+#include "wxPanelWrapper.h"
+
+namespace audacity::cloud::audiocom::sync
+{
+enum class LocationDialogResult
+{
+   Local,
+   Cloud,
+   Cancel,
+};
+
+enum class LocationDialogType
+{
+   Save,
+   Export,
+};
+
+class CloudLocationDialog final : private wxDialogWrapper
+{
+public:
+   CloudLocationDialog(wxWindow* parent, LocationDialogType type);
+   ~CloudLocationDialog() override;
+
+   LocationDialogResult ShowDialog();
+
+private:
+   LocationDialogType mType;
+   bool mDoNotShow;
+};
+} // namespace audacity::cloud::audiocom::sync
