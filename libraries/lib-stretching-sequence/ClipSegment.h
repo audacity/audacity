4a5bae4fc6c104121552a051f8a55c0cfe684d67
Matthieu Hodgkinson <saintmatthieu@gmail.com>
2024-03-25 21:04:42 +0100

    Do not rely on Observer/Publisher thread safety

--- libraries/lib-stretching-sequence/ClipSegment.h
+++ libraries/lib-stretching-sequence/ClipSegment.h
@@ -14,8 +14,8 @@
 #include "ClipTimeAndPitchSource.h"
 #include "Observer.h"
 #include "PlaybackDirection.h"
+#include <atomic>
 #include <memory>
-#include <mutex>
 
 class ClipInterface;
 class TimeAndPitchInterface;
@@ -41,7 +41,10 @@ private:
    const sampleCount mTotalNumSamplesToProduce;
    sampleCount mTotalNumSamplesProduced = 0;
    ClipTimeAndPitchSource mSource;
-   std::mutex mStretcherMutex;
+   bool mPreserveFormants;
+   int mCentShift;
+   std::atomic<bool> mUpdateFormantPreservation = false;
+   std::atomic<bool> mUpdateCentShift = false;
    // Careful that this guy is constructed after `mSource`, which it refers to
    // in its ctor.
    // todo(mhodgkinson) make this safe.
