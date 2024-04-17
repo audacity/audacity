184ca7fac6cb0a86cd654cc7057b757b50c17d77
Igor Korsukov <igor.korsukov@gmail.com>
2024-04-17 15:04:41 +0300

    updated app settings

--- au4/buildscripts/ci/packaging/Linux+BSD/mscore.1.preview.sh
+++ au4/buildscripts/ci/packaging/Linux+BSD/mscore.1.preview.sh
@@ -33,7 +33,7 @@ sed \
     -e 's!@MAN_MSCORE_UPPER@!MSCORE!g' \
     -e 's!@Variables_substituted_by_CMAKE_on_installation@!!g' \
     -e 's!@MUSESCORE_INSTALL_SUFFIX@!!g' \
-    -e 's!@MUSESCORE_NAME_VERSION@!MuseScore 4!g' \
+    -e 's!@MUSE_APP_NAME_VERSION@!MuseScore 4!g' \
     -e 's!@MAN_PORTABLE@!'"$portable"'!g' \
     -e 's!@PORTABLE_INSTALL_NAME@!MuseScorePortable!g' \
     -e 's!@CMAKE_INSTALL_PREFIX@!/usr!g' \
