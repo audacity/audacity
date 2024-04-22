6fdbf2e72f715a45f3b5a1b3d776d2cf359e47e5
Igor Korsukov <igor.korsukov@gmail.com>
2024-04-22 14:56:19 +0300

    added MacOs CI

--- au4/buildscripts/ci/macos/ci_build.cmake
+++ au4/buildscripts/ci/macos/ci_build.cmake
@@ -1,14 +1,15 @@
 
-message(STATUS "Build Linux")
+message(STATUS "Build MacOS")
 
 # Config
-set(BUILD_TOOLS "$ENV{HOME}/build_tools")
 set(ARTIFACTS_DIR "build.artifacts")
 set(ROOT_DIR ${CMAKE_CURRENT_LIST_DIR}/../../..)
 
 # Options
 set(BUILD_NUMBER "" CACHE STRING "Build number")
 set(BUILD_MODE "" CACHE STRING "Build mode")
+set(CRASH_REPORT_URL "" CACHE STRING "Crash report url")
+set(VST3_SDK_PATH "" CACHE STRING "Vst3 SDK path")
 
 if (NOT BUILD_NUMBER)
     file (STRINGS "${ARTIFACTS_DIR}/env/build_number.env" BUILD_NUMBER)
@@ -35,25 +36,29 @@ elseif(BUILD_MODE STREQUAL "stable_build")
 endif()
 
 
-# Build 
+set(BUILD_VST ON)
+if (NOT VST3_SDK_PATH)
+    message(WARNING "not set VST3_SDK_PATH, build VST module disabled")
+    set(BUILD_VST OFF)
+endif()
+
+# Build
 set(CONFIG
-    -DBUILD_TYPE=appimage
+    -DBUILD_TYPE=release_install
     -DBUILD_MODE=${APP_BUILD_MODE}
     -DBUILD_NUMBER=${BUILD_NUMBER}
-    -DINSTALL_SUFFIX=${APP_SUFFIX}
 )
+
+#$CRASH_REPORT_URL
+#$BUILD_VST
+#$VST3_SDK_PATH
 
 execute_process(
     COMMAND cmake ${CONFIG} -P ${ROOT_DIR}/ci_build.cmake
     RESULT_VARIABLE BUILD_RESULT
 )
 
-if (BUILD_RESULT GREATER 0) 
-    message(FATAL_ERROR "Failed build")
-endif()
-
-# bash ./buildscripts/ci/tools/make_release_channel_env.sh -c $AU4_BUILD_MODE
+# bash ./buildscripts/ci/tools/make_release_channel_env.sh -c $MUSE_APP_BUILD_MODE
 # bash ./buildscripts/ci/tools/make_version_env.sh $BUILD_NUMBER
-# bash ./buildscripts/ci/tools/make_revision_env.sh $AU4_REVISION
+# bash ./buildscripts/ci/tools/make_revision_env.sh $MUSESCORE_REVISION
 # bash ./buildscripts/ci/tools/make_branch_env.sh
-
