450813e7e54742ed18e998289752ddf2e4c801bb
Matthieu Hodgkinson <saintmatthieu@gmail.com>
2024-06-17 11:35:01 +0200

    Input is off by default

--- libraries/lib-dynamic-range-processor/DynamicRangeProcessorTypes.h
+++ libraries/lib-dynamic-range-processor/DynamicRangeProcessorTypes.h
@@ -68,7 +68,7 @@ constexpr double limiterLookaheadMsDefault = 1;
 constexpr double limiterReleaseMsDefault = 20;
 constexpr double limiterMaxLookaheadMs = 50;
 
-constexpr double showInputDefault = 1;
+constexpr double showInputDefault = 0;
 constexpr double showOutputDefault = 1;
 constexpr double showActualDefault = 1;
 constexpr double showTargetDefault = 0;
