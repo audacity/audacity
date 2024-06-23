9567c952b9ad498cd9fea82ad78ffec8532412da
Brad Smith <brad@comstyle.com>
2024-06-22 20:03:09 -0400

    Fixes OpenBSD/powerpc build
    Do not assume AltiVec when building on 32-bit PowerPC unless the compiler is
    targeting a newer processor. OpenBSD/powerpc targets the G3. Both Clang and
    GCC's AltiVec headers check for the compiler having AltiVec mode enabled.

--- lib-src/pffft/pfsimd_macros.h
+++ lib-src/pffft/pfsimd_macros.h
@@ -60,7 +60,7 @@
 /*
    Altivec support macros 
 */
-#if !defined(PFFFT_SIMD_DISABLE) && (defined(__ppc__) || defined(__ppc64__))
+#if !defined(PFFFT_SIMD_DISABLE) && ((defined(__ppc__) || defined(__ppc64__)) && defined(__ALTIVEC__))
 #include <altivec.h>
 typedef vector float v4sf;
 #  define SIMD_SZ 4
