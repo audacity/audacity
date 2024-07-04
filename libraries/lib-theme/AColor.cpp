12a42a648a76f1d2c66581a9e89fdf8db90909c6
Vitaly Sverchinsky <sverchinsky@gmail.com>
2024-07-04 16:27:08 +0300

    Attempt to fix black dots around track panel buttons

--- libraries/lib-theme/AColor.cpp
+++ libraries/lib-theme/AColor.cpp
@@ -328,10 +328,6 @@ void AColor::DrawHStretch(wxDC& dc, const wxRect& rect, wxBitmap& bitmap)
 
 void AColor::DrawFrame(wxDC& dc, const wxRect& r, wxBitmap& bitmap, int mid = 1)
 {
-   //sets a background to prevent black dots around rounded corners
-   dc.SetBrush(theTheme.Colour(clrLight));
-   dc.DrawRectangle(r);
-
    DrawNinePatch(dc, bitmap, r, mid);
 }
 
