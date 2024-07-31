247a876c541c34b193865f4bfa5d8aa9a5d9c0ad
Grzegorz Wojciechowski <g.wojciechowski@mu.se>
2024-07-31 17:02:21 +0200

    [AU4] Added ProjectPropertiesModel

---
+++ au4/src/project/view/projectpropertiesmodel.cpp
@@ -0,0 +1,201 @@
+/*
+ * SPDX-License-Identifier: GPL-3.0-only
+ * Audacity-CLA-applies
+ *
+ * Audacity
+ * A Digital Audio Editor
+ *
+ * Copyright (C) 2024 Audacity Limited
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+#include "projectpropertiesmodel.h"
+
+#include "translation.h"
+#include "log.h"
+
+using namespace muse;
+using namespace muse::modularity;
+using namespace au::project;
+
+ProjectPropertiesModel::ProjectPropertiesModel(QObject* parent)
+    : QAbstractListModel(parent)
+{
+    m_project = context()->currentProject();
+    // if (project) {
+    //     m_projectMetaInfo = project->metaInfo();
+    // }
+}
+
+void ProjectPropertiesModel::load()
+{
+    beginResetModel();
+
+    QVariantMap additionalProperties = m_projectMetaInfo.additionalTags;
+
+    m_properties = {
+        //! TODO AU4: add properties according to
+        //! /au4/src/project/types/projectmeta.h
+        // { WORK_TITLE_TAG, muse::qtrc("project", "Title"), m_projectMetaInfo.title, true },
+        // { AUDIO_COM_URL_TAG, muse::qtrc("project", "Audio.com URL"), m_projectMetaInfo.audioComUrl, true }
+    };
+
+    for (const QString& propertyName : additionalProperties.keys()) {
+        if (!isStandardTag(propertyName)) {
+            m_properties.append({ "", propertyName, additionalProperties[propertyName].toString() });
+        }
+    }
+
+    endResetModel();
+}
+
+QVariant ProjectPropertiesModel::data(const QModelIndex& index, int role) const
+{
+    if (!index.isValid() || index.row() >= rowCount()) {
+        return QVariant();
+    }
+
+    const Property& property = m_properties[index.row()];
+
+    switch (role) {
+    case PropertyName:
+        return property.name;
+    case PropertyValue:
+        return property.value;
+    case IsStandardProperty:
+        return property.isStandardProperty;
+    case IsMultiLineEdit:
+        return property.isMultiLineEdit;
+    }
+
+    return QVariant();
+}
+
+bool ProjectPropertiesModel::setData(const QModelIndex& index, const QVariant& value, int role)
+{
+    if (!index.isValid() || index.row() >= rowCount()) {
+        return false;
+    }
+
+    Property& property = m_properties[index.row()];
+
+    switch (role) {
+    case PropertyName:
+        if (!value.canConvert<QString>()) {
+            return false;
+        }
+        property.name = value.toString();
+        return true;
+    case PropertyValue:
+        if (!value.canConvert<QString>()) {
+            return false;
+        }
+        property.value = value.toString();
+        return true;
+    default:
+        return false;
+    }
+}
+
+int ProjectPropertiesModel::rowCount(const QModelIndex&) const
+{
+    return m_properties.size();
+}
+
+QHash<int, QByteArray> ProjectPropertiesModel::roleNames() const
+{
+    static const QHash<int, QByteArray> roles {
+        { PropertyName, "propertyName" },
+        { PropertyValue, "propertyValue" },
+        { IsStandardProperty, "isStandardProperty" },
+        { IsMultiLineEdit, "isMultiLineEdit" }
+    };
+
+    return roles;
+}
+
+QString ProjectPropertiesModel::filePath() const
+{
+    return m_project->path().toQString();
+}
+
+QString ProjectPropertiesModel::thumbnailUrl() const
+{
+    QString filePath = m_project->path().toQString();
+    int lastDotIndex = filePath.lastIndexOf('.');
+    if (lastDotIndex != -1 && lastDotIndex != 0) {
+        // Remove the current suffix
+        filePath = filePath.left(lastDotIndex);
+    }
+    filePath.append(".png");
+
+    return filePath;
+}
+
+QString ProjectPropertiesModel::version() const
+{
+    NOT_IMPLEMENTED;
+    return QString();
+}
+
+QString ProjectPropertiesModel::revision() const
+{
+    NOT_IMPLEMENTED;
+    return QString();
+}
+
+QString ProjectPropertiesModel::apiLevel() const
+{
+    NOT_IMPLEMENTED;
+    return QString();
+}
+
+void ProjectPropertiesModel::newProperty()
+{
+    int destinationIndex = m_properties.size();
+    beginInsertRows(QModelIndex(), destinationIndex, destinationIndex);
+
+    Property property = { "", "", "" };
+    m_properties.append(property);
+
+    endInsertRows();
+
+    emit propertyAdded(destinationIndex);
+}
+
+void ProjectPropertiesModel::deleteProperty(int index)
+{
+    if (index < 0 && index >= m_properties.size()) {
+        return;
+    }
+
+    beginRemoveRows(QModelIndex(), index, index);
+
+    m_properties.removeAt(index);
+
+    endRemoveRows();
+}
+
+void ProjectPropertiesModel::saveProperties()
+{
+    NOT_IMPLEMENTED;
+}
+
+void ProjectPropertiesModel::openFileLocation()
+{
+    Ret ret = interactive()->revealInFileBrowser(m_projectMetaInfo.filePath.toQString());
+
+    if (!ret) {
+        LOGE() << "Could not open folder: " << m_projectMetaInfo.filePath.toQString();
+    }
+}
---
+++ au4/src/project/view/projectpropertiesmodel.h
@@ -0,0 +1,84 @@
+/*
+ * SPDX-License-Identifier: GPL-3.0-only
+ * Audacity-CLA-applies
+ *
+ * Audacity
+ * A Digital Audio Editor
+ *
+ * Copyright (C) 2024 Audacity Limited
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "iinteractive.h"
+#include "context/iglobalcontext.h"
+#include "modularity/ioc.h"
+
+namespace au::project {
+class ProjectPropertiesModel : public QAbstractListModel
+{
+    Q_OBJECT
+
+    muse::Inject<context::IGlobalContext> context;
+    muse::Inject<muse::IInteractive> interactive;
+
+    Q_PROPERTY(QString filePath READ filePath CONSTANT)
+    Q_PROPERTY(QString thumbnailUrl READ thumbnailUrl CONSTANT)
+    Q_PROPERTY(QString version READ version CONSTANT)
+    Q_PROPERTY(QString revision READ revision CONSTANT)
+    Q_PROPERTY(QString apiLevel READ apiLevel CONSTANT)
+
+public:
+    explicit ProjectPropertiesModel(QObject* parent = nullptr);
+
+    QVariant data(const QModelIndex& index, int role) const override;
+    bool setData(const QModelIndex& index, const QVariant& value, int role) override;
+    int rowCount(const QModelIndex& parent = QModelIndex()) const override;
+    QHash<int, QByteArray> roleNames() const override;
+
+    QString filePath() const;
+    QString thumbnailUrl() const;
+    QString version() const;
+    QString revision() const;
+    QString apiLevel() const;
+
+    Q_INVOKABLE void load();
+    Q_INVOKABLE void newProperty();
+    Q_INVOKABLE void deleteProperty(int index);
+    Q_INVOKABLE void saveProperties();
+    Q_INVOKABLE void openFileLocation();
+
+signals:
+    void propertyAdded(int index);
+
+private:
+    enum Roles {
+        PropertyName = Qt::UserRole + 1,
+        PropertyValue,
+        IsStandardProperty,
+        IsMultiLineEdit
+    };
+
+    struct Property {
+        QString key, name, value;
+        bool isStandardProperty = false;
+        bool isMultiLineEdit = false;
+    };
+
+    project::ProjectMeta m_projectMetaInfo;
+    QList<Property> m_properties;
+    IAudacityProjectPtr m_project;
+};
+}
