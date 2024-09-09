ce4a725969880f83ded2bd959e6065bdac6e44de
Grzegorz Wojciechowski <grliszas14@gmail.com>
2024-09-09 08:54:46 +0200

    [AU4] Fixed recording

--- au4/src/au3wrap/internal/domaccessor.cpp
+++ au4/src/au3wrap/internal/domaccessor.cpp
@@ -1,10 +1,12 @@
 #include "domaccessor.h"
 
 #include "domau3types.h"
+#include "containers.h"
 
 #include "log.h"
 
 using namespace au::au3;
+using namespace muse;
 
 WaveTrack* DomAccessor::findWaveTrack(AudacityProject& prj, const TrackId& au3trackId)
 {
@@ -31,6 +33,7 @@ std::shared_ptr<WaveClip> DomAccessor::findWaveClip(WaveTrack* track, uint64_t a
     return nullptr;
 }
 
+// TODO: if you need this one, fix it first (indexes are not in use anymore)
 std::shared_ptr<WaveClip> DomAccessor::findWaveClip(AudacityProject& prj, const TrackId& au3trackId, size_t index)
 {
     WaveTrack* t = findWaveTrack(prj, au3trackId);
@@ -38,6 +41,38 @@ std::shared_ptr<WaveClip> DomAccessor::findWaveClip(AudacityProject& prj, const 
         return nullptr;
     }
     return findWaveClip(t, index);
+}
+
+size_t DomAccessor::findClipIndexById(const WaveTrack *track, const trackedit::ClipId &clipId)
+{
+    size_t index = 0;
+    for (const auto& interval : track->Intervals()) {
+        if (WaveClipID(interval.get()).id == clipId) {
+            return index;
+        }
+        index++;
+    }
+
+    return muse::nidx;
+}
+
+au::trackedit::ClipId DomAccessor::findClipIdByIndex(const WaveTrack *track, size_t clipIndex)
+{
+    auto it = std::next(track->Intervals().begin(), clipIndex);
+    if (it != track->Intervals().end()) {
+        return au::au3::WaveClipID((*it).get()).id;
+    }
+    return -1;
+}
+
+au::trackedit::ClipId DomAccessor::findMatchedClip(WaveTrack *track, const WaveTrack *originTrack, const trackedit::ClipId &originClipId)
+{
+    size_t idx = findClipIndexById(originTrack, originClipId);
+    if (idx == muse::nidx) {
+        return -1;
+    }
+
+    return DomAccessor::findClipIdByIndex(track, idx);
 }
 
 std::list<std::shared_ptr<WaveClip> > DomAccessor::waveClipsAsList(WaveTrack* track)
--- au4/src/au3wrap/internal/domaccessor.h
+++ au4/src/au3wrap/internal/domaccessor.h
@@ -6,6 +6,8 @@
 #include <memory>
 #include <list>
 
+#include "trackedit/trackedittypes.h"
+
 #include "libraries/lib-wave-track/WaveTrack.h"
 #include "libraries/lib-wave-track/WaveClip.h"
 
@@ -17,6 +19,9 @@ public:
     static WaveTrack* findWaveTrack(AudacityProject& prj, const TrackId& au3trackId);
     static std::shared_ptr<WaveClip> findWaveClip(WaveTrack* track, uint64_t au3ClipId);
     static std::shared_ptr<WaveClip> findWaveClip(AudacityProject& prj, const TrackId& au3trackId, size_t index);
+    static size_t findClipIndexById(const WaveTrack* track, const trackedit::ClipId& clipId);
+    static trackedit::ClipId findClipIdByIndex(const WaveTrack* track,  size_t clipIndex);
+    static trackedit::ClipId findMatchedClip(WaveTrack* track, const WaveTrack* originTrack, const trackedit::ClipId& originClipId);
     static std::list<std::shared_ptr<WaveClip> > waveClipsAsList(WaveTrack* track);
 };
 }
