823799261e0e952ab2e01fc710e111113ffd908c
Matthieu Hodgkinson <saintmatthieu@gmail.com>
2024-10-01 10:52:19 +0200

    Au3ProjectHistory clean-up

--- au4/src/trackedit/internal/au3/au3projecthistory.cpp
+++ au4/src/trackedit/internal/au3/au3projecthistory.cpp
@@ -13,44 +13,49 @@ using namespace au::trackedit;
 
 void au::trackedit::Au3ProjectHistory::init()
 {
-    auto project = reinterpret_cast<AudacityProject*>(globalContext()->currentProject()->au3ProjectPtr());
-    ::ProjectHistory::Get(*project).InitialState();
+    auto& project = projectRef();
+    ::ProjectHistory::Get(project).InitialState();
 }
 
 bool au::trackedit::Au3ProjectHistory::undoAvailable()
 {
-    auto project = reinterpret_cast<AudacityProject*>(globalContext()->currentProject()->au3ProjectPtr());
-    return ::ProjectHistory::Get(*project).UndoAvailable();
+    auto& project = projectRef();
+    return ::ProjectHistory::Get(project).UndoAvailable();
 }
 
 void au::trackedit::Au3ProjectHistory::undo()
 {
-    auto project = reinterpret_cast<AudacityProject*>(globalContext()->currentProject()->au3ProjectPtr());
-    auto& undoManager = UndoManager::Get(*project);
+    auto& project = projectRef();
+    auto& undoManager = UndoManager::Get(project);
     undoManager.Undo(
         [&]( const UndoStackElem& elem ){
-            ::ProjectHistory::Get(*project).PopState(elem.state);
-        });
+        ::ProjectHistory::Get(project).PopState(elem.state);
+    });
 }
 
 bool au::trackedit::Au3ProjectHistory::redoAvailable()
 {
-    auto project = reinterpret_cast<AudacityProject*>(globalContext()->currentProject()->au3ProjectPtr());
-    return ::ProjectHistory::Get(*project).RedoAvailable();
+    auto& project = projectRef();
+    return ::ProjectHistory::Get(project).RedoAvailable();
 }
 
 void au::trackedit::Au3ProjectHistory::redo()
 {
-    auto project = reinterpret_cast<AudacityProject*>(globalContext()->currentProject()->au3ProjectPtr());
-    auto& undoManager = UndoManager::Get(*project);
+    auto& project = projectRef();
+    auto& undoManager = UndoManager::Get(project);
     undoManager.Redo(
         [&]( const UndoStackElem& elem ){
-            ::ProjectHistory::Get(*project).PopState(elem.state);
+            ::ProjectHistory::Get(project).PopState(elem.state);
         });
 }
 
 void au::trackedit::Au3ProjectHistory::pushHistoryState(const std::string& longDescription, const std::string& shortDescription)
 {
-    auto project = reinterpret_cast<AudacityProject*>(globalContext()->currentProject()->au3ProjectPtr());
-    ::ProjectHistory::Get(*project).PushState(TranslatableString { longDescription, {} }, TranslatableString { shortDescription, {} });
+    auto& project = projectRef();
+    ::ProjectHistory::Get(project).PushState(TranslatableString { longDescription, {} }, TranslatableString { shortDescription, {} });
+}
+
+::AudacityProject& au::trackedit::Au3ProjectHistory::projectRef()
+{
+    return *reinterpret_cast<AudacityProject*>(globalContext()->currentProject()->au3ProjectPtr());
 }
