ee2901c79849fc3ac53c65d3a3101db8f44ed692
LWinterberg <87814144+LWinterberg@users.noreply.github.com>
2024-10-16 10:42:26 +0200

    delete unused manpage and manual generators

--- help/DownloadManual.cmake
+++
@@ -1,9 +0,0 @@
-message(STATUS
-   "${VERSION} -> ${DESTINATION}"
-)
-
-file(
-   DOWNLOAD
-   "https://github.com/audacity/audacity-manual/releases/download/v${VERSION}/audacity-manual-${VERSION}.tar.gz"
-   "${DESTINATION}/audacity-manual-${VERSION}.tar.gz"
-)
