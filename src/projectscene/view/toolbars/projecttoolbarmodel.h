4578689d444fe7c52e6fc3091700bd0c07523129
Eism <eismailzada@gmail.com>
2024-11-14 14:47:27 +0200

    added compact mode for projectToolBar

--- src/projectscene/view/toolbars/projecttoolbarmodel.h
+++ src/projectscene/view/toolbars/projecttoolbarmodel.h
@@ -13,9 +13,17 @@ class ProjectToolBarModel : public muse::uicomponents::AbstractToolBarModel
 {
     Q_OBJECT
 
+    Q_PROPERTY(bool isCompactMode READ isCompactMode WRITE setIsCompactMode CONSTANT FINAL)
+
     muse::Inject<au::context::IGlobalContext> context;
 
 public:
     Q_INVOKABLE void load() override;
+
+    bool isCompactMode() const;
+    void setIsCompactMode(bool isCompactMode);
+
+private:
+    bool m_isCompactMode = false;
 };
 }
