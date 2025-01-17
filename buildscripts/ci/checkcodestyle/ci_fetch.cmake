d82b2255def3bf7486925ca449204d95804f57dc
Eism <eismailzada@gmail.com>
2025-01-17 12:15:05 +0200

    fixed codestyle check

--- buildscripts/ci/checkcodestyle/ci_fetch.cmake
+++ buildscripts/ci/checkcodestyle/ci_fetch.cmake
@@ -1,6 +1,3 @@
-
-
-
 set(REMOTE_ROOT_URL https://raw.githubusercontent.com/musescore/framework_tmp/main/buildscripts/ci/checkcodestyle)
 set(LOCAL_ROOT_PATH ${CMAKE_CURRENT_LIST_DIR}/_deps)
 
@@ -12,3 +9,7 @@ foreach(FILE_PATH ${CI_FILES})
     file(DOWNLOAD ${REMOTE_ROOT_URL}/${FILE_PATH} ${LOCAL_ROOT_PATH}/${FILE_PATH} )
 endforeach()
 
+foreach(FILE_PATH ${CI_EXEC_FILES})
+    file(DOWNLOAD ${REMOTE_ROOT_URL}/${FILE_PATH} ${LOCAL_ROOT_PATH}/${FILE_PATH})
+    file(CHMOD ${LOCAL_ROOT_PATH}/${FILE_PATH} PERMISSIONS OWNER_EXECUTE GROUP_EXECUTE WORLD_EXECUTE)
+endforeach()
