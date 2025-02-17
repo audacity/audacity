09cc034c2bdda82e96d65420cb310d66129d1470
Igor <igor.korsukov@gmail.com>
2025-02-17 11:43:58 +0200

    update au3 codestyle: au3/libraries/lib-basic-ui

--- au3/libraries/lib-basic-ui/BasicUIPoint.h
+++ au3/libraries/lib-basic-ui/BasicUIPoint.h
@@ -12,17 +12,16 @@ Paul Licameli
 #define __AUDACITY_BASIC_UI_POINT__
 
 namespace BasicUI {
-
 //! A pair of screen coordinates, x increasing rightward, y downward
 /*! Origin is unspecified */
-struct Point{
-   Point() = default;
-   Point( int x, int y ) : x{x}, y{y} {}
+struct Point {
+    Point() = default;
+    Point(int x, int y)
+        : x{x}, y{y} {}
 
-   int x = -1;
-   int y = -1;
+    int x = -1;
+    int y = -1;
 };
-
 }
 
 #endif
