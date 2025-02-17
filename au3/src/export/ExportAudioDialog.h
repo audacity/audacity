17c1d8666cb1a5d96a4bf845296b54f21aa20e9c
Igor <igor.korsukov@gmail.com>
2025-02-17 11:00:01 +0200

    update au3 codestyle: src/export

--- au3/src/export/ExportAudioDialog.h
+++ au3/src/export/ExportAudioDialog.h
@@ -3,7 +3,7 @@
   Audacity: A Digital Audio Editor
 
   ExportAudioDialog.h
- 
+
   Vitaly Sverchinsky
 
 **********************************************************************/
@@ -34,108 +34,94 @@ class wxChoice;
 class wxRadioButton;
 class wxCheckBox;
 
-namespace MixerOptions
-{
+namespace MixerOptions {
 class Downmix;
 }
 
 class ExportAudioDialog final : public wxDialogWrapper
 {
-   ///\brief A private class used to store the information needed to do an export.
-   struct ExportSetting
-   {
-      double t0; /**< Start time for the export */
-      double t1; /**< End time for the export */
-      wxFileName filename; /**< The file to export to */
-      unsigned channels; /**< Number of channels for ExportMultipleByTrack */
-      Tags tags; /**< The set of metadata to use for the export */
-   };
+    ///\brief A private class used to store the information needed to do an export.
+    struct ExportSetting
+    {
+        double t0; /**< Start time for the export */
+        double t1; /**< End time for the export */
+        wxFileName filename; /**< The file to export to */
+        unsigned channels; /**< Number of channels for ExportMultipleByTrack */
+        Tags tags; /**< The set of metadata to use for the export */
+    };
 
 public:
-   enum class ExportMode
-   {
-      Auto,
-      SelectedOnly
-   };
+    enum class ExportMode
+    {
+        Auto,
+        SelectedOnly
+    };
 
-   ExportAudioDialog(wxWindow* parent,
-                     AudacityProject& project,
-                     const wxString& defaultName,
-                     const wxString& defaultFormat,
-                     ExportMode mode = ExportMode::Auto);
-   ~ExportAudioDialog() override;
+    ExportAudioDialog(wxWindow* parent, AudacityProject& project, const wxString& defaultName, const wxString& defaultFormat,
+                      ExportMode mode = ExportMode::Auto);
+    ~ExportAudioDialog() override;
 
-   bool Show(bool show = true) override;
+    bool Show(bool show = true) override;
 private:
-   
-   void PopulateOrExchange(ShuttleGui& S);
-   
-   void OnExportRangeChange(wxCommandEvent& event);
-   void OnSplitModeChange(wxCommandEvent& event);
-   void OnSplitNamePolicyChange(wxCommandEvent& event);
 
-   void OnTrimBlankSpaceBeforeFirstClip(wxCommandEvent&);
+    void PopulateOrExchange(ShuttleGui& S);
 
-   void OnIncludeAudioBeforeFirstLabelChange(wxCommandEvent&);
+    void OnExportRangeChange(wxCommandEvent& event);
+    void OnSplitModeChange(wxCommandEvent& event);
+    void OnSplitNamePolicyChange(wxCommandEvent& event);
 
-   void OnFileNamePrefixChange(wxCommandEvent&);
+    void OnTrimBlankSpaceBeforeFirstClip(wxCommandEvent&);
 
-   void OnEditMetadata(wxCommandEvent& event);
-   
-   void OnHelp(wxCommandEvent& event);
-   
-   void OnExport(wxCommandEvent& event);
+    void OnIncludeAudioBeforeFirstLabelChange(wxCommandEvent&);
 
-   void OnFormatChange(wxCommandEvent& event);
+    void OnFileNamePrefixChange(wxCommandEvent&);
 
-   void UpdateExportSettings();
-   void UpdateLabelExportSettings(const ExportPlugin& plugin, int formatIndex, bool byName, bool addNumber, const wxString& prefix);
-   void UpdateTrackExportSettings(const ExportPlugin& plugin, int formatIndex, bool byName, bool addNumber, const wxString& prefix);
+    void OnEditMetadata(wxCommandEvent& event);
 
-   ExportResult DoExportSplitByLabels(const ExportPlugin& plugin,
-                                      int formatIndex,
-                                      const ExportProcessor::Parameters& parameters,
-                                      FilePaths& exporterFiles);
-   
-   ExportResult DoExportSplitByTracks(const ExportPlugin& plugin,
-                                      int formatIndex,
-                                      const ExportProcessor::Parameters& parameters,
-                                      FilePaths& exporterFiles);
-   
-   ExportResult DoExport(const ExportPlugin& plugin,
-                         int formatIndex,
-                         const ExportProcessor::Parameters& parameters,
-                         const wxFileName& filename,
-                         int channels,
-                         double t0, double t1, bool selectedOnly,
-                         const Tags& tags,
-                         FilePaths& exportedFiles);
-   
-   AudacityProject& mProject;
+    void OnHelp(wxCommandEvent& event);
 
-   ExportFilePanel* mExportOptionsPanel{};
+    void OnExport(wxCommandEvent& event);
 
-   wxWindow* mSplitsPanel{};
-   wxCheckBox* mIncludeAudioBeforeFirstLabel{};
-   wxTextCtrl* mSplitFileNamePrefix{};
-   wxButton* mEditMetadata{};
-   wxRadioButton* mRangeProject;
-   wxRadioButton* mRangeSelection{};
-   wxRadioButton* mRangeSplit{};
-   wxRadioButton* mSplitByLabels{};
-   wxRadioButton* mSplitByTracks{};
-   wxRadioButton* mExportRangeSelection{};
-   wxRadioButton* mSplitModeTracks{};
-   wxRadioButton* mSplitModeLabels{};
-   wxRadioButton* mSplitUseName{};
-   wxRadioButton* mSplitUseNumAndName{};
-   wxRadioButton* mSplitUseNumAndPrefix{};
-   wxCheckBox* mOverwriteExisting{};
-   wxCheckBox* mSkipSilenceAtBeginning{};
+    void OnFormatChange(wxCommandEvent& event);
 
-   std::vector<ExportSetting> mExportSettings;
-   bool mExportSettingsDirty{true};
-   
-   DECLARE_EVENT_TABLE()
+    void UpdateExportSettings();
+    void UpdateLabelExportSettings(const ExportPlugin& plugin, int formatIndex, bool byName, bool addNumber, const wxString& prefix);
+    void UpdateTrackExportSettings(const ExportPlugin& plugin, int formatIndex, bool byName, bool addNumber, const wxString& prefix);
+
+    ExportResult DoExportSplitByLabels(const ExportPlugin& plugin, int formatIndex, const ExportProcessor::Parameters& parameters,
+                                       FilePaths& exporterFiles);
+
+    ExportResult DoExportSplitByTracks(const ExportPlugin& plugin, int formatIndex, const ExportProcessor::Parameters& parameters,
+                                       FilePaths& exporterFiles);
+
+    ExportResult DoExport(const ExportPlugin& plugin, int formatIndex, const ExportProcessor::Parameters& parameters,
+                          const wxFileName& filename, int channels, double t0, double t1, bool selectedOnly, const Tags& tags,
+                          FilePaths& exportedFiles);
+
+    AudacityProject& mProject;
+
+    ExportFilePanel* mExportOptionsPanel{};
+
+    wxWindow* mSplitsPanel{};
+    wxCheckBox* mIncludeAudioBeforeFirstLabel{};
+    wxTextCtrl* mSplitFileNamePrefix{};
+    wxButton* mEditMetadata{};
+    wxRadioButton* mRangeProject;
+    wxRadioButton* mRangeSelection{};
+    wxRadioButton* mRangeSplit{};
+    wxRadioButton* mSplitByLabels{};
+    wxRadioButton* mSplitByTracks{};
+    wxRadioButton* mExportRangeSelection{};
+    wxRadioButton* mSplitModeTracks{};
+    wxRadioButton* mSplitModeLabels{};
+    wxRadioButton* mSplitUseName{};
+    wxRadioButton* mSplitUseNumAndName{};
+    wxRadioButton* mSplitUseNumAndPrefix{};
+    wxCheckBox* mOverwriteExisting{};
+    wxCheckBox* mSkipSilenceAtBeginning{};
+
+    std::vector<ExportSetting> mExportSettings;
+    bool mExportSettingsDirty{ true };
+
+    DECLARE_EVENT_TABLE()
 };
-
--- au3/src/export/ExportMixerPanel.h
+++ au3/src/export/ExportMixerPanel.h
@@ -18,37 +18,37 @@
 
 class wxMemoryDC;
 
-namespace MixerOptions { class Downmix; }
+namespace MixerOptions {
+class Downmix;
+}
 
 ///\class ExportMixerPanel
 ///\brief Panel that displays mixing for advanced mixing option.
 class ExportMixerPanel final : public wxPanelWrapper
 {
 public:
-   ExportMixerPanel( wxWindow *parent, wxWindowID id,
-         MixerOptions::Downmix *mixerSpec, wxArrayString trackNames,
-         const wxPoint& pos = wxDefaultPosition,
-         const wxSize& size = wxDefaultSize);
-   virtual ~ExportMixerPanel();
+    ExportMixerPanel(wxWindow* parent, wxWindowID id, MixerOptions::Downmix* mixerSpec, wxArrayString trackNames,
+                     const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxDefaultSize);
+    virtual ~ExportMixerPanel();
 
-   void OnMouseEvent(wxMouseEvent & event);
-   void OnPaint(wxPaintEvent & event);
+    void OnMouseEvent(wxMouseEvent& event);
+    void OnPaint(wxPaintEvent& event);
 
 private:
-   std::unique_ptr<wxBitmap> mBitmap;
-   wxRect mEnvRect;
-   int mWidth;
-   int mHeight;
-   MixerOptions::Downmix *mMixerSpec;
-   ArrayOf<wxRect> mChannelRects;
-   ArrayOf<wxRect> mTrackRects;
-   int mSelectedTrack, mSelectedChannel;
-   wxArrayString mTrackNames;
-   int mBoxWidth, mChannelHeight, mTrackHeight;
+    std::unique_ptr<wxBitmap> mBitmap;
+    wxRect mEnvRect;
+    int mWidth;
+    int mHeight;
+    MixerOptions::Downmix* mMixerSpec;
+    ArrayOf<wxRect> mChannelRects;
+    ArrayOf<wxRect> mTrackRects;
+    int mSelectedTrack, mSelectedChannel;
+    wxArrayString mTrackNames;
+    int mBoxWidth, mChannelHeight, mTrackHeight;
 
-   void SetFont( wxMemoryDC &memDC, const wxString &text, int width, int height );
-   double Distance( wxPoint &a, wxPoint &b );
-   bool IsOnLine( wxPoint p, wxPoint la, wxPoint lb );
+    void SetFont(wxMemoryDC& memDC, const wxString& text, int width, int height);
+    double Distance(wxPoint& a, wxPoint& b);
+    bool IsOnLine(wxPoint p, wxPoint la, wxPoint lb);
 
-   DECLARE_EVENT_TABLE()
+    DECLARE_EVENT_TABLE()
 };
