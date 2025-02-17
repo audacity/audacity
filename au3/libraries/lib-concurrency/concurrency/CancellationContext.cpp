c8c14cb7725bf12c3cc98be2b5bb64b3004617c2
Igor <igor.korsukov@gmail.com>
2025-02-17 11:48:28 +0200

    update au3 codestyle: au3/libraries/lib-concurrency

--- au3/libraries/lib-concurrency/concurrency/CancellationContext.cpp
+++ au3/libraries/lib-concurrency/concurrency/CancellationContext.cpp
@@ -10,57 +10,58 @@
 
 #include "ICancellable.h"
 
-namespace audacity::concurrency
-{
+namespace audacity::concurrency {
 CancellationContext::CancellationContext(Tag)
 {
 }
 
 std::shared_ptr<CancellationContext> CancellationContext::Create()
 {
-   return std::make_shared<CancellationContext>(Tag {});
+    return std::make_shared<CancellationContext>(Tag {});
 }
 
 void CancellationContext::Cancel()
 {
-   if (mCancelled.exchange(true))
-      return;
+    if (mCancelled.exchange(true)) {
+        return;
+    }
 
-   std::vector<CancellableWPtr> cancellableObjects;
+    std::vector<CancellableWPtr> cancellableObjects;
 
-   {
-      auto lock = std::lock_guard { mCancellableObjectsMutex };
-      std::swap(cancellableObjects, mCancellableObjects);
-   }
+    {
+        auto lock = std::lock_guard { mCancellableObjectsMutex };
+        std::swap(cancellableObjects, mCancellableObjects);
+    }
 
-   std::for_each(
-      cancellableObjects.begin(), cancellableObjects.end(),
-      [](auto& wptr)
-      {
-         if (auto lock = wptr.lock())
+    std::for_each(
+        cancellableObjects.begin(), cancellableObjects.end(),
+        [](auto& wptr)
+    {
+        if (auto lock = wptr.lock()) {
             lock->Cancel();
-      });
+        }
+    });
 }
 
 void CancellationContext::OnCancelled(CancellableWPtr cancellable)
 {
-   auto locked = cancellable.lock();
-   if (!locked)
-      return;
+    auto locked = cancellable.lock();
+    if (!locked) {
+        return;
+    }
 
-   if (mCancelled.load(std::memory_order_acquire))
-   {
-      locked->Cancel();
-      return;
-   }
+    if (mCancelled.load(std::memory_order_acquire)) {
+        locked->Cancel();
+        return;
+    }
 
-   auto lock = std::lock_guard { mCancellableObjectsMutex };
-   mCancellableObjects.erase(
-      std::remove_if(
-         mCancellableObjects.begin(), mCancellableObjects.end(),
-         [](auto& wptr) { return wptr.expired(); }),
-      mCancellableObjects.end());
+    auto lock = std::lock_guard { mCancellableObjectsMutex };
+    mCancellableObjects.erase(
+        std::remove_if(
+            mCancellableObjects.begin(), mCancellableObjects.end(),
+            [](auto& wptr) { return wptr.expired(); }),
+        mCancellableObjects.end());
 
-   mCancellableObjects.push_back(std::move(cancellable));
+    mCancellableObjects.push_back(std::move(cancellable));
 }
 } // namespace audacity::concurrency
