e5584e07f5fcbef1b1cc0f274fb2b0a1cbd4cb44
Igor <igor.korsukov@gmail.com>
2025-02-17 11:47:20 +0200

    update au3 codestyle: au3/libraries/lib-command-parameters

--- au3/libraries/lib-command-parameters/ShuttlePrefs.h
+++ au3/libraries/lib-command-parameters/ShuttlePrefs.h
@@ -17,21 +17,21 @@
 class COMMAND_PARAMETERS_API ShuttlePrefs final
 {
 public:
-   bool mbStoreInClient{};
-   wxString mValueString;
+    bool mbStoreInClient{};
+    wxString mValueString;
 
-   // constructors and destructors
-   ShuttlePrefs(){}
-   ~ShuttlePrefs() {}
+    // constructors and destructors
+    ShuttlePrefs() {}
+    ~ShuttlePrefs() {}
 
 public:
-   bool TransferBool( const wxString & Name, bool & bValue, const bool & bDefault );
+    bool TransferBool(const wxString& Name, bool& bValue, const bool& bDefault);
 //   bool TransferFloat( const wxString & Name, float & fValue, const float &fDefault );
-   bool TransferDouble( const wxString & Name, double & dValue, const double &dDefault );
-   bool TransferInt(const wxString & Name, int & iValue, const int &iDefault);
-   bool TransferString(const wxString & Name, wxString & strValue, const wxString &strDefault);
-   bool TransferWrappedType(const wxString & Name, WrappedType & W);
-   bool ExchangeWithMaster(const wxString & Name);
+    bool TransferDouble(const wxString& Name, double& dValue, const double& dDefault);
+    bool TransferInt(const wxString& Name, int& iValue, const int& iDefault);
+    bool TransferString(const wxString& Name, wxString& strValue, const wxString& strDefault);
+    bool TransferWrappedType(const wxString& Name, WrappedType& W);
+    bool ExchangeWithMaster(const wxString& Name);
 };
 
 #endif
--- au3/libraries/lib-command-parameters/WrappedType.cpp
+++ au3/libraries/lib-command-parameters/WrappedType.cpp
@@ -33,220 +33,207 @@
 /// @return true iff the wrapped type is a string.
 bool WrappedType::IsString()
 {
-   return eWrappedType == eWrappedString;
+    return eWrappedType == eWrappedString;
 }
-
-
 
 wxString WrappedType::ReadAsString()
 {
-   switch( eWrappedType )
-   {
-   case eWrappedString:
-      return *mpStr;
-      break;
-   case eWrappedInt:
-      return wxString::Format(wxT("%i"),*mpInt );
-      break;
-   case eWrappedDouble:
-      return wxString::Format(wxT("%.8g"),*mpDouble );
-      break;
-   case eWrappedBool:
-      return (* mpBool) ? wxT("true") : wxT("false" );
-      break;
-   case eWrappedEnum:
-      wxASSERT( false );
-      break;
-   default:
-      wxASSERT( false );
-      break;
-   }
-   return wxT("ERROR"); //Compiler pacifier
+    switch (eWrappedType) {
+    case eWrappedString:
+        return *mpStr;
+        break;
+    case eWrappedInt:
+        return wxString::Format(wxT("%i"), *mpInt);
+        break;
+    case eWrappedDouble:
+        return wxString::Format(wxT("%.8g"), *mpDouble);
+        break;
+    case eWrappedBool:
+        return (*mpBool) ? wxT("true") : wxT("false");
+        break;
+    case eWrappedEnum:
+        wxASSERT(false);
+        break;
+    default:
+        wxASSERT(false);
+        break;
+    }
+    return wxT("ERROR"); //Compiler pacifier
 }
 
 int WrappedType::ReadAsInt()
 {
-   switch( eWrappedType )
-   {
-   case eWrappedString:
-   {
-      long l;
-      mpStr->ToLong(&l);
-      return (int) l;
-   }
-   case eWrappedInt:
-      return *mpInt;
-   case eWrappedDouble:
-      return (int)*mpDouble;
-   case eWrappedBool:
-      return (* mpBool) ? 1 : 0;
-   case eWrappedEnum:
-      wxASSERT( false );
-      break;
-   default:
-      wxASSERT( false );
-      break;
-   }
-   return -1;//Compiler pacifier
+    switch (eWrappedType) {
+    case eWrappedString:
+    {
+        long l;
+        mpStr->ToLong(&l);
+        return (int)l;
+    }
+    case eWrappedInt:
+        return *mpInt;
+    case eWrappedDouble:
+        return (int)*mpDouble;
+    case eWrappedBool:
+        return (*mpBool) ? 1 : 0;
+    case eWrappedEnum:
+        wxASSERT(false);
+        break;
+    default:
+        wxASSERT(false);
+        break;
+    }
+    return -1;//Compiler pacifier
 }
 
 double WrappedType::ReadAsDouble()
 {
-   switch( eWrappedType )
-   {
-   case eWrappedString:
-      return Internat::CompatibleToDouble( *mpStr );
-      break;
-   case eWrappedInt:
-      return (double)*mpInt;
-      break;
-   case eWrappedDouble:
-      return * mpDouble;
-      break;
-   case eWrappedBool:
-      return (* mpBool)? 1.0 : 0.0;
-      break;
-   case eWrappedEnum:
-      wxASSERT( false );
-      break;
-   default:
-      wxASSERT( false );
-      break;
-   }
-   return -1.0f;//Compiler pacifier
+    switch (eWrappedType) {
+    case eWrappedString:
+        return Internat::CompatibleToDouble(*mpStr);
+        break;
+    case eWrappedInt:
+        return (double)*mpInt;
+        break;
+    case eWrappedDouble:
+        return *mpDouble;
+        break;
+    case eWrappedBool:
+        return (*mpBool) ? 1.0 : 0.0;
+        break;
+    case eWrappedEnum:
+        wxASSERT(false);
+        break;
+    default:
+        wxASSERT(false);
+        break;
+    }
+    return -1.0f;//Compiler pacifier
 }
 
 bool WrappedType::ReadAsBool()
 {
-   switch( eWrappedType )
-   {
-   case eWrappedString:
-      return mpStr->IsSameAs( wxT("true"), false ); // case free comparison.
-      break;
-   case eWrappedInt:
-      return  *mpInt != 0;
-      break;
-   case eWrappedDouble:
-      wxASSERT( false );// DANGEROUS USE OF WrappedType.  Can't rely on equality.
-      return * mpDouble != 0.0f; // this is what the code would be...
-      break;
-   case eWrappedBool:
-      return * mpBool;
-      break;
-   case eWrappedEnum:
-      wxASSERT( false );
-      break;
-   default:
-      wxASSERT( false );
-      break;
-   }
-   return false;//Compiler pacifier
+    switch (eWrappedType) {
+    case eWrappedString:
+        return mpStr->IsSameAs(wxT("true"), false); // case free comparison.
+        break;
+    case eWrappedInt:
+        return *mpInt != 0;
+        break;
+    case eWrappedDouble:
+        wxASSERT(false);// DANGEROUS USE OF WrappedType.  Can't rely on equality.
+        return *mpDouble != 0.0f; // this is what the code would be...
+        break;
+    case eWrappedBool:
+        return *mpBool;
+        break;
+    case eWrappedEnum:
+        wxASSERT(false);
+        break;
+    default:
+        wxASSERT(false);
+        break;
+    }
+    return false;//Compiler pacifier
 }
 
-
-void WrappedType::WriteToAsString( const wxString & InStr)
+void WrappedType::WriteToAsString(const wxString& InStr)
 {
-   switch( eWrappedType )
-   {
-   case eWrappedString:
-      *mpStr = InStr;
-      break;
-   case eWrappedInt:
-   {
-      long l;
-      InStr.ToLong(&l);
-      *mpInt = (int) l;
-      break;
-   }
-   case eWrappedDouble:
-      *mpDouble = Internat::CompatibleToDouble( InStr );
-      break;
-   case eWrappedBool:
-      *mpBool = InStr.IsSameAs( wxT("true"), false ); // case free comparison.;
-      break;
-   case eWrappedEnum:
-      wxASSERT( false );
-      break;
-   default:
-      wxASSERT( false );
-      break;
-   }
+    switch (eWrappedType) {
+    case eWrappedString:
+        *mpStr = InStr;
+        break;
+    case eWrappedInt:
+    {
+        long l;
+        InStr.ToLong(&l);
+        *mpInt = (int)l;
+        break;
+    }
+    case eWrappedDouble:
+        *mpDouble = Internat::CompatibleToDouble(InStr);
+        break;
+    case eWrappedBool:
+        *mpBool = InStr.IsSameAs(wxT("true"), false); // case free comparison.;
+        break;
+    case eWrappedEnum:
+        wxASSERT(false);
+        break;
+    default:
+        wxASSERT(false);
+        break;
+    }
 }
 
-void WrappedType::WriteToAsInt( const int InInt)
+void WrappedType::WriteToAsInt(const int InInt)
 {
-   switch( eWrappedType )
-   {
-   case eWrappedString:
-      *mpStr = wxString::Format( wxT("%i"), InInt );
-      break;
-   case eWrappedInt:
-      *mpInt = InInt;
-      break;
-   case eWrappedDouble:
-      *mpDouble = (double)InInt;
-      break;
-   case eWrappedBool:
-      * mpBool = (InInt !=0);
-      break;
-   case eWrappedEnum:
-      wxASSERT( false );
-      break;
-   default:
-      wxASSERT( false );
-      break;
-   }
+    switch (eWrappedType) {
+    case eWrappedString:
+        *mpStr = wxString::Format(wxT("%i"), InInt);
+        break;
+    case eWrappedInt:
+        *mpInt = InInt;
+        break;
+    case eWrappedDouble:
+        *mpDouble = (double)InInt;
+        break;
+    case eWrappedBool:
+        *mpBool = (InInt != 0);
+        break;
+    case eWrappedEnum:
+        wxASSERT(false);
+        break;
+    default:
+        wxASSERT(false);
+        break;
+    }
 }
 
-void WrappedType::WriteToAsDouble( const double InDouble)
+void WrappedType::WriteToAsDouble(const double InDouble)
 {
-   switch( eWrappedType )
-   {
-   case eWrappedString:
-      *mpStr = wxString::Format( wxT("%.8g"), InDouble );
-      break;
-   case eWrappedInt:
-      *mpInt = (int)InDouble;
-      break;
-   case eWrappedDouble:
-      *mpDouble = InDouble;
-      break;
-   case eWrappedBool:
-      wxASSERT( false );
-      * mpBool = InDouble != 0.0;
-      break;
-   case eWrappedEnum:
-      wxASSERT( false );
-      break;
-   default:
-      wxASSERT( false );
-      break;
-   }
+    switch (eWrappedType) {
+    case eWrappedString:
+        *mpStr = wxString::Format(wxT("%.8g"), InDouble);
+        break;
+    case eWrappedInt:
+        *mpInt = (int)InDouble;
+        break;
+    case eWrappedDouble:
+        *mpDouble = InDouble;
+        break;
+    case eWrappedBool:
+        wxASSERT(false);
+        *mpBool = InDouble != 0.0;
+        break;
+    case eWrappedEnum:
+        wxASSERT(false);
+        break;
+    default:
+        wxASSERT(false);
+        break;
+    }
 }
 
-
-void WrappedType::WriteToAsBool( const bool InBool)
+void WrappedType::WriteToAsBool(const bool InBool)
 {
-   switch( eWrappedType )
-   {
-   case eWrappedString:
-      *mpStr = InBool ? wxT("true") : wxT("false" );
-      break;
-   case eWrappedInt:
-      *mpInt = InBool ? 1 : 0;
-      break;
-   case eWrappedDouble:
-      *mpDouble = InBool ? 1.0 : 0.0;
-      break;
-   case eWrappedBool:
-      *mpBool = InBool;
-      break;
-   case eWrappedEnum:
-      wxASSERT( false );
-      break;
-   default:
-      wxASSERT( false );
-      break;
-   }
+    switch (eWrappedType) {
+    case eWrappedString:
+        *mpStr = InBool ? wxT("true") : wxT("false");
+        break;
+    case eWrappedInt:
+        *mpInt = InBool ? 1 : 0;
+        break;
+    case eWrappedDouble:
+        *mpDouble = InBool ? 1.0 : 0.0;
+        break;
+    case eWrappedBool:
+        *mpBool = InBool;
+        break;
+    case eWrappedEnum:
+        wxASSERT(false);
+        break;
+    default:
+        wxASSERT(false);
+        break;
+    }
 }
-
