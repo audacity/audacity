2a15b6334cf86c7a665a2c31ec81fc390bc60935
Igor <igor.korsukov@gmail.com>
2025-02-17 11:49:25 +0200

    update au3 codestyle: au3/libraries/lib-c...

--- au3/libraries/lib-crashpad-configurer/internal/Util.h
+++ au3/libraries/lib-crashpad-configurer/internal/Util.h
@@ -4,15 +4,15 @@
 #include <codecvt>
 
 template<typename StringType>
-typename std::enable_if<std::is_same<StringType, std::string>::value, StringType>::type 
+typename std::enable_if<std::is_same<StringType, std::string>::value, StringType>::type
 makeFilePath(const std::string& pathUTF8)
 {
-	return pathUTF8;
+    return pathUTF8;
 }
 
 template<typename StringType>
 typename std::enable_if<!std::is_same<StringType, std::string>::value, StringType>::type
 makeFilePath(const std::string& pathUTF8)
 {
-	return std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>, wchar_t>{}.from_bytes(pathUTF8);
+    return std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>, wchar_t> {}.from_bytes(pathUTF8);
 }
