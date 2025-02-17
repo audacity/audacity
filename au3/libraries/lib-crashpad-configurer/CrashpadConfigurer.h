f224b8d6a3d78c4419797031b10e767f8a4aba2d
Igor <igor.korsukov@gmail.com>
2025-02-17 11:49:25 +0200

    update au3 codestyle: au3/libraries/lib-c...

--- au3/libraries/lib-crashpad-configurer/CrashpadConfigurer.h
+++ au3/libraries/lib-crashpad-configurer/CrashpadConfigurer.h
@@ -6,23 +6,23 @@
 
 class CRASHPAD_CONFIGURER_API CrashpadConfigurer final
 {
-   std::string mHandlerPath;
-   std::string mDatabasePath;
-   std::string mMetricsDir;
-   std::string mURL;
-   std::vector<std::string> mArguments;
-   std::map<std::string, std::string> mAnnotations;
-   std::vector<std::string> mAttachments;
-   bool mDatabaseUploadsEnabled { false };
+    std::string mHandlerPath;
+    std::string mDatabasePath;
+    std::string mMetricsDir;
+    std::string mURL;
+    std::vector<std::string> mArguments;
+    std::map<std::string, std::string> mAnnotations;
+    std::vector<std::string> mAttachments;
+    bool mDatabaseUploadsEnabled { false };
 public:
-   CrashpadConfigurer& SetHandlerPathUTF8(const std::string& handlerPath);
-   CrashpadConfigurer& SetDatabasePathUTF8(const std::string& database);
-   CrashpadConfigurer& SetMetricsDirUTF8(const std::string& metricsDir);
-   CrashpadConfigurer& SetURL(const std::string&);
-   CrashpadConfigurer& SetArguments(const std::vector<std::string>& arguments);
-   CrashpadConfigurer& SetAnnotations(const std::map<std::string, std::string>& annotations);
-   CrashpadConfigurer& SetAttachmentsUTF8(const std::vector<std::string>& attachments);
-   CrashpadConfigurer& SetDatabaseUploadEnabled(bool enabled);
+    CrashpadConfigurer& SetHandlerPathUTF8(const std::string& handlerPath);
+    CrashpadConfigurer& SetDatabasePathUTF8(const std::string& database);
+    CrashpadConfigurer& SetMetricsDirUTF8(const std::string& metricsDir);
+    CrashpadConfigurer& SetURL(const std::string&);
+    CrashpadConfigurer& SetArguments(const std::vector<std::string>& arguments);
+    CrashpadConfigurer& SetAnnotations(const std::map<std::string, std::string>& annotations);
+    CrashpadConfigurer& SetAttachmentsUTF8(const std::vector<std::string>& attachments);
+    CrashpadConfigurer& SetDatabaseUploadEnabled(bool enabled);
 
-   void Start();
+    void Start();
 };
