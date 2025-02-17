ab69c7c27fa7565d6c1aadff9aaf54879504d0b8
Igor <igor.korsukov@gmail.com>
2025-02-17 11:45:54 +0200

    update au3 codestyle: au3/libraries/lib-channel

--- au3/libraries/lib-channel/Channel.cpp
+++ au3/libraries/lib-channel/Channel.cpp
@@ -24,47 +24,52 @@ Channel::~Channel() = default;
 
 size_t Channel::GetChannelIndex() const
 {
-   auto &group = DoGetChannelGroup();
-   int index = -1;
-   for (size_t ii = 0, nn = group.NChannels(); ii < nn; ++ii)
-      if (group.GetChannel(ii).get() == this) {
-         index = ii;
-         break;
-      }
-   assert(index >= 0);
-   return index;
+    auto& group = DoGetChannelGroup();
+    int index = -1;
+    for (size_t ii = 0, nn = group.NChannels(); ii < nn; ++ii) {
+        if (group.GetChannel(ii).get() == this) {
+            index = ii;
+            break;
+        }
+    }
+    assert(index >= 0);
+    return index;
 }
 
-const ChannelGroup &Channel::GetChannelGroup() const
+const ChannelGroup& Channel::GetChannelGroup() const
 {
-   return DoGetChannelGroup();
+    return DoGetChannelGroup();
 }
 
-ChannelGroup &Channel::GetChannelGroup()
+ChannelGroup& Channel::GetChannelGroup()
 {
-   return DoGetChannelGroup();
+    return DoGetChannelGroup();
 }
 
 ChannelGroup::~ChannelGroup() = default;
 
 double ChannelGroup::GetStartTime() const
 {
-   const auto &range = Intervals();
-   if (range.empty())
-      return 0;
-   return std::accumulate(range.first, range.second,
-      std::numeric_limits<double>::max(),
-      [](double acc, const auto &pInterval){
-         return std::min(acc, pInterval->Start()); });
+    const auto& range = Intervals();
+    if (range.empty()) {
+        return 0;
+    }
+    return std::accumulate(range.first, range.second,
+                           std::numeric_limits<double>::max(),
+                           [](double acc, const auto& pInterval){
+        return std::min(acc, pInterval->Start());
+    });
 }
 
 double ChannelGroup::GetEndTime() const
 {
-   const auto &range = Intervals();
-   if (range.empty())
-      return 0;
-   return std::accumulate(range.first, range.second,
-      std::numeric_limits<double>::lowest(),
-      [](double acc, const auto &pInterval){
-         return std::max(acc, pInterval->End()); });
+    const auto& range = Intervals();
+    if (range.empty()) {
+        return 0;
+    }
+    return std::accumulate(range.first, range.second,
+                           std::numeric_limits<double>::lowest(),
+                           [](double acc, const auto& pInterval){
+        return std::max(acc, pInterval->End());
+    });
 }
