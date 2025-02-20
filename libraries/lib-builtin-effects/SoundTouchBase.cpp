83a73141c6d1fde0a9930ffd38f0d4686dc8f6e2
Dmitry Makarenko <kryksyh@gmail.com>
2025-02-20 11:06:13 +0300

    Fix #8290 Skip empty tracks when applying Pitch and Tempo effects

--- libraries/lib-builtin-effects/SoundTouchBase.cpp
+++ libraries/lib-builtin-effects/SoundTouchBase.cpp
@@ -115,6 +115,9 @@ bool SoundTouchBase::ProcessWithTimeWarper(InitFunction initer,
          if (!orig.GetSelected())
             return fallthrough();
 
+         if (!orig.NIntervals())
+            return fallthrough();
+
          // Process only if the right marker is to the right of the left marker
          if (mT1 > mT0) {
             //Transform the marker timepoints to samples
