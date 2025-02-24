b8c5d0e540c186728d6e3052b74b9230317221c6
Grzegorz Wojciechowski <grliszas14@gmail.com>
2025-02-24 13:29:08 +0100

    Added project configuration mock

--- src/playback/tests/environment.cpp
+++ src/playback/tests/environment.cpp
@@ -6,8 +6,27 @@
 
 #include "au3wrap/au3wrapmodule.h"
 
+#include "project/tests/mocks/projectconfigurationmock.h"
+
+using namespace ::testing;
+using namespace au::project;
+
 static muse::testing::SuiteEnvironment playback_se
     = muse::testing::SuiteEnvironment()
       .setDependencyModules({ new au::au3::Au3WrapModule() })
-      .setPostInit([]() {
+      .setPreInit([](){
+    std::shared_ptr<ProjectConfigurationMock> projectConfigurator(new ProjectConfigurationMock(), [](ProjectConfigurationMock*){});
+
+    ON_CALL(*projectConfigurator, temporaryDir())
+    .WillByDefault(Return(""));
+
+    muse::modularity::globalIoc()->registerExport<IProjectConfiguration>("utests", projectConfigurator);
+}).setPostInit([]() {
+}).setDeInit([](){
+    std::shared_ptr<IProjectConfiguration> projectConfiguratorPtr
+        = muse::modularity::globalIoc()->resolve<IProjectConfiguration>("utests");
+    muse::modularity::globalIoc()->unregister<IProjectConfiguration>("utests");
+
+    IProjectConfiguration* projectConfigurator = projectConfiguratorPtr.get();
+    delete projectConfigurator;
 });
