68322e62557df26f30a58a07a735eb3f01447818
Dmitry Makarenko <kryksyh@gmail.com>
2025-03-04 16:14:42 +0300

    Fix clipping wxStaticText

--- conan/conanfile.py
+++ conan/conanfile.py
@@ -91,7 +91,7 @@ class wxWidgetsAudacityDependency:
     override: bool = False
 
     def reference(self, conanfile):
-        return f"{self.name}/3.1.3.6-audacity@audacity/stable"
+        return f"{self.name}/3.1.3.7-audacity@audacity/stable"
 
     def apply_options(self, conanfile, package):
         opts = [
