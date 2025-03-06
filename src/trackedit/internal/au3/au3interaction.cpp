a8cd2c50a62e2bdd5db117134a900b45cda1f3a8
iliasmuse <i.bergstrom@mu.se>
2025-03-06 10:27:00 +0100

    Removed the use of auto where the type is not clear on the same line.

--- src/trackedit/internal/au3/au3interaction.cpp
+++ src/trackedit/internal/au3/au3interaction.cpp
@@ -2171,11 +2171,11 @@ bool Au3Interaction::undo()
 
     auto trackeditProject = globalContext()->currentProject()->trackeditProject();
 
-    auto before = trackeditProject->buildTracksAndClips();
+    TracksAndClips before = trackeditProject->buildTracksAndClips();
 
     projectHistory()->undo();
 
-    auto after = trackeditProject->buildTracksAndClips();
+    TracksAndClips after = trackeditProject->buildTracksAndClips();
 
     m_changeDetection.notifyOfUndoRedo(before, after);
 
@@ -2195,11 +2195,11 @@ bool Au3Interaction::redo()
 
     auto trackeditProject = globalContext()->currentProject()->trackeditProject();
 
-    auto before = trackeditProject->buildTracksAndClips();
+    TracksAndClips before = trackeditProject->buildTracksAndClips();
 
     projectHistory()->redo();
 
-    auto after = trackeditProject->buildTracksAndClips();
+    TracksAndClips after = trackeditProject->buildTracksAndClips();
 
     m_changeDetection.notifyOfUndoRedo(before, after);
 
