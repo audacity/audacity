8698daf247b643079f0d872e03c6348337f6994e
Gabriel Sartori <g.sartori@mu.se>
2025-05-03 23:05:30 +0200

    Avoid using au4 types on au3 legacy code

--- au3/libraries/lib-audio-devices/IMeterChannel.h
+++ au3/libraries/lib-audio-devices/IMeterChannel.h
@@ -3,10 +3,6 @@
 */
 #pragma once
 
-#include "global/async/channel.h"
-
-#include "au3audio/audiotypes.h"
-
 constexpr int64_t MAIN_METER_KEY = -1;
 
 class AUDIO_DEVICES_API IMeterChannel
@@ -16,5 +12,4 @@ public:
     virtual void push(uint8_t channel, float signal, int64_t key = MAIN_METER_KEY) = 0;
     virtual void sendAll() = 0;
     virtual void reset() = 0;
-    virtual muse::async::Channel<au::audio::audioch_t, au::audio::AudioSignalVal> dataChanged(int64_t key = MAIN_METER_KEY) = 0;
 };
--- src/playback/internal/au3/au3audiooutmeter.h
+++ src/playback/internal/au3/au3audiooutmeter.h
@@ -4,11 +4,13 @@
 
 #pragma once
 
+#include "libraries/lib-audio-devices/IMeterChannel.h"
+
 #include "global/async/asyncable.h"
 #include "global/async/promise.h"
 #include "global/async/channel.h"
 
-#include "libraries/lib-audio-devices/IMeterChannel.h"
+#include "au3audio/audiotypes.h"
 
 namespace au::playback {
 class OutMeter : public IMeterChannel, public muse::async::Asyncable
@@ -18,7 +20,7 @@ public:
     void push(uint8_t channel, float signal, int64_t key) override;
     void sendAll() override;
     void reset() override;
-    muse::async::Channel<au::audio::audioch_t, au::audio::AudioSignalVal> dataChanged(int64_t key = MAIN_METER_KEY) override;
+    muse::async::Channel<au::audio::audioch_t, au::audio::AudioSignalVal> dataChanged(int64_t key = MAIN_METER_KEY);
 
 private:
     struct Data
