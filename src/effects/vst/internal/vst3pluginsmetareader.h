758ec64db3c870812fb12a5aca636e6b31ed0399
Igor <igor.korsukov@gmail.com>
2025-06-04 15:32:29 +0200

    updated muse to 250604

--- src/effects/vst/internal/vst3pluginsmetareader.h
+++ src/effects/vst/internal/vst3pluginsmetareader.h
@@ -9,6 +9,8 @@ namespace au::effects {
 class Vst3PluginsMetaReader : public muse::audioplugins::IAudioPluginMetaReader
 {
 public:
+
+    muse::audio::AudioResourceType metaType() const override;
     bool canReadMeta(const muse::io::path_t& pluginPath) const override;
     muse::RetVal<muse::audio::AudioResourceMetaList> readMeta(const muse::io::path_t& pluginPath) const override;
 };
