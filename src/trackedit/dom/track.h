4db9ebf58c8b2d260b1f74e68112ef3994aa4fed
Gabriel Sartori <g.sartori@mu.se>
2025-07-10 06:13:31 -0300

    Add rate information on au4 track

--- src/trackedit/dom/track.h
+++ src/trackedit/dom/track.h
@@ -26,12 +26,37 @@ enum class TrackFormat : int {
     Float32
 };
 
+struct TrackFormatInfo {
+    TrackFormat format;
+    const char* description;
+};
+
+inline const std::array<TrackFormatInfo, 3>& availableTrackFormats()
+{
+    static constexpr std::array<TrackFormatInfo, 3> AVAILABLE_TRACK_FORMATS = { {
+        { au::trackedit::TrackFormat::Int16, "16-bit PCM" },
+        { au::trackedit::TrackFormat::Int24, "24-bit PCM" },
+        { au::trackedit::TrackFormat::Float32, "32-bit Float" }
+    } };
+    return AVAILABLE_TRACK_FORMATS;
+}
+
+inline const std::array<int, 12>& availableTrackSampleRates()
+{
+    static constexpr std::array<int, 12> AVAILABLE_SAMPLE_RATES = { {
+        8000, 11025, 16000, 22050, 44100, 48000,
+        88200, 96000, 176400, 192000, 352800, 384000
+    } };
+    return AVAILABLE_SAMPLE_RATES;
+}
+
 struct Track {
     TrackId id;
     muse::String title;
     TrackType type = TrackType::Undefined;
     muse::draw::Color color;
     TrackFormat format = TrackFormat::Undefined;
+    int rate;
 };
 
 using TrackList = std::vector<Track>;
