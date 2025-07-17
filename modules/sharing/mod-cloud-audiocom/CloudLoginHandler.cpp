f54cc78efd296619bfeab43b0bcb7ea8d3754be5
Dmitry Makarenko <kryksyh@gmail.com>
2025-07-17 11:33:31 +0300

    9028: Open Login Dialog from the Welcome Screen

---
+++ modules/sharing/mod-cloud-audiocom/CloudLoginHandler.cpp
@@ -0,0 +1,64 @@
+#include <wx/app.h>
+#include <wx/event.h>
+
+#include "AppEvents.h"
+#include "BasicUI.h"
+#include "CodeConversions.h"
+#include "ui/dialogs/LoginDialog.h"
+#include "OAuthService.h"
+#include "UserService.h"
+#include "menus/CloudLoginHelper.h"
+
+constexpr auto AudioComTourURL = "https://audio.com/tour?mtm_campaign=audacitydesktop&mtm_content=app_launch_reg";
+
+class CloudLoginHandler
+{
+
+public:
+   CloudLoginHandler() {
+      AppEvents::OnAppInitialized([this] {
+         mSubscription = CloudLoginHelper::Get().Subscribe([this](const auto&) {
+            HandleCloudLogin();
+            return true;
+         });
+      });
+   }
+
+   bool HandleCloudLogin() {
+      using namespace audacity::cloud::audiocom;
+
+      auto& oauthService = GetOAuthService();
+      if (!oauthService.HasRefreshToken() && !ShowLoginDialog())
+         return false;
+
+      if (!oauthService.HasAccessToken())
+         oauthService.ValidateAuth({}, {}, true);
+
+      auto& userService = GetUserService();
+      userService.UpdateUserData();
+
+      mUserServiceSubscription = userService.Subscribe([this, &userService, &oauthService](const auto&) {
+         if (userService.GetUserSlug().empty() && !ShowLoginDialog())
+            return;
+
+         auto userId = audacity::ToUTF8(userService.GetUserId());
+         auto url = oauthService.MakeAudioComAuthorizeURL(userId, AudioComTourURL);
+         BasicUI::OpenInDefaultBrowser(url);
+      });
+
+      return true;
+   }
+
+   bool ShowLoginDialog() {
+      bool result = LoginDialog::SignIn(
+         wxTheApp->GetTopWindow(),
+         LoginDialog::Mode::Create);
+      return result;
+   }
+
+private:
+   Observer::Subscription mSubscription;
+   Observer::Subscription mUserServiceSubscription;
+};
+
+static CloudLoginHandler sEventHandler;
