28015efb9310a7638b4b32d873ee7b1087c7949d
Dmitry Makarenko <kryksyh@gmail.com>
2025-07-23 18:44:21 +0300

    Fix out of bounds memory access when scrolling a spectrogram #8851
    Cherry-picked from: bfdcee7cfd4feddccccf9c3f6f0d55eefadf5557

--- au3/libraries/lib-wave-track/WaveClip.cpp
+++ au3/libraries/lib-wave-track/WaveClip.cpp
@@ -1865,7 +1865,6 @@ void WaveClip::Resample(int rate, const std::function<void(size_t)>& progressRep
                 progressReport(results->first);
             }
         }
-
     }
 
     if (error) {
@@ -2182,7 +2181,7 @@ bool WaveClip::CheckInvariants() const
          end = mSequences.end();
     // There must be at least one pointer
     if (iter != end) {
-        // All pointers mut be non-null
+        // All pointers must be non-null
         auto& pFirst = *iter++;
         if (pFirst) {
             // All sequences must have the same sample formats, and sample block
