31f4d3003437ddad7652d711c0f21e366555c8d7
Matthieu Hodgkinson <saintmatthieu@gmail.com>
2025-08-07 14:48:50 +0200

    Normalize loudness UI

--- src/effects/builtin/loudness/NormalizeLoudnessView.qml
+++ src/effects/builtin/loudness/NormalizeLoudnessView.qml
@@ -11,17 +11,103 @@ EffectBase {
     property bool isApplyAllowed: true
 
     width: 400
-    implicitHeight: 400
+    implicitHeight: column.height
 
-    model: normalize
+    model: normalizeLoudness
 
     NormalizeLoudnessViewModel {
-        id: normalize
+        id: normalizeLoudness
 
         instanceId: root.instanceId
     }
 
+    Column {
+        id: column
+
+        spacing: 16
+        bottomPadding: 16
+
+        Row {
+            spacing: 6
+
+            StyledTextLabel {
+                anchors.verticalCenter: parent.verticalCenter
+                text: qsTrc("effects/loudness", "Normalize")
+            }
+
+            StyledDropdown {
+                id: algorithmDropdown
+
+                width: 194
+                anchors.verticalCenter: parent.verticalCenter
+
+                model: [qsTrc("effects/loudness", "RMS"), qsTrc("effects/loudness", "Perceived loudness")]
+                currentIndex: root.model.useRmsAlgorithm ? 0 : 1
+                onActivated: function(index) {
+                    root.model.useRmsAlgorithm = index === 0;
+                }
+            }
+
+            StyledTextLabel {
+                anchors.verticalCenter: parent.verticalCenter
+                text: qsTrc("effects/loudness", "to")
+            }
+
+            IncrementalPropertyControl {
+                width: 125
+                anchors.verticalCenter: parent.verticalCenter
+
+                measureUnitsSymbol: root.model.useRmsAlgorithm ? "dB" : "LUFS"
+                step: 1
+                decimals: 1
+                minValue: root.model.targetMin
+                maxValue: root.model.targetMax
+                currentValue: root.model.useRmsAlgorithm ? root.model.rmsTarget : root.model.perceivedLoudnessTarget
+                onValueEdited: function(newValue) {
+                    if (root.model.useRmsAlgorithm) {
+                        root.model.rmsTarget = newValue;
+                    } else {
+                        root.model.perceivedLoudnessTarget = newValue;
+                    }
+                }
+            }
+        }
+
+        Row {
+            spacing: 8
+
+            CheckBox {
+                checked: root.model.normalizeStereoChannelsIndependently
+                onClicked: {
+                    root.model.normalizeStereoChannelsIndependently = !checked;
+                }
+            }
+
+            StyledTextLabel {
+                anchors.verticalCenter: parent.verticalCenter
+                text: qsTrc("effects/loudness", "Normalize stereo channels independently")
+            }
+        }
+
+        Row {
+            spacing: 8
+
+            CheckBox {
+                enabled: !root.model.useRmsAlgorithm
+                checked: root.model.dualMono
+                onClicked: {
+                    root.model.dualMono = !checked;
+                }
+            }
+
+            StyledTextLabel {
+                anchors.verticalCenter: parent.verticalCenter
+                text: qsTrc("effects/loudness", "Treat mono as dual mono (recommended)")
+            }
+        }
+    }
+
     Component.onCompleted: {
-        normalizeLoudness.init()
+        root.model.init()
     }
 }
--- src/effects/builtin/loudness/normalizeloudnessviewmodel.cpp
+++ src/effects/builtin/loudness/normalizeloudnessviewmodel.cpp
@@ -20,5 +20,129 @@ NormalizeLoudnessEffect* NormalizeLoudnessViewModel::effect() const
 
 void NormalizeLoudnessViewModel::doReload()
 {
+    emit useRmsAlgorithmChanged();
+    emit perceivedLoudnessTargetChanged();
+    emit rmsTargetChanged();
+    emit normalizeStereoChannelsIndependentlyChanged();
+    emit dualMonoChanged();
+}
+
+bool NormalizeLoudnessViewModel::useRmsAlgorithm() const
+{
+    const NormalizeLoudnessEffect* e = effect();
+    if (!e) {
+        return false;
+    }
+    return e->mNormalizeTo == NormalizeLoudnessEffect::kRMS;
+}
+
+void NormalizeLoudnessViewModel::setUseRmsAlgorithm(bool useRmsAlgorithm)
+{
+    NormalizeLoudnessEffect* const e = effect();
+    if (!e) {
+        return;
+    }
+    modifySettings([e, useRmsAlgorithm](EffectSettings&) {
+        e->mNormalizeTo = useRmsAlgorithm ? NormalizeLoudnessEffect::kRMS : NormalizeLoudnessEffect::kLoudness;
+    });
+    emit useRmsAlgorithmChanged();
+}
+
+double NormalizeLoudnessViewModel::perceivedLoudnessTarget() const
+{
+    const NormalizeLoudnessEffect* e = effect();
+    if (!e) {
+        return 0.0;
+    }
+    return e->mLUFSLevel;
+}
+
+void NormalizeLoudnessViewModel::setPerceivedLoudnessTarget(double perceivedLoudnessTarget)
+{
+    NormalizeLoudnessEffect* const e = effect();
+    if (!e) {
+        return;
+    }
+    modifySettings([e, perceivedLoudnessTarget](EffectSettings&) {
+        e->mLUFSLevel = perceivedLoudnessTarget;
+    });
+    emit perceivedLoudnessTargetChanged();
+}
+
+double NormalizeLoudnessViewModel::rmsTarget() const
+{
+    const NormalizeLoudnessEffect* e = effect();
+    if (!e) {
+        return 0.0;
+    }
+    return e->mRMSLevel;
+}
+
+void NormalizeLoudnessViewModel::setRmsTarget(double rmsTarget)
+{
+    NormalizeLoudnessEffect* const e = effect();
+    if (!e) {
+        return;
+    }
+    modifySettings([e, rmsTarget](EffectSettings&) {
+        e->mRMSLevel = rmsTarget;
+    });
+    emit rmsTargetChanged();
+}
+
+double NormalizeLoudnessViewModel::targetMin() const
+{
+    // Doesn't matter which target we use, they have the same min
+    static_assert(NormalizeLoudnessEffect::LUFSLevel.min == NormalizeLoudnessEffect::RMSLevel.min);
+    return NormalizeLoudnessEffect::LUFSLevel.min;
+}
+
+double NormalizeLoudnessViewModel::targetMax() const
+{
+    // Doesn't matter which target we use, they have the same max
+    static_assert(NormalizeLoudnessEffect::LUFSLevel.max == NormalizeLoudnessEffect::RMSLevel.max);
+    return NormalizeLoudnessEffect::LUFSLevel.max;
+}
+
+bool NormalizeLoudnessViewModel::normalizeStereoChannelsIndependently() const
+{
+    const NormalizeLoudnessEffect* e = effect();
+    if (!e) {
+        return false;
+    }
+    return e->mStereoInd;
+}
+
+void NormalizeLoudnessViewModel::setNormalizeStereoChannelsIndependently(bool normalizeStereoChannelsIndependently)
+{
+    NormalizeLoudnessEffect* const e = effect();
+    if (!e) {
+        return;
+    }
+    modifySettings([e, normalizeStereoChannelsIndependently](EffectSettings&) {
+        e->mStereoInd = normalizeStereoChannelsIndependently;
+    });
+    emit normalizeStereoChannelsIndependentlyChanged();
+}
+
+bool NormalizeLoudnessViewModel::dualMono() const
+{
+    const NormalizeLoudnessEffect* e = effect();
+    if (!e) {
+        return false;
+    }
+    return e->mDualMono;
+}
+
+void NormalizeLoudnessViewModel::setDualMono(bool dualMono)
+{
+    NormalizeLoudnessEffect* const e = effect();
+    if (!e) {
+        return;
+    }
+    modifySettings([e, dualMono](EffectSettings&) {
+        e->mDualMono = dualMono;
+    });
+    emit dualMonoChanged();
 }
 }
