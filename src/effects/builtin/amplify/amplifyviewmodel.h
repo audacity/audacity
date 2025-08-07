638ed8f74845eb3ddd0b23f9ebc27c949793f34e
Paul MARTIN <luapmartin@gmail.com>
2025-08-07 20:49:28 +0200

    Refactor AmplifyView to use properties from AmplifyViewModel for labels and values

--- src/effects/builtin/amplify/amplifyviewmodel.h
+++ src/effects/builtin/amplify/amplifyviewmodel.h
@@ -13,14 +13,26 @@ class AmplifyEffect;
 class AmplifyViewModel : public AbstractEffectModel
 {
     Q_OBJECT
+
+    Q_PROPERTY(QString title READ title CONSTANT FINAL)
+
+    Q_PROPERTY(QString ampLabel READ ampLabel CONSTANT FINAL)
     Q_PROPERTY(float amp READ amp WRITE setAmp NOTIFY ampChanged FINAL)
     Q_PROPERTY(float ampMin READ ampMin NOTIFY ampChanged FINAL)
     Q_PROPERTY(float ampMax READ ampMax NOTIFY ampChanged FINAL)
+    Q_PROPERTY(QString ampMeasureUnitsSymbol READ ampMeasureUnitsSymbol CONSTANT FINAL)
+    Q_PROPERTY(int ampDecimals READ ampDecimals CONSTANT FINAL)
+    Q_PROPERTY(double ampStep READ ampStep CONSTANT FINAL)
 
+    Q_PROPERTY(QString newPeakLabel READ newPeakLabel CONSTANT FINAL)
     Q_PROPERTY(float newPeak READ newPeak WRITE setNewPeak NOTIFY newPeakChanged FINAL)
     Q_PROPERTY(float newPeakMin READ newPeakMin NOTIFY newPeakChanged FINAL)
     Q_PROPERTY(float newPeakMax READ newPeakMax NOTIFY newPeakChanged FINAL)
+    Q_PROPERTY(QString newPeakMeasureUnitsSymbol READ newPeakMeasureUnitsSymbol CONSTANT FINAL)
+    Q_PROPERTY(int newPeakDecimals READ newPeakDecimals CONSTANT FINAL)
+    Q_PROPERTY(double newPeakStep READ newPeakStep CONSTANT FINAL)
 
+    Q_PROPERTY(QString canClipLabel READ canClipLabel CONSTANT FINAL)
     Q_PROPERTY(bool canClip READ canClip WRITE setCanClip NOTIFY canClipChanged FINAL)
 
     Q_PROPERTY(bool isApplyAllowed READ isApplyAllowed NOTIFY isApplyAllowedChanged FINAL)
@@ -30,16 +42,27 @@ class AmplifyViewModel : public AbstractEffectModel
 public:
     AmplifyViewModel() = default;
 
+    QString title() const;
+
+    QString ampLabel() const;
     float amp() const;
     void setAmp(float newAmp);
     float ampMin() const;
     float ampMax() const;
+    QString ampMeasureUnitsSymbol() const;
+    int ampDecimals() const;
+    double ampStep() const;
 
+    QString newPeakLabel() const;
     float newPeak() const;
     void setNewPeak(float newNewPeak);
     float newPeakMin() const;
     float newPeakMax() const;
+    QString newPeakMeasureUnitsSymbol() const;
+    int newPeakDecimals() const;
+    double newPeakStep() const;
 
+    QString canClipLabel() const;
     bool canClip() const;
     void setCanClip(bool newCliping);
 
