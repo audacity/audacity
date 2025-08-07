d2a852eb805775a4d632c12576678c50539076a7
Matthieu Hodgkinson <saintmatthieu@gmail.com>
2025-08-07 13:45:43 +0200

    LoudnessBase -> NormalizeLoudnessEffect

--- src/effects/builtin/loudness/normalizeloudnessviewmodel.cpp
+++ src/effects/builtin/loudness/normalizeloudnessviewmodel.cpp
@@ -3,19 +3,19 @@
  */
 #include "normalizeloudnessviewmodel.h"
 
-#include "libraries/lib-builtin-effects/LoudnessBase.h"
+#include "normalizeloudnesseffect.h"
 
 #include "log.h"
 
 namespace au::effects {
-LoudnessBase* NormalizeLoudnessViewModel::effect() const
+NormalizeLoudnessEffect* NormalizeLoudnessViewModel::effect() const
 {
     const EffectId effectId = this->effectId();
     if (effectId.isEmpty()) {
         return nullptr;
     }
     Effect* const e = effectsProvider()->effect(effectId);
-    return dynamic_cast<LoudnessBase*>(e);
+    return dynamic_cast<NormalizeLoudnessEffect*>(e);
 }
 
 void NormalizeLoudnessViewModel::doReload()
