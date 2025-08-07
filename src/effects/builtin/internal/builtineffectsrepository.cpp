70b596f9e69c0027e097eb13c1ee381986ba80d8
Matthieu Hodgkinson <saintmatthieu@gmail.com>
2025-08-07 13:45:43 +0200

    LoudnessBase -> NormalizeLoudnessEffect

--- src/effects/builtin/internal/builtineffectsrepository.cpp
+++ src/effects/builtin/internal/builtineffectsrepository.cpp
@@ -17,7 +17,7 @@
 
 #include "amplify/amplifyeffect.h"
 #include "amplify/amplifyviewmodel.h"
-#include "libraries/lib-builtin-effects/LoudnessBase.h"
+#include "loudness/normalizeloudnesseffect.h"
 #include "loudness/normalizeloudnessviewmodel.h"
 #include "clickremoval/clickremovaleffect.h"
 #include "clickremoval/clickremovalviewmodel.h"
@@ -107,7 +107,7 @@ void BuiltinEffectsRepository::preInit()
     static BuiltinEffectsModule::Registration< Repair > regRepair;
     static BuiltinEffectsModule::Registration< ReverseEffect > regReverse;
     static BuiltinEffectsModule::Registration< AmplifyEffect > regAmplify;
-    static BuiltinEffectsModule::Registration< LoudnessBase > regLoudness;
+    static BuiltinEffectsModule::Registration< NormalizeLoudnessEffect > regLoudness;
     static BuiltinEffectsModule::Registration< GraphicEq > regGraphicEq;
     static BuiltinEffectsModule::Registration< ClickRemovalEffect > regClickRemoval;
     static BuiltinEffectsModule::Registration< NormalizeEffect > regNormalize;
@@ -181,9 +181,9 @@ void BuiltinEffectsRepository::updateEffectMetaList()
                     EffectCategoryId::VolumeAndCompression,
                     false
                     );
-        } else if (symbol == LoudnessBase::Symbol) {
+        } else if (symbol == NormalizeLoudnessEffect::Symbol) {
             qmlRegisterType<NormalizeLoudnessViewModel>("Audacity.Effects", 1, 0, "NormalizeLoudnessViewModel");
-            regView(LoudnessBase::Symbol, u"qrc:/loudness/NormalizeLoudnessView.qml");
+            regView(NormalizeLoudnessEffect::Symbol, u"qrc:/loudness/NormalizeLoudnessView.qml");
             regMeta(desc,
                     muse::mtrc("effects", "Loudness normalization"),
                     muse::mtrc("effects", "Sets the loudness of one or more tracks"),
