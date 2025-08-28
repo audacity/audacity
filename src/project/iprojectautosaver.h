a5d9cb8461164d1d95242f9f34d4ba59ad7a38ee
Paul MARTIN <luapmartin@gmail.com>
2025-08-28 10:35:44 +0200

    8826 more cleanups to autosave logic

--- src/project/iprojectautosaver.h
+++ src/project/iprojectautosaver.h
@@ -25,6 +25,7 @@
 #include "io/path.h"
 
 #include "modularity/imoduleinterface.h"
+#include "iaudacityproject.h"
 
 namespace au::project {
 class IProjectAutoSaver : MODULE_EXPORT_INTERFACE
@@ -34,10 +35,11 @@ class IProjectAutoSaver : MODULE_EXPORT_INTERFACE
 public:
     virtual ~IProjectAutoSaver() = default;
 
-    virtual bool projectHasUnsavedChanges(const muse::io::path_t& projectPath) const = 0;
+    virtual bool projectHasUnsavedChanges(IAudacityProjectPtr project) const = 0;
     virtual void removeProjectUnsavedChanges(const muse::io::path_t& projectPath) = 0;
 
-    virtual bool isAutosaveOfNewlyCreatedProject(const muse::io::path_t& projectPath) const = 0;
+    virtual bool isPathToNewlyCreatedProject(const muse::io::path_t& projectPath) const = 0;
+    virtual bool isNewlyCreatedProject(IAudacityProjectPtr project) const = 0;
 };
 }
 
