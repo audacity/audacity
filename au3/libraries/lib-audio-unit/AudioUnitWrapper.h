c4009834746a6015243a159f35aee64fce01e644
Dmitry Makarenko <kryksyh@gmail.com>
2025-09-13 13:07:15 +0300

    Unqualified std::move()

--- au3/libraries/lib-audio-unit/AudioUnitWrapper.h
+++ au3/libraries/lib-audio-unit/AudioUnitWrapper.h
@@ -62,7 +62,7 @@ struct AudioUnitEffectSettings {
 
     AudioUnitEffectSettings() = default;
     AudioUnitEffectSettings(Map map)
-        : values{move(map)} {}
+        : values{std::move(map)} {}
 
     //! Get a pointer to a durable copy of `name`
     //! May allocate memory
