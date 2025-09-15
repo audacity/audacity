fa0052428b1b9556fdde43d839bbdeb718176b9a
Matthieu Hodgkinson <saintmatthieu@gmail.com>
2025-09-15 14:55:10 +0200

    disambiguate production from test meter models

---
+++ src/effects/builtin/dynamics/timeline/test/meters/dynamicseffectoutputmetertestmodel.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "abstractdynamicsmetertestmodel.h"
+
+namespace au::effects {
+class DynamicsEffectOutputMeterTestModel : public AbstractDynamicsMeterTestModel
+{
+    Q_OBJECT
+
+public:
+    Direction direction() const override { return Direction::Upwards; }
+};
+} // namespace au::effects
