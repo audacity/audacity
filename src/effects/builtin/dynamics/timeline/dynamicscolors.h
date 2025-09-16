2937139de97472e10cd63e14e7c9a0047274ffd6
Matthieu Hodgkinson <saintmatthieu@gmail.com>
2025-09-16 14:25:20 +0200

    Colors are centralized

---
+++ src/effects/builtin/dynamics/timeline/dynamicscolors.h
@@ -0,0 +1,40 @@
+/*
+ * Audacity: A Digital Audio Editor
+ */
+#pragma once
+
+#include <QColor>
+#include <QObject>
+#include <QQmlEngine>
+
+namespace au::effects {
+class DynamicsColors : public QObject
+{
+    Q_OBJECT
+    QML_ELEMENT
+    QML_SINGLETON
+
+public:
+    static QColor gridColor() { return QColor("#7B7B81"); }
+    static QColor backgroundColor() { return QColor("#313147"); }
+
+    static QColor timelineDataFillColor() { return QColor("#565695"); }
+    static QColor timelineDataFillColorSemiTransparent() { return QColor("#80565695"); }
+
+    static QColor timelineCompressionDbColor() { return QColor("#FFD12C"); }
+    static QColor timelineCompressionDbColorSemiTransparent() { return QColor("#80FFD12C"); }
+
+    static QColor timelineOutputDbLineColor() { return QColor("white"); }
+
+    Q_PROPERTY(QColor gridColor READ gridColor CONSTANT)
+    Q_PROPERTY(QColor backgroundColor READ backgroundColor CONSTANT)
+
+    Q_PROPERTY(QColor timelineDataFillColor READ timelineDataFillColor CONSTANT)
+    Q_PROPERTY(QColor timelineDataFillColorSemiTransparent READ timelineDataFillColorSemiTransparent CONSTANT)
+
+    Q_PROPERTY(QColor timelineCompressionDbColor READ timelineCompressionDbColor CONSTANT)
+    Q_PROPERTY(QColor timelineCompressionDbColorSemiTransparent READ timelineCompressionDbColorSemiTransparent CONSTANT)
+
+    Q_PROPERTY(QColor timelineOutputDbLineColor READ timelineOutputDbLineColor CONSTANT)
+};
+}
--- src/effects/builtin/internal/builtineffectsrepository.cpp
+++ src/effects/builtin/internal/builtineffectsrepository.cpp
@@ -23,6 +23,7 @@
 #include "clickremoval/clickremovalviewmodel.h"
 #include "dynamics/timeline/meters/compressiondbmetermodel.h"
 #include "dynamics/timeline/meters/outputdbmetermodel.h"
+#include "dynamics/timeline/dynamicscolors.h"
 #include "dynamics/timeline/dynamicstimeline.h"
 #include "dynamics/timeline/timelinesourcemodel.h"
 #include "dynamics/timeline/stopwatch.h"
@@ -358,6 +359,12 @@ void BuiltinEffectsRepository::updateEffectMetaList()
     if (hasDynamicRangeProcessor) {
         // These types are used by both Compressor and Limiter, so register them only if at least one of these effects is present.
         qmlRegisterType<DynamicsTimeline>("Audacity.BuiltinEffects", 1, 0, "DynamicsTimeline");
+        qmlRegisterSingletonType<DynamicsColors>("Audacity.BuiltinEffects", 1, 0, "DynamicsColors",
+                                                 [](QQmlEngine* engine, QJSEngine* scriptEngine) -> QObject* {
+            Q_UNUSED(engine)
+            Q_UNUSED(scriptEngine)
+            return new DynamicsColors();
+        });
         qmlRegisterType<TimelineSourceModel>("Audacity.BuiltinEffects", 1, 0, "TimelineSourceModel");
         qmlRegisterType<CompressionDbMeterModel>("Audacity.BuiltinEffects", 1, 0, "CompressionDbMeterModel");
         qmlRegisterType<OutputDbMeterModel>("Audacity.BuiltinEffects", 1, 0, "OutputDbMeterModel");
