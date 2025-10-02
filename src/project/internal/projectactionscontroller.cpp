81123d2f6a685933d38e7c80b2d0662c5ee33feb
Gabriel Sartori <g.sartori@mu.se>
2025-10-02 11:36:38 -0300

    Test

--- src/project/internal/projectactionscontroller.cpp
+++ src/project/internal/projectactionscontroller.cpp
@@ -4,6 +4,7 @@
 #include <qfiledialog.h>
 
 #include "async/async.h"
+#include "buildscripts/pch/pch.h"
 #include "global/defer.h"
 #include "global/translation.h"
 
@@ -315,29 +316,29 @@ muse::io::path_t ProjectActionsController::selectOpeningFile()
 io::path_t ProjectActionsController::selectImportFile()
 {
     std::string audioFileExt
-        = "*.aac *.ac3 *.mp3 *.wma *.wav *.flac *.ogg *.opus *.aif *.aiff *.amr *.ape *.au *.dts *.mpc *.tta *.wv *.shn *.voc *.mmf";
+        = "*.aac;*.ac3;*.mp3;*.wma;*.wav;*.flac;*.ogg;*.opus;*.aif;*.aiff;*.amr;*.ape;*.au;*.dts;*.mpc;*.tta;*.wv;*.shn;*.voc;*.mmf";
     std::string videoFileExt
-        = "*.avi *.mp4 *.mkv *.mov *.flv *.wmv *.asf *.webm *.mpg *.mpeg *.m4v *.ts *.gxf *.mxf *.nut *.dv *.3gp *.3g2 *.mj2";
+        = "*.avi;*.mp4;*.mkv;*.mov;*.flv;*.wmv;*.asf;*.webm;*.mpg;*.mpeg;*.m4v;*.ts;*.gxf;*.mxf;*.nut;*.dv;*.3gp;*.3g2;*.mj2";
     std::string gameMediaFileExt
         =
-            "*.roq *.bethsoftvid *.c93 *.dsicin *.dxa *.ea *.cdata *.film_cpk *.idcin *.ipmovie *.psxstr *.rl2 *.siff *.smk *.thp *.tiertexseq *.vmd *.wc3movie *.wsaud *.wsvqa *.txd";
-    std::string streamingFileExt = "*.rtsp *.sdp *.nsv *.pva *.msnwctcp *.lmlm4 *.redir";
-    std::string animationAndImageFileExt = "*.gif *.flic *.swf *.image2 *.image2pipe";
+            "*.roq;*.bethsoftvid;*.c93;*.dsicin;*.dxa;*.ea;*.cdata;*.film_cpk;*.idcin;*.ipmovie;*.psxstr;*.rl2;*.siff;*.smk;*.thp;*.tiertexseq;*.vmd;*.wc3movie;*.wsaud;*.wsvqa;*.txd";
+    std::string streamingFileExt = "*.rtsp;*.sdp;*.nsv;*.pva;*.msnwctcp;*.lmlm4;*.redir";
+    std::string animationAndImageFileExt = "*.gif;*.flic;*.swf;*.image2;*.image2pipe";
     std::string rawFileExt
         =
-            "*.al *.ul *.s16be *.u16be *.s8 *.u8 *.ub *.uw *.4xm *.MTV *.afc  *.aifc  *.apc  *.apl  *.mac  *.avs  *.302  *.daud  *.ffm  *.cgi  *.mm  *.mpegtsraw  *.mpegvideo  *.nuv  *.sw  *.sb  *.son  *.sol  *.vfwcap";
+            "*.al;*.ul;*.s16be;*.u16be;*.s8;*.u8;*.ub;*.uw;*.4xm;*.MTV;*.afc;*.aifc;*.apc;*.apl;*.mac;*.avs;*.302;*.daud;*.ffm;*.cgi;*.mm;*.mpegtsraw;*.mpegvideo;*.nuv;*.sw;*.sb;*.son;*.sol;*.vfwcap";
 
     std::string allExt = audioFileExt + " " + videoFileExt + " " + gameMediaFileExt + " " + streamingFileExt + " "
                          + animationAndImageFileExt + " " + rawFileExt;
 
     std::vector<std::string> filter {
-        trc("project", "All supported files") + " (" + allExt + ")",
-        trc("project", "Audio files") + " (" + audioFileExt + ")",
-        trc("project", "Video files") + " (" + videoFileExt + ")",
-        trc("project", "Game media files") + " (" + gameMediaFileExt + ")",
-        trc("project", "Streaming files") + " (" + streamingFileExt + ")",
-        trc("project", "Animation and image files") + " (" + animationAndImageFileExt + ")",
-        trc("project", "Raw files") + " (" + rawFileExt + ")"
+        "All supported files (" + allExt + ")",
+        "Audio files (" + audioFileExt + ")",
+        "Video files (" + videoFileExt + ")",
+        "Game media files (" + gameMediaFileExt + ")",
+        "Streaming files (" + streamingFileExt + ")",
+        "Animation and image files (" + animationAndImageFileExt + ")",
+        "Raw files (" + rawFileExt + ")"
     };
 
     io::path_t defaultDir = configuration()->lastOpenedProjectsPath();
@@ -351,8 +352,7 @@ io::path_t ProjectActionsController::selectImportFile()
     }
 
     io::path_t filePath = interactive()->selectOpeningFileSync(muse::trc("project",
-                                                                         "Open"), defaultDir, filter,
-                                                               QFileDialog::HideNameFilterDetails | QFileDialog::DontUseNativeDialog);
+                                                                         "Open"), defaultDir, filter, QFileDialog::HideNameFilterDetails);
 
     if (!filePath.empty()) {
         configuration()->setLastOpenedProjectsPath(io::dirpath(filePath));
