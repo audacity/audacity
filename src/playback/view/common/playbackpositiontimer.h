d7d8f5a05a64d336421c8f6c6e3e2b21f5fed643
Matthieu Hodgkinson <saintmatthieu@gmail.com>
2025-10-13 23:29:02 +0200

    PlaybackPositionTimer does not rely on the timer events of other objects (and only calls when playback is active)

--- src/playback/view/common/playbackpositiontimer.h
+++ src/playback/view/common/playbackpositiontimer.h
@@ -2,12 +2,14 @@
 
 #include "playback/iplayback.h"
 
+#include "framework/global/async/asyncable.h"
 #include "framework/global/modularity/ioc.h"
 
 #include <QQuickItem>
+#include <QTimer>
 
 namespace au::playback {
-class PlaybackPositionTimer : public QQuickItem
+class PlaybackPositionTimer : public QQuickItem, public muse::async::Asyncable
 {
     muse::Inject<IPlayback> playback;
 
@@ -18,7 +20,10 @@ public:
 private:
     void itemChange(ItemChange change, const ItemChangeData& value) override;
     void doBeforeSynchronizing();
+    void componentComplete() override;
+    std::shared_ptr<IPlayer> player() const;
 
     QMetaObject::Connection m_beforeSynchronizingConnection{};
+    QTimer m_timer;
 };
 }
