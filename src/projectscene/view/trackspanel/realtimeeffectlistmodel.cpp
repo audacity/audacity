6eef24e281786e5f80358addef6fd17b127c74d3
Matthieu Hodgkinson <saintmatthieu@mailbox.org>
2025-11-05 11:50:31 +0100

    9755 realtime effects no persistent fix (#9818)
    Resolves: #9755
    
    <!-- Use "x" to fill the checkboxes below like [x] -->
    
    - [x] I signed [CLA](https://www.audacityteam.org/cla/)
    - [x] The title of the pull request describes an issue it addresses
    - [x] If changes are extensive, then there is a sequence of easily
    reviewable commits
    - [x] Each commit's message describes its purpose and effects
    - [x] There are no behavior changes unnecessary for the stated purpose
    of the PR
    
    Recommended:
    - [x] Each commit compiles and runs on my machine without known
    undesirable changes of behavior
    
    QA:
    
    - [ ] Autobot test cases have been run
    
    Co-authored-by: Matthieu Hodgkinson <saintmatthieu@gmail.com>

--- src/projectscene/view/trackspanel/realtimeeffectlistmodel.cpp
+++ src/projectscene/view/trackspanel/realtimeeffectlistmodel.cpp
@@ -25,6 +25,13 @@ void RealtimeEffectListModel::onProjectChanged()
                 emit trackNameChanged();
             }
         });
+        const std::vector<au::trackedit::TrackId> trackIds = project->trackIdList();
+        for (const auto& trackId : trackIds) {
+            if (belongsWithMe(trackId)) {
+                // Project was just opened, we can use brute-force `onChanged`, it won't have an impact on scrollbar position or alike.
+                onChanged(trackId);
+            }
+        }
     } else {
         beginResetModel();
         m_trackEffectLists.clear();
