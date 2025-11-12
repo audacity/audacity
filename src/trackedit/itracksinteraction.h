6d622361182f18fa823a37193bcfd51222a1a8e9
Gabriel Sartori <g.sartori@mu.se>
2025-11-12 14:56:18 -0300

    fix(rulers): limit zoom on db waveform to avoid not integer db steps

--- src/trackedit/itracksinteraction.h
+++ src/trackedit/itracksinteraction.h
@@ -68,6 +68,7 @@ public:
     virtual void verticalZoomIn(const trackedit::TrackId& trackId) = 0;
     virtual void verticalZoomOut(const trackedit::TrackId& trackId) = 0;
     virtual void resetVerticalZoom(const trackedit::TrackId& trackId) = 0;
+    virtual void adjustVerticalZoom(const trackedit::TrackId& trackId) = 0;
 
     virtual double nearestZeroCrossing(double time) const = 0;
 
