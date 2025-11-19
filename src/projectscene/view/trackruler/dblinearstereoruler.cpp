48f2368185de379dc64f53d65e12c1dff078f4e7
Gabriel Sartori <g.sartori@mu.se>
2025-11-19 06:37:01 -0300

    fix(rulers): remove small steps when too cramped

--- src/projectscene/view/trackruler/dblinearstereoruler.cpp
+++ src/projectscene/view/trackruler/dblinearstereoruler.cpp
@@ -10,6 +10,7 @@ using namespace au::projectscene;
 namespace {
 constexpr int MIN_FULL_STEP_TO_INF_HEIGHT = 14;
 constexpr int MIN_ADJACENT_STEPS_HEIGHT = 10;
+constexpr int MIN_ADJACENT_SMALL_STEPS_HEIGHT = 5;
 constexpr int MIN_FULL_STEP_TO_ZERO_HEIGHT = 10;
 constexpr int MIN_CHANNEL_HEIGHT = 30;
 const std::vector<int> FULL_STEP_SIZES = { 1, 2, 3, 6, 9 };
@@ -83,9 +84,27 @@ std::vector<TrackRulerSmallStep> DbLinearStereoRuler::smallSteps() const
     std::vector<double> channelHeights = { m_height* m_channelHeightRatio, m_height* (1.0 - m_channelHeightRatio) };
     std::vector<TrackRulerSmallStep> steps;
     for (size_t channel = 0; channel < 2; ++channel) {
-        const int lowestFullStep = computeLowestFullStepValue(channelHeights[channel]);
-        const std::vector<int> valuesList = fullStepValues(channelHeights[channel]);
-        for (int i = lowestFullStep; i < m_maxDisplayValueDB; i++) {
+        std::vector<int> valuesList = fullStepValues(channelHeights[channel]);
+        valuesList.push_back(static_cast<int>(m_maxDisplayValueDB));
+
+        std::vector<std::tuple<int, int> > fullStepRanges;
+        fullStepRanges.reserve(valuesList.size() - 1);
+        for (size_t i = 0; i < valuesList.size() - 1; ++i) {
+            fullStepRanges.emplace_back(valuesList[i], valuesList[i + 1]);
+        }
+
+        // We ensure small steps will be shown onlu if there is enough room between full steps
+        int lowestSmallStep = static_cast<int>(m_maxDisplayValueDB);
+        for (const auto& [startValue, endValue] : fullStepRanges) {
+            double firstPos = valueToPosition(startValue, channelHeights[channel], false);
+            double secondPos = valueToPosition(endValue, channelHeights[channel], false);
+            if (((firstPos - secondPos) / (endValue - startValue)) > MIN_ADJACENT_SMALL_STEPS_HEIGHT) {
+                lowestSmallStep = startValue + 1;
+                break;
+            }
+        }
+
+        for (int i = lowestSmallStep; i < m_maxDisplayValueDB; i++) {
             if (std::find(valuesList.begin(), valuesList.end(), i) != valuesList.end()) {
                 continue;
             }
