0c5a69f8e41d2ad0b6d9de602a7b347cb650047b
Matthieu Hodgkinson <saintmatthieu@mailbox.org>
2025-12-02 13:33:13 +0100

    fix(spectral tools): tracks use own SpectrogramSettings

--- src/trackedit/internal/au3/au3trackspectrogramconfiguration.cpp
+++ src/trackedit/internal/au3/au3trackspectrogramconfiguration.cpp
@@ -19,12 +19,12 @@ std::shared_ptr<Au3TrackSpectrogramConfiguration> Au3TrackSpectrogramConfigurati
                                                                                            const au::context::IGlobalContext& context)
 {
     const project::IAudacityProjectPtr project = context.currentProject();
-    const auto au3Project = reinterpret_cast<const au::au3::Au3Project*>(project->au3ProjectPtr());
-    const au::au3::Au3WaveTrack* waveTrack = au::au3::DomAccessor::findWaveTrack(*au3Project, au::au3::Au3TrackId { trackId });
+    auto au3Project = reinterpret_cast<au::au3::Au3Project*>(project->au3ProjectPtr());
+    au::au3::Au3WaveTrack* waveTrack = au::au3::DomAccessor::findWaveTrack(*au3Project, au::au3::Au3TrackId { trackId });
     IF_ASSERT_FAILED(waveTrack) {
         return nullptr;
     }
-    return std::make_shared<Au3TrackSpectrogramConfiguration>(::SpectrogramSettings::Get(*waveTrack));
+    return std::make_shared<Au3TrackSpectrogramConfiguration>(::SpectrogramSettings::Own(*waveTrack));
 }
 
 Au3TrackSpectrogramConfiguration::Au3TrackSpectrogramConfiguration(::SpectrogramSettings& settings)
