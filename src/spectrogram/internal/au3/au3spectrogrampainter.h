04b3e53b77b318ebaeaf773b85e46c5714c7a73d
Matthieu Hodgkinson <saintmatthieu@mailbox.org>
2025-12-04 00:58:08 +0100

    refactor(spectral tools): WaveMetrics removed from spectrogram

--- src/spectrogram/internal/au3/au3spectrogrampainter.h
+++ src/spectrogram/internal/au3/au3spectrogrampainter.h
@@ -25,7 +25,7 @@ public:
     void init();
 
     void paintClip(QPainter&, int xBegin, int xEnd, int trackHeight, double viewportT0, double viewportT1, double zoom,
-                   const trackedit::ClipKey&, const projectscene::WaveMetrics&, const SelectedRegion&) override;
+                   const trackedit::ClipKey&, const SelectedRegion&) override;
 
 private:
     std::weak_ptr<au3::Au3Project> m_au3Project;
