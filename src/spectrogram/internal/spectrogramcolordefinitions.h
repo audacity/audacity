f13333c18a5e699ede897fc3d1e4d1d20451e6ec
Matthieu Hodgkinson <saintmatthieu@mailbox.org>
2025-12-03 18:08:28 +0100

    fix(spectral tools): Reinstate lost color definitions, needed for classic spectrogram sceheme

--- src/spectrogram/internal/spectrogramcolordefinitions.h
+++ src/spectrogram/internal/spectrogramcolordefinitions.h
@@ -9,4 +9,13 @@ DEFINE_COLOUR(clrUnselected, QColor(30,  30,  30), wxT("Unselected"));
 DEFINE_COLOUR(clrSelected,   QColor(93,  65,  93), wxT("Selected"));
 
 DEFINE_COLOUR(clrSpectro1,            QColor(191,  191,  191),  wxT("Spectro1"));
+DEFINE_COLOUR(clrSpectro2,            QColor(76,  153,  255),  wxT("Spectro2"));
+DEFINE_COLOUR(clrSpectro3,            QColor(229,   25,  229),  wxT("Spectro3"));
+DEFINE_COLOUR(clrSpectro4,            QColor(255,    0,    0),  wxT("Spectro4"));
+DEFINE_COLOUR(clrSpectro5,            QColor(255,  255,  255),  wxT("Spectro5"));
+
 DEFINE_COLOUR(clrSpectro1Sel,         QColor(143,  143,  143),  wxT("Spectro1Sel"));
+DEFINE_COLOUR(clrSpectro2Sel,         QColor(57,  116,  191),  wxT("Spectro2Sel"));
+DEFINE_COLOUR(clrSpectro3Sel,         QColor(172,   19,  172),  wxT("Spectro3Sel"));
+DEFINE_COLOUR(clrSpectro4Sel,         QColor(191,    0,    0),  wxT("Spectro4Sel"));
+DEFINE_COLOUR(clrSpectro5Sel,         QColor(191,  191,  191),  wxT("Spectro5Sel"));
