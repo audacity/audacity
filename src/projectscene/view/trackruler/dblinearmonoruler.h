19e15e62188f19f6f36c940c70c8b3838583e097
Gabriel Sartori <g.sartori@mu.se>
2025-12-03 06:34:48 -0300

    fix(rulers): adjust colapsed values for half wave and db linear rulers

--- src/projectscene/view/trackruler/dblinearmonoruler.h
+++ src/projectscene/view/trackruler/dblinearmonoruler.h
@@ -4,6 +4,7 @@
 #pragma once
 
 #include "projectscene/view/trackruler/dblinearbaseruler.h"
+#include "view/trackruler/itrackruler.h"
 
 namespace au::projectscene {
 class DbLinearMonoRuler : public DbLinearBaseRuler
@@ -13,5 +14,8 @@ public:
     double stepToPosition(double step, size_t channel, bool isNegativeSample) const override;
     std::vector<TrackRulerFullStep> fullSteps() const override;
     std::vector<TrackRulerSmallStep> smallSteps() const override;
+private:
+    std::vector<TrackRulerFullStep> fullStepsForHalfWave() const;
+    std::vector<TrackRulerFullStep> fullStepsForFullWave() const;
 };
 }
