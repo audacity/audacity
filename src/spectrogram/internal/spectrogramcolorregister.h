7d586a89ced9a25757d8b71fc874b33c1bb78d98
Matthieu Hodgkinson <saintmatthieu@mailbox.org>
2025-12-04 12:39:06 +0100

    refactor(spectral tools): SpectrogramColorRegister clean-up

--- src/spectrogram/internal/spectrogramcolorregister.h
+++ src/spectrogram/internal/spectrogramcolorregister.h
@@ -10,45 +10,21 @@
 #include <unordered_set>
 #include <vector>
 
-struct ThemeSet
-{
-    // wxImage, wxBitmap copy cheaply using reference counting
-    std::vector<QColor> mColours;
-
-    bool bInitialised = false;
-};
-
-class SpectrogramColorRegisterBase
+class SpectrogramColorRegister final
 {
 public:
-    SpectrogramColorRegisterBase (const SpectrogramColorRegisterBase&) = delete;
-    SpectrogramColorRegisterBase& operator =(const SpectrogramColorRegisterBase&) = delete;
-
-    SpectrogramColorRegisterBase() = default;
-    virtual ~SpectrogramColorRegisterBase() = default;
-
-    virtual void EnsureInitialised() = 0;
-
     using NameSet = std::unordered_set<wxString>;
 
+    void EnsureInitialised();
     void RegisterColour(NameSet& allNames, int& iIndex, const QColor& Clr, const wxString& Name);
 
     QColor& Colour(int iIndex);
-
-protected:
-    wxArrayString mColourNames;
-
-    ThemeSet mSet;
-};
-
-class SpectrogramColorRegister final : public SpectrogramColorRegisterBase
-{
-public:
-    SpectrogramColorRegister() = default;
-    ~SpectrogramColorRegister() override = default;
-
-    void EnsureInitialised() override;
     void RegisterColours();
+
+private:
+    wxArrayString mColourNames;
+    std::vector<QColor> mColours;
+    bool mColorsInitialized = false;
 };
 
 extern SpectrogramColorRegister spectrogramColorRegister;
