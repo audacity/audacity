audacity/audacity

commit 32e5e34ad5c7229acba2c8a78cda528a6ad34232

Add middle-mouse drag panning to TrackPanel

src/TrackPanel.cpp
@@ -741,6 +741,21 @@ void TrackPanel::OnMouseEvent(wxMouseEvent & event)
       mTimer.Start(std::chrono::milliseconds{kTimerInterval}.count(), FALSE);
    }
 
+   if (event.MiddleDown()) {
+      mIsPanning = true;
+      mLastPanX = event.GetX();
+      CaptureMouse();
+   }
+   else if (event.MiddleUp()) {
+      mIsPanning = false;
+      ReleaseMouse();
+   }
+   else if (event.Dragging() && mIsPanning) {
+      int dx = event.GetX() - mLastPanX;
+      mLastPanX = event.GetX();
+
+      Viewport::Get(*GetProject()).ScrollHorizontalByPixels(-dx);
+   }
 
    if (event.ButtonUp()) {
       //ShowTrack should be called after processing the up-click.

libraries/lib-viewport/Viewport.cpp
@@ -218,6 +218,24 @@ void Viewport::SetHorizontalThumb(double scrollto, bool doScroll)
       DoScroll();
 }
 
+void Viewport::ScrollHorizontalByPixels(int deltaPixels)
+{
+    if (!mpCallbacks)
+        return;
+    int current = mpCallbacks->GetHorizontalThumbPosition();
+    int max = mpCallbacks->GetHorizontalRange() -
+              mpCallbacks->GetHorizontalThumbSize();
+    int newPos = std::clamp(current - deltaPixels, 0, max);
+    sbarH += deltaPixels;
+    sbarH = std::clamp<wxInt64>(
+        sbarH,
+        -PixelWidthBeforeTime(0.0),
+        std::max(sbarTotal - PixelWidthBeforeTime(0.0) - sbarScreen, 0.)
+    );
+    mpCallbacks->SetHorizontalThumbPosition(newPos);
+    DoScroll();
+}
+
 bool Viewport::ScrollUpDown(int delta)
 {
    int oldPos = mpCallbacks ? mpCallbacks->GetVerticalThumbPosition() : 0;

libraries/lib-viewport/Viewport.h
@@ -129,6 +129,8 @@ public:
 
    void SetHorizontalThumb(double scrollto, bool doScroll = true);
 
+   void ScrollHorizontalByPixels(int deltaPixels);
+
    //! Set timeline magnification; unchanged left edge time
    void Zoom(double pixelsPerSecond);
 

src/TrackPanel.h
@@ -214,6 +214,9 @@ protected:
 
    bool mRefreshBacking;
 
+   bool mIsPanning;
+   int mLastPanX;
+
 
 protected:
 

commit 661601e144449d1eeda13b6ad1edc485bb9daa28

TrackPanel: Initialize new members
Commit 32e5e34ad5c7229acba2c8a78cda528a6ad34232 introduced
new members to TrackPanel class, mIsPanning and mLastPanX,
but they were not initialized when TrackPanel instances were
constucted. Because of this, mIsPanning could be initialized
to something true-ish, which manifested itself as scrolling
to random place when trying to select part of the track in my
Audacity builds.

Fixes: 32e5e34ad5c7229acba2c8a78cda528a6ad34232
Signed-off-by: Ivan A. Melnikov <iv@altlinux.org>

src/TrackPanel.cpp
@@ -266,7 +266,9 @@ TrackPanel::TrackPanel(wxWindow * parent, wxWindowID id,
      mTracks(tracks),
      mRuler(ruler),
      mTrackArtist(nullptr),
-     mRefreshBacking(false)
+     mRefreshBacking(false),
+     mIsPanning(false),
+     mLastPanX(0)
 #ifndef __WXGTK__   //Get rid if this pragma for gtk
 #pragma warning( default: 4355 )
 #endif