2460e9846e398bc86a7287ac6e2a5147d00ccebd
Paul MARTIN <luapmartin@gmail.com>
2025-12-24 14:47:30 +0100

    feat(effect): add effectparametersprovider interface and implementation

---
+++ src/effects/effects_base/internal/effectparametersprovider.h
@@ -0,0 +1,50 @@
+/*
+* Audacity: A Digital Audio Editor
+*/
+#pragma once
+
+#include "framework/global/modularity/ioc.h"
+#include "framework/global/async/asyncable.h"
+
+#include "../ieffectparametersprovider.h"
+#include "../ieffectinstancesregister.h"
+#include "../ieffectsprovider.h"
+
+namespace au::effects {
+class EffectParametersProvider : public IEffectParametersProvider, public muse::async::Asyncable
+{
+    muse::Inject<IEffectInstancesRegister> instancesRegister;
+    muse::Inject<IEffectsProvider> effectsProvider;
+
+public:
+    EffectParametersProvider() = default;
+
+    // IEffectParametersProvider interface
+    ParameterInfoList parameters(EffectInstanceId instanceId) const override;
+    ParameterInfo parameter(EffectInstanceId instanceId, const muse::String& parameterId) const override;
+
+    double parameterValue(EffectInstanceId instanceId, const muse::String& parameterId) const override;
+    bool setParameterValue(EffectInstanceId instanceId, const muse::String& parameterId, double value) override;
+
+    muse::String parameterValueString(EffectInstanceId instanceId, const muse::String& parameterId, double value) const override;
+
+    bool supportsParameterExtraction(const EffectId& effectId) const override;
+
+    muse::async::Notification parameterValuesChanged() const override;
+
+private:
+    // Extract parameters from VST3 plugin
+    ParameterInfoList extractVST3Parameters(EffectInstance* instance) const;
+
+    // Extract parameters from LV2 plugin
+    ParameterInfoList extractLV2Parameters(EffectInstance* instance) const;
+
+    // Extract parameters from Audio Unit plugin
+    ParameterInfoList extractAudioUnitParameters(EffectInstance* instance) const;
+
+    // Helper to determine effect family
+    EffectFamily getEffectFamily(const EffectId& effectId) const;
+
+    mutable muse::async::Notification m_parameterValuesChanged;
+};
+}
