4d243287a84232f2c1b42da7dfd81664ee559808
Paul MARTIN <luapmartin@gmail.com>
2026-01-26 14:01:48 +0100

    Hack to make build work

--- src/effects/nyquist/internal/nyquisteffectsrepository.h
+++ src/effects/nyquist/internal/nyquisteffectsrepository.h
@@ -4,6 +4,7 @@
 #pragma once
 
 #include "../inyquisteffectsrepository.h"
+#include "au3-nyquist-effects/LoadNyquist.h"
 
 namespace au::effects {
 class NyquistEffectsRepository : public INyquistEffectsRepository
@@ -12,5 +13,10 @@ public:
     NyquistEffectsRepository() = default;
 
     EffectMetaList effectMetaList() const override;
+
+private:
+    // This member forces the linker to include LoadNyquist.cpp,
+    // which contains DECLARE_BUILTIN_PROVIDER for the Nyquist module
+    ::NyquistEffectsModule m_module;
 };
 }
