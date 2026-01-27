2d79d322c82281858711f113a44d2f1bcf03cce1
Matthieu Hodgkinson <saintmatthieu@mailbox.org>
2026-01-27 18:23:45 +0100

    refactor: use stack instead of heap for SpectrogramHit

--- src/spectrogram/view/spectrogramhit.cpp
+++ src/spectrogram/view/spectrogramhit.cpp
@@ -4,28 +4,8 @@
 #include "spectrogramhit.h"
 
 namespace au::spectrogram {
-SpectrogramHit::SpectrogramHit(int trackId, int channel, double spectrogramY, double spectrogramHeight)
-    : QObject(nullptr),
-    trackId(trackId),
-    channel(channel),
-    spectrogramY(spectrogramY),
-    spectrogramHeight(spectrogramHeight)
+SpectrogramHit SpectrogramHitFactory::createSpectrogramHit(int trackId, int channel, double spectrogramY, double spectrogramHeight)
 {
-}
-
-SpectrogramHit::SpectrogramHit(const SpectrogramHit& other)
-    : QObject(nullptr),
-    trackId(other.trackId),
-    channel(other.channel),
-    spectrogramY(other.spectrogramY),
-    spectrogramHeight(other.spectrogramHeight)
-{
-}
-
-SpectrogramHit* SpectrogramHitFactory::createSpectrogramHit(int trackId, int channel, double spectrogramY, double spectrogramHeight)
-{
-    auto hit = new SpectrogramHit{ trackId, channel, spectrogramY, spectrogramHeight };
-    QQmlEngine::setObjectOwnership(hit, QQmlEngine::CppOwnership);
-    return hit;
+    return SpectrogramHit{ trackId, channel, spectrogramY, spectrogramHeight };
 }
 } // namespace au::spectrogram
