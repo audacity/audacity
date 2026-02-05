98ff3a8694ca8fbe43a60b4d220e46aa9787c611
Paul MARTIN <luapmartin@gmail.com>
2026-02-05 11:25:04 +0100

    namespace conflicts tweaks

--- au3/libraries/au3-module-manager/PluginDescriptor.h
+++ au3/libraries/au3-module-manager/PluginDescriptor.h
@@ -74,7 +74,7 @@ public:
     // So config compatibility will break if it is changed across Audacity versions
     wxString GetEffectFamily() const;
 
-    EffectType GetEffectType() const;
+    ::EffectType GetEffectType() const;
     bool IsEffectDefault() const;
     bool IsEffectInteractive() const;
     bool IsEffectLegacy() const;
@@ -108,7 +108,7 @@ public:
 
     // "family" should be an untranslated string wrapped in wxT()
     void SetEffectFamily(const wxString& family);
-    void SetEffectType(EffectType type);
+    void SetEffectType(::EffectType type);
     void SetEffectDefault(bool dflt);
     void SetEffectInteractive(bool interactive);
     void SetEffectLegacy(bool legacy);
@@ -143,7 +143,7 @@ private:
     // Effects
 
     wxString mEffectFamily;
-    EffectType mEffectType { EffectTypeNone };
+    ::EffectType mEffectType { EffectTypeNone };
     bool mEffectInteractive { false };
     bool mEffectDefault { false };
     bool mEffectLegacy { false };
