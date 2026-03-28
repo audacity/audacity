4ce59cae9e2bf1a79e6e50189a03aaa0cefe2843
Paul MARTIN <luapmartin@gmail.com>
2026-03-28 18:17:02 +0100

    move factoryreset tests to framework

--- src/appshell/tests/environment.cpp
+++ src/appshell/tests/environment.cpp
@@ -4,19 +4,5 @@
 
 #include "testing/environment.h"
 
-#include "workspace/tests/mocks/workspaceconfigurationmock.h"
-
-using namespace ::testing;
-
 static muse::testing::SuiteEnvironment appshell_se
-    = muse::testing::SuiteEnvironment()
-      .setPreInit([](){
-    auto workspaceConfig = std::make_shared<::testing::NiceMock<muse::workspace::WorkspaceConfigurationMock> >();
-
-    ON_CALL(*workspaceConfig, defaultWorkspaceName())
-    .WillByDefault(Return("Modern"));
-
-    muse::modularity::globalIoc()->registerExport<muse::workspace::IWorkspaceConfiguration>("utests", workspaceConfig);
-}).setDeInit([](){
-    muse::modularity::globalIoc()->unregister<muse::workspace::IWorkspaceConfiguration>("utests");
-});
+    = muse::testing::SuiteEnvironment();
