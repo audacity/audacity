b1b0216c7506016a291eed56b0bd8244d723f397
Gabriel Sartori <g.sartori@mu.se>
2026-04-27 06:51:04 -0300

    feat: Add warn sync check on configuration

--- src/au3cloud/internal/au3cloudconfiguration.cpp
+++ src/au3cloud/internal/au3cloudconfiguration.cpp
@@ -4,11 +4,21 @@
 #include "au3cloudconfiguration.h"
 
 #include "framework/global/io/dir.h"
+#include "framework/global/settings.h"
 
 #include "au3-cloud-audiocom/CloudLibrarySettings.h"
 #include "au3-cloud-audiocom/ServiceConfig.h"
 
 using namespace au::au3cloud;
+
+namespace {
+const muse::Settings::Key WARN_ON_SYNC_ERROR("cloud", "cloud/warnOnSyncError");
+}
+
+void Au3CloudConfiguration::init()
+{
+    muse::settings()->setDefaultValue(WARN_ON_SYNC_ERROR, muse::Val(true));
+}
 
 muse::io::path_t Au3CloudConfiguration::cloudProjectsPath() const
 {
@@ -28,4 +38,14 @@ std::vector<std::string> Au3CloudConfiguration::preferredAudioFormats() const
 std::string Au3CloudConfiguration::exportConfig(const std::string& mimeType) const
 {
     return audacity::cloud::audiocom::GetServiceConfig().GetExportConfig(mimeType);
+}
+
+bool Au3CloudConfiguration::shouldWarnOnSyncError() const
+{
+    return muse::settings()->value(WARN_ON_SYNC_ERROR).toBool();
+}
+
+void Au3CloudConfiguration::setWarnOnSyncError(bool warn)
+{
+    muse::settings()->setSharedValue(WARN_ON_SYNC_ERROR, muse::Val(warn));
 }
--- src/au3cloud/internal/au3cloudconfiguration.h
+++ src/au3cloud/internal/au3cloudconfiguration.h
@@ -9,10 +9,15 @@ namespace au::au3cloud {
 class Au3CloudConfiguration : public IAu3CloudConfiguration
 {
 public:
+    void init();
+
     muse::io::path_t cloudProjectsPath() const override;
     void setCloudProjectsPath(const muse::io::path_t& path) override;
 
     std::vector<std::string> preferredAudioFormats() const override;
     std::string exportConfig(const std::string& mimeType) const override;
+
+    bool shouldWarnOnSyncError() const override;
+    void setWarnOnSyncError(bool warn) override;
 };
 }
