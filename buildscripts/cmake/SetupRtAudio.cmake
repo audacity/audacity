ea31ad682e8c11bd7f054eda50bf17ff6a772b8d
Dmitry Makarenko <kryksyh@gmail.com>
2026-05-14 19:20:52 +0300

    fix install step

--- buildscripts/cmake/SetupRtAudio.cmake
+++ buildscripts/cmake/SetupRtAudio.cmake
@@ -3,25 +3,17 @@
 
 include(FetchContent)
 
-set(RTAUDIO_API_JACK     OFF CACHE BOOL "")
-set(RTAUDIO_API_PULSE    OFF CACHE BOOL "")
-set(RTAUDIO_API_OSS      OFF CACHE BOOL "")
-set(RTAUDIO_API_ASIO     OFF CACHE BOOL "")
-set(RTAUDIO_API_DS       OFF CACHE BOOL "")
-set(RTAUDIO_BUILD_TESTING OFF CACHE BOOL "")
+set(RTAUDIO_BUILD_TESTING     OFF CACHE BOOL "")
 set(RTAUDIO_BUILD_STATIC_LIBS ON  CACHE BOOL "")
 set(RTAUDIO_BUILD_SHARED_LIBS OFF CACHE BOOL "")
-set(RTAUDIO_STATIC_MSVCRT OFF CACHE BOOL "")
+set(RTAUDIO_STATIC_MSVCRT     OFF CACHE BOOL "")
 
-if (OS_IS_MAC)
-    set(RTAUDIO_API_CORE  ON  CACHE BOOL "")
-elseif (OS_IS_WIN)
-    set(RTAUDIO_API_WASAPI ON CACHE BOOL "")
-    set(RTAUDIO_API_DS     ON CACHE BOOL "")
-    set(RTAUDIO_API_ASIO   ON CACHE BOOL "")
+if (OS_IS_WIN)
+	set(RTAUDIO_API_DS     ON CACHE BOOL "")
+    set(RTAUDIO_API_ASIO  ON CACHE BOOL "")
 elseif (OS_IS_LIN OR OS_IS_FBSD)
-    set(RTAUDIO_API_ALSA  ON  CACHE BOOL "")
-    set(RTAUDIO_API_PULSE ON  CACHE BOOL "")
+	set(RTAUDIO_API_ALSA  ON  CACHE BOOL "")
+    set(RTAUDIO_API_PULSE ON CACHE BOOL "")
 endif()
 
 FetchContent_Declare(
@@ -30,7 +22,13 @@ FetchContent_Declare(
     GIT_TAG        6.0.1
     GIT_SHALLOW    TRUE
 )
-FetchContent_MakeAvailable(rtaudio)
+
+FetchContent_GetProperties(rtaudio)
+if (NOT rtaudio_POPULATED)
+    FetchContent_Populate(rtaudio)
+    add_subdirectory(${rtaudio_SOURCE_DIR} ${rtaudio_BINARY_DIR} EXCLUDE_FROM_ALL)
+endif()
+
 if (MSVC AND RTAUDIO_API_ASIO)
     target_compile_options(rtaudio PRIVATE /UUNICODE /U_UNICODE)
 endif()
