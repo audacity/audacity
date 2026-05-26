027855030e858cad1a035fb952f0f59d7bbb9433
Grzegorz Wojciechowski <grliszas14@gmail.com>
2026-05-26 13:54:54 +0200

    Register loaded plugins with PluginManager so PluginSettings paths resolve

--- src/effects/builtin/internal/builtineffectsloader.h
+++ src/effects/builtin/internal/builtineffectsloader.h
@@ -5,6 +5,7 @@
 
 #include "effects/effects_base/internal/au3effectloader.h"
 #include "au3-effects/LoadEffects.h"
+#include "au3-module-manager/PluginManager.h"
 
 namespace au::effects {
 class BuiltinEffectsLoader final : public Au3EffectLoader
@@ -14,6 +15,15 @@ public:
         : Au3EffectLoader(m_module, muse::audio::AudioResourceType::NativeEffect) {}
 
 private:
+    void doInit() override
+    {
+        // Register the builtin effects with PluginManager so that
+        // PluginManager::SettingsPath can build per-effect config paths.
+        // Otherwise all builtin effects share a single "UserPresets/" group
+        // in audacity.cfg, leaking user presets across effects.
+        m_module.AutoRegisterPlugins(::PluginManager::Get());
+    }
+
     ::BuiltinEffectsModule m_module;
 };
 } // namespace au::effects
