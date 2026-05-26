66d0df705243a03d307f4e2b08d336f51abe51ac
Dmitry Makarenko <kryksyh@gmail.com>
2026-05-26 16:40:33 +0300

    update copyright year

--- src/AboutDialog.cpp
+++ src/AboutDialog.cpp
@@ -446,7 +446,7 @@ void AboutDialog::PopulateAudacityPage( ShuttleGui & S )
       << wxT("<p><br>&nbsp; &nbsp; ")
       /* i18n-hint Audacity's name substitutes for first and third %s,
        and a "copyright" symbol for the second */
-      << XO("%s software is copyright %s 1999-2024 %s Team.")
+      << XO("%s software is copyright %s 1999-2026 %s Team.")
          .Format(
             Verbatim("<b>%s<sup>&reg;</sup></b>").Format( ProgramName ),
             wxT("&copy;"),
