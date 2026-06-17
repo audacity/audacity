3e040252f4bdf25ea072278a19366a71bcf978ce
LeoWattenberg <48789553+LeoWattenberg@users.noreply.github.com>
2026-06-17 13:42:12 +0200

    Add fade controls to playback toolbar

--- src/projectscene/internal/projectsceneuiactions.cpp
+++ src/projectscene/internal/projectsceneuiactions.cpp
@@ -379,6 +379,8 @@ const ToolConfig& ProjectSceneUiActions::defaultPlaybackToolBarConfig()
             { "", true },
             { "trim-audio-outside-selection", true },
             { "silence-audio-selection", true },
+            { FADE_IN_EFFECT_ACTION, true },
+            { FADE_OUT_EFFECT_ACTION, true },
             { "", true },
             // { "metronome", false },
             // { "", true },
