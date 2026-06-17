ea14b99315adcfea6509c75d854e01a5d7e3873b
Dmitry Makarenko <kryksyh@gmail.com>
2026-06-17 14:23:05 +0300

    de-vendor lib-src third-party libraries

--- au3/lib-src/libsoxr/src/cr64s.c
+++
@@ -1,8 +0,0 @@
-/* SoX Resampler Library      Copyright (c) 2007-16 robs@users.sourceforge.net
- * Licence for this file: LGPL v2.1                  See LICENCE for details. */
-
-#define RATE_CB    _soxr_rate64s_cb
-#define CORE_STR   "cr64s"
-
-#define CORE_TYPE  (CORE_DBL|CORE_SIMD_POLY|CORE_SIMD_HALF|CORE_SIMD_DFT)
-#include "cr-core.c"
