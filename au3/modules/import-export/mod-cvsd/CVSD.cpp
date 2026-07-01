a85f02412ba22edc201ba30b4a8ee7429d4d7266
PawBud <sanskarbajpai2907@gmail.com>
2026-07-01 14:50:55 +0200

    cvsd: moved the cvsd encoding and deconding function to export cpp due to linking errors.

--- au3/modules/import-export/mod-cvsd/CVSD.cpp
+++ au3/modules/import-export/mod-cvsd/CVSD.cpp
@@ -7,15 +7,15 @@ Audacity: A Digital Audio Editor
 https://www.bluetooth.com/wp-content/uploads/Files/Specification/HTML/Core-62/out/en/br-edr-controller/baseband-specification.html#UUID-00be0b31-be51-dfbe-ac3b-d46c5138704b
 
 **********************************************************************/
+// Todo: Check out why linking fails
+// The error message
+// ld: warning: building for macOS-11.0, but linking with dylib '/opt/homebrew/opt/qtbase/lib/QtCore.framework/Versions/A/QtCore' which was built for newer version 14.0
+// Undefined symbols for architecture arm64:
+//   "CVSDEncode(char const*, unsigned int)", referenced from:
+//       ExportCVSDProcessor::Process(ExportProcessorDelegate&) in libau3wrap.a[16](RegisterExportPlugins.cpp.o)
+// ld: symbol(s) not found for architecture arm64
+// clang++: error: linker command failed with exit code 1 (use -v to see invocation)
 
-#include "CVSD.h"
+#include "ModuleConstants.h"
 
-void CVSDEncode()
-{
-
-}
-
-void CVSDDecode(std::unique_ptr<wxFFile> openedFile)
-{
-
-}
+DEFINE_MODULE_ENTRIES
