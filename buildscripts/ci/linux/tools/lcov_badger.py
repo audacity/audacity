9cb91eab831191d24144ce62cce91a6ec0bf68ef
Eism <eismailzada@gmail.com>
2026-07-06 11:25:33 +0300

    fixed positioning of PERCENT

--- buildscripts/ci/linux/tools/lcov_badger.py
+++ buildscripts/ci/linux/tools/lcov_badger.py
@@ -18,8 +18,8 @@ SVG_TEMPLATE = """<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://ww
         <g fill="#fff" text-anchor="middle" font-family="Verdana,Geneva,DejaVu Sans,sans-serif" text-rendering="geometricPrecision" font-size="110">
                 <text aria-hidden="true" x="325" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="530">Coverage</text>
                 <text x="325" y="140" transform="scale(.1)" fill="#fff" textLength="530">Coverage</text>
-                <text aria-hidden="true" x="835" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="330">{{PERCENT}}%</text>
-                <text x="835" y="140" transform="scale(.1)" fill="#fff" textLength="330">{{PERCENT}}%</text>
+                <text aria-hidden="true" x="835" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)">{{PERCENT}}%</text>
+                <text x="835" y="140" transform="scale(.1)" fill="#fff">{{PERCENT}}%</text>
         </g>
 </svg>"""
 
