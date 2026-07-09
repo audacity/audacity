0f409576723f9c4847f22a8f1dbb019a156535c3
Eism <eismailzada@gmail.com>
2026-07-09 13:49:58 +0300

    updating the state of items if the history has changed
    the same as in AppMenuModel

--- src/projectscene/view/toolbars/undoredotoolbarmodel.cpp
+++ src/projectscene/view/toolbars/undoredotoolbarmodel.cpp
@@ -9,17 +9,18 @@ using namespace au::projectscene;
 using namespace muse::uicomponents;
 using namespace muse::actions;
 
+static const muse::actions::ActionCode UNDO_ACTION_CODE("action://undo");
+static const muse::actions::ActionCode REDO_ACTION_CODE("action://redo");
+
 void UndoRedoToolBarModel::load()
 {
     if (m_loaded) {
         return;
     }
 
-    AbstractToolBarModel::load();
-
     muse::actions::ActionCodeList itemsCodes = {
-        "action://undo",
-        "action://redo",
+        UNDO_ACTION_CODE,
+        REDO_ACTION_CODE,
     };
 
     ToolBarItemList items;
@@ -31,9 +32,59 @@ void UndoRedoToolBarModel::load()
 
     setItems(items);
 
+    AbstractToolBarModel::load();
+
     context()->currentProjectChanged().onNotify(this, [this]() {
-        load();
+        updateItems();
+
+        subscribeOnHistoryChanges();
     });
 
+    subscribeOnHistoryChanges();
+
     m_loaded = true;
+}
+
+void UndoRedoToolBarModel::onActionsStateChanges(const muse::actions::ActionCodeList& codes)
+{
+    auto history = projectHistory();
+
+    for (const muse::actions::ActionCode& code : codes) {
+        if (code == UNDO_ACTION_CODE) {
+            ToolBarItem* undoItem = findItemPtr(UNDO_ACTION_CODE);
+            if (undoItem) {
+                const muse::TranslatableString undoActionName = history ? history->topMostUndoActionName() : muse::TranslatableString();
+                undoItem->setTitle(undoActionName.isEmpty()
+                                   ? muse::TranslatableString("action", "Undo")
+                                   : muse::TranslatableString("action", "Undo ‘%1’").arg(undoActionName));
+            }
+        } else if (code == REDO_ACTION_CODE) {
+            ToolBarItem* redoItem = findItemPtr(REDO_ACTION_CODE);
+            if (redoItem) {
+                const muse::TranslatableString redoActionName = history ? history->topMostRedoActionName() : muse::TranslatableString();
+                redoItem->setTitle(redoActionName.isEmpty()
+                                   ? muse::TranslatableString("action", "Redo")
+                                   : muse::TranslatableString("action", "Redo ‘%1’").arg(redoActionName));
+            }
+        }
+    }
+
+    AbstractToolBarModel::onActionsStateChanges(codes);
+}
+
+void UndoRedoToolBarModel::updateItems()
+{
+    onActionsStateChanges({ UNDO_ACTION_CODE, REDO_ACTION_CODE });
+}
+
+void UndoRedoToolBarModel::subscribeOnHistoryChanges()
+{
+    auto history = projectHistory();
+    if (!history) {
+        return;
+    }
+
+    history->historyChanged().onReceive(this, [this](const trackedit::HistoryEvent&) {
+        updateItems();
+    }, Asyncable::Mode::SetReplace);
 }
--- src/projectscene/view/toolbars/undoredotoolbarmodel.h
+++ src/projectscene/view/toolbars/undoredotoolbarmodel.h
@@ -7,19 +7,25 @@
 
 #include "modularity/ioc.h"
 #include "context/iglobalcontext.h"
+#include "trackedit/iprojecthistory.h"
 
 namespace au::projectscene {
 class UndoRedoToolBarModel : public muse::uicomponents::AbstractToolBarModel
 {
     Q_OBJECT
 
-    muse::ContextInject<au::context::IGlobalContext> context { this };
+    muse::ContextInject<context::IGlobalContext> context { this };
+    muse::ContextInject<trackedit::IProjectHistory> projectHistory { this };
 
 public:
 
     Q_INVOKABLE void load() override;
 
 private:
+    void onActionsStateChanges(const muse::actions::ActionCodeList& codes) override;
+
+    void updateItems();
+    void subscribeOnHistoryChanges();
 
     bool m_loaded = false;
 };
