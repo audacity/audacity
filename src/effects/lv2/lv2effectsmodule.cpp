24c149b2e4ea662c8a9c6a5833a11e93024f9f09
Grzegorz Wojciechowski <grliszas14@gmail.com>
2026-07-21 17:04:31 +0200

    Provide fallback UI parameters for LV2 effects

--- src/effects/lv2/lv2effectsmodule.cpp
+++ src/effects/lv2/lv2effectsmodule.cpp
@@ -8,9 +8,11 @@
 
 #include "effects/effects_base/ieffectloadersregister.h"
 #include "effects/effects_base/ieffectviewlaunchregister.h"
+#include "effects/effects_base/iparameterextractorregistry.h"
 #include "effects/effects_base/view/effectsviewutils.h"
 
 #include "internal/lv2effectloader.h"
+#include "internal/lv2parameterextractorservice.h"
 #include "internal/lv2pluginmetareader.h"
 #include "internal/lv2pluginsscanner.h"
 #include "internal/lv2viewlauncher.h"
@@ -61,6 +63,11 @@ void Lv2EffectsModule::resolveImports()
     auto loadersRegister = globalIoc()->resolve<IEffectLoadersRegister>(mname);
     if (loadersRegister) {
         loadersRegister->registerLoader(m_effectLoader);
+    }
+
+    auto paramExtractorRegistry = globalIoc()->resolve<IParameterExtractorRegistry>(mname);
+    if (paramExtractorRegistry) {
+        paramExtractorRegistry->registerExtractor(std::make_shared<Lv2ParameterExtractorService>());
     }
 
     // auto ir = ioc()->resolve<IInteractiveUriRegister>(mname);
