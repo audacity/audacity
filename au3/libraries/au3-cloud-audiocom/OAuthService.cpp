234a3c484c2c046a4f451c59f2228a6c40992e5f
Gabriel Sartori <gabriel@embarc.com.br>
2026-08-25 06:18:25 -0300

    cloud: fix url encode
    (cherry picked from commit 71165e1856494e5f9d99a583d0063ed2ec7ff22c)

--- au3/libraries/au3-cloud-audiocom/OAuthService.cpp
+++ au3/libraries/au3-cloud-audiocom/OAuthService.cpp
@@ -389,7 +389,7 @@ std::string OAuthService::MakeAudioComAuthorizeURL(std::string_view userId, std:
         GetServiceConfig().GetAuthWithRedirectURL(), "?",
         tokenPrefix, token, "&",
         userPrefix, userId, "&",
-        urlPrefix, redirectUrl
+        urlPrefix, UrlEncode(std::string(redirectUrl))
         );
 
     if (SendAnonymousUsageInfo->Read()) {
