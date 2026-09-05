4a08e657f56784aeafc5aa461e68b559d13923f5
PawBud <sanskarbajpai2907@gmail.com>
2026-09-05 16:41:33 +0200

    config: removed the old CVSD config struct.

--- au3/modules/import-export/mod-cvsd/CVSD.h
+++ au3/modules/import-export/mod-cvsd/CVSD.h
@@ -31,47 +31,4 @@ struct CVSD_BITPACKER {
     int partialBits = 0;
 };
 
-struct CVSD_CONFIG {
-    // 16 bit config for CVSD
-    unsigned int num_bits = 1;
-
-    // J (bit history window size for syllabic companding)
-    static constexpr unsigned int bitref = 4;
-
-    // b(k) = sgn{x(k) - y(k-1)}
-    // Is the incoming signal higher than the current accumulator?
-    bool b = false;
-
-    // alpha = 1 if {bitref} bits in the last {bitref} output bits are equal, otherwise 0.
-    bool alpha = false;
-
-    // The predicted output so far for a sample
-    // y(k)
-    float accumulator = 0.0f;
-
-    // Current step size (δ(k)) - start at minimum valid value
-    // This ensures the step size is always within bounds from the first iteration
-    float accumulatorStepSize = 10.0f;
-
-    float accumulatorDecay = 0.96875f;
-
-    // Accumulator bounds (matches 16-bit PCM range)
-    float minAccumulatorSize = -32768.0f;
-    float maxAccumulatorSize = 32767.0f;
-
-    // β (step size decay factor) - applied when bits are not homogenous
-    double stepSizeDecay = 0.9990234375;
-
-    // α (syllabic companding factor) - applied when last 4 bits are all 0s or all 1s
-    double syllabicCompandingFactor = 1.0009765625;
-
-    // Accumulator step size limits (δ_min and δ_max)
-    float minAccumulatorStepSize = 10.0f;
-    float maxAccumulatorStepSize = 1280.0f;
-
-    // Bit history for syllabic companding (stores last 4 bits)
-    u_int8_t bitHistory = 0;
-
-};
-
 #endif //AUDACITY_CVSD_H
