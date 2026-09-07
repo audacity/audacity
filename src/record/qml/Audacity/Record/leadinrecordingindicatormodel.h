85ca719122025e87f90bf746ded7aa51d3f1e483
Paul MARTIN <luapmartin@gmail.com>
2026-09-07 16:21:10 +0200

    Fix codestyle in record qml header

--- src/record/qml/Audacity/Record/leadinrecordingindicatormodel.h
+++ src/record/qml/Audacity/Record/leadinrecordingindicatormodel.h
@@ -17,13 +17,14 @@ class LeadInRecordingIndicatorModel : public QObject, public muse::async::Asynca
 {
     Q_OBJECT
     QML_ELEMENT
+
+public:
     Q_PROPERTY(bool visible READ visible NOTIFY visibleChanged FINAL)
     Q_PROPERTY(double startTime READ startTime NOTIFY visibleChanged FINAL)
     Q_PROPERTY(QVariantList trackIds READ trackIds NOTIFY visibleChanged FINAL)
 
     muse::ContextInject<IRecordController> recordController{ this };
 
-public:
     explicit LeadInRecordingIndicatorModel(QObject* parent = nullptr);
 
     Q_INVOKABLE void init();
