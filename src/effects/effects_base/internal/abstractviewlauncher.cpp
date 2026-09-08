5e1f69b6fff2a6bc297551602a693f51807affa0
Dmitry Makarenko <kryksyh@gmail.com>
2026-09-08 10:15:00 +0300

    Do not create instance unless it is neccessary

--- src/effects/effects_base/internal/abstractviewlauncher.cpp
+++ src/effects/effects_base/internal/abstractviewlauncher.cpp
@@ -50,9 +50,8 @@ void AbstractViewLauncher::hideRealtimeEffect(const RealtimeEffectStatePtr& stat
     IF_ASSERT_FAILED(state) {
         return;
     }
-    const auto instance = std::dynamic_pointer_cast<effects::EffectInstance>(state->GetInstance());
+    const auto instance = std::dynamic_pointer_cast<effects::EffectInstance>(state->PeekInstance());
     if (!instance) {
-        LOGW() << "Could not get instance for " << au::au3::wxToStdString(state->GetID());
         return;
     }
     const auto instanceId = instance->id();
